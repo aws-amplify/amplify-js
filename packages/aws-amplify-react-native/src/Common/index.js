f2f4cdd5afab0be3c4c7b47046441098e27c5fe0
Sharma <nidsharm@amazon.com>
2017-12-18 12:04:23 -0800

    Updating minor version for npm publish

--- packages/aws-amplify-react-native/src/Common/index.js
+++ packages/aws-amplify-react-native/src/Common/index.js
@@ -21,7 +21,7 @@ export { default as Hub } from './Hub';
 export { default as JS } from './JS';
 
 export const Constants = {
-    userAgent: 'aws-amplify/1.0.0 react-native'
+    userAgent: 'aws-amplify/0.1.9 react-native'
 };
 
 const logger = new Logger('Common');
