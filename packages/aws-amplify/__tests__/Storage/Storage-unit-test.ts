3a7d13878f6a8e5952d62390b0b39f26dd5fa549
Iurii Kucherov <yuyokk@gmail.com>
2018-02-21 18:54:13 +0200

    Cleanup whitespaces

--- packages/aws-amplify/__tests__/Storage/Storage-unit-test.ts
+++ packages/aws-amplify/__tests__/Storage/Storage-unit-test.ts
@@ -48,7 +48,7 @@ jest.mock('aws-sdk/clients/s3', () => {
                 return;
             }
     };
-    
+
     return S3;
 });
 
@@ -238,7 +238,7 @@ describe('Storage', () => {
                         });
                     });
                 });
-            
+
             await storage.get('key', { downloaded: false });
 
             const curCredSpyOn2 = jest.spyOn(Auth.prototype, 'currentCredentials')
@@ -249,7 +249,7 @@ describe('Storage', () => {
                         });
                     });
                 });
-                
+
             await storage.get('key', { downloaded: false });
 
             expect(curCredSpyOn.mock.calls.length).toBe(2);
@@ -257,7 +257,7 @@ describe('Storage', () => {
             curCredSpyOn.mockClear();
         });
     });
-    
+
     describe('put test', () => {
         test('put object succefully', async () => {
             const curCredSpyOn = jest.spyOn(Auth.prototype, 'currentCredentials')
@@ -273,9 +273,9 @@ describe('Storage', () => {
             expect.assertions(2);
             expect(await storage.put('key', 'obejct', {})).toEqual({"key": "path/itemsKey"});
             expect(spyon.mock.calls[0][0]).toEqual({
-                "Body": "obejct", 
-                "Bucket": "bucket", 
-                "ContentType": "binary/octet-stream", 
+                "Body": "obejct",
+                "Bucket": "bucket",
+                "ContentType": "binary/octet-stream",
                 "Key": "public/key"
             });
             spyon.mockClear();
@@ -297,9 +297,9 @@ describe('Storage', () => {
             expect.assertions(3);
             expect(await storage.put('key', 'obejct', {track: true})).toEqual({"key": "path/itemsKey"});
             expect(spyon.mock.calls[0][0]).toEqual({
-                "Body": "obejct", 
-                "Bucket": "bucket", 
-                "ContentType": "binary/octet-stream", 
+                "Body": "obejct",
+                "Bucket": "bucket",
+                "ContentType": "binary/octet-stream",
                 "Key": "public/key"
             });
             expect(spyon2).toBeCalledWith('storage', {attrs: {"method": "put", "result": "success"}, metrics: null}, 'Storage');
@@ -327,7 +327,7 @@ describe('Storage', () => {
             try{
                 await storage.put('key', 'obejct', {});
             } catch (e) {
-                expect(e).toBe('err'); 
+                expect(e).toBe('err');
             }
 
             spyon.mockClear();
@@ -350,9 +350,9 @@ describe('Storage', () => {
             expect.assertions(2);
             expect(await storage.put('key', 'obejct', {level: 'private', contentType: 'text/plain'})).toEqual({"key": "/itemsKey"});
             expect(spyon.mock.calls[0][0]).toEqual({
-                "Body": "obejct", 
-                "Bucket": "bucket", 
-                "ContentType": "text/plain", 
+                "Body": "obejct",
+                "Bucket": "bucket",
+                "ContentType": "text/plain",
                 "Key": "private/id/key"
             });
             spyon.mockClear();
@@ -440,7 +440,7 @@ describe('Storage', () => {
             try{
                 await storage.remove('key', {});
             } catch (e) {
-                expect(e).toBe('err'); 
+                expect(e).toBe('err');
             }
 
             spyon.mockClear();
@@ -535,7 +535,7 @@ describe('Storage', () => {
                 }]);
             expect(spyon.mock.calls[0][0]).toEqual({"Bucket": 'bucket', "Prefix": "public/path"});
             expect(spyon2).toBeCalledWith(
-                'storage', 
+                'storage',
                 {attrs: {"method": "list", "result": "success"}, metrics: null}, 'Storage');
 
             spyon.mockClear();
