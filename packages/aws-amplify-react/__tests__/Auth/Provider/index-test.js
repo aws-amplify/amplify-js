75b6c808b6a823180c01b9bfd37282bab3d9ad8a
James Polanco <jpolanco@foreseemed.com>
2018-03-29 12:02:36 -0700

    Adding ability to specify OAuth/Federation clients the React app supports
    The `withFederated()` HoC now supports an optional second argument that is
    an opts object that allows us to specify exactly which SSO/OAuth clients
    we want to support within an application. By default, all three (Amazon, Google and Facebook)
    are mixed in. We can opt-in specifically by passing in an object with the
    services we want to support. For example, using both Amazon and Facebook (excluding Google):
    
    ```javascript
    const Comp = withFederated(MockComp, { amazon: true, facebook: true });
    ```
    
    This commit also upgraded Jest to the current 22.x version, Enzyme 3.1.x and added
    in jest-enzyme to the dev dependencies. I have found the jest-enzyme matchers
    (https://github.com/FormidableLabs/enzyme-matchers) to be very convenient when
    testing React applications and components. If you want me to rollback this
    addition I can, and will update the config/tests.
    
    Fixes #554

--- packages/aws-amplify-react/__tests__/Auth/Provider/index-test.js
+++ packages/aws-amplify-react/__tests__/Auth/Provider/index-test.js
@@ -4,16 +4,74 @@ import withGoogle from '../../../src/Auth/Provider/withGoogle';
 import { withFederated } from '../../../src/Auth/Provider/index';
 
 describe('withFederated test', () => {
+    const MockComp = class extends Component {
+        render() {
+            return <div />;
+        }
+    };
+
     describe('render test', () => {
         test('render correctly', () => {
-            const MockComp = class extends Component {
-                render() {
-                    return <div />;
-                }
-            }
             const Comp = withFederated(MockComp);
-            const wrapper = shallow(<Comp/>);
+            const wrapper = shallow(<Comp />);
             expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe('HoC options', () => {
+        it('should default with Amazon, Google and Facebook', () => {
+            const Comp = withFederated(MockComp);
+            const amzWrapper = shallow(<Comp />).dive();
+            expect(amzWrapper).toHaveProp('amz');
+            expect(amzWrapper).toHaveProp('amazonSignIn');
+            const gaWrapper = amzWrapper.dive();
+            expect(gaWrapper).toHaveProp('ga');
+            expect(gaWrapper).toHaveProp('googleSignIn');
+            const fbWrapper = gaWrapper.dive();
+            expect(fbWrapper).toHaveProp('fb');
+            expect(fbWrapper).toHaveProp('facebookSignIn');
+        });
+
+        it('should allow to only specify amazon', () => {
+            const Comp = withFederated(MockComp, { amazon: true });
+            const amzWrapper = shallow(<Comp />).dive();
+            expect(amzWrapper).toHaveProp('amz');
+            expect(amzWrapper).toHaveProp('amazonSignIn');
+            const baseComp = amzWrapper.dive();
+            expect(baseComp.props()).toEqual({});
+        });
+
+        it('should allow to only specify facebook', () => {
+            const Comp = withFederated(MockComp, { facebook: true });
+            const fbWrapper = shallow(<Comp />).dive();
+            expect(fbWrapper).toHaveProp('fb');
+            expect(fbWrapper).toHaveProp('facebookSignIn');
+            const baseComp = fbWrapper.dive();
+            expect(baseComp.props()).toEqual({});
+        });
+
+        it('should allow to only specify google', () => {
+            const Comp = withFederated(MockComp, { google: true });
+            const gaWrapper = shallow(<Comp />).dive();
+            expect(gaWrapper).toHaveProp('ga');
+            expect(gaWrapper).toHaveProp('googleSignIn');
+            const baseComp = gaWrapper.dive();
+            expect(baseComp.props()).toEqual({});
+        });
+
+        it('should allow 2 of 3 providers', () => {
+            const Comp = withFederated(MockComp, {
+                amazon: true,
+                facebook: true
+            });
+            const amzWrapper = shallow(<Comp />).dive();
+            expect(amzWrapper).toHaveProp('amz');
+            expect(amzWrapper).toHaveProp('amazonSignIn');
+            const fbWrapper = amzWrapper.dive();
+            expect(fbWrapper).toHaveProp('fb');
+            expect(fbWrapper).toHaveProp('facebookSignIn');
+            const baseComp = fbWrapper.dive();
+            expect(baseComp.props()).toEqual({});
         });
     });
 });
--- packages/aws-amplify-react/test_Setup/enzymeSetup.js
+++ packages/aws-amplify-react/test_Setup/enzymeSetup.js
@@ -1,5 +1,6 @@
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-enzyme';
 
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
