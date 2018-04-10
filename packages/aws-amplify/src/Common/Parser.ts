5d0c7319176402c67bf11fa2dc858d3e82e95ff7
powerful23 <wenzhuo235@gmail.com>
2018-04-10 10:25:14 -0700

    mionr change

--- packages/aws-amplify/src/Common/Parser.ts
+++ packages/aws-amplify/src/Common/Parser.ts
@@ -5,7 +5,7 @@ export default class Parser {
         const amplifyConfig: AmplifyConfig = {};
         // Analytics
         if (config['aws_mobile_analytics_app_id']) {
-            const Analytics = {};
+            const Analytics = config.Analytics ? config.Analytics : {};
             Analytics['appId'] = config['aws_mobile_analytics_app_id'];
             Analytics['region'] = config['aws_mobile_analytics_app_region'];
             amplifyConfig.Analytics = Analytics;
