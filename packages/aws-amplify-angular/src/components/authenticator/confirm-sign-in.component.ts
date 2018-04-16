ae96ada05bc3a6c31d2896fb5f8d720c9b60261d
Dustin Noyes <dnnoyes@amazon.com>
2018-04-16 13:29:27 -0700

    removing unneeded dependency

--- packages/aws-amplify-angular/src/components/authenticator/confirm-sign-in.component.ts
+++ packages/aws-amplify-angular/src/components/authenticator/confirm-sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Inject } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AmplifyService, AuthState } from '../../providers';
 
 const template = `
