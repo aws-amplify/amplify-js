99bbbedf5a8041241ba2f277835f1ebd8b116867
Dustin Noyes <dnnoyes@amazon.com>
2018-04-25 18:27:26 -0700

    re adding API.ts

--- packages/aws-amplify/src/API/API.ts
+++ packages/aws-amplify/src/API/API.ts
@@ -329,6 +329,8 @@ export default class APIClass {
             await this.createInstance();
         }
 
+        debugger;
+
         const {
             aws_appsync_region: region,
             aws_appsync_graphqlEndpoint: appSyncGraphqlEndpoint,
