1eafc5e886c82768cd16410d2d16b362db46d0fc
Sharma <nidsharm@amazon.com>
2018-04-27 14:51:34 -0700

    ios

--- packages/aws-amplify-google-signin/ios/RNAwsAmplifyGoogleSignin.h
+++ packages/aws-amplify-google-signin/ios/RNAwsAmplifyGoogleSignin.h
@@ -1,11 +1,7 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
+#import <React/RCTEventEmitter.h>
+#import <GoogleSignIn/GoogleSignIn.h>
 
-@interface RNAwsAmplifyGoogleSignin : NSObject <RCTBridgeModule>
+@interface RNAwsAmplifyGoogleSignin :  RCTEventEmitter <RCTBridgeModule, GIDSignInDelegate>
 
 @end
-  
