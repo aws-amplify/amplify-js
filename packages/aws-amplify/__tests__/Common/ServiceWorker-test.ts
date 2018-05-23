0085b2885c063ecddd984295c4e9b99a9330780e
Michael Labieniec <michaellabieniec@gmail.com>
2018-05-23 11:26:58 -0700

    update timeout on test

--- packages/aws-amplify/__tests__/Common/ServiceWorker-test.ts
+++ packages/aws-amplify/__tests__/Common/ServiceWorker-test.ts
@@ -130,7 +130,6 @@ describe('ServiceWorker test', () => {
             return expect(serviceWorker.enablePush('publickKey')).resolves.toBe(subscription);
         });
         test('can enable push when user is not subscribed', async () => {
-            jest.setTimeout(10000);
             const subscription = null;
 
             const bla = {
@@ -149,6 +148,6 @@ describe('ServiceWorker test', () => {
             await serviceWorker.register();
 
             return expect(serviceWorker.enablePush('publickKey')).resolves.toBe(subscription);
-        });
+        }, 10);
     });
 });
