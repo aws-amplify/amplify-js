18006d6e958e8f8d0924f7f9303035c09dd4745c
powerful23 <wenzhuo235@gmail.com>
2018-06-05 10:50:59 -0700

    minor change

--- packages/core/__tests__/Platform-test.ts
+++
@@ -1,9 +0,0 @@
-import Platform from '../src/Platform';
-
-describe('Platform test', () => {
-    describe('isReactNative test', () => {
-        test('happy case', () => {
-            expect(Platform.isReactNative).toBe(false);
-        });
-    });
-});
