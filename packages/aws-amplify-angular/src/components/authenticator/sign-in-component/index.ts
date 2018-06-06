e6aa42bafec78c57ca96f99ddbbe10263dd97557
Dustin Noyes <dnnoyes@amazon.com>
2018-06-06 15:13:41 -0700

    switching branches

--- packages/aws-amplify-angular/src/components/authenticator/sign-in-component/index.ts
+++ packages/aws-amplify-angular/src/components/authenticator/sign-in-component/index.ts
@@ -1,4 +1,4 @@
-export { SignInComponent } from './sign-in.resolver';
+export { SignInComponent } from './sign-in.component.factory';
 export { SignInClass } from './sign-in.class';
 export { SignInComponentCore } from './sign-in.component.core';
 export { SignInComponentIonic } from './sign-in.component.ionic';
