d17eff8cbd5ab869538644ab5123dcdb3d110b0b
powerful23 <wenzhuo235@gmail.com>
2018-06-11 10:47:44 -0700

    minor change

--- packages/amazon-cognito-identity-js/src/Client.js
+++ packages/amazon-cognito-identity-js/src/Client.js
@@ -69,7 +69,7 @@ export default class Client {
         // first check if we have a service error
         if (response && response.headers && response.headers.get('x-amzn-errortype')) {
           try {
-            const code = (response.headers.get('x-amz-errortype')).split(':')[0];
+            const code = (response.headers.get('x-amzn-errortype')).split(':')[0];
             error = {
               code,
               name: code,
@@ -78,6 +78,11 @@ export default class Client {
             };
           } catch (ex) {
               // pass through so it doesn't get swallowed if we can't parse it
+              error = {
+                code: 'UnknownError',
+                message: response.headers.get('x-amzn-errortype'),
+              }
+              return callback(error);
           }
         // otherwise check if error is Network error
         } else if (err instanceof Error && err.message === 'Network error') {
