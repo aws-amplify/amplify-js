e8e27071b89f08d74aa241cc0e727a79140088b2
powerful23 <wenzhuo235@gmail.com>
2018-07-05 15:20:19 -0700

    unit tests

--- packages/aws-amplify-react/src/Auth/Provider/withFacebook.jsx
+++ packages/aws-amplify-react/src/Auth/Provider/withFacebook.jsx
@@ -60,7 +60,7 @@ export default function withFacebook(Comp) {
                         if (onStateChange) {
                             onStateChange('signedIn', authUser);
                         }
-                    })
+                    });
             });
         }
 
