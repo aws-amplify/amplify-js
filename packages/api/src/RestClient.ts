297434f214cd81798812e0b84645340dab582307
Rodriguez Elorza <elorzafe@amazon.com>
2018-07-12 16:51:43 -0700

    Adding debug message when no credentials available

--- packages/api/src/RestClient.ts
+++ packages/api/src/RestClient.ts
@@ -122,10 +122,13 @@ export class RestClient {
 
         }
 
-        // Signing the request in case there are credentials available
+        // Signing the request in case there credentials are available
         return Credentials.get()
             .then(credentials => this._signed({ ...params, ...extraParams }, credentials, isAllResponse))
-            .catch(err => this._request(params, isAllResponse));
+            .catch(err => {
+                logger.debug('No credentials available, the request will be unsigned');
+                return this._request(params, isAllResponse)
+            });
     }
 
     /**
