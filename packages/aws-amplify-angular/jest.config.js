d20d149d520a6c381f3ddb379fed8273c2082c65
Dustin Noyes <dnnoyes@amazon.com>
2018-08-13 11:16:25 -0700

    reverted jest config

--- packages/aws-amplify-angular/jest.config.js
+++ packages/aws-amplify-angular/jest.config.js
@@ -1,18 +1,5 @@
 module.exports = {
   preset: "jest-preset-angular",
   roots: ['src'],
-  setupTestFrameworkScriptFile: "<rootDir>/src/setup-jest.ts",
-  transform: {
-    "^.+\\.js$": "babel-jest"
-  },
-  transformIgnorePatterns: [
-    "node_modules/(?!(ionic))"
-  ],
-  globals: {
-    "ts-jest": {
-      tsConfigFile: "src/tsconfig.spec.json",
-      useBabelrc: true
-    },
-    "__TRANSFORM_HTML__": true
-  }
+  setupTestFrameworkScriptFile: "<rootDir>/src/setup-jest.ts"
 }
