0cb2b5e06a3b247ea2537a08afa140f94508fb6b
Dustin Noyes <dnnoyes@amazon.com>
2018-08-20 09:05:27 -0700

    fixing line length linting

--- packages/aws-amplify-angular/src/aws-amplify-ionic-module.ts
+++ packages/aws-amplify-angular/src/aws-amplify-ionic-module.ts
@@ -1,6 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AmplifyService } from './providers/amplify.service';
+// tslint:disable:max-line-length
 import { AuthenticatorIonicComponent } from './components/authenticator/authenticator/authenticator.component.ionic';
 import { ConfirmSignInComponentIonic } from './components/authenticator/confirm-sign-in-component/confirm-sign-in-component.ionic';
 import { ConfirmSignUpComponentIonic } from './components/authenticator/confirm-sign-up-component/confirm-sign-up.component.ionic';
@@ -13,6 +14,7 @@ import { PhotoPickerIonicComponent } from './components/storage/photo-picker-com
 import { S3AlbumComponentIonic } from './components/storage/s3-album-component/s3-album.component.ionic';
 import { S3ImageComponentIonic } from './components/storage/s3-image-component/s3-image.component.ionic';
 import { ChatbotComponentIonic } from './components/interactions/chatbot/chatbot.component.ionic';
+// tslint:enable:max-line-length
 
 const components = [
   AuthenticatorIonicComponent,
