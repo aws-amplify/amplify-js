7c739d34ebf2144ef22353393be039cf6dbff464
Jonas Büth <apps@cornr.de>
2018-08-30 14:20:26 +0200

    Fix Facebook Refresh Token. https://github.com/aws-amplify/amplify-js/issues/1282

--- packages/core/src/OAuthHelper/FacebookOAuth.ts
+++ packages/core/src/OAuthHelper/FacebookOAuth.ts
@@ -65,7 +65,7 @@ export default class FacebookOAuth {
         }
 
         return new Promise((res, rej) => {
-            fb.login(
+            fb.getLoginStatus(
                 fbResponse => {
                     if (!fbResponse || !fbResponse.authResponse) {
                         logger.debug('no response from facebook when refreshing the jwt token');
