12c8ea33e8480f39cd57fb23bc6c404be2fc2f8c
Dustin Noyes <dnnoyes@amazon.com>
2018-09-11 12:05:49 -0700

    trying diff configs

--- packages/aws-amplify-vue/jest.config.js
+++ packages/aws-amplify-vue/jest.config.js
@@ -3,21 +3,21 @@ module.exports = {
     'js',
     'jsx',
     'json',
-    'vue'
+    'vue',
   ],
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.jsx?$': 'babel-jest'
+    '^.+\\.jsx?$': 'babel-jest',
   },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   snapshotSerializers: [
-    'jest-serializer-vue'
+    'jest-serializer-vue',
   ],
   testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
   ],
-  testURL: 'http://localhost/'
-}
+  testURL: 'http://localhost/',
+};
--- packages/aws-amplify-vue/vue.config.js
+++ packages/aws-amplify-vue/vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  
   css: {
     modules: true,
     /*
