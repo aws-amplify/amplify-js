bef262661728657bdc4a813f46f4bf84ae22e548
Dustin Noyes <dnnoyes@amazon.com>
2018-09-10 20:48:40 -0700

    babel and lint

--- packages/aws-amplify-vue/jest.config.js
+++ packages/aws-amplify-vue/jest.config.js
@@ -17,7 +17,7 @@ module.exports = {
     'jest-serializer-vue',
   ],
   testMatch: [
-    '**/aws-amplify-vue/__tests__/*.(js|jsx|ts|tsx)',
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
   ],
   testURL: 'http://localhost/',
 };
