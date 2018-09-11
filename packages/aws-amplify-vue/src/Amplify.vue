ad32aa03d3369382cdcb31773565a8358e0a6ff7
Dustin Noyes <dnnoyes@amazon.com>
2018-09-11 08:35:56 -0700

    removes spread operator from entry

--- packages/aws-amplify-vue/src/Amplify.vue
+++ packages/aws-amplify-vue/src/Amplify.vue
@@ -3,14 +3,39 @@
 </template>
 
 <script>
-import * as components from './components';
+// TODO: FIX SPREAD OPERATOR ERROR IN CIRCLECI TO ELIMINATE EXPLICIT IMPORTS AND USE BARRELS
+import { 
+  Authenticator,
+  SignIn,
+  SignUp,
+  SignOut,
+  ConfirmSignUp,
+  ConfirmSignIn,
+  ForgotPassword,
+  SetMfa,
+  Chatbot,
+  PhotoPicker,
+  S3Album,
+  S3Image
+} from './components';
 import { AmplifyPlugin } from './plugins/AmplifyPlugin';
 import AmplifyEventBus from './events/AmplifyEventBus';
 
 export default {
   name: 'amplify',
   components: {
-    ...components,
+    Authenticator,
+    SignIn,
+    SignUp,
+    SignOut,
+    ConfirmSignUp,
+    ConfirmSignIn,
+    ForgotPassword,
+    SetMfa,
+    Chatbot,
+    PhotoPicker,
+    S3Album,
+    S3Image
   },
   AmplifyPlugin,
   AmplifyEventBus,
