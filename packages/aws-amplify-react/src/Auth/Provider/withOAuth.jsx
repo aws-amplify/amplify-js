898fcc892df55feaaea33d93cae65e5ab9987633
powerful23 <wenzhuo235@gmail.com>
2018-10-02 15:21:07 -0700

    minor change

--- packages/aws-amplify-react/src/Auth/Provider/withOAuth.jsx
+++ packages/aws-amplify-react/src/Auth/Provider/withOAuth.jsx
@@ -16,6 +16,15 @@ export default function withOAuth(Comp, options) {
         constructor(props) {
             super(props);
             this.signIn = this.signIn.bind(this);
+            this.initialize = this.initialize.bind(this);
+        }
+
+        componentDidMount() {
+            this.initialize();
+        }
+
+        initialize() {
+            
         }
 
         signIn() {
