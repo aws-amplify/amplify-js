cbbc4ff499204469cbbcf226f22b222e55176165
powerful23 <wenzhuo235@gmail.com>
2018-10-19 14:36:08 -0700

    minor change

--- packages/core/src/JS.ts
+++ packages/core/src/JS.ts
@@ -160,4 +160,27 @@ export default class JS {
             isNode
         };
     }
+
+    /**
+     * transfer the first letter of the keys to lowercase
+     * @param obj the object
+     */
+    static transferKeyToLowerCase(obj, whiteList=[]) {
+        if (typeof obj !== 'object' ) return obj;
+
+        let ret = {};
+
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                let transferedKey = key;
+                if (!whiteList.includes(key)) {
+                    transferedKey = key[0].toLowerCase() + key.slice(1);
+                }
+
+                ret[transferedKey] = JS.transferKeyToLowerCase(obj[key], whiteList);
+            }
+        }
+        
+        return ret;
+    }
 }
