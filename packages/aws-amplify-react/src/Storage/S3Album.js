77947c03455de47bf47246d941bba4985836048e
yiksanchan <evan.chanyiksan@gmail.com>
2018-11-16 22:57:14 -0800

    Remove unused imports and functions

--- packages/aws-amplify-react/src/Storage/S3Album.js
+++ packages/aws-amplify-react/src/Storage/S3Album.js
@@ -14,14 +14,13 @@
 import * as React from 'react';
 import { Component } from 'react';
 
-import { ClientDevice, JS, ConsoleLogger as Logger, Hub } from '@aws-amplify/core';
+import { JS, ConsoleLogger as Logger, Hub } from '@aws-amplify/core';
 import Storage from '@aws-amplify/storage';
 
 import { Picker } from '../Widget';
 import AmplifyTheme from '../AmplifyTheme';
 import S3Image from './S3Image';
 import S3Text from './S3Text';
-import { calcKey } from './Common';
 
 const logger = new Logger('Storage.S3Album');
 
@@ -68,7 +67,6 @@ export default class S3Album extends Component {
     }
 
     handlePick(data) {
-        const that = this;
         const { onPick, onLoad, onError, track, level } = this.props;
 
         if (onPick) { onPick(data); }
@@ -116,11 +114,6 @@ export default class S3Album extends Component {
 
         const selected_items = this.state.items.filter(item => item.selected);
         onSelect(item, selected_items);
-    }
-
-    onHubCapsule(capsule) {
-        const theme = this.props.theme || AmplifyTheme;
-        this.setState({ theme: Object.assign({}, theme) });
     }
 
     componentDidMount() {
