7b4af3861e85f30ebd38ca022491153b5d02630c
Dustin Noyes <dnnoyes@amazon.com>
2019-02-01 10:52:58 -0800

    ng packagr

---
+++ packages/aws-amplify-angular2/projects/awesome/src/public_api.ts
@@ -0,0 +1,11 @@
+/*
+ * Public API Surface of awesome
+ */
+// export { AmplifyAPIService } from './lib/providers/modules/amplify.api.service';
+export { AmplifyAuthService } from './lib/providers/modules/amplify.auth.service';
+export { AmplifyStorageService } from './lib/providers/modules/amplify.storage.service';
+export * from './lib/aws-amplify-angular.module';
+export * from './lib/aws-amplify-ionic-module';
+export * from './lib/providers';
+export * from './lib/components';
+export * from './lib/directives';
