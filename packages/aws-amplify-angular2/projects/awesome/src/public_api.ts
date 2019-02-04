77fa87280d742176fb21dfd1768c55777ad636a5
Dustin Noyes <dnnoyes@amazon.com>
2019-02-04 12:03:44 -0800

    poc working

--- packages/aws-amplify-angular2/projects/awesome/src/public_api.ts
+++ packages/aws-amplify-angular2/projects/awesome/src/public_api.ts
@@ -1,9 +1,7 @@
 /*
  * Public API Surface of awesome
  */
-// export { AmplifyAPIService } from './lib/providers/modules/amplify.api.service';
-export { AmplifyAuthService } from './lib/providers/modules/amplify.auth.service';
-export { AmplifyStorageService } from './lib/providers/modules/amplify.storage.service';
+
 export * from './lib/aws-amplify-angular.module';
 export * from './lib/aws-amplify-ionic-module';
 export * from './lib/providers';
