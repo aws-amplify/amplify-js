7a4a73d7274f2ddde1b036cc8d02051e00872eca
Dustin Noyes <dnnoyes@amazon.com>
2019-02-04 15:31:29 -0800

    removed extra dummy package

--- packages/aws-amplify-angular2/projects/awesome/src/public_api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of awesome
- */
-
-export * from './lib/aws-amplify-angular.module';
-export * from './lib/aws-amplify-ionic-module';
-export * from './lib/providers';
-export * from './lib/components';
-export * from './lib/directives';
