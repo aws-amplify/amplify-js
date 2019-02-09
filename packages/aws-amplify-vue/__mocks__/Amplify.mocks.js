5200961acc869a209b6e3138d8dce018627a4204
jordanranz <jordanmranz@gmail.com>
2019-02-08 16:14:04 -0800

    Update Vue unit tests

--- packages/aws-amplify-vue/__mocks__/Amplify.mocks.js
+++ packages/aws-amplify-vue/__mocks__/Amplify.mocks.js
@@ -39,9 +39,11 @@ module.exports = {
     start: jest.fn(),
     isMuted: jest.fn(() => { return false }),
     isVRCapable: jest.fn(() => { return false }),
-    onSceneEvent: jest.fn(() => Promise.resolve({})),
+    onSceneEvent: jest.fn(),
     setMuted: jest.fn(),
     enableAudio: jest.fn(),
     enterVR: jest.fn(),
+    exitVR: jest.fn(),
+    vrPresentationActive: jest.fn(() => { return false })
   }
 };
