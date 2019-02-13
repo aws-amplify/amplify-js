0e7a4e595b942df097fa0d665edfdf982f908694
Dustin Noyes <dnnoyes@amazon.com>
2019-02-13 14:51:47 -0800

    test angular package

---
+++ packages/amplifyangular2/projects/aws-amplify-angular/src/public_api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of aws-amplify-angular
+ */
+
+export * from './lib/aws-amplify-angular.module';
+export * from './lib/aws-amplify-ionic-module';
+export * from './lib/providers/index';
+export * from './lib/components/index';
+export * from './lib/directives/index';
