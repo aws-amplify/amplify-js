63336dd89ceefdb0f9bde3f1f6663d06126d627c
Jonathan Orozco <jonatho1@uci.edu>
2019-03-13 10:43:25 -0700

    cleaned up unneeded html elements and added copyright comments

--- packages/aws-amplify-vue/src/components/authenticator/Provider/FacebookButton.vue
+++ packages/aws-amplify-vue/src/components/authenticator/Provider/FacebookButton.vue
@@ -1,10 +1,23 @@
+/*
+ * Copyright 2017-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
 <template>
   <button :id="amplifyUI.facebookSignInButton" :class="amplifyUI.signInButton" variant="facebookSignInButton" @click="signIn">
     <span :class="amplifyUI.signInButtonIcon">
       <svg viewBox='0 0 279 538' xmlns='http://www.w3.org/2000/svg'><g id='Page-1' fill='none' fillRule='evenodd'><g id='Artboard' fill='#FFF'><path d='M82.3409742,538 L82.3409742,292.936652 L0,292.936652 L0,196.990154 L82.2410458,196.990154 L82.2410458,126.4295 C82.2410458,44.575144 132.205229,0 205.252865,0 C240.227794,0 270.306232,2.59855099 279,3.79788222 L279,89.2502322 L228.536175,89.2502322 C188.964542,89.2502322 181.270057,108.139699 181.270057,135.824262 L181.270057,196.89021 L276.202006,196.89021 L263.810888,292.836708 L181.16913,292.836708 L181.16913,538 L82.3409742,538 Z' id='Fill-1' /></g></g></svg>
     </span>
     <span :class="amplifyUI.signInButtonContent">
-        {{$Amplify.I18n.get('Sign In with Facebook')}}
+        {{ $Amplify.I18n.get('Sign In with Facebook') }}
     </span>
   </button>
 </template>
@@ -17,7 +30,7 @@ import constants from '../common/constants.js';
 export default {
   name: 'FacebookButton',
   props: {
-    facebook_app_id: {
+    facebookAppId: {
       type: String,
       required: true,
       validator: (val) => {
@@ -48,7 +61,7 @@ export default {
 
       const fb = window.FB;
       fb.init({
-        appId   : this.facebook_app_id,
+        appId   : this.facebookAppId,
         cookie  : true,
         xfbml   : true,
         version : 'v2.11'
@@ -117,7 +130,7 @@ export default {
     this.logger = new this.$Amplify.Logger(this.$options.name);
   },
   mounted: function() {
-    if (this.facebook_app_id && !window.FB) this.createScript();
+    if (this.facebookAppId && !window.FB) this.createScript();
   }
 }
 </script>
