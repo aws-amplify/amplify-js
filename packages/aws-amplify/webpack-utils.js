11e7cb5d4d000fe197d75d30c7e5403a218667da
Justin Grant <justin@justingrant.net>
2019-04-10 10:06:34 -0700

    Commit to fix CircleCI stuck build

--- packages/aws-amplify/webpack-utils.js
+++ packages/aws-amplify/webpack-utils.js
@@ -1,25 +1,29 @@
-// This module fixes sourcemap paths for the various Amplify packages. 
-// It's needed because packages at build time live in different (relative) folder 
-// locations than the folders where packages are installed from npm. 
-// For example, /packages/aws-amplify/src/index.ts imports '@aws-amplify/ui'.
-// At build time, the aws-amplify and amplify-ui folders are siblings, but when 
-// the aws-amplify package is installed from npm, the amplify-ui folder is 
-// installed at ./node_modules/@aws-amplify/ui, which is a new name and is 
-// no longer a sibling to ./node_modules/aws-amplify like it was at build time. 
-// These changes mean that end users can't easily debug into Amplify code, especially
+// This module fixes sourcemap paths for the various Amplify packages. It's
+// needed because packages at build time live in different (relative) folder
+// locations than the folders where packages are installed from npm. For
+// example, /packages/aws-amplify/src/index.ts imports @aws-amplify/ui. At build
+// time, the aws-amplify and amplify-ui folders are siblings, but when the
+// aws-amplify package is installed from npm, the amplify-ui folder is installed
+// at ./node_modules/@aws-amplify/ui, which is a new name and is no longer a
+// sibling to ./node_modules/aws-amplify like it was at build time. These
+// changes mean that end users can't easily debug into Amplify code, especially
 // using IDEs like VSCode.
 //
-// The code in this file changes paths inside Amplify sourcemaps to work around 
+// The code in this file changes paths inside Amplify sourcemaps to work around
 // the issues above. The following changes are made: 
-// 1. sourcemap paths that point to node_modules dependencies (e.g. lodash)
-//    are mapped to webpack:///./node_modules/*
-// 2. sourcemap paths that point to sibling packages under the @aws-amplify alias 
-//    (like the UI example above) are mapped to webpack:///./node_modules/@aws-amplify/*
+// 1. sourcemap paths that point to node_modules dependencies (e.g. lodash) are
+//    mapped to webpack:///./node_modules/*
+// 2. sourcemap paths that point to sibling packages under the @aws-amplify
+//    alias (like the UI example above) are mapped (using package names, not
+//    folders) to webpack:///./node_modules/@aws-amplify/*
+// 3. other paths, e.g. relative paths in the same package, or webpack or node
+//    builtins, will be left alone (same behavior as current webpack config).
 //
-// These path mappings are designed to be compatible with VSCode's default source mapping
-// options here: https://github.com/Microsoft/vscode-chrome-debug#sourcemaps
+// These path mappings are designed to be compatible with VSCode's default
+// source mapping options here:
+// https://github.com/Microsoft/vscode-chrome-debug#sourcemaps
 //
-// IMPORTANT: if new packages are added to Amplify, they should be added to the map below.
+// IMPORTANT: if new packages are added to Amplify, add them to the map below.
 
 const packageFolderMap = {
   'amazon-cognito-identity-js': 'amazon-cognito-identity-js',
