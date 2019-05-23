d364fa7d7a7c2f55894090a406a10df4bc5225f5
Dustin Noyes <dnnoyes@amazon.com>
2019-05-22 19:51:26 -0700

    changed e var name to event

--- packages/aws-amplify-react/src/Auth/ConfirmSignIn.jsx
+++ packages/aws-amplify-react/src/Auth/ConfirmSignIn.jsx
@@ -63,8 +63,8 @@ export default class ConfirmSignIn extends AuthPiece {
             });
     }
 
-    confirm(e) {
-        e.preventDefault();
+    confirm(event) {
+        event.preventDefault();
         const user = this.props.authData;
         const { code } = this.inputs;
         const mfaType = user.challengeName === 'SOFTWARE_TOKEN_MFA' ? 'SOFTWARE_TOKEN_MFA' : null;
