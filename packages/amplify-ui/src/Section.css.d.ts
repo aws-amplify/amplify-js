8fa15581faba22a7fe2b8c6cde8b18312f5cc3b3
Rich Buggy <rich@buggy.id.au>
2019-06-06 22:37:16 +1000

    Merge changes from master

--- packages/amplify-ui/src/Section.css.d.ts
+++ packages/amplify-ui/src/Section.css.d.ts
@@ -3,6 +3,7 @@ export const container: string;
 export const actionRow: string;
 export const sectionHeader: string;
 export const sectionHeaderHint: string;
+export const sectionHeaderContent: string;
 export const sectionBody: string;
 export const sectionFooter: string;
 export const sectionFooterPrimaryContent: string;
