a90811faa1b95467254cc94844191b275f5a514f
nextgen-jp <52084623+nextgen-jp@users.noreply.github.com>
2019-06-27 17:26:50 -0500

    Allow phone number only signup.
    signUpWithPhoneNumberFields should default to phone_number and password.

--- packages/aws-amplify-react-native/src/Auth/common/default-sign-up-fields.js
+++ packages/aws-amplify-react-native/src/Auth/common/default-sign-up-fields.js
@@ -58,28 +58,17 @@ export const signUpWithEmailFields = [
   }
 ];
 
- export const signUpWithPhoneNumberFields = [
-  {
-      label: 'Phone Number',
-      key: 'phone_number',
-      placeholder: 'Phone Number',
-      required: true,
-      displayOrder: 1
-  },
-  {
-      label: 'Password',
-      key: 'password',
-      required: true,
-      placeholder: 'Password',
-      type: 'password',
-      displayOrder: 2,
-  },
-  {
-      label: 'Email',
-      key: 'email',
-      required: true,
-      placeholder: 'Email',
-      type: 'email',
-      displayOrder: 3
-  },
-]; 
+export const signUpWithPhoneNumberFields = [{
+    label: 'Phone Number',
+    key: 'phone_number',
+    placeholder: 'Phone Number',
+    required: true,
+    displayOrder: 1
+}, {
+    label: 'Password',
+    key: 'password',
+    required: true,
+    placeholder: 'Password',
+    type: 'password',
+    displayOrder: 2
+}];
