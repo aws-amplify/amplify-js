53abda01b57ac28a2db81e5c5341bf2087e93281
wlee221 <wlee221@live.unc.edu>
2019-07-05 10:37:11 -0700

    Add identify implementation in abstract provider

--- packages/predictions/src/types/Providers/AbstractIdentifyPredictionsProvider.ts
+++ packages/predictions/src/types/Providers/AbstractIdentifyPredictionsProvider.ts
@@ -1,8 +1,34 @@
-import { AbstractPredictionsProvider } from ".";
+import { AbstractPredictionsProvider } from '.';
+import {
+    IdentifyEntityInput, IdentifyFacesInput, isIdentifyEntityInput,
+    isIdentifyFacesInput, IdentifyEntityOutput, IdentifyFacesOutput
+} from '../Predictions';
+import { Logger } from '@aws-amplify/core';
+const logger = new Logger('AbstractIdentifyPredictionsProvider');
 
 export abstract class AbstractIdentifyPredictionsProvider extends AbstractPredictionsProvider {
 
     getCategory(): string {
-        return "Identify";
+        return 'Identify';
+    }
+
+    identify(input: IdentifyEntityInput | IdentifyFacesInput): Promise<any> {
+        if (isIdentifyEntityInput(input)) {
+            logger.debug('identifyEntity');
+            return this.identifyEntity(input);
+        } else if (isIdentifyFacesInput(input)) {
+            logger.debug('identifyFaces');
+            return this.identifyFaces(input);
+        } else {
+            return this.orchestrateWithGraphQL(input);
+        }
+    }
+
+    protected identifyEntity(input: IdentifyEntityInput): Promise<IdentifyEntityOutput> {
+        throw new Error('identifyEntity is not implemented by this provider');
+    }
+
+    protected identifyFaces(input: IdentifyFacesInput): Promise<IdentifyFacesOutput> {
+        throw new Error('identifyFaces is not implemented by this provider');
     }
 }
