f181a37d5bed3c719b0a9815ccca454acb7e9dab
Jordan Ranz <jordanmranz@gmail.com>
2019-07-24 14:29:06 -0700

    Clean up linting (#3711)
    * Remove husky pre-hooks until linting is functioning
    
    * Clean up linter
    
    * Revert quotation change

--- packages/amplify-ui-components/.eslintrc.js
+++ packages/amplify-ui-components/.eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
@@ -12,6 +11,8 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/no-empty-interface': 0
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-unused-vars': 0
   },
 };
