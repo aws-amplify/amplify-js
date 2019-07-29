6999c65d33783ce69f3fceb0a766f0bce0662c34
Rodriguez Elorza <elorzafe@amazon.com>
2019-07-29 15:11:34 -0700

    "fix issue with provider config"

--- packages/predictions/package/src/types/Providers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './AbstractPredictionsProvider';
-export * from './AbstractConvertPredictionsProvider';
-export * from './AbstractIdentifyPredictionsProvider';
-export * from './AbstractInferPredictionsProvider';
-export * from './AbstractInterpretPredictionsProvider';
