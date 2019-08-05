aeb41ed8504a3f81528dd90aef8c217f83669fbc
Manuel Iglesias <manuelig@amazon.com>
2019-08-05 14:50:19 -0700

    fix(@aws-amplify/core): Edge browser misidentified as Chrome

--- packages/core/src/ClientDevice/browser.ts
+++ packages/core/src/ClientDevice/browser.ts
@@ -71,11 +71,11 @@ function browserType(userAgent) {
     const operaMatch = /.+(Opera[\s[A-Z]*|OPR[\sA-Z]*)\/([0-9\.]+).*/i.exec(userAgent);
     if (operaMatch) { return { type: operaMatch[1], version: operaMatch[2]}; }
 
-    const cfMatch = /.+(Chrome|Firefox|FxiOS)\/([0-9\.]+).*/i.exec(userAgent);
-    if (cfMatch) { return { type: cfMatch[1], version: cfMatch[2]}; }
-
     const ieMatch = /.+(Trident|Edge)\/([0-9\.]+).*/i.exec(userAgent);
     if (ieMatch) { return { type: ieMatch[1], version: ieMatch[2]}; }
+    
+    const cfMatch = /.+(Chrome|Firefox|FxiOS)\/([0-9\.]+).*/i.exec(userAgent);
+    if (cfMatch) { return { type: cfMatch[1], version: cfMatch[2]}; }
 
     const sMatch = /.+(Safari)\/([0-9\.]+).*/i.exec(userAgent);
     if (sMatch) { return { type: sMatch[1], version: sMatch[2]}; }
