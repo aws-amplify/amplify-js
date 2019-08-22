c0d075dd2c56c9689e52688542ed717882740343
T.J. Leing <leint@amazon.com>
2019-08-21 18:37:48 -0700

    Add amplify-hint tests and stories

--- packages/amplify-ui-components/src/components/amplify-button/amplify-button.e2e.ts
+++ packages/amplify-ui-components/src/components/amplify-button/amplify-button.e2e.ts
@@ -1,22 +1,23 @@
 import { E2EPage, newE2EPage } from '@stencil/core/testing';
+import { pixelThreshold } from '../../common/testing';
 
 describe('amplify-button', () => {
   let page: E2EPage;
 
   beforeEach(async () => {
     page = await newE2EPage();
+    await page.setContent('<amplify-button>FOO</amplify-button>');
   });
 
   it('renders', async () => {
-    await page.setContent('<amplify-button></amplify-button>');
     const element = await page.find('amplify-button');
-    expect(element).toHaveProperty('type');
+    expect(element).not.toBeNull();
+
+    const screenshot = await page.compareScreenshot('amplify-form-section', { fullPage: true });
+    expect(screenshot).toMatchScreenshot({ allowableMismatchedPixels: pixelThreshold });
   });
 
   it('fires its onButtonClicked callback upon being clicked', async () => {
-    console.log("started the test");
-    await page.setContent('<amplify-button></amplify-button>');
-
     const buttonElement = await page.find('amplify-button');
     expect(buttonElement).not.toBeNull();
 
@@ -25,30 +26,14 @@ describe('amplify-button', () => {
 
     // This block here, and the .exposeFunction() above, are both necessary
     // if you ever want to pass a function into an object's props
-    /*
-    await page.$eval('amplify-button', (buttonElement: any) => {
-      buttonElement.onButtonClicked = this.exposedfunc;
-      buttonElement.type = 'submit'; // Adding a type so that the component rerenders
+    await page.$eval('amplify-button', (el: any) => {
+      el.onButtonClicked = this.exposedfunc;
     });
     await page.waitForChanges();
-    */
 
     const button = await page.find('button');
-    console.log(button);
-    //await button.click().catch(e => e);
-    //await page.click('button');
-    //await page.click('baton'); // <= fails with the error: "No node found for selector: baton" -- so page.click('button') has to be finding the button, too
-    //await page.waitForChanges();
-    /*
-    */
-   /*
-    await page.focus('button');
-    await page.keyboard.type('\n');
-    */
-   /*
-    await page.$eval('button', (elem: any) => elem.click());
-    */
-
-    //expect(func).toBeCalledTimes(1);
+    expect(func).not.toHaveBeenCalled();
+    await button.click();
+    //expect(func).toHaveBeenCalledTimes(1);
   });
 });
