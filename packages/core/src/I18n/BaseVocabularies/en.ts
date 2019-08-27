735debf0eaaaef6cb89f9774f74e44fe23df30e5
Daniel Muller <daniel@spuul.com>
2019-08-27 12:09:13 +0800

    Add English translations to serve as base

---
+++ packages/core/src/I18n/BaseVocabularies/en.ts
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+ 
+export const messages = {
+    'Account recovery requires verified contact information':
+        'Account recovery requires verified contact information',
+    'An account with the given email already exists.':
+        'An account with the given email already exists.',
+    'Back to Sign In': 'Back to Sign In',
+    'Change Password': 'Change Password',
+    'Code': 'Code',
+    'Confirm Sign In': 'Confirm Sign In',
+    'Confirm Sign Up': 'Confirm Sign Up',
+    'Confirm a Code': 'Confirm a Code',
+    'Confirm': 'Confirm',
+    'Confirmation Code': 'Confirmation Code',
+    'Create Account': 'Create Account',
+    'Create a new account': 'Create a new account',
+    'Create account': 'Create account',
+    'Email': 'Email',
+    'Enter your password': 'Enter your password',
+    'Enter your username': 'Enter your username',
+    'Forget your password? ': 'Forget your password? ',
+    'Forgot Password': 'Forgot Password',
+    'Have an account? ': 'Have an account? ',
+    'Incorrect username or password':
+        'Incorrect username or password',
+    'Invalid password format': 'Invalid password format',
+    'Invalid phone number format':
+        `Invalid phone number format:
+Use a Number with format: +12345678900`,
+    'Loading...': 'Loading...',
+    'Lost your code? ': 'Lost your code? ',
+    'Lost your code?': 'Lost your code?',
+    'New Password': 'New Password',
+    'No account? ': 'No account? ',
+    'Password attempts exceeded': 'Password attempts exceeded',
+    'Password': 'Password',
+    'Phone Number': 'Phone Number',
+    'Resend Code': 'Resend Code',
+    'Resend a Code': 'Resend a Code',
+    'Reset password': 'Reset password',
+    'Reset your password': 'Reset your password',
+    'Send Code': 'Send Code',
+    'Sign In': 'Sign In',
+    'Sign Out': 'Sign Out',
+    'Sign Up': 'Sign Up',
+    'Sign in to your account': 'Sign in to your account',
+    'Sign in': 'Sign in',
+    'Skip': 'Skip',
+    'Submit': 'Submit',
+    'User already exists': 'User already exists',
+    'User does not exist': 'User does not exist',
+    'Username cannot be empty': 'Username cannot be empty',
+    'Username': 'Username',
+    'Verify Contact': 'Verify Contact',
+    'Verify': 'Verify'
+};
