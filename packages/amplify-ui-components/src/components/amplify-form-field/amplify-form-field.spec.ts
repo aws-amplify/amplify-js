4ef54d5b8d05320f4d0c5eba5007ffadde4e4b57
T.J. Leing <leint@amazon.com>
2019-08-27 15:59:38 -0700

    Form field extra overrideStyle tests

--- packages/amplify-ui-components/src/components/amplify-form-field/amplify-form-field.spec.ts
+++ packages/amplify-ui-components/src/components/amplify-form-field/amplify-form-field.spec.ts
@@ -2,6 +2,17 @@ import { newSpecPage } from '@stencil/core/testing';
 import { AmplifyFormField } from './amplify-form-field'
 
 describe('amplify-form-field spec:', () => {
+  describe('Component logic ->', () => {
+    let formField;
+
+    beforeEach(() => {
+      formField = new AmplifyFormField();
+    });
+
+    it('should have style override prop set to false by default', () => {
+      expect(formField.overrideStyle).toBe(false);
+    });
+  });
   describe('Render logic ->', () => {
     it('renders no label or description if none are provided', async () => {
       const page = await newSpecPage({
@@ -34,6 +45,15 @@ describe('amplify-form-field spec:', () => {
       const page = await newSpecPage({
         components: [AmplifyFormField],
         html: `<amplify-form-field label='label' description='description' field-id='id'><input slot="input" /></amplify-form-field>`,
+      });
+
+      expect(page.root).toMatchSnapshot();
+    });
+
+    it('renders without Emotion CSS classes when style override is true', async () => {
+      const page = await newSpecPage({
+        components: [AmplifyFormField],
+        html: `<amplify-form-field label='label' description='description' field-id='id' override-style='true'></amplify-form-field>`,
       });
 
       expect(page.root).toMatchSnapshot();
