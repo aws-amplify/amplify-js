99f9f6a00ca53753c24cd136b343d1addcff5d8a
Thomas Leing <bluezebragames@gmail.com>
2019-08-27 14:14:26 -0700

    Label component (#3927)
    * Label component
    
    * Swap `` and '', thanks Sam!

--- packages/amplify-ui-components/src/components/amplify-label/amplify-label.e2e.ts
+++ packages/amplify-ui-components/src/components/amplify-label/amplify-label.e2e.ts
@@ -1,19 +1,19 @@
 import { E2EPage, newE2EPage } from '@stencil/core/testing';
 import { pixelThreshold } from '../../common/testing';
 
-describe('amplify-hint e2e:', () => {
+describe('amplify-label e2e:', () => {
   let page: E2EPage;
 
   beforeEach(async () => {
     page = await newE2EPage();
-    await page.setContent('<amplify-hint>This is a hint</amplify-hint>');
+    await page.setContent('<amplify-label>This is a label</amplify-label>');
   });
 
-  it('renders with text `This is a hint`', async () => {
-    const element = await page.find('amplify-hint');
+  it('renders with text `This is a label`', async () => {
+    const element = await page.find('amplify-label');
     expect(element).not.toBeNull();
 
-    const screenshot = await page.compareScreenshot('amplify-hint', { fullPage: true });
+    const screenshot = await page.compareScreenshot('amplify-label', { fullPage: true });
     expect(screenshot).toMatchScreenshot({ allowableMismatchedPixels: pixelThreshold });
   });
 });
