21435b8a9fcbcb7770cd5ce2738cf99409bbc434
T.J. Leing <leint@amazon.com>
2019-08-26 19:21:48 -0700

    Modify all past component tests - use "*component-name*: spec/e2e" in overall describe block - standardize "Render logic ->" and "Component logic ->"

--- packages/amplify-ui-components/src/components/amplify-select/amplify-select.e2e.ts
+++ packages/amplify-ui-components/src/components/amplify-select/amplify-select.e2e.ts
@@ -1,7 +1,7 @@
 import { newE2EPage } from '@stencil/core/testing';
 import { pixelThreshold } from '../../common/testing';
 
-describe('amplify-select:', () => {
+describe('amplify-select e2e:', () => {
   it('should render a amplify-select', async () => {
     const page = await newE2EPage();
     await page.setContent(`<amplify-select></amplify-select>`);
