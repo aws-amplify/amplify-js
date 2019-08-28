5fd512eb02b2d1efeabef835e117415b8beb83dc
T.J. Leing <leint@amazon.com>
2019-08-28 13:45:09 -0700

    Hint component should use AMPLIFY_UI_PREFIX

--- packages/amplify-ui-components/src/common/constants.ts
+++ packages/amplify-ui-components/src/common/constants.ts
@@ -2,9 +2,7 @@
 export const AMPLIFY_UI_PREFIX = 'amplify-ui';
 
 // Classnames
-export const AMPLIFY_UI_HINT = `${AMPLIFY_UI_PREFIX}-hint`;
 export const AMPLIFY_UI_LINK = `${AMPLIFY_UI_PREFIX}-link`;
-export const AMPLIFY_UI_FORM_FIELD = `${AMPLIFY_UI_PREFIX}-form-field`;
 export const AMPLIFY_UI_TEXT_INPUT = `${AMPLIFY_UI_PREFIX}-text-input`;
 export const AMPLIFY_UI_SCENE = `${AMPLIFY_UI_PREFIX}-scene`;
 export const AMPLIFY_UI_SCENE_LOADING = `${AMPLIFY_UI_PREFIX}-scene-loading`;
