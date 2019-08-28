09268a9a331d42bc641c64c828e59ffe0624803d
T.J. Leing <leint@amazon.com>
2019-08-28 13:18:10 -0700

    Remove duplicate test

--- packages/amplify-ui-components/src/components/amplify-form-section/amplify-form-section.spec.ts
+++ packages/amplify-ui-components/src/components/amplify-form-section/amplify-form-section.spec.ts
@@ -18,15 +18,6 @@ describe('amplify-form-section spec:', () => {
     });
   });
   describe('Render logic ->', () => {
-    it('should render a form section with only a class of `amplify-ui--form-section` when style override is true', async () => {
-      const page = await newSpecPage({
-        components: [AmplifyFormSection],
-        html: `<amplify-form-section override-style="true"></amplify-form-section>`
-      });
-
-      expect(page.root).toMatchSnapshot();
-    });
-
     it('should render a form section with a submit button text of `Go`', async () => {
       const page = await newSpecPage({
         components: [AmplifyFormSection],
