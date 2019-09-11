69f8e8b4a06a22143497785247d730f4c75ce735
Ori Harel <ori.harel@gmail.com>
2019-09-12 00:15:42 +0300

    added comment

--- packages/auth/src/Auth.ts
+++ packages/auth/src/Auth.ts
@@ -216,6 +216,7 @@ export default class AuthClass {
             });
 
             // **NOTE** - Remove this in a future major release as it is a breaking change
+            // customize if needed
             this._urlListener = config.urlListener || urlListener;
             this._urlListener(({ url }) => {
                 this._handleAuthResponse(url);
