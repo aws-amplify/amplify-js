d7ef678725d4174e6f361fbe6c27621dbd4a4b78
Sam Martinez <samlmar@amazon.com>
2019-09-24 15:10:08 -0700

    feat: Add country dial code component with select comp base (#3999)
    * Add country dial code component with select comp base
    
    * chore: Add missing screenshot
    
    * Add country dial code component with select comp base

---
+++ packages/amplify-ui-components/src/components/amplify-country-dial-code/amplify-country-dial-code.spec.ts
@@ -0,0 +1,30 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { AmplifyCountryDialCode } from './amplify-country-dial-code';
+
+describe('amplify-country-dial-code spec:', () => {
+  describe('Component logic ->', () => {
+    let countryDialCode;
+
+    beforeEach(() => {
+      countryDialCode = new AmplifyCountryDialCode();
+    });
+
+    it('should have style override set to false by default', () => {
+      expect(countryDialCode.overrideStyle).toBe(false);
+    });
+
+    it('should have options be defined by default', () => {
+      expect(countryDialCode.options).toBeDefined();
+    });
+  });
+  describe('Render logic ->', () => {
+    it('shoulder render a `amplif-select` component with country dial codes as default', async () => {
+      const page = await newSpecPage({
+        components: [AmplifyCountryDialCode],
+        html: `<amplify-country-dial-code></amplify-country-dial-code>`
+      });
+
+      expect(page.root).toMatchSnapshot();
+    });
+  });
+});
