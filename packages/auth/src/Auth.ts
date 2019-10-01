c546128b84b8af39253385ab9e95489f2c118d3a
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2019-10-01 15:55:54 -0400

    Test code change

--- packages/auth/src/Auth.ts
+++ packages/auth/src/Auth.ts
@@ -1891,4 +1891,9 @@ export default class AuthClass {
 		const type = this.noUserPoolErrorHandler(this._config);
 		return Promise.reject(new NoUserPoolError(type));
 	}
+
+	private doNothing() {
+		console.log('Yo');
+		return 'Codecov, what do you think of this?';
+	}
 }
