fd90dee4131a9286b0ec396537691edaaf8883ca
jordanranz <jordanmranz@gmail.com>
2019-10-08 16:25:03 -0700

    Clean up icon button snapshot tests

--- packages/amplify-ui-components/src/components/amplify-examples/scene-example.tsx
+++ packages/amplify-ui-components/src/components/amplify-examples/scene-example.tsx
@@ -1,12 +1,10 @@
 import { h } from '@stencil/core';
 import Auth from '@aws-amplify/auth';
 import XR from '@aws-amplify/xr';
-// import Amplify from 'aws-amplify';
 import awsExports from './src/aws-exports';
 
 Auth.configure(awsExports);
 XR.configure(awsExports);
-// Amplify.configure(aws_exports);
 
 const sceneExample = () => <amplify-scene sceneName={'scene1'} />;
 
--- packages/amplify-ui-components/src/components/amplify-icon-button/amplify-icon-button.spec.ts
+++ packages/amplify-ui-components/src/components/amplify-icon-button/amplify-icon-button.spec.ts
@@ -1,39 +1,21 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { AmplifyIconButton } from './amplify-icon-button';
+import { icons } from '../amplify-icon/icons';
+
+/** Helper functions */
+async function snapshotTestIconButton(iconName: string) {
+  const page = await newSpecPage({
+    components: [AmplifyIconButton],
+    html: `<amplify-icon-button name='${iconName}'></amplify-icon>`,
+  });
+
+  expect(page.root).toMatchSnapshot();
+}
 
 describe('amplify-icon-button spec:', () => {
   describe('Render logic ->', () => {
-    it('renders with no button text', async () => {
-      const page = await newSpecPage({
-        components: [AmplifyIconButton],
-        html: `<amplify-icon-button></amplify-icon-button>`,
-      });
-
-      expect(page.root).toMatchSnapshot();
-    });
-    it('renders with button text FOO', async () => {
-      const page = await newSpecPage({
-        components: [AmplifyIconButton],
-        html: `<amplify-icon-button>FOO</amplify-icon-button>`,
-      });
-
-      expect(page.root).toMatchSnapshot();
-    });
-    it('renders without Emotion CSS class when overrideStyle is true', async () => {
-      const page = await newSpecPage({
-        components: [AmplifyIconButton],
-        html: `<amplify-icon-button override-style='true'>FOO</amplify-icon-button>`,
-      });
-
-      expect(page.root).toMatchSnapshot();
-    });
-    it('renders with danger class when button type is reset', async () => {
-      const page = await newSpecPage({
-        components: [AmplifyIconButton],
-        html: `<amplify-icon-button type='reset'>FOO</amplify-icon-button>`,
-      });
-
-      expect(page.root).toMatchSnapshot();
+    Object.keys(icons).map(name => {
+      it(`renders ${name} icon button correctly`, async () => snapshotTestIconButton(name));
     });
   });
   describe('Component logic ->', () => {
