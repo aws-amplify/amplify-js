e5c7cc21526c54d92811d41643f4d304262413ad
jordanranz <jordanmranz@gmail.com>
2019-10-15 14:26:18 -0700

    Clean up package.json and examples

--- packages/amplify-ui-components/src/components/amplify-examples/amplify-examples.tsx
+++ packages/amplify-ui-components/src/components/amplify-examples/amplify-examples.tsx
@@ -3,15 +3,6 @@ import example1 from './example1';
 import example4 from './example4';
 import sceneExample from './scene-example';
 
-// import Auth from '@aws-amplify/auth';
-// import XR from '@aws-amplify/xr';
-// // import Amplify from 'aws-amplify';
-// import aws_exports from './src/aws-exports';
-
-// Auth.configure(aws_exports);
-// XR.configure(aws_exports);
-// Amplify.configure(aws_exports);
-
 const examples = [sceneExample, example1, example4];
 
 const Tabs = ({ active, set }) => (
