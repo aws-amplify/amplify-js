1ee235bae39e6e73956e023ed90fcff2530b6a2f
Eric Clemmons <eric@smarterspam.com>
2019-10-31 11:34:52 -0700

    Revert "Remove @storybook/addon-console"
    This reverts commit be900d38003a63418d33207304979be03808c8bd.

--- packages/amplify-ui-components/.storybook/config.ts
+++ packages/amplify-ui-components/.storybook/config.ts
@@ -1,6 +1,17 @@
 import { configure, addDecorator } from '@storybook/html';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
+import { setConsoleOptions } from '@storybook/addon-console';
+
+addDecorator((storyFn, context) => {
+  setConsoleOptions({
+    log: `${context.kind}/${context.story}`,
+    warn: `${context.kind}/${context.story}/warn`,
+    error: `${context.kind}/${context.story}/error`,
+  });
+
+  return storyFn(context);
+});
 
 addDecorator(withKnobs);
 addDecorator(withA11y);
