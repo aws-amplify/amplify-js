94845782941211f39f1887856171bf0a3293646d
jordanranz <jordanmranz@gmail.com>
2019-11-11 13:39:56 -0800

    Clean up user prop change and showing greeting

--- packages/amplify-ui-components/src/components/amplify-greetings/amplify-greetings.tsx
+++ packages/amplify-ui-components/src/components/amplify-greetings/amplify-greetings.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop, State } from '@stencil/core';
+import { Component, h, Prop, State, Watch } from '@stencil/core';
 import { greetings } from './amplify-greetings.style';
 import { AuthState, CognitoUserInterface } from '../../common/types/auth-types';
 
@@ -16,15 +16,21 @@ export class AmplifyGreetings {
   /** Passed from the Authenticatior component in order to change Authentication state */
   @Prop() handleAuthStateChange: (nextAuthState: AuthState, data?: object) => void;
 
-  @State() showGreetingMessage: boolean = false;
+  @Watch('user')
+  watchHandler(newUserValue: CognitoUserInterface) {
+    // If the user passed in has a username, show username greeting message
+    if (newUserValue && newUserValue.username) this.showUsernameGreeting = true;
+  }
+
+  @State() showUsernameGreeting: boolean = false;
 
   render() {
-    if (this.user !== null) this.showGreetingMessage = true;
     return (
       <header class={greetings}>
-        {this.logo && <span>Logo</span>}
+        {// TODO: user <amplify-logo> instead of <span>
+        this.logo && <span>Logo</span>}
         <amplify-nav>
-          {this.showGreetingMessage && <span>Hello, {this.user.username}</span>}
+          {this.showUsernameGreeting && <span>Hello, {this.user.username}</span>}
           <amplify-sign-out handleAuthStateChange={this.handleAuthStateChange} />
         </amplify-nav>
       </header>
