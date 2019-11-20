c274a38c4ccd96ce8b7d2ee5f67251f407fbc30d
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2019-11-20 16:41:36 -0500

    Override _init method in FirehoseProvider

--- packages/analytics/src/Providers/AWSKinesisFirehoseProvider.ts
+++ packages/analytics/src/Providers/AWSKinesisFirehoseProvider.ts
@@ -17,78 +17,95 @@ import * as Firehose from 'aws-sdk/clients/firehose';
 
 const logger = new Logger('AWSKineisFirehoseProvider');
 
-// events buffer
-const BUFFER_SIZE = 1000;
-const FLUSH_SIZE = 100;
-const FLUSH_INTERVAL = 5*1000; // 5s
-const RESEND_LIMIT = 5;
-
 export default class AWSKinesisFirehoseProvider extends AWSKinesisProvider {
+	private _kinesisFirehose;
 
-    private _kinesisFirehose;
-    private _credentials;
+	constructor(config?) {
+		super(config);
+	}
 
-    constructor(config?) {
-        super(config);
-    }
+	/**
+	 * get provider name of the plugin
+	 */
+	public getProviderName(): string {
+		return 'AWSKinesisFirehose';
+	}
 
-    /**
-     * get provider name of the plugin
-     */
-    public getProviderName(): string {
-        return 'AWSKinesisFirehose';
-    }
+	protected _sendEvents(group) {
+		if (group.length === 0) {
+			return;
+		}
 
-    protected _sendEvents(group) {
-        if (group.length === 0) {
-            // logger.debug('events array is empty, directly return');
-            return;
-        }
+		const { config, credentials } = group[0];
 
-        const { config, credentials } = group[0];
+		const initClients = this._init(config, credentials);
+		if (!initClients) return false;
 
-        const initClients = this._init(config, credentials);
-        if (!initClients) return false;
+		const records = {};
 
-        const records = {};
+		group.map(params => {
+			// spit by streamName
+			const evt = params.event;
+			const { streamName } = evt;
+			if (records[streamName] === undefined) {
+				records[streamName] = [];
+			}
 
-        group.map(params => {
-            // spit by streamName
-            const evt = params.event;
-            const { streamName } = evt;
-            if (records[streamName] === undefined) {
-                records[streamName] = [];
-            }
+			const PartitionKey =
+				evt.partitionKey || `partition-${credentials.identityId}`;
 
-            const PartitionKey = evt.partitionKey || ('partition-' + credentials.identityId);
-            Object.assign(evt.data, { PartitionKey });
-            const Data = JSON.stringify(evt.data);
-            const record = { Data };
-            records[streamName].push(record);
-        });
+			Object.assign(evt.data, { PartitionKey });
+			const Data = JSON.stringify(evt.data);
+			const record = { Data };
+			records[streamName].push(record);
+		});
 
-        Object.keys(records).map(streamName => {
-            logger.debug('putting records to kinesis', streamName, 'with records', records[streamName]);
-            this._kinesisFirehose.putRecordBatch(
-                {
-                    Records: records[streamName],
-                    DeliveryStreamName: streamName
-                },
-                (err, data)=> {
-                    if (err) logger.debug('Failed to upload records to Kinesis', err);
-                    else logger.debug('Upload records to stream', streamName);
-                }
-            );
-        });
-    }
+		Object.keys(records).map(streamName => {
+			logger.debug(
+				'putting records to kinesis',
+				streamName,
+				'with records',
+				records[streamName]
+			);
+			this._kinesisFirehose.putRecordBatch(
+				{
+					Records: records[streamName],
+					DeliveryStreamName: streamName,
+				},
+				err => {
+					if (err) logger.debug('Failed to upload records to Kinesis', err);
+					else logger.debug('Upload records to stream', streamName);
+				}
+			);
+		});
+	}
 
-    protected _initKinesis(region, credentials) {
-        logger.debug('initialize kinesis firehose with credentials', credentials);
-        this._kinesisFirehose = new Firehose({
-            apiVersion: '2015-08-04',
-            region,
-            credentials
-        });
-        return true;
-    }
+	protected _init(config, credentials) {
+		logger.debug('init clients');
+
+		if (
+			this._kinesisFirehose &&
+			this._config.credentials &&
+			this._config.credentials.sessionToken === credentials.sessionToken &&
+			this._config.credentials.identityId === credentials.identityId
+		) {
+			logger.debug('no change for analytics config, directly return from init');
+			return true;
+		}
+
+		this._config.credentials = credentials;
+		const { region } = config;
+
+		return this._initKinesis(region, credentials);
+	}
+
+	protected _initKinesis(region, credentials) {
+		logger.debug('initialize kinesis firehose with credentials', credentials);
+		this._kinesisFirehose = new Firehose({
+			apiVersion: '2015-08-04',
+			region,
+			credentials,
+		});
+		return true;
+	}
 }
