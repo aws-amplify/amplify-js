8eedad04eaa0bfb8b55506c314edae381564b3ff
Eric Clemmons <eric@smarterspam.com>
2019-11-21 13:16:43 -0800

    amplify-sign-in-button uses amplify-icon

--- packages/amplify-ui-components/src/components/amplify-sign-in-button/amplify-sign-in-button.tsx
+++ packages/amplify-ui-components/src/components/amplify-sign-in-button/amplify-sign-in-button.tsx
@@ -2,6 +2,7 @@ import { Component, h, Prop, Host } from '@stencil/core';
 import { cx } from 'emotion';
 
 import * as classNames from './amplify-sign-in-button.style';
+import { icons, IconNameType } from '../amplify-icon/icons';
 
 @Component({
   tag: 'amplify-sign-in-button',
@@ -14,9 +15,11 @@ export class AmplifySignInButton {
     return (
       <Host class={cx(this.provider, classNames.Host)}>
         <button>
-          <span class="icon">
-            <slot name="icon" />
-          </span>
+          {this.provider in icons && (
+            <span class="icon">
+              <amplify-icon name={this.provider as IconNameType} />
+            </span>
+          )}
 
           <span class="content">
             <slot />
