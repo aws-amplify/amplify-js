38f6b419204fd13ace9458937e64fa81fe581579
Ashish Nanda <ashish.nanda.5591@gmail.com>
2019-11-26 16:45:45 -0800

    Updating import to prevent module resolution errors

--- packages/pushnotification/src/PushNotification.ts
+++ packages/pushnotification/src/PushNotification.ts
@@ -18,7 +18,7 @@ import {
 	Platform,
 	AppState,
 } from 'react-native';
-import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import * as PushNotificationIOS from '@react-native-community/push-notification-ios';
 import Amplify, { ConsoleLogger as Logger } from '@aws-amplify/core';
 
 const logger = new Logger('Notification');
