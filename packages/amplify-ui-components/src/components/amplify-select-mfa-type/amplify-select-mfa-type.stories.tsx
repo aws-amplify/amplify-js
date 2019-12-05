5d79d4e54c2711b05ff47df8ee79923a42db3fe9
Sam Martinez <>
2019-12-04 16:02:03 -0800

    Adjust storybook to proper naming

--- packages/amplify-ui-components/src/components/amplify-select-mfa-type/amplify-select-mfa-type.stories.tsx
+++ packages/amplify-ui-components/src/components/amplify-select-mfa-type/amplify-select-mfa-type.stories.tsx
@@ -1,9 +1,7 @@
 import { storiesOf } from '@storybook/html';
-import { knobs } from '../../common/testing';
 
-const signOutStories = storiesOf('amplify-sign-out', module);
+const selectMfaTypeStories = storiesOf('amplify-select-mfa-type', module);
 
-signOutStories.add('default', () => {
-  const override = knobs.overrideStyleKnob();
-  return `<amplify-sign-out override-style=${override}></amplify-sign-out>`;
+selectMfaTypeStories.add('default', () => {
+  return `<amplify-select-mfa-type />`;
 });
