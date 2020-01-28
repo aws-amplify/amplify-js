01a2ea39687e621c7d6f830d3c87948620394e5c
jordanranz <jordanmranz@gmail.com>
2020-01-28 10:31:14 -0800

    Revert bundles change and update snapshots

--- packages/amplify-ui-components/stencil.config.ts
+++ packages/amplify-ui-components/stencil.config.ts
@@ -24,12 +24,6 @@ export const config: Config = {
       '@aws-sdk/eventstream-marshaller': ['EventStreamMarshaller'],
     },
   },
-  bundles: [
-    { components: ['amplify-amazon-button', 'amplify-auth-fields', 'amplify-auth0-button', 'amplify-authenticator', 'amplify-sign-in'] },
-    { components: ['amplify-confirm-sign-in', 'amplify-confirm-sign-up', 'amplify-facebook-button', 'amplify-federated-buttons', 'amplify-federated-sign-in'] },
-    { components: ['amplify-google-button', 'amplify-forgot-password', 'amplify-select-mfa-type', 'amplify-sign-out', 'amplify-sign-up'] },
-    { components: ['amplify-totp-setup', 'amplify-verify-contact'] },
-  ],
   outputTargets: [
     // See: https://github.com/ionic-team/stencil-ds-plugins#angular
     angularOutputTarget({
