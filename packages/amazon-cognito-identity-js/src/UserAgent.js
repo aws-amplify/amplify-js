f354e47a6d7668cfab0711f87595192fcf8358f0
jordanranz <jordanmranz@gmail.com>
2020-01-31 16:25:00 -0800

    UserAgent unit tests for cognito sdk

--- packages/amazon-cognito-identity-js/src/UserAgent.js
+++ packages/amazon-cognito-identity-js/src/UserAgent.js
@@ -1,22 +1,26 @@
-// class for defining the amzn user-agent
-export default UserAgent;
+const DEFAULT_USER_AGENT = 'amazon-cognito-identity-js';
 // constructor
 function UserAgent() {}
 // public
-UserAgent.prototype.userAgent = 'amazon-cognito-identity-js';
+UserAgent.prototype.userAgent = DEFAULT_USER_AGENT;
 
 export const appendToCognitoUserAgent = content => {
-	if (content && !UserAgent.prototype.userAgent.includes(content)) {
-		if (
-			!UserAgent.prototype.userAgent ||
-			UserAgent.prototype.userAgent === ''
-		) {
-			UserAgent.prototype.userAgent = content;
-		} else {
-			UserAgent.prototype.userAgent = UserAgent.prototype.userAgent.concat(
-				' ',
-				content
-			);
-		}
+	if (!content) {
+		return;
+	}
+	if (
+		UserAgent.prototype.userAgent &&
+		!UserAgent.prototype.userAgent.includes(content)
+	) {
+		UserAgent.prototype.userAgent = UserAgent.prototype.userAgent.concat(
+			' ',
+			content
+		);
+	}
+	if (!UserAgent.prototype.userAgent || UserAgent.prototype.userAgent === '') {
+		UserAgent.prototype.userAgent = content;
 	}
 };
+
+// class for defining the amzn user-agent
+export default UserAgent;
