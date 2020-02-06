39fcc6e78342e087b10dc650709c66304de4db60
jordanranz <jordanmranz@gmail.com>
2020-02-06 09:42:08 -0800

    Add semi colon to import

--- packages/amplify-ui-components/src/global/theme.ts
+++ packages/amplify-ui-components/src/global/theme.ts
@@ -10,7 +10,7 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
-import { injectGlobal } from 'emotion'
+import { injectGlobal } from 'emotion';
 
 injectGlobal`
   :root {
