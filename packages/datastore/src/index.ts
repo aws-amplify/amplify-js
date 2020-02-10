29d71075b2e32a1bcb7bcf2d4c04e5df163c7b68
pravgupt@amazon.com <pravgupt@8c85905cd208.ant.amazon.com>
2020-02-10 12:22:19 -0800

    Fix merge conflict

--- packages/datastore/src/index.ts
+++ packages/datastore/src/index.ts
@@ -1,5 +1,5 @@
 import { Amplify } from '@aws-amplify/core';
-import { dataStore as DataStore, initSchema } from './datastore/datastore';
+import { DataStore, initSchema } from './datastore/datastore';
 import { Predicates } from './predicates';
 
 export * from './types';
