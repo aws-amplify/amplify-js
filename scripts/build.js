55680d639458b644e26aab25f2002ee828100a2e
pravgupt@amazon.com <pravgupt@8c85905cd208.ant.amazon.com>
2020-02-10 10:10:15 -0800

    Revert transpiling to es5 as angular rollup has some issues with es2015 transpiled code

--- scripts/build.js
+++ scripts/build.js
@@ -196,7 +196,7 @@ function buildES6(typeScriptCompiler) {
 		downlevelIteration: true,
 		jsx: jsx,
 		sourceMap: true,
-		target: 'es2015',
+		target: 'es5',
 		module: 'es2015',
 		moduleResolution: 'node',
 		declaration: true,
