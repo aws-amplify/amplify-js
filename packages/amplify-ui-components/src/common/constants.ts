c5676b66c17b1aaebea6ccfe2ebcee7e22c40001
jordanranz <jordanmranz@gmail.com>
2020-02-12 12:30:21 -0800

    Remove unused constants and update snaps

--- packages/amplify-ui-components/src/common/constants.ts
+++ packages/amplify-ui-components/src/common/constants.ts
@@ -1,11 +1,3 @@
-// Namespace
-export const AMPLIFY_UI_PREFIX = 'amplify-ui';
-
-// Classnames
-export const AMPLIFY_UI_TEXT_INPUT = `${AMPLIFY_UI_PREFIX}-text-input`;
-export const AMPLIFY_UI_SCENE = `${AMPLIFY_UI_PREFIX}-scene`;
-export const AMPLIFY_UI_SCENE_LOADING = `${AMPLIFY_UI_PREFIX}-scene-loading`;
-
 // Dictionaries
 
 /*
