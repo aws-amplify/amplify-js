c6fbabfb139543b1212e4bd3d28a297543b8046f
Ashika Kasiviswanathan <akasivis@amazon.com>
2020-02-19 19:08:24 -0800

    update colors

--- packages/amplify-ui-components/src/global/theme.ts
+++ packages/amplify-ui-components/src/global/theme.ts
@@ -18,38 +18,54 @@ injectGlobal`
     /* Typography */
     --amplify-font-family: 'Amazon Ember', 'Helvetica Neue Light', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif';
 
-    --amplify-primary-font-size: 1em;
-    --amplify-secondary-font-size: 0.875em;
-    --amplify-third-font-size: 0.813em;
-
-    --amplify-text-xs:;
-    --amplify-text-sm:;
-    --amplify-text-md:;
-    --amplify-text-lg:;
-    --amplify-text-xl:;
-    --amplify-text-xxl:;
-
+    --amplify-text-xxs: 0.75rem;
+    --amplify-text-xs: 0.81rem;
+    --amplify-text-sm: 0.875rem;
+    --amplify-text-md: 1rem;
+    --amplify-text-lg: 1.5rem;
+    --amplify-text-xl: 2rem;
+    --amplify-text-xxl: 2.5rem;
 
     /* Colors */
     --amplify-amazon-orange: #ff9900;
     --amplify-light-amazon-orange: #ffac31;
     --amplify-dark-amazon-orange: #e88b01;
+
     --amplify-light-squid-ink: #31465f;
     --amplify-deep-squid-ink: #152939;
+    --amplify-dark-squid-ink: #1F2A37;
+
+
+    /* Neutral */
     --amplify-grey: #828282;
     --amplify-light-grey: #c4c4c4;
     --amplify-white: #ffffff;
+
     --amplify-dark-blue: #31465f;
     --amplify-light-blue: #00a1c9; 
     --amplify-red: #dd3f5b;
 
-    /* Space Scale */
+    --amplify-primary-color: var(--amplify-amazon-orange);
+    --amplify-primary-contrast: var(--amplify-white);
+    --amplify-primary-shade: var(--amplify-light-amazon-orange);
+    --amplify-primary-tint: var(--amplify-dark-amazon-orange); 
+
+    --amplify-secondary-color: var(--amplify-deep-squid-ink);
+    --amplify-secondary-contrast: var(--amplify-white);
+    --amplify-secondary-shade: var(--amplify-light-squid-ink);
+    --amplify-secondary-tint: var(--amplify-dark-squid-ink); 
+
+    --amplify-tertiary-color: #5d8aff;
+    --amplify-tertiary-contrast: var(--amplify-white);
+    --amplify-tertiary-shade: #7da1ff;
+    --amplify-tertiary-tint: #537BE5;
 
     /* Breakpoints */
-    --breakpointSm: 480px;
-    --breakpointMd: 768px;
-    --breakpointLg: 1024px;
-    --breakpointXl: 1280px;
+    --breakpointSm: 320px;
+    --breakpointMd: 672px;
+    --breakpointLg: 1056px;
+    --breakpointXl: 1321px;
+    --breakpointMax: 1584px;
 
 
     /* Theme */
