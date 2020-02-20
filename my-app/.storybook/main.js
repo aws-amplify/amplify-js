0e258df86d656444fe6dfabb2ff073cddd3b240a
Eric Clemmons <eric@smarterspam.com>
2020-02-20 14:59:25 -0800

    Working Storybook with CRA + TS + Docs + MDX

---
+++ my-app/.storybook/main.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+	stories: ['../src/stories/**/*.stories.(ts|tsx|js|jsx|mdx)'],
+	addons: [
+		'@storybook/addon-actions',
+		'@storybook/addon-links',
+		{
+			name: '@storybook/preset-create-react-app',
+			options: {
+				tsDocgenLoaderOptions: {
+					tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
+				},
+			},
+		},
+		{
+			name: '@storybook/addon-docs',
+			options: {
+				configureJSX: true,
+			},
+		},
+	],
+};
