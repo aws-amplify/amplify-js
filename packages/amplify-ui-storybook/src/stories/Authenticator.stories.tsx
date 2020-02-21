dcb9112b787b4a69d645d8d466c418e835dce9b5
Eric Clemmons <eric@smarterspam.com>
2020-02-20 16:45:12 -0800

    Test importing AmplifyAuthenticator

---
+++ packages/amplify-ui-storybook/src/stories/Authenticator.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+
+export default {
+	title: 'Authenticator',
+	component: AmplifyAuthenticator,
+};
+
+export const defaults = () => <AmplifyAuthenticator />;
