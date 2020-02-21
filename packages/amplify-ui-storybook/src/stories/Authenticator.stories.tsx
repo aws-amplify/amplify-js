ecd1361f0dc3369eda2b422c1f357e42687e54f2
Eric Clemmons <eric@smarterspam.com>
2020-02-20 20:32:07 -0800

    Add Welcome + Authenticator stories

--- packages/amplify-ui-storybook/src/stories/Authenticator.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
-
-export default {
-	title: 'Authenticator',
-	component: AmplifyAuthenticator,
-};
-
-export const defaults = () => <AmplifyAuthenticator />;
