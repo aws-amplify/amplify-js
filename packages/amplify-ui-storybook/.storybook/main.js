175c0a7849df6145ac8d39d020e6e7e015818952
Eric Clemmons <eric@smarterspam.com>
2020-02-20 20:31:34 -0800

    Add @storybook/addon-knobs

--- packages/amplify-ui-storybook/.storybook/main.js
+++ packages/amplify-ui-storybook/.storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
 	stories: ['../src/stories/**/*.stories.(ts|tsx|js|jsx|mdx)'],
 	addons: [
 		'@storybook/addon-actions',
+		'@storybook/addon-knobs',
 		'@storybook/addon-links',
 		{
 			name: '@storybook/preset-create-react-app',
