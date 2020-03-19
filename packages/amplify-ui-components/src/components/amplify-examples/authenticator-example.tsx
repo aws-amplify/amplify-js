8cb188b705b0d56f59fb1a5f43b04e79742e6bff
Jordan Ranz <jordanmranz@gmail.com>
2020-03-19 15:36:32 -0700

    fix(@aws-amplify/ui-components): SVG type fix (#5152)
    * Install new stencil versions and use browser key in stencil configs node resolve
    
    * Update snapshot and readmes
    
    * Fix svg fill rule prop type, keep authenticator example vanilla
    
    * Update snapshots
    
    * Revert commented config
    
    * Update snap

--- packages/amplify-ui-components/src/components/amplify-examples/authenticator-example.tsx
+++ packages/amplify-ui-components/src/components/amplify-examples/authenticator-example.tsx
@@ -1,6 +1,6 @@
 import { h } from '@stencil/core';
 
-const Authenticator = () => <amplify-authenticator usernameAlias="email"></amplify-authenticator>;
+const Authenticator = () => <amplify-authenticator></amplify-authenticator>;
 
 export const AuthenticatorExample = {
   title: 'Sample Authenticator',
