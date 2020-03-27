4af4679dbbbb4bb18d985e8d1cc11a4d86177649
Ashika <35131273+ashika01@users.noreply.github.com>
2020-03-27 12:50:43 -0700

    Responsiveness of UI components (#5182)
    * breakpoint refactor
    * minor cleanup
    * cleanup inline style
    * update snapshots

--- packages/amplify-ui-components/src/components/amplify-confirm-sign-in/amplify-confirm-sign-in.tsx
+++ packages/amplify-ui-components/src/components/amplify-confirm-sign-in/amplify-confirm-sign-in.tsx
@@ -118,7 +118,10 @@ export class AmplifyConfirmSignIn {
         loading={this.loading}
         secondaryFooterContent={
           <span>
-            <amplify-button variant="anchor" onClick={() => this.handleAuthStateChange(AuthState.SignIn)}>
+            <amplify-button
+              variant="anchor"
+              onClick={() => this.handleAuthStateChange(AuthState.SignIn)}
+            >
               {I18n.get(Translations.BACK_TO_SIGN_IN)}
             </amplify-button>
           </span>
