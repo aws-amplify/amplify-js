d4bbb50f7eb3fe9298b2528d3c5c397836a46a47
Eric Clemmons <eric@smarterspam.com>
2020-04-01 13:46:51 -0700

    Add test for responseType

---
+++ packages/storage/src/providers/axios-http-handler.test.ts
@@ -0,0 +1,52 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { EventEmitter } from 'events';
+
+import { AxiosHttpHandler } from './axios-http-handler';
+
+jest.mock('axios');
+
+const request = {
+	method: 'get',
+	path: '/',
+	protocol: 'http:',
+	hostname: 'localhost',
+	port: 3000,
+	query: {},
+	headers: {},
+	clone: null,
+};
+
+const options = {};
+
+describe('AxiosHttpHandler', () => {
+	beforeAll(() => {
+		axios.request.mockResolvedValue({});
+	});
+
+	describe('.handle', () => {
+		it('should not set responseType by default', async () => {
+			const handler = new AxiosHttpHandler({});
+			await handler.handle(request, options);
+
+			expect(axios.request).toHaveBeenLastCalledWith({
+				data: undefined,
+				headers: {},
+				method: 'get',
+				url: 'http://localhost:3000/',
+			});
+		});
+
+		it('should add responseType: "blob" when bufferBody is true', async () => {
+			const handler = new AxiosHttpHandler({ bufferBody: true });
+			await handler.handle(request, options);
+
+			expect(axios.request).toHaveBeenLastCalledWith({
+				data: undefined,
+				headers: {},
+				method: 'get',
+				responseType: 'blob',
+				url: 'http://localhost:3000/',
+			});
+		});
+	});
+});
