6c2e6853251ab60a8beb51d85ff5066b4e12f2d3
Andrei Alecu <andrei.alecu@vecttor.co>
2020-04-11 20:53:24 +0300

    fix: remove require cycles

--- packages/analytics/src/Providers/AWSKinesisFirehoseProvider.ts
+++ packages/analytics/src/Providers/AWSKinesisFirehoseProvider.ts
@@ -12,7 +12,7 @@
  */
 
 import { ConsoleLogger as Logger } from '@aws-amplify/core';
-import { AWSKinesisProvider } from './index';
+import { AWSKinesisProvider } from './AWSKinesisProvider';
 import {
 	PutRecordBatchCommand,
 	FirehoseClient,
@@ -46,7 +46,7 @@ export class AWSKinesisFirehoseProvider extends AWSKinesisProvider {
 
 		const records = {};
 
-		group.map(params => {
+		group.map((params) => {
 			// spit by streamName
 			const evt = params.event;
 			const { streamName } = evt;
@@ -63,7 +63,7 @@ export class AWSKinesisFirehoseProvider extends AWSKinesisProvider {
 			records[streamName].push(record);
 		});
 
-		Object.keys(records).map(streamName => {
+		Object.keys(records).map((streamName) => {
 			logger.debug(
 				'putting records to kinesis',
 				streamName,
@@ -78,8 +78,10 @@ export class AWSKinesisFirehoseProvider extends AWSKinesisProvider {
 						DeliveryStreamName: streamName,
 					})
 				)
-				.then(res => logger.debug('Upload records to stream', streamName))
-				.catch(err => logger.debug('Failed to upload records to Kinesis', err));
+				.then((res) => logger.debug('Upload records to stream', streamName))
+				.catch((err) =>
+					logger.debug('Failed to upload records to Kinesis', err)
+				);
 		});
 	}
 
