fb954be365440f35ba3d57241e1fde749d398521
jordanranz <jordanmranz@gmail.com>
2020-04-20 17:32:29 -0700

    Expose Hub constants

--- packages/amplify-ui-components/src/index.ts
+++ packages/amplify-ui-components/src/index.ts
@@ -1,3 +1,4 @@
 export { AuthState } from './common/types/auth-types';
 export { Translations } from './common/Translations';
+export { AUTH_CHANNEL, UI_AUTH_CHANNEL, TOAST_AUTH_ERROR_EVENT, AUTH_STATE_CHANGE_EVENT } from './common/constants';
 export * from './components';
