24eeee1aae62efcfeed87f37ad808abbb2ae8d7f
Ashish Nanda <ashish.nanda.5591@gmail.com>
2020-04-21 16:52:17 -0700

    Removing the need to install NetInfo in ReactNative Apps when not using DataStore (#5235)
    * Removing the need to install NetInfo in ReactNative Apps when not using DataStore

--- packages/core/__tests__/Util-test.ts
+++ packages/core/__tests__/Util-test.ts
@@ -1,6 +1,8 @@
 'use strict';
 
 import { jitteredExponentialRetry, NonRetryableError } from '../src/Util';
+import ReachabilityNative from '../src/Util/Reachability.native';
+import Reachability from '../src/Util/Reachability';
 import { ConsoleLogger as Logger } from '../src/Logger';
 Logger.LOG_LEVEL = 'DEBUG';
 describe('Util', () => {
@@ -42,5 +44,27 @@ describe('Util', () => {
 		}
 
 		expect(testFunc).toBeCalledTimes(1);
+	});
+	test('Should throw an Error when NetInfo is not passed to networkMonitor in React Native Reachability', () => {
+		const subscribe = netInfo => {
+			new ReachabilityNative().networkMonitor(netInfo);
+		};
+
+		function subscribeWithNetInfo() {
+			// Pass an object that has a fetch attribute (imitate NetInfo)
+			subscribe({ fetch: {} });
+		}
+
+		expect(subscribe).toThrowError(
+			'NetInfo must be passed to networkMonitor to enable reachability in React Native'
+		);
+		expect(subscribeWithNetInfo).not.toThrowError();
+	});
+	test('Should not throw an Error when NetInfo is not passed to networkMonitor in Web Reachability', () => {
+		const subscribe = () => {
+			new Reachability().networkMonitor();
+		};
+
+		expect(subscribe).not.toThrowError();
 	});
 });
