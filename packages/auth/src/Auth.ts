fe6cd41cff04b804224f7295a35321072d17f3e8
Antonio Angelino <antonio@sametab.com>
2020-04-23 12:19:03 +0200

    Reverted style changes.

--- packages/auth/src/Auth.ts
+++ packages/auth/src/Auth.ts
@@ -70,7 +70,7 @@ const logger = new Logger('AuthClass');
 const USER_ADMIN_SCOPE = 'aws.cognito.signin.user.admin';
 
 const AMPLIFY_SYMBOL = (typeof Symbol !== 'undefined' &&
-	typeof Symbol.for === 'function'
+typeof Symbol.for === 'function'
 	? Symbol.for('amplify_default')
 	: '@@amplify_default') as Symbol;
 
@@ -1164,7 +1164,7 @@ export class AuthClass {
 						} else {
 							logger.debug(
 								`Unable to get the user data because the ${USER_ADMIN_SCOPE} ` +
-								`is not in the scopes of the access token`
+									`is not in the scopes of the access token`
 							);
 							return res(user);
 						}
@@ -1215,7 +1215,7 @@ export class AuthClass {
 				if (e === 'No userPool') {
 					logger.error(
 						'Cannot get the current user because the user pool is missing. ' +
-						'Please make sure the Auth module is configured with a valid Cognito User Pool ID'
+							'Please make sure the Auth module is configured with a valid Cognito User Pool ID'
 					);
 				}
 				logger.debug('The user is not authenticated by the error', e);
@@ -1764,7 +1764,7 @@ export class AuthClass {
 					logger.warn(`There is already a signed in user: ${loggedInUser} in your app.
 																	You should not call Auth.federatedSignIn method again as it may cause unexpected behavior.`);
 				}
-			} catch (e) { }
+			} catch (e) {}
 
 			const { token, identity_id, expires_at } = response;
 			// Because Credentials.set would update the user info with identity id
