9c8862fb5ac8c1b4bee7ff96ee858fa97fac298d
jordanranz <jordanmranz@gmail.com>
2020-05-08 12:30:55 -0700

    Update withAuthenticator unit test

--- packages/amplify-ui-react/__tests__/withAuthenticator-test.tsx
+++ packages/amplify-ui-react/__tests__/withAuthenticator-test.tsx
@@ -9,7 +9,7 @@ describe('withAuthenticator', () => {
 		const Wrapped = withAuthenticator(App);
 
 		expect(renderToStaticMarkup(<Wrapped />)).toMatchInlineSnapshot(
-			`"<div class=\"amplify-authenticator-wrapper\" style=\"display:flex;align-items:center;justify-content:center;height:100vh\"><amplify-authenticator></amplify-authenticator></div>"`
+			`"<amplify-container><amplify-authenticator></amplify-authenticator></amplify-container>"`
 		);
 	});
 });
