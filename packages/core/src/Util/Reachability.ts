58e50ed7b19316cd5d2bf2c649765df115162cb8
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2020-06-09 12:57:24 -0400

    handle multiple instances

--- packages/core/src/Util/Reachability.ts
+++ packages/core/src/Util/Reachability.ts
@@ -5,11 +5,11 @@ type NetworkStatus = {
 };
 
 export default class ReachabilityNavigator implements Reachability {
-	// expose observer for integration testing
-	private static _observer: Observer<any>;
+	private static _observers: Array<Observer<NetworkStatus>> = [];
+	private _observer: Observer<NetworkStatus> = null;
+
 	networkMonitor(netInfo?: any): Observable<NetworkStatus> {
 		return new Observable(observer => {
-			ReachabilityNavigator._observer = observer;
 			observer.next({ online: window.navigator.onLine });
 
 			const notifyOnline = () => observer.next({ online: true });
@@ -18,15 +18,30 @@ export default class ReachabilityNavigator implements Reachability {
 			window.addEventListener('online', notifyOnline);
 			window.addEventListener('offline', notifyOffline);
 
+			this._observer = observer;
+			ReachabilityNavigator._observers.push(observer);
+
 			return () => {
 				window.removeEventListener('online', notifyOnline);
 				window.removeEventListener('offline', notifyOffline);
+				ReachabilityNavigator._observers = ReachabilityNavigator._observers.filter(
+					_observer => _observer !== this._observer
+				);
 			};
 		});
+	}
+
+	// expose observers to simulate offline mode for integration testing
+	private static _observerOverride(status: NetworkStatus): void {
+		for (const _observer of ReachabilityNavigator._observers) {
+			if (!(_observer.next && typeof _observer.next === 'function')) {
+				continue;
+			}
+			_observer.next(status);
+		}
 	}
 }
 
 interface Reachability {
 	networkMonitor(netInfo?: any): Observable<NetworkStatus>;
-	_observer?: Observer<NetworkStatus>;
 }
