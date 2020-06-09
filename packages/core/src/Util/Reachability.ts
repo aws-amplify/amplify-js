5bff13dac9a6b20377914787bb7952ffc5c6e9a4
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2020-06-09 13:58:28 -0400

    fix type

--- packages/core/src/Util/Reachability.ts
+++ packages/core/src/Util/Reachability.ts
@@ -1,11 +1,13 @@
-import Observable, { Observer } from 'zen-observable-ts';
+import Observable, { ZenObservable } from 'zen-observable-ts';
 
 type NetworkStatus = {
 	online: boolean;
 };
 
 export default class ReachabilityNavigator implements Reachability {
-	private static _observers: Array<Observer<NetworkStatus>> = [];
+	private static _observers: Array<
+		ZenObservable.SubscriptionObserver<NetworkStatus>
+	> = [];
 
 	networkMonitor(netInfo?: any): Observable<NetworkStatus> {
 		return new Observable(observer => {
@@ -33,7 +35,7 @@ export default class ReachabilityNavigator implements Reachability {
 	// expose observers to simulate offline mode for integration testing
 	private static _observerOverride(status: NetworkStatus): void {
 		for (const observer of ReachabilityNavigator._observers) {
-			if (observer.complete) {
+			if (observer.closed) {
 				ReachabilityNavigator._observers = ReachabilityNavigator._observers.filter(
 					_observer => _observer !== observer
 				);
