4969eea65a13e6647cb9f97c413ab988ce3d32e7
Alex Hinson <alexmhinson@gmail.com>
2020-06-10 12:59:02 -0700

    update android sdk script

--- .circleci/android-rn.sh
+++ .circleci/android-rn.sh
@@ -18,4 +18,18 @@ case $1 in
 	yes | sdkmanager --licenses >/dev/null
 	yes | sdkmanager --list
 	;;
+  wait-for-avd)
+    boot=""
+	echo "Waiting for AVD to finish booting"
+	export PATH=$(dirname $(dirname $(command -v android)))/platform-tools:$PATH
+	until [[ "$boot" =~ "1" ]]; do
+	sleep 5
+	boot=$(adb -e shell getprop sys.boot_completed 2>&1)
+	done
+	# extra time to let the OS settle
+	sleep 15
+	adb shell settings put global window_animation_scale 0
+	adb shell settings put global transition_animation_scale 0
+	adb shell settings put global animator_duration_scale 0
+	echo "Android Virtual Device is now ready."
 esac
