2deabbd33f3753b3e2a5182355e81938d1b7c71d
Ashika Kasiviswanathan <akasivis@amazon.com>
2020-06-19 00:51:58 -0700

    add base s3-text and text-picker

---
+++ packages/amplify-ui-components/src/components/amplify-s3-text-picker/amplify-s3-text-picker.tsx
@@ -0,0 +1,67 @@
+import { Component, Prop, h, State, Host } from '@stencil/core';
+import { AccessLevel } from '../../common/types/storage-types';
+import { Storage } from '@aws-amplify/storage';
+import { Logger } from '@aws-amplify/core';
+import { NO_STORAGE_MODULE_FOUND } from '../../common/constants';
+import { calcKey, getTextSource } from '../../common/helpers';
+
+const logger = new Logger('S3TextPicker');
+
+@Component({
+  tag: 'amplify-s3-text-picker',
+  styleUrl: 'amplify-s3-text-picker.scss',
+})
+export class AmplifyS3TextPicker {
+  /* String representing directory location to text file */
+  @Prop() path: string;
+  /* The content type header used when uploading to S3 */
+  @Prop() contentType: string = 'text/*';
+  /* The access level of the image */
+  @Prop() level: AccessLevel = AccessLevel.Public;
+  /* Whether or not to use track the get/put of the image */
+  @Prop() track: boolean;
+  /* Cognito identity id of the another user's image */
+  @Prop() identityId: string;
+  /* Callback used to generate custom key value */
+  @Prop() fileToKey: (data: object) => string;
+
+  @State() src: string = 'PICK A FILE';
+
+  async handleInput(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      throw new Error('No file was selected');
+    }
+
+    const { path = '', level, fileToKey, track, identityId } = this;
+    const key = path + calcKey(file, fileToKey);
+    if (!Storage || typeof Storage.put !== 'function') {
+      throw new Error(NO_STORAGE_MODULE_FOUND);
+    }
+
+    try {
+      const data = await Storage.put(key, file, {
+        level,
+        contentType: file['type'],
+        track,
+      });
+      logger.debug('handle pick data', data);
+      this.src = await getTextSource(key, level, track, identityId, logger);
+    } catch (error) {
+      logger.debug(error);
+      throw new Error(error);
+    }
+  }
+
+  render() {
+    return (
+      <Host>
+        <div class="text-container">
+          <pre>{this.src}</pre>
+        </div>
+        <amplify-picker inputHandler={e => this.handleInput(e)}></amplify-picker>
+      </Host>
+    );
+  }
+}
