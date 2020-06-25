839c4d4b0f4a2cad80e55ed80636e880ba1bf6ca
Ashika Kasiviswanathan <akasivis@amazon.com>
2020-06-24 23:25:53 -0700

    update album with grid and picker

--- packages/amplify-ui-components/src/components/amplify-picker/readme.md
+++ packages/amplify-ui-components/src/components/amplify-picker/readme.md
@@ -2,7 +2,6 @@
 
 <!-- Auto Generated Below -->
 
-
 ## Properties
 
 | Property       | Attribute      | Description | Type                 | Default                              |
@@ -11,27 +10,29 @@
 | `inputHandler` | --             |             | `(e: Event) => void` | `undefined`                          |
 | `pickerText`   | `picker-text`  |             | `string`             | `I18n.get(Translations.PICKER_TEXT)` |
 
-
 ## Dependencies
 
 ### Used by
 
- - [amplify-photo-picker](../amplify-photo-picker)
- - [amplify-s3-text-picker](../amplify-s3-text-picker)
+- [amplify-photo-picker](../amplify-photo-picker)
+- [amplify-s3-text-picker](../amplify-s3-text-picker)
+- [amplify-s3-album](../amplify-s3-album)
 
 ### Depends on
 
 - [amplify-button](../amplify-button)
 
 ### Graph
+
 ```mermaid
 graph TD;
   amplify-picker --> amplify-button
   amplify-photo-picker --> amplify-picker
   amplify-s3-text-picker --> amplify-picker
+  amplify-s3-album --> amplify-picker
   style amplify-picker fill:#f9f,stroke:#333,stroke-width:4px
 ```
 
-----------------------------------------------
+---
 
-*Built with [StencilJS](https://stenciljs.com/)*
+_Built with [StencilJS](https://stenciljs.com/)_
--- packages/amplify-ui-components/src/components/amplify-s3-album/amplify-s3-album.tsx
+++ packages/amplify-ui-components/src/components/amplify-s3-album/amplify-s3-album.tsx
@@ -3,9 +3,27 @@ import { AccessLevel } from '../../common/types/storage-types';
 import { Storage } from '@aws-amplify/storage';
 import { Logger, filenameToContentType } from '@aws-amplify/core';
 import { NO_STORAGE_MODULE_FOUND } from '../../common/constants';
+import { calcKey } from '../../common/helpers';
+import { v4 as uuid } from 'uuid';
 
 const logger = new Logger('S3Album');
-
+const imageFileType = [
+  'apng',
+  'bmp',
+  'gif',
+  'ico',
+  'cur',
+  'jpg',
+  'jpeg',
+  'jfif',
+  'pjpeg',
+  'pjp',
+  'png',
+  'svg',
+  'tif',
+  'tiff',
+  'webp',
+];
 @Component({
   tag: 'amplify-s3-album',
   styleUrl: 'amplify-s3-album.scss',
@@ -22,13 +40,18 @@ export class AmplifyS3Album {
   @Prop() track: boolean;
   /* Cognito identity id of the another user's image */
   @Prop() identityId: string;
+  /* Callback used to generate custom key value */
+  @Prop() fileToKey: (data: object) => string;
   /* Filter to be applied on album list */
   @Prop() filter;
   /* Sort to be applied on album list */
   @Prop() sort;
+  /* Boolean to enable or disable picker */
+  @Prop() picker: boolean = true;
 
   @State() albumItems = [];
-  private imgArr = [];
+
+  private imgArr = {};
 
   private list = async () => {
     const { path = '', level, track, identityId } = this;
@@ -43,7 +66,6 @@ export class AmplifyS3Album {
         track,
         identityId,
       });
-      logger.debug('album list', data);
       this.marshal(data);
     } catch (error) {
       logger.warn(error);
@@ -51,22 +73,22 @@ export class AmplifyS3Album {
   };
 
   private marshal = list => {
-    const contentType = this.contentType || '';
     list.forEach(item => {
-      if (item.contentType) {
-        return;
-      }
-      const isString = typeof contentType === 'string';
-      item.contentType = isString ? contentType : this.getContentType(item);
-      if (!item.contentType) {
-        item.contentType = this.getContentType(item);
+      const name = item.key.toLowerCase();
+      const ext = name.split('.')[1];
+      if (imageFileType.includes(ext)) {
+        if (!item.contentType || (item.contentType && item.contentType === 'binary/octet-stream')) {
+          item.contentType = this.getContentType(item);
+        }
       }
     });
-
-    let items = this.filter ? this.filter(list) : list;
+    logger.debug('sanitized album list', list);
+    const filtered = list.filter(item => item.contentType && item.contentType.startsWith('image/'));
+    let items = this.filter ? this.filter(filtered) : filtered;
     items = this.sort ? this.sort(items) : items;
     this.albumItems = items;
-    this.splitArr(items);
+    logger.debug('album items', this.albumItems);
+    this.constructImgArray(this.albumItems);
   };
 
   getContentType(item) {
@@ -77,32 +99,69 @@ export class AmplifyS3Album {
     this.list();
   }
 
-  splitArr = items => {
-    let i;
-    let j = items.length;
-    for (i = 0; i < j; i += 5) {
-      this.imgArr.push(items.slice(i, i + 5));
+  constructImgArray = list => {
+    list.map(item => {
+      this.imgArr[`${item['key']}`] = item['key'];
+    });
+    console.log(this.imgArr);
+  };
+
+  handlePick = async (event: Event) => {
+    const file = (event.target as HTMLInputElement).files[0];
+    const { path = '', level, track, fileToKey } = this;
+    const key = path + calcKey(file, fileToKey);
+
+    if (!Storage || typeof Storage.put !== 'function') {
+      throw new Error(NO_STORAGE_MODULE_FOUND);
+    }
+
+    try {
+      const data = await Storage.put(key, file, {
+        level,
+        contentType: file['type'],
+        track,
+      });
+      logger.debug(data);
+      if (Object.keys(this.imgArr).includes(key)) {
+        this.albumItems = [...this.albumItems];
+        this.imgArr[key] = `${key}-${uuid()}`;
+      } else {
+        this.albumItems = [...this.albumItems, { key }];
+        this.imgArr[key] = key;
+      }
+      console.log('updated', this.albumItems);
+    } catch (error) {
+      logger.error(error);
+      throw new Error(error);
     }
   };
 
   render() {
+    console.log(this.albumItems);
     return (
-      <div class="album-container">
-        {this.imgArr.map(arr => {
-          console.log('arr', arr);
-          return (
-            <div class="grid-row">
-              {arr.map(item => {
-                return (
-                  <div class="grid-item">
-                    <amplify-s3-image imgKey={item.key}></amplify-s3-image>
-                    <span class="img-overlay"></span>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
+      <div>
+        <div class="album-container">
+          <div class="grid-row">
+            {this.albumItems.map(item => {
+              return (
+                <div class="grid-item" key={`key-${item.key}`}>
+                  <amplify-s3-image
+                    key={this.imgArr[item.key]}
+                    imgKey={item.key}
+                    level={this.level}
+                    path={this.path}
+                    identityId={this.identityId}
+                    fileToKey={this.fileToKey}
+                    track={this.track}
+                  ></amplify-s3-image>
+                  <span class="img-overlay"></span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        {this.picker && <amplify-picker inputHandler={e => this.handlePick(e)} acceptValue="image/*" />}
       </div>
     );
   }
