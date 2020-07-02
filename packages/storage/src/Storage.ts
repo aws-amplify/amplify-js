39f57e47c57eb5a87363ace0b8c864948666fe2d
Eric Clemmons <eric@smarterspam.com>
2020-07-01 19:39:15 -0700

    Remove unused export

--- packages/storage/src/Storage.ts
+++ packages/storage/src/Storage.ts
@@ -11,7 +11,7 @@
  * and limitations under the License.
  */
 
-import { Amplify, ConsoleLogger as Logger, Parser } from '@aws-amplify/core';
+import { ConsoleLogger as Logger, Parser } from '@aws-amplify/core';
 import { AWSS3Provider } from './providers';
 import { StorageProvider } from './types';
 
