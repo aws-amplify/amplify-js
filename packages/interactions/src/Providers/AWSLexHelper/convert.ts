8d010ff3f5e711d76ace4878bfb7860dc36ba36a
wlee221 <wlee221@live.unc.edu>
2020-07-15 20:52:14 -0700

    Separate convert function to platform specific files

---
+++ packages/interactions/src/Providers/AWSLexHelper/convert.ts
@@ -0,0 +1,22 @@
+import { Readable } from 'stream';
+import { AcceptType } from '../../types';
+export const convert = (
+	stream: Readable | ReadableStream | Blob,
+	accept: AcceptType
+): Promise<ArrayBuffer | Blob | Uint8Array> => {
+	console.log('hello');
+	// if (stream instanceof Readable) {
+	// 	throw new Error('Node.js is not supported currently.');
+	// } else {
+	if (stream instanceof ArrayBuffer || stream instanceof Blob) {
+		// stream instanceof ReadableStream | Blob
+		const response = new Response(stream);
+		if (accept === 'ArrayBuffer') {
+			return response.arrayBuffer();
+		} else if (accept === 'Blob') {
+			return response.blob();
+		} else {
+			return response.arrayBuffer().then(buffer => new Uint8Array(buffer));
+		}
+	}
+};
