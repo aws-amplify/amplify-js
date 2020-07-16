e0a77140807963878b9eabd319b441b31f37b2d2
Alex Hinson <alexmhinson@gmail.com>
2020-07-16 09:18:03 -0700

    update sequencer sorting

--- packages/auth/testSequencer.js
+++ packages/auth/testSequencer.js
@@ -5,11 +5,14 @@ class CustomSequencer extends Sequencer {
 		// Test structure information
 		// https://github.com/facebook/jest/blob/6b8b1404a1d9254e7d5d90a8934087a9c9899dab/packages/jest-runner/src/types.ts#L17-L21
 		const copyTests = Array.from(tests);
-		return copyTests.sort((testA, testB) =>
-			testA.path > testB.path && !testB.path.includes('auth-unit-test.ts')
-				? 1
-				: -1
-		);
+		return copyTests.sort((testA, testB) => {
+			if (testA.path.includes('auth-unit-test.ts')) {
+				return 1;
+			} else if (testB.path.includes('auth-unit-test.ts')) {
+				return -1;
+			}
+			return testA.path > testB.path ? 1 : -1;
+		});
 	}
 }
 
