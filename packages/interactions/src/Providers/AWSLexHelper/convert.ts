bbcdc0ce9679742c53b9a6183d3995e056296ff9
wlee221 <wlee221@live.unc.edu>
2020-07-15 23:14:51 -0700

    implement Node.js handling for Interactions

--- packages/interactions/src/Providers/AWSLexHelper/convert.ts
+++ packages/interactions/src/Providers/AWSLexHelper/convert.ts
@@ -1,19 +1,30 @@
 import { Readable } from 'stream';
 import { AcceptType } from '../../types';
-export const convert = (
+export const convert = async (
 	stream: Readable | ReadableStream | Blob,
 	accept: AcceptType
 ): Promise<ArrayBuffer | Blob | Uint8Array> => {
-	if (stream instanceof Readable) {
-		throw new Error('Node.js is not supported currently.');
-	} else { // if stream instanceof ReadableStream | Blob
-		const response = new Response(stream);
-		if (accept === 'ArrayBuffer') {
-			return response.arrayBuffer();
-		} else if (accept === 'Blob') {
-			return response.blob();
-		} else {
-			return response.arrayBuffer().then(buffer => new Uint8Array(buffer));
-		}
+	let audio = stream instanceof Readable ? await readReadable(stream) : stream;
+	const response = new Response(audio);
+	if (accept === 'ArrayBuffer') {
+		return response.arrayBuffer();
+	} else if (accept === 'Blob') {
+		return response.blob();
+	} else {
+		return response.arrayBuffer().then(buffer => new Uint8Array(buffer));
 	}
+};
+
+const readReadable = (stream: Readable): Promise<Blob> => {
+	if (!stream.isPaused()) stream.pause();
+	let chunks: Array<string | Buffer> = [];
+	return new Promise((res, rej) => {
+		stream.on('data', chunk => {
+			chunks.push(chunk);
+		});
+		stream.on('end', () => {
+			return res(new Blob(chunks));
+		});
+		stream.on('error', rej);
+	});
 };
