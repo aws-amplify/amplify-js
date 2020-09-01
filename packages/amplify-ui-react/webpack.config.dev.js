7d8de8bd11d20c2690c6d52b9ad8bf16034b9006
jordanranz <jordanmranz@gmail.com>
2020-09-01 13:45:19 -0700

    Clean up webpack config, change root

--- packages/amplify-ui-react/webpack.config.dev.js
+++ packages/amplify-ui-react/webpack.config.dev.js
@@ -3,8 +3,4 @@ var config = require('./webpack.config.js');
 var entry = {
 	'@aws-amplify/ui-react': './lib/index.js',
 };
-module.exports = Object.assign(config, {
-	entry,
-	mode: 'development',
-	node: { fs: 'empty' },
-});
+module.exports = Object.assign(config, { entry, mode: 'development' });
