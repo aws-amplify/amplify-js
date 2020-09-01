af0167bb8f6e46f7e028e02e625312e6f1370d0d
Eric Clemmons <eric@smarterspam.com>
2020-08-31 21:19:08 -0700

    withSSRContext explicitly supports only API, Auth, Credentials, DataStore

--- packages/aws-amplify/src/withSSRContext.ts
+++ packages/aws-amplify/src/withSSRContext.ts
@@ -1,5 +1,14 @@
-import { AmplifyClass, JS, UniversalStorage } from '@aws-amplify/core';
+import API from '@aws-amplify/api';
+import { Auth } from '@aws-amplify/auth';
+import { Credentials } from '@aws-amplify/core';
 import {
+	AmplifyClass,
+	browserOrNode,
+	UniversalStorage,
+} from '@aws-amplify/core';
+
+import {
+	DataStore,
 	PersistentModel,
 	PersistentModelConstructor,
 } from '@aws-amplify/datastore';
@@ -8,7 +17,17 @@ import { NextPageContext } from 'next';
 // ! We have to use this exact reference, since it gets mutated with Amplify.Auth
 import { Amplify } from './index';
 
-const { isBrowser } = JS.browserOrNode();
+const { isBrowser } = browserOrNode();
+
+const requiredModules = [
+	// API cannot function without Auth
+	Auth,
+	// Auth cannot function without Credentials
+	Credentials,
+];
+
+// These modules have been tested with SSR
+const defaultModules = [API, Auth, DataStore];
 
 // Helper for converting JSON back into DataStore models (while respecting IDs)
 function deserializeModel<T extends PersistentModel>(
@@ -29,53 +48,33 @@ function serializeModel<T extends PersistentModel>(model: T | T[]): JSON {
 	return JSON.parse(JSON.stringify(model));
 }
 
-export function withSSRContext(context?: Pick<NextPageContext, 'req'>) {
+type Context = Pick<NextPageContext, 'req'> & {
+	modules?: any[];
+};
+
+export function withSSRContext(context?: Context) {
+	const { modules = defaultModules, req } = context;
 	const previousConfig = Amplify.configure();
 	const amplify = new AmplifyClass();
-	const storage = new UniversalStorage(context);
+	const storage = new UniversalStorage({ req });
 
 	// Replace Auth singleton's existing Storage with UniversalStorage, since UI components reference category singletons
 	if (isBrowser) {
 		Amplify.configure({ storage });
 	}
 
-	const previousModules = Object.keys(Amplify)
-		.map(
-			property =>
-				Amplify[property] &&
-				Amplify[property].getModuleName &&
-				Amplify[property]
-		)
-		.filter(Boolean);
+	requiredModules.forEach(m => {
+		if (!modules.includes(m)) {
+			// @ts-ignore This expression is not constructable.
+			// Type 'Function' has no construct signatures.ts(2351)
+			amplify.register(new m.constructor());
+		}
+	});
 
-	previousModules
-		.map(module => {
-			switch (module.getModuleName()) {
-				// Modules that have been tested with SSR and confirmed to require instanced Credentials
-				case 'API':
-				case 'Auth':
-				case 'DataStore':
-				case 'GraphQLAPI':
-				case 'RestAPI':
-					return new module.constructor();
-
-				// Modules that a static reference is safe to use, without need for new Credentials.
-				case 'I18n':
-
-				// Modules that haven't been tested with SSR
-				case 'Analytics':
-				case 'PubSub':
-				case 'Predictions':
-
-				// Any other module on Amplify.* stays the same
-				default:
-					return module;
-			}
-		})
-		.forEach(instance => {
-			// Ensure this instance of Amplify is associated with new instances of categories
-			amplify.register(instance);
-		});
+	// Associate new module instances with this amplify
+	modules.forEach(m => {
+		amplify.register(new m.constructor());
+	});
 
 	// Configure new Amplify instances with previous configuration
 	amplify.configure({ ...previousConfig, storage });
