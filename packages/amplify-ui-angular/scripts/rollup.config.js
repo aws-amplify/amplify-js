7975c74fb03d62154a48242ab8c1a1e430d9132e
Eric Clemmons <eric@smarterspam.com>
2020-09-01 18:44:37 -0700

    Revert "Explicitly set file paths (because rollup bin can't find it)"
    This reverts commit 4b65b77b2739a6782af90f362f586605cf1d4cbb.

--- packages/amplify-ui-angular/scripts/rollup.config.js
+++ packages/amplify-ui-angular/scripts/rollup.config.js
@@ -1,29 +1,24 @@
-import path from 'path';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-	input: path.resolve(__dirname, '../build/es2015/core.js'),
-	output: {
-		file: path.resolve(__dirname, '../dist/fesm2015.js'),
-		format: 'es',
-	},
-	external: id => {
-		// inline @ionic/core deps
-		if (id === '@ionic/core') {
-			return false;
-		}
-		// anything else is external
-		// Windows: C:\xxxxxx\xxx
-		const colonPosition = 1;
-		return !(
-			id.startsWith('.') ||
-			id.startsWith('/') ||
-			id.charAt(colonPosition) === ':'
-		);
-	},
-	plugins: [
-		resolve({
-			module: true,
-		}),
-	],
+  input: 'build/es2015/core.js',
+  output: {
+    file: 'dist/fesm2015.js',
+    format: 'es'
+  },
+  external: (id) => {
+    // inline @ionic/core deps
+    if (id === '@ionic/core') {
+      return false;
+    }
+    // anything else is external
+    // Windows: C:\xxxxxx\xxx
+    const colonPosition = 1;
+    return !(id.startsWith('.') || id.startsWith('/') || id.charAt(colonPosition) === ':');
+  },
+  plugins: [
+    resolve({
+      module: true,
+    })
+  ]
 };
