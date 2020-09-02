4b65b77b2739a6782af90f362f586605cf1d4cbb
Eric Clemmons <eric@smarterspam.com>
2020-09-01 18:20:06 -0700

    Explicitly set file paths (because rollup bin can't find it)

--- packages/amplify-ui-angular/scripts/rollup.config.legacy.js
+++ packages/amplify-ui-angular/scripts/rollup.config.legacy.js
@@ -1,9 +1,10 @@
+import path from 'path';
 import config from './rollup.config';
 
 const newConfig = {
-  ...config,
-  input: 'build/es5/core.js',
+	...config,
+	input: path.resolve(__dirname, '../build/es5/core.js'),
 };
-newConfig.output.file = 'dist/fesm5.js';
+newConfig.output.file = path.resolve(__dirname, '../dist/fesm5.js');
 
 export { newConfig as default };
