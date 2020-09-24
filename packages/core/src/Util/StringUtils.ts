cdd7b2b8df832aa831539f9dfbc55e7abc17a737
Divyesh Chitroda <div5yesh@gmail.com>
2020-09-24 19:43:10 -0400

    avoid using TextEncoder to support IE11

--- packages/core/src/Util/StringUtils.ts
+++ packages/core/src/Util/StringUtils.ts
@@ -1,9 +1,18 @@
-import { toHex, fromHex } from '@aws-sdk/util-hex-encoding';
-
 export function urlSafeEncode(str: string) {
-	return toHex(new TextEncoder().encode(str));
+	return str
+		.split('')
+		.map(char =>
+			char
+				.charCodeAt(0)
+				.toString(16)
+				.padStart(2, '0')
+		)
+		.join('');
 }
 
 export function urlSafeDecode(hex: string) {
-	return new TextDecoder().decode(fromHex(hex));
+	return hex
+		.match(/.{2}/g)
+		.map(char => String.fromCharCode(parseInt(char, 16)))
+		.join('');
 }
