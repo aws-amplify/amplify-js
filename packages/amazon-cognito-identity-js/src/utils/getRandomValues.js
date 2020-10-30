90c8008f131aa205f105db96a54df009c2001b66
Alex Hinson <alexmhinson@gmail.com>
2020-10-30 13:12:51 -0700

    update default crypto value

--- packages/amazon-cognito-identity-js/src/utils/getRandomValues.js
+++ packages/amazon-cognito-identity-js/src/utils/getRandomValues.js
@@ -1,1 +1,13 @@
-export default window.crypto.getRandomValues;
+let crypto = {};
+
+// Native crypto from window (Browser)
+if (typeof window !== 'undefined' && window.crypto) {
+	crypto = window.crypto;
+}
+
+// Native (experimental IE 11) crypto from window (Browser)
+if (!crypto && typeof window !== 'undefined' && window.msCrypto) {
+	crypto = window.msCrypto;
+}
+
+export default crypto.getRandomValues;
