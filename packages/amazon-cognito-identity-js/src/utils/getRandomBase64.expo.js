b1873babe46f41c405ae4dd58a38de953a06e781
Alex Hinson <alexmhinson@gmail.com>
2020-10-30 15:07:29 -0700

    update NativeModules import

--- packages/amazon-cognito-identity-js/src/utils/getRandomBase64.expo.js
+++ packages/amazon-cognito-identity-js/src/utils/getRandomBase64.expo.js
@@ -1,10 +1,12 @@
 import { NativeModules } from 'react-native';
 
 // In the Expo managed workflow the getRandomBase64 method is provided by ExpoRandom.getRandomBase64String
-if (!NativeModules.ExpoRandom) {
-	throw new Error(
-		'Expo managed workflow support for amazon-cognito-identity-js is only available in SDK 39 and higher.'
-	);
-}
+const getRandomBase64String = NativeModules.ExpoRandom
+	? NativeModules.ExpoRandom.getRandomBase64String
+	: () => {
+			throw new Error(
+				'Expo managed workflow support for amazon-cognito-identity-js is only available in SDK 39 and higher.'
+			);
+	  };
 
-export default NativeModules.ExpoRandom.getRandomBase64String;
+export default getRandomBase64String;
--- packages/amazon-cognito-identity-js/src/utils/getRandomBase64.native.js
+++ packages/amazon-cognito-identity-js/src/utils/getRandomBase64.native.js
@@ -1,2 +1,8 @@
 import { NativeModules } from 'react-native';
-export default NativeModules.RNGetRandomValues.getRandomBase64;
+const getRandomBase64 = NativeModules.RNGetRandomValues
+	? NativeModules.RNGetRandomValues.getRandomBase64
+	: () => {
+			throw Error('Validate that amazon-cognito-identity-js has been linked');
+	  };
+
+export default getRandomBase64;
