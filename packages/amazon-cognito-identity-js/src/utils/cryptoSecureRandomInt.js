08a3455365d30cb65129841afd57444b27347e2e
Alex Hinson <alexmhinson@gmail.com>
2020-10-31 04:53:48 -0700

    fix (amazon-cognito-identity-js): use Node randomBytes

--- packages/amazon-cognito-identity-js/src/utils/cryptoSecureRandomInt.js
+++ packages/amazon-cognito-identity-js/src/utils/cryptoSecureRandomInt.js
@@ -20,10 +20,20 @@ if (!crypto && typeof global !== 'undefined' && global.crypto) {
  * As Math.random() is cryptographically not safe to use
  */
 export default function cryptoSecureRandomInt() {
-	if (crypto && typeof crypto.getRandomValues === 'function') {
-		try {
-			return crypto.getRandomValues(new Uint32Array(1))[0];
-		} catch (err) {}
+	if (crypto) {
+		// Use getRandomValues method (Browser)
+		if (typeof crypto.getRandomValues === 'function') {
+			try {
+				return crypto.getRandomValues(new Uint32Array(1))[0];
+			} catch (err) {}
+		}
+
+		// Use randomBytes method (NodeJS)
+		if (typeof crypto.randomBytes === 'function') {
+			try {
+				return crypto.randomBytes(4).readInt32LE();
+			} catch (err) {}
+		}
 	}
 
 	throw new Error(
