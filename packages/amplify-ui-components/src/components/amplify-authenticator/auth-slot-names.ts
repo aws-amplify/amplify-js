b68f8ea06368bb54918b46b7a228e9b3b79dd3b3
wlee221 <wlee221@live.unc.edu>
2021-01-08 14:02:29 -0800

    fix typo

--- packages/amplify-ui-components/src/components/amplify-authenticator/auth-slot-names.ts
+++ packages/amplify-ui-components/src/components/amplify-authenticator/auth-slot-names.ts
@@ -4,7 +4,7 @@ export const authSlotNames: Partial<Record<AuthState, string>> = {
   [AuthState.SignIn]: 'sign-in',
   [AuthState.ConfirmSignIn]: 'confirm-sign-in',
   [AuthState.SignUp]: 'sign-up',
-  [AuthState.ConfirmSignUp]: 'sign-up',
+  [AuthState.ConfirmSignUp]: 'confirm-sign-up',
   [AuthState.ForgotPassword]: 'forgot-password',
   [AuthState.ResetPassword]: 'require-new-password',
   [AuthState.VerifyContact]: 'verify-contact',
