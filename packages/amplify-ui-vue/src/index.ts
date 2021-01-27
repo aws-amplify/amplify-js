87e4c244f5353929e27dc153ef960dae28a9938b
wlee221 <wlee221@live.unc.edu>
2021-01-27 15:30:21 -0800

    Add warning message for Vue 2

--- packages/amplify-ui-vue/src/index.ts
+++ packages/amplify-ui-vue/src/index.ts
@@ -7,8 +7,14 @@ import Vue from 'vue';
 // Tell Vue to ignore all components defined in the @aws-amplify/ui-components
 // package. The regex assumes all components names are prefixed
 // 'amplify-'
-Vue.config.ignoredElements = [/amplify-\w*/];
-
+if (Vue) {
+	Vue.config.ignoredElements = [/amplify-\w*/];
+} else {
+	console.error(
+		'No Vue 2 export was found. For later versions of vue, please use `@aws-amplify/ui-components` according to this \
+		guide: https://docs.amplify.aws/start/getting-started/setup/q/integration/vue. Otherwise, make sure `Vue 2` is installed.'
+	);
+}
 // Bind the custom elements to the window object
 applyPolyfills().then(() => {
 	defineCustomElements(window);
