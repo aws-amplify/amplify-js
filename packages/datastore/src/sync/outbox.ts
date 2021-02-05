2f02e6a784089f8730ad6bb0f8046c6424322a98
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2021-02-05 18:27:27 -0500

    wrap dequeue and peek in runExclusive. Remove no longer needed code.

--- packages/datastore/src/sync/outbox.ts
+++ packages/datastore/src/sync/outbox.ts
@@ -1,6 +1,10 @@
 import { MutationEvent } from './index';
 import { ModelPredicateCreator } from '../predicates';
-import { ExclusiveStorage as Storage, StorageFacade } from '../storage/storage';
+import {
+	ExclusiveStorage as Storage,
+	StorageFacade,
+	Storage as StorageClass,
+} from '../storage/storage';
 import {
 	InternalSchema,
 	NamespaceResolver,
@@ -28,9 +32,19 @@ class MutationEventOutbox {
 		mutationEvent: MutationEvent
 	): Promise<void> {
 		storage.runExclusive(async s => {
-			const predicate = this.currentPredicate(mutationEvent);
-			const existing = await s.query(this.MutationEvent, predicate);
-			const [first] = existing;
+			const mutationEventModelDefinition = this.schema.namespaces[SYNC].models[
+				'MutationEvent'
+			];
+
+			const predicate = ModelPredicateCreator.createFromExisting<MutationEvent>(
+				mutationEventModelDefinition,
+				c =>
+					c
+						.modelId('eq', mutationEvent.modelId)
+						.id('ne', this.inProgressMutationEventId)
+			);
+
+			const [first] = await s.query(this.MutationEvent, predicate);
 
 			if (first === undefined) {
 				await s.save(mutationEvent, undefined, this.ownSymbol);
@@ -41,12 +55,9 @@ class MutationEventOutbox {
 
 			if (first.operation === TransformerMutationType.CREATE) {
 				if (incomingMutationType === TransformerMutationType.DELETE) {
-					// get predicate again to avoid race condition with inProgressMutationEventId
-					const predicate = this.currentPredicate(mutationEvent);
-					// delete all for model
 					await s.delete(this.MutationEvent, predicate);
 				} else {
-					// first gets updated with incoming's data, condition intentionally skiped
+					// first gets updated with incoming's data, condition intentionally skipped
 					await s.save(
 						this.MutationEvent.copyOf(first, draft => {
 							draft.data = mutationEvent.data;
@@ -59,22 +70,9 @@ class MutationEventOutbox {
 				const { condition: incomingConditionJSON } = mutationEvent;
 				const incomingCondition = JSON.parse(incomingConditionJSON);
 
-				const updated = await this.reconcileOutboxOnEnqueue(
-					existing,
-					mutationEvent
-				);
-
 				// If no condition
 				if (Object.keys(incomingCondition).length === 0) {
-					// get predicate again to avoid race condition with inProgressMutationEventId
-					const predicate = this.currentPredicate(mutationEvent);
-					// delete all for model
 					await s.delete(this.MutationEvent, predicate);
-				}
-
-				if (updated) {
-					await s.save(updated, undefined, this.ownSymbol);
-					return;
 				}
 
 				// Enqueue new one
@@ -84,7 +82,7 @@ class MutationEventOutbox {
 	}
 
 	public async dequeue(
-		storage: Storage,
+		storage: StorageClass,
 		record?: PersistentModel
 	): Promise<MutationEvent> {
 		const head = await this.peek(storage);
@@ -141,94 +139,46 @@ class MutationEventOutbox {
 		return result;
 	}
 
-	private async reconcileOutboxOnEnqueue(
-		existing: MutationEvent[],
-		mutationEvent: MutationEvent
-	): Promise<MutationEvent | undefined> {
-		const { _version, _lastChangedAt } = existing.reduce(
-			(acc, cur) => {
-				const oldData = JSON.parse(cur.data);
-				const { _version: lastVersion } = acc;
-				const { _version: _v, _lastChangedAt: _lCA } = oldData;
-
-				if (_v > lastVersion) {
-					return { _version: _v, _lastChangedAt: _lCA };
-				}
-
-				return acc;
-			},
-			{
-				_version: 0,
-				_lastChangedAt: 0,
-			}
-		);
-
-		const currentData = JSON.parse(mutationEvent.data);
-		const currentVersion = currentData._version;
-
-		if (currentVersion < _version) {
-			const newData = { ...currentData, _version, _lastChangedAt };
-			const newMutation = new this.MutationEvent({
-				...mutationEvent,
-				data: JSON.stringify(newData),
-			});
-			return newMutation;
-		}
-	}
-
 	private async reconcileOutboxOnDequeue(
-		storage: Storage,
+		storage: StorageClass,
 		record: PersistentModel
 	): Promise<void> {
-		storage.runExclusive(async s => {
-			const mutationEventModelDefinition = this.schema.namespaces[SYNC].models[
-				'MutationEvent'
-			];
-
-			const predicate = ModelPredicateCreator.createFromExisting<MutationEvent>(
-				mutationEventModelDefinition,
-				c => c.modelId('eq', record.id).id('ne', this.inProgressMutationEventId)
-			);
-
-			const outdatedMutations = await s.query(this.MutationEvent, predicate);
-
-			if (!outdatedMutations.length) {
-				return;
-			}
-
-			const { _version, _lastChangedAt } = record;
-
-			const reconciledMutations = outdatedMutations.map(m => {
-				const oldData = JSON.parse(m.data);
-
-				const newData = { ...oldData, _version, _lastChangedAt };
-
-				return this.MutationEvent.copyOf(m, draft => {
-					draft.data = JSON.stringify(newData);
-				});
-			});
-
-			await s.delete(this.MutationEvent, predicate);
-
-			await Promise.all(
-				reconciledMutations.map(
-					async m => await s.save(m, undefined, this.ownSymbol)
-				)
-			);
-		});
-	}
-
-	private currentPredicate(mutationEvent: MutationEvent) {
 		const mutationEventModelDefinition = this.schema.namespaces[SYNC].models[
 			'MutationEvent'
 		];
 
-		return ModelPredicateCreator.createFromExisting<MutationEvent>(
+		const predicate = ModelPredicateCreator.createFromExisting<MutationEvent>(
 			mutationEventModelDefinition,
-			c =>
-				c
-					.modelId('eq', mutationEvent.modelId)
-					.id('ne', this.inProgressMutationEventId)
+			c => c.modelId('eq', record.id).id('ne', this.inProgressMutationEventId)
+		);
+
+		const outdatedMutations = await storage.query(
+			this.MutationEvent,
+			predicate
+		);
+
+		if (!outdatedMutations.length) {
+			return;
+		}
+
+		const { _version, _lastChangedAt } = record;
+
+		const reconciledMutations = outdatedMutations.map(m => {
+			const oldData = JSON.parse(m.data);
+
+			const newData = { ...oldData, _version, _lastChangedAt };
+
+			return this.MutationEvent.copyOf(m, draft => {
+				draft.data = JSON.stringify(newData);
+			});
+		});
+
+		await storage.delete(this.MutationEvent, predicate);
+
+		await Promise.all(
+			reconciledMutations.map(
+				async m => await storage.save(m, undefined, this.ownSymbol)
+			)
 		);
 	}
 }
