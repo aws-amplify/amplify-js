b9997432df5b8ff1c30b2b81ea84a8a1db7d160a
Alex Hinson <alexmhinson@gmail.com>
2021-02-25 15:04:35 -0800

    add jitteredRetry tests

---
+++ packages/datastore/__tests__/sync.test.ts
@@ -0,0 +1,157 @@
+describe('Sync', () => {
+	describe('jitteredRetry', () => {
+		const defaultQuery = `query {
+			syncPosts {
+				items {
+					id
+					title
+					count
+					_version
+					_lastChangedAt
+					_deleted
+				}
+				nextToken
+				startedAt
+			}
+		}`;
+		const defaultVariables = {};
+		const defaultOpName = 'syncPosts';
+		const defaultModelDefinition = { name: 'Post' };
+
+		beforeEach(() => {
+			jest.resetModules();
+			jest.resetAllMocks();
+		});
+
+		it('should return partial data and send Hub event', async () => {
+			const rejectResponse = {
+				data: {
+					syncPosts: {
+						items: [
+							{
+								id: '1',
+								title: 'Item 1',
+							},
+							null,
+							{
+								id: '3',
+								title: 'Item 3',
+							},
+						],
+					},
+				},
+				errors: [
+					{
+						message: 'Item 2 error',
+					},
+				],
+			};
+
+			const hubDispatchMock = jest.fn();
+			const coreMocks = {
+				Hub: {
+					dispatch: hubDispatchMock,
+					listen: jest.fn(),
+				},
+			};
+
+			const SyncProcessor = jitteredRetrySyncProcessorSetup({
+				rejectResponse,
+				coreMocks,
+			});
+
+			const data = await SyncProcessor.jitteredRetry({
+				query: defaultQuery,
+				variables: defaultVariables,
+				opName: defaultOpName,
+				modelDefinition: defaultModelDefinition,
+			});
+
+			expect(data).toMatchSnapshot();
+
+			expect(hubDispatchMock).toHaveBeenCalledWith('datastore', {
+				event: 'syncQueriesPartialSyncError',
+				data: {
+					errors: [
+						{
+							message: 'Item 2 error',
+						},
+					],
+					modelName: 'Post',
+				},
+			});
+		});
+
+		it('should throw error if no data is returned', async () => {
+			const rejectResponse = {
+				data: null,
+				errors: [
+					{
+						message: 'General error',
+					},
+				],
+			};
+
+			const SyncProcessor = jitteredRetrySyncProcessorSetup({
+				rejectResponse,
+			});
+
+			try {
+				await SyncProcessor.jitteredRetry({
+					query: defaultQuery,
+					variables: defaultVariables,
+					opName: defaultOpName,
+					modelDefinition: defaultModelDefinition,
+				});
+			} catch (e) {
+				expect(e).toMatchSnapshot();
+			}
+		});
+	});
+});
+
+function jitteredRetrySyncProcessorSetup({
+	rejectResponse,
+	resolveResponse,
+	coreMocks,
+}: {
+	rejectResponse?: any;
+	resolveResponse?: any;
+	coreMocks?: object;
+}) {
+	jest.mock('@aws-amplify/api', () => ({
+		graphql: () =>
+			new Promise((res, rej) => {
+				if (resolveResponse) {
+					res(resolveResponse);
+				} else if (rejectResponse) {
+					rej(rejectResponse);
+				}
+			}),
+	}));
+
+	jest.mock('@aws-amplify/core', () => ({
+		...jest.requireActual('@aws-amplify/core'),
+		// No need to retry any thrown errors right now,
+		// so we're overriding jitteredExponentialRetry
+		jitteredExponentialRetry: (fn, args) => fn(...args),
+		...coreMocks,
+	}));
+
+	const SyncProcessorClass = require('../src/sync/processors/sync')
+		.SyncProcessor;
+
+	const testInternalSchema = {
+		namespaces: {},
+		version: '',
+	};
+
+	const SyncProcessor = new SyncProcessorClass(
+		testInternalSchema,
+		1000, // default maxRecordsToSync
+		10000, // default syncPageSize
+		null
+	);
+
+	return SyncProcessor;
+}
