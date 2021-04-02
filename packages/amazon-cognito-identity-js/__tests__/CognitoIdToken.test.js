9bb6a57e95c9e00077f95b025f48a76afe778b15
Eddy <eddvar@amazon.com>
2021-04-02 10:24:02 -0400

    test(amazon-cognito-identity-js): At 100% coverage for a wrapper class of CognitoJWT Token

--- packages/amazon-cognito-identity-js/__tests__/CognitoIdToken.test.js
+++ packages/amazon-cognito-identity-js/__tests__/CognitoIdToken.test.js
@@ -1,9 +1,8 @@
 import CognitoIdToken from '../src/CognitoIdToken'
 
 describe('Constructor for CognitoId Token', () => {
-
 	test('Constructing a CognitoID Token', () => {
-		const cognitoToken = new CognitoIdToken('testingJWTTokenString')
+		const cognitoToken = new CognitoIdToken('testToken')
 		expect(typeof cognitoToken.jwtToken).toBe('string')
 	})
 })
