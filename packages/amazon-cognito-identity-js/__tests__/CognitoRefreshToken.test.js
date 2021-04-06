6bc675985bd2f52a163a7a69298865c00a56832e
Eddy <eddvar@amazon.com>
2021-04-06 13:38:37 -0400

    test(cognito-identity-js): improving coverage for cognito access token

--- packages/amazon-cognito-identity-js/__tests__/CognitoRefreshToken.test.js
+++ packages/amazon-cognito-identity-js/__tests__/CognitoRefreshToken.test.js
@@ -1,8 +1,14 @@
 import CognitoRefreshToken from '../src/CognitoRefreshToken'
+import { sampleEncodedToken } from './constants'
 
 describe('Cognito Refresh Token ', () => {
-    const cognitoRefToken = new CognitoRefreshToken('JWT Refresh Token')
-    test('Constructor methods', () => {
+
+    test('Testing getter method for a proper token', () => {
+        const cognitoRefToken = new CognitoRefreshToken(sampleEncodedToken)
         expect(cognitoRefToken.getToken()).toBe(cognitoRefToken.token)
+    })
+    test('Testing getter method for a refresh token with no value', () => {
+        const cognitoRefToken = new CognitoRefreshToken()
+        expect(cognitoRefToken.getToken()).toBe('')
     })
 })
