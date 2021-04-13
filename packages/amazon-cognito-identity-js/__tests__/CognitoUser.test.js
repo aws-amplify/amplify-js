b9415b2d05620d031e2b794f6c16f6996e6a7118
Nick Arocho <nick@boldlyfine.com>
2021-04-13 11:13:33 -0700

    test(amazon-cognito-identity-js): sync with main && add test file for CognitoUser.js (#8090)
    * adding testing branch
    
    * test(amazon-cognito-identity-js): remove empty describe block
    
    * test(amazon-cognito-identity-js): update test script in package.json to collect coverage
    
    * test(amazon-cognito-identity-js): write basic tests for AuthenticationDetails class
    
    * test(amazon-cognito-identity-js): cognito access and JWT token test (incomplete)
    
    * chore(issue forms): update for deprecated fields (#8065)
    
    * update for deprecated fields and all textareas now being rich text editor-enabled
    
    * fix description
    
    * add missing wording
    
    * chore: stale bot updates (#8080)
    
    * chore:Update NOTICE file to remove copyright date
    
    * chore: Remove unused labels from stalebot
    
    * feat(@aws-amplify/ui-components): AmplifyAuthFields accept type for default fields (#6690)
    
    * AmplifyAuthFields should accept type for default fields
    
    * Update packages/amplify-ui-components/src/components/amplify-auth-fields/amplify-auth-fields.spec.ts
    
    Co-authored-by: William Lee <43682783+wlee221@users.noreply.github.com>
    
    * Update packages/amplify-ui-components/src/components/amplify-auth-fields/amplify-auth-fields.spec.ts
    
    Co-authored-by: William Lee <43682783+wlee221@users.noreply.github.com>
    
    Co-authored-by: William Lee <43682783+wlee221@users.noreply.github.com>
    
    * chore(amazon-cognito-identity-js): prettier formatting
    
    * test(amazon-cognito-identity-js): create test file for CognitoUser and test constructor
    
    Co-authored-by: Eddy Varela <eddvar@amazon.com>
    Co-authored-by: Nick Arocho <nicaroch@amazon.com>
    Co-authored-by: siegerts <stephen.siegert@gmail.com>
    Co-authored-by: Sam Martinez <samlmar@amazon.com>
    Co-authored-by: Maytee Chinavanichkit <maytee@standard.ai>
    Co-authored-by: William Lee <43682783+wlee221@users.noreply.github.com>

---
+++ packages/amazon-cognito-identity-js/__tests__/CognitoUser.test.js
@@ -0,0 +1,46 @@
+import CognitoUser from '../src/CognitoUser';
+import CognitoUserPool from '../src/CognitoUserPool';
+import { clientId, userPoolId } from './constants';
+
+const minimalData = { UserPoolId: userPoolId, ClientId: clientId };
+const cognitoUserPool = new CognitoUserPool(minimalData);
+
+describe('CognitoUser constructor', () => {
+	test('constructor throws error when bad (or no) data is passed', () => {
+		const errorMsg = 'Username and Pool information are required.';
+
+		// no data at all
+		expect(() => {
+			new CognitoUser({});
+		}).toThrow(errorMsg);
+
+		// missing Pool
+		expect(() => {
+			new CognitoUser({
+				Username: 'username',
+				Pool: null,
+			});
+		}).toThrow(errorMsg);
+
+		// missing Username
+		expect(() => {
+			new CognitoUser({
+				Username: null,
+				Pool: userPoolId,
+			});
+		}).toThrow(errorMsg);
+	});
+
+	test('happy case constructor', () => {
+		const spyon = jest.spyOn(cognitoUserPool, 'getClientId');
+
+		expect(() => {
+			new CognitoUser({
+				Username: 'username',
+				Pool: cognitoUserPool,
+			});
+		}).not.toThrowError();
+
+		expect(spyon).toBeCalled();
+	});
+});
