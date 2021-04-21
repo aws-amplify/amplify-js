84ffeb38fe329d0632b62d72402e015d9313f088
wlee221 <wlee221@live.unc.edu>
2021-04-21 01:00:16 -0700

    Local prettier run

--- packages/amplify-ui-components/src/common/constants.ts
+++ packages/amplify-ui-components/src/common/constants.ts
@@ -18,17 +18,21 @@ export const AUTHENTICATOR_AUTHSTATE = 'amplify-authenticator-authState';
 
 // Error message Common Constants
 export const PHONE_EMPTY_ERROR_MESSAGE = 'Phone number can not be empty';
-export const NO_AUTH_MODULE_FOUND = 'No Auth module found, please ensure @aws-amplify/auth is imported';
-export const NO_STORAGE_MODULE_FOUND = 'No Storage module found, please ensure @aws-amplify/storage is imported';
+export const NO_AUTH_MODULE_FOUND =
+	'No Auth module found, please ensure @aws-amplify/auth is imported';
+export const NO_STORAGE_MODULE_FOUND =
+	'No Storage module found, please ensure @aws-amplify/storage is imported';
 export const NO_INTERACTIONS_MODULE_FOUND =
-  'No Interactions module found, please ensure @aws-amplify/interactions is imported';
+	'No Interactions module found, please ensure @aws-amplify/interactions is imported';
 
 // TOTP Messages
 export const SETUP_TOTP = 'SETUP_TOTP';
 
 // Select MFA Types Messages
-export const USER_NOT_SETUP_SOFTWARE_TOKEN_MFA = 'User has not set up software token mfa';
-export const USER_NOT_VERIFIED_SOFTWARE_TOKEN_MFA = 'User has not verified software token mfa';
+export const USER_NOT_SETUP_SOFTWARE_TOKEN_MFA =
+	'User has not set up software token mfa';
+export const USER_NOT_VERIFIED_SOFTWARE_TOKEN_MFA =
+	'User has not verified software token mfa';
 
 // Common events
 export const SUCCESS = 'SUCCESS';
