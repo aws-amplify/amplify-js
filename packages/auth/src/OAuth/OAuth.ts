f2b830da5988e6597292f163d22cfa621312aba4
Manuel Iglesias <6154160+manueliglesias@users.noreply.github.com>
2021-04-28 18:50:00 -0700

    fix(@aws-amplify/auth): Handle URIs with no path part
    Fixes #7468

--- packages/auth/src/OAuth/OAuth.ts
+++ packages/auth/src/OAuth/OAuth.ts
@@ -125,7 +125,10 @@ export default class OAuth {
 			.map(pairings => pairings.split('='))
 			.reduce((accum, [k, v]) => ({ ...accum, [k]: v }), { code: undefined });
 
-		if (!code || parse(currentUrl).pathname !== parse(this._config.redirectSignIn).pathname) {
+		const currentUrlPathname = parse(currentUrl).pathname || '/';
+		const redirectSignInPathname = parse(currentUrl).pathname || '/';
+
+		if (!code || currentUrlPathname !== redirectSignInPathname) {
 			return;
 		}
 
