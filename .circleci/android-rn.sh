f369be2981860df833d9047ecf291c68ac2f36fb
Alex Hinson <alexmhinson@gmail.com>
2021-06-17 14:17:50 -0700

    test android-29

--- .circleci/android-rn.sh
+++ .circleci/android-rn.sh
@@ -22,9 +22,10 @@ case $1 in
 	;;
   sdkmanager)
 	yes | sdkmanager "platform-tools" "tools" >/dev/null
-	yes | sdkmanager "platforms;android-28" "system-images;android-28;default;x86_64" >/dev/null
-	yes | sdkmanager "emulator;30.7.3" --channel=3 >/dev/null
-	yes | sdkmanager "build-tools;28.0.3" >/dev/null
+	yes | sdkmanager "platforms;android-29" "system-images;android-29;default;x86_64" >/dev/null
+	yes | sdkmanager "ndk-bundle" >/dev/null
+	yes | sdkmanager "emulator" --channel=3 >/dev/null
+	yes | sdkmanager "build-tools;29.0.2" >/dev/null
 	yes | sdkmanager --licenses >/dev/null
 	yes | sdkmanager --list
 	;;
