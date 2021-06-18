7f3b1dcd3cb71892051b2af614a09ca501600a87
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-06-18 09:31:22 +0200

    feat(@aws-amplify/ui-components): Trim inputs before calling forgotPasswordSubmit
    Ref #7408

--- packages/amplify-ui-components/src/components/amplify-forgot-password/amplify-forgot-password.spec.ts
+++ packages/amplify-ui-components/src/components/amplify-forgot-password/amplify-forgot-password.spec.ts
@@ -11,6 +11,7 @@ describe('amplify-forgot-password spec:', () => {
 			Auth.forgotPassword = jest.fn().mockResolvedValue({
 				CodeDeliveryDetails: 'CodeDeliveryDetails mocked value',
 			});
+			Auth.forgotPasswordSubmit = jest.fn().mockResolvedValue({});
 			amplifyForgotPassword = new AmplifyForgotPassword();
 		});
 
@@ -43,6 +44,23 @@ describe('amplify-forgot-password spec:', () => {
 			expect(amplifyForgotPassword.delivery).toBe(null);
 			await promise;
 			expect(amplifyForgotPassword.delivery).toBe('CodeDeliveryDetails mocked value');
+			expect(amplifyForgotPassword.loading).toBe(false);
+		});
+
+		it('should trim the `userInput` text on `submit`', async () => {
+			const event = { preventDefault: jest.fn() };
+			amplifyForgotPassword.forgotPasswordAttrs.userInput = 'foo ';
+			amplifyForgotPassword.delivery = 'bar';
+			const promise = amplifyForgotPassword.submit(event);
+			expect(event.preventDefault).toHaveBeenCalledWith();
+			expect(amplifyForgotPassword.loading).toBe(true);
+			expect(Auth.forgotPasswordSubmit).toHaveBeenCalledWith(
+				amplifyForgotPassword.forgotPasswordAttrs.userInput.trim(),
+				amplifyForgotPassword.forgotPasswordAttrs.code,
+				amplifyForgotPassword.forgotPasswordAttrs.password);
+			expect(amplifyForgotPassword.delivery).toBe('bar');
+			await promise;
+			expect(amplifyForgotPassword.delivery).toBeNull();
 			expect(amplifyForgotPassword.loading).toBe(false);
 		});
 	});
