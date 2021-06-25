fe6838c85d7454a8cc0a898bf206c2116b3743df
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2021-06-25 12:49:33 -0400

    feat(@aws-amplify/datastore): add RN SQLite adapter (#8481)
    Fully functional SQLite adapter 
    
    remaining TODOs: 
    * implement cascading deletes
    * better utilize SQL constructs when dealing with relational data, instead of relying on lib code
    
    Co-authored-by: Manuel Iglesias <6154160+manueliglesias@users.noreply.github.com>

---
+++ packages/datastore/__tests__/SQLiteUtils.test.ts
@@ -0,0 +1,321 @@
+import {
+	generateSchemaStatements,
+	queryByIdStatement,
+	queryAllStatement,
+	queryOneStatement,
+	modelInsertStatement,
+	modelUpdateStatement,
+	whereClauseFromPredicate,
+	limitClauseFromPagination,
+	orderByClauseFromSort,
+	deleteByIdStatement,
+	deleteByPredicateStatement,
+} from '../src/storage/adapter/SQLiteUtils';
+import {
+	InternalSchema,
+	PersistentModelConstructor,
+	QueryOne,
+} from '../src/types';
+import { Model, testSchema, internalTestSchema } from './helpers';
+import { initSchema as initSchemaType } from '../src/datastore/datastore';
+
+let initSchema: typeof initSchemaType;
+
+const INTERNAL_TEST_SCHEMA_STATEMENTS = [
+	'CREATE TABLE IF NOT EXISTS Setting (id PRIMARY KEY NOT NULL, key TEXT NOT NULL, value TEXT NOT NULL);',
+	'CREATE TABLE IF NOT EXISTS Model (id PRIMARY KEY NOT NULL, field1 TEXT NOT NULL, optionalField1 TEXT, dateCreated TEXT NOT NULL, emails TEXT NOT NULL, ips TEXT, metadata TEXT, _version INTEGER, _lastChangedAt INTEGER, _deleted NUMERIC);',
+	'CREATE TABLE IF NOT EXISTS LocalModel (id PRIMARY KEY NOT NULL, field1 TEXT NOT NULL, _version INTEGER, _lastChangedAt INTEGER, _deleted NUMERIC);',
+	'CREATE TABLE IF NOT EXISTS MutationEvent (id PRIMARY KEY NOT NULL, model TEXT NOT NULL, data TEXT NOT NULL, modelId TEXT NOT NULL, operation TEXT NOT NULL, condition TEXT NOT NULL);',
+	'CREATE TABLE IF NOT EXISTS ModelMetadata (id PRIMARY KEY NOT NULL, namespace TEXT NOT NULL, model TEXT NOT NULL, lastSync INTEGER, lastFullSync INTEGER, fullSyncInterval INTEGER NOT NULL);',
+];
+
+describe('SQLiteUtils tests', () => {
+	let Model: PersistentModelConstructor<Model>;
+
+	beforeAll(async () => {
+		({ initSchema } = require('../src/datastore/datastore'));
+
+		const classes = initSchema(testSchema());
+
+		({ Model } = classes as {
+			Model: PersistentModelConstructor<Model>;
+		});
+	});
+
+	describe('createSchemaStatements', () => {
+		it('should generate valid CREATE TABLE statements from internal schema', () => {
+			const schema: InternalSchema = internalTestSchema();
+
+			expect(generateSchemaStatements(schema)).toEqual(
+				INTERNAL_TEST_SCHEMA_STATEMENTS
+			);
+		});
+	});
+
+	describe('queryByIdStatement', () => {
+		it('should generate valid SELECT by id statement', () => {
+			const model = new Model({
+				field1: 'test',
+				dateCreated: new Date().toISOString(),
+			});
+
+			const expected = [`SELECT * FROM Model WHERE id = ?`, [model.id]];
+
+			expect(queryByIdStatement(model.id, 'Model')).toEqual(expected);
+		});
+	});
+
+	describe('queryAllStatement', () => {
+		it('should generate valid SELECT all statement', () => {
+			const expected = [`SELECT * FROM Model ORDER BY _rowid_ ASC`, []];
+
+			expect(queryAllStatement('Model')).toEqual(expected);
+		});
+
+		it('should generate valid SELECT all statement - with predicates, sort, & pagination', () => {
+			const tableName = 'Model';
+
+			const predicateGroup = {
+				type: 'and',
+				predicates: [
+					{
+						field: 'firstName',
+						operator: 'eq',
+						operand: 'Bob',
+					},
+					{
+						field: 'lastName',
+						operator: 'beginsWith',
+						operand: 'Sm',
+					},
+					{
+						field: 'sortOrder',
+						operator: 'gt',
+						operand: 5,
+					},
+				],
+			};
+
+			const sortPredicateGroup = [
+				{
+					field: 'sortOrder',
+					sortDirection: 'ASCENDING',
+				},
+				{
+					field: 'lastName',
+					sortDirection: 'DESCENDING',
+				},
+			];
+
+			const limit = 10;
+			const page = 3;
+
+			const expected = [
+				`SELECT * FROM Model WHERE (firstName = ? AND lastName LIKE ? AND sortOrder > ?) ORDER BY sortOrder ASC, lastName DESC, _rowid_ ASC LIMIT ? OFFSET ?`,
+				['Bob', 'Sm%', 5, 10, 30],
+			];
+
+			expect(
+				queryAllStatement(
+					tableName,
+					predicateGroup as any,
+					sortPredicateGroup as any,
+					limit,
+					page
+				)
+			).toEqual(expected);
+		});
+	});
+
+	describe('queryOneStatement', () => {
+		it('should generate valid SELECT statement for query first', () => {
+			const expected = [`SELECT * FROM Model ORDER BY _rowid_ LIMIT 1`, []];
+
+			expect(queryOneStatement(QueryOne.FIRST, 'Model')).toEqual(expected);
+		});
+
+		it('should generate valid SELECT statement for query last', () => {
+			const expected = [
+				`SELECT * FROM Model ORDER BY _rowid_ DESC LIMIT 1`,
+				[],
+			];
+
+			expect(queryOneStatement(QueryOne.LAST, 'Model')).toEqual(expected);
+		});
+	});
+
+	describe('modelInsertStatement', () => {
+		it('should generate valid SELECT by id statement', () => {
+			const model = new Model({
+				field1: 'test',
+				dateCreated: new Date().toISOString(),
+			});
+
+			const expected = [
+				'INSERT INTO Model (field1, dateCreated, id, _version, _lastChangedAt, _deleted) VALUES (?, ?, ?, ?, ?, ?)',
+				[
+					model.field1,
+					model.dateCreated,
+					model.id,
+					undefined,
+					undefined,
+					undefined,
+				],
+			];
+
+			expect(modelInsertStatement(model, 'Model')).toEqual(expected);
+		});
+	});
+
+	describe('modelUpdateStatement', () => {
+		it('should generate valid SELECT by id statement', () => {
+			const model = new Model({
+				field1: 'test',
+				dateCreated: new Date().toISOString(),
+			});
+
+			const expected = [
+				`UPDATE Model SET field1=?, dateCreated=?, _version=?, _lastChangedAt=?, _deleted=? WHERE id=?`,
+				[
+					model.field1,
+					model.dateCreated,
+					undefined,
+					undefined,
+					undefined,
+					model.id,
+				],
+			];
+
+			expect(modelUpdateStatement(model, 'Model')).toEqual(expected);
+		});
+	});
+
+	describe('whereClauseFromPredicate', () => {
+		it('should generate valid WHERE clause from predicate', () => {
+			const predicateGroup = {
+				type: 'and',
+				predicates: [
+					{
+						field: 'firstName',
+						operator: 'eq',
+						operand: 'Bob',
+					},
+					{
+						field: 'lastName',
+						operator: 'beginsWith',
+						operand: 'Sm',
+					},
+					{
+						field: 'sortOrder',
+						operator: 'gt',
+						operand: 5,
+					},
+				],
+			};
+
+			const expected = [
+				`WHERE (firstName = ? AND lastName LIKE ? AND sortOrder > ?)`,
+				['Bob', 'Sm%', 5],
+			];
+
+			expect(whereClauseFromPredicate(predicateGroup as any)).toEqual(expected);
+		});
+	});
+
+	describe('limitClauseFromPagination', () => {
+		it('should generate valid LIMIT clause from pagination limit', () => {
+			const limit = 10;
+
+			const expected = ['LIMIT ?', [10]];
+
+			expect(limitClauseFromPagination(limit)).toEqual(expected);
+		});
+
+		it('should generate valid LIMIT clause from pagination limit and page', () => {
+			const limit = 10;
+			const page = 3;
+
+			const expected = ['LIMIT ? OFFSET ?', [10, 30]];
+
+			expect(limitClauseFromPagination(limit, page)).toEqual(expected);
+		});
+	});
+
+	describe('orderByClauseFromSort', () => {
+		it('should generate valid ORDER BY clause from pagination sort', () => {
+			const sortPredicateGroup = [
+				{
+					field: 'sortOrder',
+					sortDirection: 'ASCENDING',
+				},
+			];
+
+			const expected = 'ORDER BY sortOrder ASC, _rowid_ ASC';
+
+			expect(orderByClauseFromSort(sortPredicateGroup as any)).toEqual(
+				expected
+			);
+		});
+
+		it('should generate valid ORDER BY clause from pagination sort - multi field', () => {
+			const sortPredicateGroup = [
+				{
+					field: 'sortOrder',
+					sortDirection: 'ASCENDING',
+				},
+				{
+					field: 'lastName',
+					sortDirection: 'DESCENDING',
+				},
+			];
+
+			const expected = 'ORDER BY sortOrder ASC, lastName DESC, _rowid_ ASC';
+
+			expect(orderByClauseFromSort(sortPredicateGroup as any)).toEqual(
+				expected
+			);
+		});
+	});
+
+	describe('deleteByIdStatement', () => {
+		it('should generate valid DELETE statement', () => {
+			const model = new Model({
+				field1: 'test',
+				dateCreated: new Date().toISOString(),
+			});
+
+			const expected = ['DELETE FROM Model WHERE id=?', [model.id]];
+
+			expect(deleteByIdStatement(model.id, 'Model')).toEqual(expected);
+		});
+	});
+
+	describe('deleteByPredicateStatement', () => {
+		it('should generate valid DELETE statement', () => {
+			const model = new Model({
+				field1: 'test',
+				dateCreated: new Date().toISOString(),
+			});
+
+			const predicateGroup = {
+				type: 'and',
+				predicates: [
+					{
+						field: 'createdAt',
+						operator: 'gt',
+						operand: '2021-06-20',
+					},
+				],
+			};
+
+			const expected = [
+				'DELETE FROM Model WHERE (createdAt > ?)',
+				['2021-06-20'],
+			];
+
+			expect(
+				deleteByPredicateStatement('Model', predicateGroup as any)
+			).toEqual(expected);
+		});
+	});
+});
--- packages/datastore/src/types.ts
+++ packages/datastore/src/types.ts
@@ -175,7 +175,7 @@ export namespace GraphQLScalarType {
 	export function getJSType(
 		scalar: keyof Omit<
 			typeof GraphQLScalarType,
-			'getJSType' | 'getValidationFunction'
+			'getJSType' | 'getValidationFunction' | 'getSQLiteType'
 		>
 	): 'string' | 'number' | 'boolean' {
 		switch (scalar) {
@@ -197,14 +197,44 @@ export namespace GraphQLScalarType {
 			case 'AWSTimestamp':
 				return 'number';
 			default:
-				exhaustiveCheck(scalar);
+				exhaustiveCheck(scalar as never);
+		}
+	}
+
+	export function getSQLiteType(
+		scalar: keyof Omit<
+			typeof GraphQLScalarType,
+			'getJSType' | 'getValidationFunction' | 'getSQLiteType'
+		>
+	): 'TEXT' | 'INTEGER' | 'REAL' | 'NUMERIC' | 'BLOB' {
+		switch (scalar) {
+			case 'Boolean':
+				return 'NUMERIC';
+			case 'ID':
+			case 'String':
+			case 'AWSDate':
+			case 'AWSTime':
+			case 'AWSDateTime':
+			case 'AWSEmail':
+			case 'AWSJSON':
+			case 'AWSURL':
+			case 'AWSPhone':
+			case 'AWSIPAddress':
+				return 'TEXT';
+			case 'Int':
+			case 'AWSTimestamp':
+				return 'INTEGER';
+			case 'Float':
+				return 'REAL';
+			default:
+				exhaustiveCheck(scalar as never);
 		}
 	}
 
 	export function getValidationFunction(
 		scalar: keyof Omit<
 			typeof GraphQLScalarType,
-			'getJSType' | 'getValidationFunction'
+			'getJSType' | 'getValidationFunction' | 'getSQLiteType'
 		>
 	): ((val: string | number) => boolean) | undefined {
 		switch (scalar) {
@@ -246,7 +276,7 @@ export function isGraphQLScalarType(
 	obj: any
 ): obj is keyof Omit<
 	typeof GraphQLScalarType,
-	'getJSType' | 'getValidationFunction'
+	'getJSType' | 'getValidationFunction' | 'getSQLiteType'
 > {
 	return obj && GraphQLScalarType[obj] !== undefined;
 }
