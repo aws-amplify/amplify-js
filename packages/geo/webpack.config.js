38c0c3d325e91e6c20e3ec6d9defe044219ac23b
Tré Ammatuna <16496746+TreTuna@users.noreply.github.com>
2021-07-21 13:44:51 -0700

    feat(Geo): add Geo category initial scaffolding - Geo milestone 1 - PR 1/6 (#8615)

---
+++ packages/geo/webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+	entry: {
+		'aws-amplify-geo.min': './lib-esm/index.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: __dirname + '/dist',
+		library: 'aws_amplify_geo',
+		libraryTarget: 'umd',
+		umdNamedDefine: true,
+		devtoolModuleFilenameTemplate: require('../aws-amplify/webpack-utils')
+			.devtoolModuleFilenameTemplate,
+	},
+	// Enable sourcemaps for debugging webpack's output.
+	devtool: 'source-map',
+	resolve: {
+		extensions: ['.mjs', '.js', '.json'],
+	},
+	mode: 'production',
+	module: {
+		rules: [
+			// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+			//{ enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				use: [
+					'babel-loader',
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+						},
+					},
+				],
+			},
+		],
+	},
+};
