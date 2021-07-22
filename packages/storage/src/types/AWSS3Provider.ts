ec358e702020b4be65fe2ec377f975ececf2449e
James Au <auchu@amazon.com>
2021-07-22 15:08:44 -0700

    add mobile hub config to types

--- packages/storage/src/types/AWSS3Provider.ts
+++ packages/storage/src/types/AWSS3Provider.ts
@@ -11,61 +11,74 @@ import { CancelTokenSource } from 'axios';
 
 type ListObjectsCommandOutputContent = _Object;
 
-export interface S3ProviderGetConfig extends StorageOptions {
-	download?: boolean;
-	track?: boolean;
-	expires?: number;
-	provider?: 'AWSS3';
-	progressCallback?: (progress: any) => any;
-	cancelTokenSource?: CancelTokenSource;
-	cacheControl?: GetObjectRequest['ResponseCacheControl'];
-	contentDisposition?: GetObjectRequest['ResponseContentDisposition'];
-	contentEncoding?: GetObjectRequest['ResponseContentEncoding'];
-	contentLanguage?: GetObjectRequest['ResponseContentLanguage'];
-	contentType?: GetObjectRequest['ResponseContentType'];
-	SSECustomerAlgorithm?: GetObjectRequest['SSECustomerAlgorithm'];
-	SSECustomerKey?: GetObjectRequest['SSECustomerKey'];
-	SSECustomerKeyMD5?: GetObjectRequest['SSECustomerKeyMD5'];
+interface StorageMobileHubConfig {
+	Storage: {
+		AWSS3: {
+			bucket: string;
+			region: string;
+			dangerouslyConnectToHttpEndpointForTesting?: boolean;
+		};
+	};
 }
 
-export type S3ProviderGetOuput<T> = T extends { download: true }
-	? GetObjectCommandOutput
-	: string;
+export type S3ProviderGetConfig =
+	| (StorageOptions & {
+			download?: boolean;
+			track?: boolean;
+			expires?: number;
+			provider?: 'AWSS3';
+			progressCallback?: (progress: any) => any;
+			cancelTokenSource?: CancelTokenSource;
+			cacheControl?: GetObjectRequest['ResponseCacheControl'];
+			contentDisposition?: GetObjectRequest['ResponseContentDisposition'];
+			contentEncoding?: GetObjectRequest['ResponseContentEncoding'];
+			contentLanguage?: GetObjectRequest['ResponseContentLanguage'];
+			contentType?: GetObjectRequest['ResponseContentType'];
+			SSECustomerAlgorithm?: GetObjectRequest['SSECustomerAlgorithm'];
+			SSECustomerKey?: GetObjectRequest['SSECustomerKey'];
+			SSECustomerKeyMD5?: GetObjectRequest['SSECustomerKeyMD5'];
+	  })
+	| StorageMobileHubConfig;
 
-export interface S3ProviderPutConfig extends StorageOptions {
-	progressCallback?: (progress: any) => any;
-	track?: boolean;
-	cancelTokenSource?: CancelTokenSource;
-	serverSideEncryption?: PutObjectRequest['ServerSideEncryption'];
-	SSECustomerAlgorithm?: PutObjectRequest['SSECustomerAlgorithm'];
-	SSECustomerKey?: PutObjectRequest['SSECustomerKey'];
-	SSECustomerKeyMD5?: PutObjectRequest['SSECustomerKeyMD5'];
-	SSEKMSKeyId?: PutObjectRequest['SSEKMSKeyId'];
-	acl?: PutObjectRequest['ACL'];
-	bucket?: PutObjectRequest['Bucket'];
-	cacheControl?: PutObjectRequest['CacheControl'];
-	contentDisposition?: PutObjectRequest['ContentDisposition'];
-	contentEncoding?: PutObjectRequest['ContentEncoding'];
-	contentType?: PutObjectRequest['ContentType'];
-	expires?: PutObjectRequest['Expires'];
-	metadata?: PutObjectRequest['Metadata'];
-	tagging?: PutObjectRequest['Tagging'];
-}
+export type S3ProviderGetOuput<T> = T extends { download: true } ? GetObjectCommandOutput : string;
+
+export type S3ProviderPutConfig =
+	| (StorageOptions & {
+			progressCallback?: (progress: any) => any;
+			provider?: 'AWSS3';
+			track?: boolean;
+			cancelTokenSource?: CancelTokenSource;
+			serverSideEncryption?: PutObjectRequest['ServerSideEncryption'];
+			SSECustomerAlgorithm?: PutObjectRequest['SSECustomerAlgorithm'];
+			SSECustomerKey?: PutObjectRequest['SSECustomerKey'];
+			SSECustomerKeyMD5?: PutObjectRequest['SSECustomerKeyMD5'];
+			SSEKMSKeyId?: PutObjectRequest['SSEKMSKeyId'];
+			acl?: PutObjectRequest['ACL'];
+			bucket?: PutObjectRequest['Bucket'];
+			cacheControl?: PutObjectRequest['CacheControl'];
+			contentDisposition?: PutObjectRequest['ContentDisposition'];
+			contentEncoding?: PutObjectRequest['ContentEncoding'];
+			contentType?: PutObjectRequest['ContentType'];
+			expires?: PutObjectRequest['Expires'];
+			metadata?: PutObjectRequest['Metadata'];
+			tagging?: PutObjectRequest['Tagging'];
+	  })
+	| StorageMobileHubConfig;
 
 export interface S3ProviderPutOutput {
 	key: string;
 }
 
-export interface S3ProviderRemoveConfig extends StorageOptions {
-	bucket?: string;
-}
+export type S3ProviderRemoveConfig = (StorageOptions & { bucket?: string }) | StorageMobileHubConfig;
 
 export type S3ProviderRemoveOutput = DeleteObjectCommandOutput;
 
-export interface S3ProviderListConfig extends StorageOptions {
-	bucket?: string;
-	maxKeys?: number;
-}
+export type S3ProviderListConfig =
+	| (StorageOptions & {
+			bucket?: string;
+			maxKeys?: number;
+	  })
+	| StorageMobileHubConfig;
 
 export interface S3ProviderListOutputItem {
 	key: ListObjectsCommandOutputContent['Key'];
@@ -86,23 +99,25 @@ export type S3CopySource = S3CopyTarget;
 
 export type S3CopyDestination = Omit<S3CopyTarget, 'identityId'>;
 
-export interface S3ProviderCopyConfig extends StorageOptions {
-	cancelTokenSource?: CancelTokenSource;
-	bucket?: CopyObjectRequest['Bucket'];
-	cacheControl?: CopyObjectRequest['CacheControl'];
-	contentDisposition?: CopyObjectRequest['ContentDisposition'];
-	contentLanguage?: CopyObjectRequest['ContentLanguage'];
-	contentType?: CopyObjectRequest['ContentType'];
-	expires?: CopyObjectRequest['Expires'];
-	tagging?: CopyObjectRequest['Tagging'];
-	acl?: CopyObjectRequest['ACL'];
-	metadata?: CopyObjectRequest['Metadata'];
-	serverSideEncryption?: CopyObjectRequest['ServerSideEncryption'];
-	SSECustomerAlgorithm?: CopyObjectRequest['SSECustomerAlgorithm'];
-	SSECustomerKey?: CopyObjectRequest['SSECustomerKey'];
-	SSECustomerKeyMD5?: CopyObjectRequest['SSECustomerKeyMD5'];
-	SSEKMSKeyId?: CopyObjectRequest['SSEKMSKeyId'];
-}
+export type S3ProviderCopyConfig =
+	| (StorageOptions & {
+			cancelTokenSource?: CancelTokenSource;
+			bucket?: CopyObjectRequest['Bucket'];
+			cacheControl?: CopyObjectRequest['CacheControl'];
+			contentDisposition?: CopyObjectRequest['ContentDisposition'];
+			contentLanguage?: CopyObjectRequest['ContentLanguage'];
+			contentType?: CopyObjectRequest['ContentType'];
+			expires?: CopyObjectRequest['Expires'];
+			tagging?: CopyObjectRequest['Tagging'];
+			acl?: CopyObjectRequest['ACL'];
+			metadata?: CopyObjectRequest['Metadata'];
+			serverSideEncryption?: CopyObjectRequest['ServerSideEncryption'];
+			SSECustomerAlgorithm?: CopyObjectRequest['SSECustomerAlgorithm'];
+			SSECustomerKey?: CopyObjectRequest['SSECustomerKey'];
+			SSECustomerKeyMD5?: CopyObjectRequest['SSECustomerKeyMD5'];
+			SSEKMSKeyId?: CopyObjectRequest['SSEKMSKeyId'];
+	  })
+	| StorageMobileHubConfig;
 
 export type S3ProviderCopyOutput = {
 	key: string;
