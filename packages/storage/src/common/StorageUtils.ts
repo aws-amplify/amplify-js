aa65f9085faca8fd257e712431f456132ac3237a
James Au <auchu@amazon.com>
2021-10-07 11:58:39 -0700

    add util function to determine byteLength

--- packages/storage/src/common/StorageUtils.ts
+++ packages/storage/src/common/StorageUtils.ts
@@ -19,3 +19,23 @@ export const listSingleFile = async ({
 	const obj = Contents.find(o => o.Key === key);
 	return obj;
 };
+
+export const byteLength = (x: unknown) => {
+	if (typeof x === 'string') {
+		return x.length;
+	} else if (isArrayBuffer(x)) {
+		return x.byteLength;
+	} else if (isBlob(x)) {
+		return x.size;
+	} else {
+		throw new Error(`Cannot determine byte length of ${x}`);
+	}
+};
+
+const isBlob = (x: unknown): x is Blob => {
+	return typeof x !== 'undefined' && x instanceof Blob;
+};
+
+const isArrayBuffer = (x: unknown): x is ArrayBuffer => {
+	return typeof x !== 'undefined' && x instanceof ArrayBuffer;
+};
