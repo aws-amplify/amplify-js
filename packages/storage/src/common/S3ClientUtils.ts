baa5b27bd942717e2c8c5b74b1424810bc81a9f1
James Au <auchu@amazon.com>
2021-10-20 12:00:13 -0700

    use obj destructure in getPrefix

--- packages/storage/src/common/S3ClientUtils.ts
+++ packages/storage/src/common/S3ClientUtils.ts
@@ -8,14 +8,26 @@ export const getPrefix = (config: {
 	identityId?: string;
 	credentials: ICredentials;
 }): string => {
-	const { credentials, level } = config;
+	const { credentials, level, customPrefix, identityId } = config;
 
-	const customPrefix = config.customPrefix || {};
-	const identityId = config.identityId || credentials.identityId;
-	const privatePath = (customPrefix.private !== undefined ? customPrefix.private : 'private/') + identityId + '/';
+	const resolvedCustomPrefix = customPrefix || {};
+	const resolvedIdentityId = identityId || credentials.identityId;
+	const privatePath =
+		(resolvedCustomPrefix.private !== undefined
+			? resolvedCustomPrefix.private
+			: 'private/') +
+		resolvedIdentityId +
+		'/';
 	const protectedPath =
-		(customPrefix.protected !== undefined ? customPrefix.protected : 'protected/') + identityId + '/';
-	const publicPath = customPrefix.public !== undefined ? customPrefix.public : 'public/';
+		(resolvedCustomPrefix.protected !== undefined
+			? resolvedCustomPrefix.protected
+			: 'protected/') +
+		resolvedIdentityId +
+		'/';
+	const publicPath =
+		resolvedCustomPrefix.public !== undefined
+			? resolvedCustomPrefix.public
+			: 'public/';
 
 	switch (level) {
 		case 'private':
@@ -27,17 +39,20 @@ export const getPrefix = (config: {
 	}
 };
 
-export const createPrefixMiddleware = (opt: Record<string, any>, key: string): InitializeMiddleware<any, any> => (
-	next,
-	_context
-) => async args => {
+export const createPrefixMiddleware = (
+	opt: Record<string, any>,
+	key: string
+): InitializeMiddleware<any, any> => (next, _context) => async args => {
 	if (Object.prototype.hasOwnProperty.call(args.input, 'Key')) {
 		const credentials = await Credentials.get();
 		const cred = Credentials.shear(credentials);
 		const prefix = getPrefix({ ...opt, credentials: cred });
+		const clonedInput = Object.assign({}, args.input);
 		if (Object.prototype.hasOwnProperty.call(args.input, 'Key')) {
-			const clonedInput = Object.assign({}, args.input) as { Key: string };
 			clonedInput.Key = prefix + key;
+			args.input = clonedInput;
+		} else if (Object.prototype.hasOwnProperty.call(args.input, 'Prefix')) {
+			clonedInput.Prefix = prefix + key;
 			args.input = clonedInput;
 		}
 	}
