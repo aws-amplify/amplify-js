f05b2b466cb21621377705918468f1e006c696cf
Caleb Pollman <cpollman@amazon.com>
2021-10-22 15:09:02 -0700

    remove extraneous flex-start prop from Paragraph

--- packages/aws-amplify-react-native/src/InAppMessaging/ui/Paragraph/styles.ts
+++ packages/aws-amplify-react-native/src/InAppMessaging/ui/Paragraph/styles.ts
@@ -13,4 +13,4 @@
 
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({ container: { alignItems: 'flex-start', flex: 1 } });
+export const styles = StyleSheet.create({ container: { flex: 1 } });
