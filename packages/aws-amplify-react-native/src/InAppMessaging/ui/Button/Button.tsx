16250dad4004f3bfd13e2b3906c7a8bd42c2ea0a
Caleb Pollman <cpollman@amazon.com>
2021-10-24 10:40:38 -0700

    feat(in-app-messaging): add Button UI component

---
+++ packages/aws-amplify-react-native/src/InAppMessaging/ui/Button/Button.tsx
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+import React from 'react';
+import { Pressable, Text } from 'react-native';
+
+import { styles } from './styles';
+import { ButtonProps } from './types';
+
+export function Button({ children, textStyle, ...pressableProps }: ButtonProps) {
+	return (
+		<Pressable {...pressableProps}>
+			{() => {
+				if (typeof children === 'string') {
+					return <Text style={[styles.text, textStyle]}>{children}</Text>;
+				}
+				return children;
+			}}
+		</Pressable>
+	);
+}
