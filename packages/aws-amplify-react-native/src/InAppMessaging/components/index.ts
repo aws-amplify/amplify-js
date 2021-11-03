0e6549e5bd29a2b6db2ac3383fbf6d464a4152de
Caleb Pollman <cpollman@amazon.com>
2021-11-03 15:16:41 -0700

    clean up some estraneous type exports

--- packages/aws-amplify-react-native/src/InAppMessaging/components/index.ts
+++ packages/aws-amplify-react-native/src/InAppMessaging/components/index.ts
@@ -2,4 +2,4 @@ export { default as BannerMessage, BannerMessageProps } from './BannerMessage';
 export { CarouselMessageProps } from './CarouselMessage';
 export { default as FullScreenMessage, FullScreenMessageProps } from './FullScreenMessage';
 export { default as InAppMessageDisplay } from './InAppMessageDisplay';
-export { InAppMessageComponentPosition, InAppMessageComponentStyle, InAppMessageComponentStyles } from './types';
+export { InAppMessageComponentStyle, InAppMessageComponentStyles } from './types';
