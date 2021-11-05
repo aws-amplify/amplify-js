bbfd8c0ac7d035853f4fae5f6f731adfbdd0d08e
Joel Warrington <joelwarrington@gmail.com>
2021-11-05 10:38:42 -0600

    Added INCORRECT_USERNAME_OR_PASSWORD

--- packages/auth/src/common/AuthErrorStrings.ts
+++ packages/auth/src/common/AuthErrorStrings.ts
@@ -18,7 +18,14 @@ export enum AuthErrorStrings {
    * **The user is attempting to sign-up with an email already configured in the User Pool.**
    */
 	EMAIL_ALREADY_EXISTS = 'An account with the given email already exists.',
-	
+
+	/**
+   * **The user provides an invalid password required to sign-in for a valid username.**
+	 *
+	 * @see `AuthErrorStrings.USER_DOES_NOT_EXIST` will occur if username does not exist
+   */
+	INCORRECT_USERNAME_OR_PASSWORD = 'Incorrect username or password.',
+
 	/**
    * **The user session is invalid and are required to sign-in again.**
 	 * 
@@ -26,7 +33,7 @@ export enum AuthErrorStrings {
 	 * - User attempts sign-in and does not respond to MFA challenge quick enough via `confirmSignIn`
    */
 	INVALID_SESSION = 'Invalid session for the user, session is expired.',
-	
+
 	/**
    * **The user provided an invalid code when responding to MFA challenge.**
 	 * 
@@ -34,7 +41,7 @@ export enum AuthErrorStrings {
 	 * - User attempts sign-in and provides incorrect code to `confirmSignIn`
    */
 	INVALID_MFA_CODE = 'Invalid code received for user',
-	
+
 	/**
    * **The user provided an invalid code when responding to an email confirmation.**
 	 * 
@@ -42,14 +49,14 @@ export enum AuthErrorStrings {
 	 * - User attempts confirm sign-up and provides incorrect code to `confirmSignUp`
    */
 	INVALID_EMAIL_CONFIRMATION_CODE = 'Invalid code provided, please request a code again.',
-	
+
 	/**
    * **The user attempts to confirm their email a second time, when it's already confirmed.**
 	 *
 	 * @see `AuthErrorStrings.USER_IS_ALREADY_CONFIRMED`
    */
 	USER_CANNOT_BE_CONFIRMED_IS_ALREADY_CONFIRMED = 'User cannot be confirmed. Current status is CONFIRMED',
-	
+
 	/**
    * **Method performing action on user yields no results**
 	 *
