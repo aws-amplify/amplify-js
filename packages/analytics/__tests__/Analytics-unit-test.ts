8c1088c145b02f97528ddbd6972b00dea0234296
Chris F <5827964+cshfang@users.noreply.github.com>
2021-11-30 18:52:20 -0800

    chore(in-app-message): In app messaging/add unit tests (#9253)
    * chore: Add unit tests
    
    * Add licensing to top of analytics test
    
    * Fixed typo
    
    * Addressed PR feedback
    
    * Remove quiet time test
    
    Co-authored-by: Chris Fang <chrfang@amazon.com>

--- packages/analytics/__tests__/Analytics-unit-test.ts
+++ packages/analytics/__tests__/Analytics-unit-test.ts
@@ -1,33 +1,24 @@
+/*
+ * Copyright 2017-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 jest.mock('../src/vendor/dom-utils', () => {
 	return {
 		delegate: jest.fn(),
 	};
 });
 
-import {
-	ClientDevice,
-	Parser,
-	ConsoleLogger as Logger,
-	Credentials,
-} from '@aws-amplify/core';
-import { AnalyticsOptions, EventAttributes, EventMetrics } from '../src/types';
+import { ClientDevice, Parser, Hub } from '@aws-amplify/core';
 import { AnalyticsClass as Analytics } from '../src/Analytics';
 import AWSAnalyticsProvider from '../src/Providers/AWSPinpointProvider';
-
-const options: AnalyticsOptions = {
-	appId: 'appId',
-	platform: 'platform',
-	clientId: 'clientId',
-	region: 'region',
-};
-
-const credentials = {
-	accessKeyId: 'accessKeyId',
-	sessionToken: 'sessionToken',
-	secretAccessKey: 'secretAccessKey',
-	identityId: 'identityId',
-	authenticated: true,
-};
 
 jest.useFakeTimers();
 
@@ -37,7 +28,12 @@ const record_spyon = jest
 		return handlers.resolve();
 	});
 
+const hubSpy = jest.spyOn(Hub, 'dispatch');
+
 describe('Analytics test', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
 	describe('configure test', () => {
 		test('happy case with default parser', () => {
 			const analytics = new Analytics();
@@ -83,6 +79,16 @@ describe('Analytics test', () => {
 			analytics.configure({ mock: 'value' });
 
 			await analytics.startSession();
+			expect(hubSpy).toBeCalledWith(
+				'analytics',
+				{
+					event: 'record',
+					data: { name: '_session.start' },
+					message: 'Recording Analytics session start event',
+				},
+				'Analytics',
+				expect.anything()
+			);
 			expect(record_spyon).toBeCalled();
 		});
 	});
@@ -95,6 +101,16 @@ describe('Analytics test', () => {
 			analytics.configure({ mock: 'value' });
 
 			await analytics.stopSession();
+			expect(hubSpy).toBeCalledWith(
+				'analytics',
+				{
+					event: 'record',
+					data: { name: '_session.stop' },
+					message: 'Recording Analytics session stop event',
+				},
+				'Analytics',
+				expect.anything()
+			);
 			expect(record_spyon).toBeCalled();
 		});
 	});
@@ -105,12 +121,23 @@ describe('Analytics test', () => {
 			const provider = new AWSAnalyticsProvider();
 			analytics.addPluggable(provider);
 			analytics.configure({ mock: 'value' });
-
-			await analytics.record({
+			const event = {
 				name: 'event',
 				attributes: 'attributes',
 				metrics: 'metrics',
-			});
+			};
+
+			await analytics.record(event);
+			expect(hubSpy).toBeCalledWith(
+				'analytics',
+				{
+					event: 'record',
+					data: event,
+					message: 'Recording Analytics event',
+				},
+				'Analytics',
+				expect.anything()
+			);
 			expect(record_spyon).toBeCalled();
 		});
 	});
---
+++ packages/notifications/__tests__/InAppMessaging/Providers/AWSPinpointProvider/utils.test.ts
@@ -0,0 +1,341 @@
+/*
+ * Copyright 2017-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+import { InAppMessageCampaign as PinpointInAppMessage } from '@aws-sdk/client-pinpoint';
+import { Amplify, ConsoleLogger, Hub } from '@aws-amplify/core';
+import cloneDeep from 'lodash/cloneDeep';
+
+import { InAppMessagingEvent } from '../../../../src/InAppMessaging';
+import { AWSPinpointMessageEvent } from '../../../../src/InAppMessaging/Providers/AWSPinpointProvider/types';
+import {
+	clearMemo,
+	dispatchInAppMessagingEvent,
+	extractContent,
+	extractMetadata,
+	getStartOfDay,
+	isBeforeEndDate,
+	isQuietTime,
+	matchesAttributes,
+	matchesEventType,
+	matchesMetrics,
+	recordAnalyticsEvent,
+} from '../../../../src/InAppMessaging/Providers/AWSPinpointProvider/utils';
+
+import {
+	inAppMessages,
+	extractedContent,
+	extractedMetadata,
+	pinpointInAppMessage,
+} from '../../../../__mocks__/data';
+
+jest.mock('@aws-amplify/core');
+
+const HOUR_IN_MS = 1000 * 60 * 60;
+
+const loggerDebugSpy = jest.spyOn(ConsoleLogger.prototype, 'debug');
+
+describe('AWSPinpoint InAppMessaging Provider Utils', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	test('dispatchInAppMessagingEvent dispatches Hub event', () => {
+		const event = 'foo';
+		const data = { bar: 'baz' };
+		const message = 'qux';
+
+		dispatchInAppMessagingEvent(event, data, message);
+
+		expect(Hub.dispatch).toBeCalledWith(
+			'inAppMessaging',
+			{ event, data, message },
+			'InAppMessaging',
+			expect.anything() // expect.any(Symbol) is fixed in a later Jest version
+		);
+	});
+
+	describe('recordAnalyticsEvent', () => {
+		Amplify.Analytics = { record: jest.fn() };
+		const [message] = inAppMessages;
+
+		test('records an analytics event', () => {
+			Amplify.Analytics = { record: jest.fn() };
+
+			recordAnalyticsEvent(AWSPinpointMessageEvent.MESSAGE_DISPLAYED, message);
+
+			expect(Amplify.Analytics.record).toBeCalledWith({
+				name: AWSPinpointMessageEvent.MESSAGE_DISPLAYED,
+				attributes: {
+					campaign_id: 'top-banner',
+					delivery_type: 'IN_APP_MESSAGE',
+					treatment_id: 'T1',
+				},
+			});
+		});
+
+		test('does not try to record an event without a message', () => {
+			recordAnalyticsEvent(AWSPinpointMessageEvent.MESSAGE_DISPLAYED, null);
+
+			expect(loggerDebugSpy).toBeCalledWith(
+				expect.stringContaining('Unable to record')
+			);
+			expect(Amplify.Analytics.record).not.toBeCalled();
+		});
+
+		test('does not try to record an event without a message', () => {
+			Amplify.Analytics = null;
+
+			recordAnalyticsEvent(AWSPinpointMessageEvent.MESSAGE_DISPLAYED, message);
+
+			expect(loggerDebugSpy).toBeCalledWith(
+				expect.stringContaining('module is not registered')
+			);
+		});
+	});
+
+	test('getStartOfDay returns a date string for the start of day', () => {
+		const dateString = getStartOfDay();
+		const date = new Date(dateString);
+
+		expect(date.getHours()).toBe(0);
+		expect(date.getMinutes()).toBe(0);
+		expect(date.getSeconds()).toBe(0);
+		expect(date.getMilliseconds()).toBe(0);
+	});
+
+	describe('matchesEventType', () => {
+		let message: PinpointInAppMessage;
+		beforeEach(() => {
+			message = cloneDeep(pinpointInAppMessage);
+			clearMemo();
+		});
+
+		test('checks if an event name matches a Pinpoint message', () => {
+			const clickEvent: InAppMessagingEvent = { name: 'clicked' };
+			const swipeEvent: InAppMessagingEvent = { name: 'swiped' };
+			const dragEvent: InAppMessagingEvent = { name: 'dragged' };
+
+			expect(matchesEventType(message, clickEvent)).toBe(true);
+			expect(matchesEventType(message, swipeEvent)).toBe(true);
+			expect(matchesEventType(message, dragEvent)).toBe(false);
+		});
+
+		test('memoizes matches', () => {
+			const clickEvent: InAppMessagingEvent = { name: 'clicked' };
+			message.Schedule.EventFilter.Dimensions.EventType.Values = ['clicked'];
+
+			expect(matchesEventType(message, clickEvent)).toBe(true);
+
+			// This is a contrived way of validating the memo logic and should never happen in practice
+			message.Schedule.EventFilter.Dimensions.EventType.Values = [];
+
+			expect(matchesEventType(message, clickEvent)).toBe(true);
+
+			clearMemo();
+
+			expect(matchesEventType(message, clickEvent)).toBe(false);
+		});
+	});
+
+	describe('matchesAttributes', () => {
+		let message: PinpointInAppMessage;
+		beforeEach(() => {
+			message = cloneDeep(pinpointInAppMessage);
+			clearMemo();
+		});
+
+		test('checks if event attributes matches a Pinpoint message', () => {
+			const matchingEvent: InAppMessagingEvent = {
+				name: 'action.taken',
+				attributes: {
+					favoriteFood: 'pizza',
+					favoriteAnimal: 'dog',
+					favoriteHobby: 'skydiving',
+				},
+			};
+			const nonMatchingEvent: InAppMessagingEvent = {
+				name: 'action.taken',
+				attributes: {
+					favoriteFood: 'pizza',
+					favoriteAnimal: 'monkey',
+				},
+			};
+			const missingAttributesEvent: InAppMessagingEvent = {
+				name: 'action.taken',
+				attributes: { favoriteFood: 'sushi' },
+			};
+			const noAttributesEvent: InAppMessagingEvent = { name: 'action.taken' };
+
+			// Everything matches if there are no attributes on the message
+			expect(matchesAttributes(message, matchingEvent)).toBe(true);
+			expect(matchesAttributes(message, nonMatchingEvent)).toBe(true);
+			expect(matchesAttributes(message, missingAttributesEvent)).toBe(true);
+			expect(matchesAttributes(message, noAttributesEvent)).toBe(true);
+
+			clearMemo();
+
+			message.Schedule.EventFilter.Dimensions.Attributes = {
+				favoriteFood: { Values: ['pizza', 'sushi'] },
+				favoriteAnimal: { Values: ['dog', 'giraffe'] },
+			};
+
+			expect(matchesAttributes(message, matchingEvent)).toBe(true);
+			expect(matchesAttributes(message, nonMatchingEvent)).toBe(false);
+			expect(matchesAttributes(message, missingAttributesEvent)).toBe(false);
+			expect(matchesAttributes(message, noAttributesEvent)).toBe(false);
+		});
+
+		test('memoizes matches', () => {
+			const event: InAppMessagingEvent = {
+				name: 'action.taken',
+				attributes: { favoriteFood: 'sushi' },
+			};
+			message.Schedule.EventFilter.Dimensions.Attributes = {
+				favoriteFood: { Values: ['pizza', 'sushi'] },
+			};
+
+			expect(matchesAttributes(message, event)).toBe(true);
+
+			// This is a contrived way of validating the memo logic and should never happen in practice
+			message.Schedule.EventFilter.Dimensions.Attributes = {
+				favoriteFood: { Values: ['pizza'] },
+			};
+
+			expect(matchesAttributes(message, event)).toBe(true);
+
+			clearMemo();
+
+			expect(matchesAttributes(message, event)).toBe(false);
+		});
+	});
+
+	describe('matchesMetrics', () => {
+		let message: PinpointInAppMessage;
+		beforeEach(() => {
+			message = cloneDeep(pinpointInAppMessage);
+			clearMemo();
+		});
+
+		test('checks if event metrics matches a Pinpoint message', () => {
+			const matchingEvent: InAppMessagingEvent = {
+				name: 'action.taken',
+				metrics: {
+					lotSize: 2000,
+					yearBuilt: 2000,
+					bedrooms: 3,
+					bathrooms: 2,
+					listPrice: 600000,
+					viewed: 300,
+				},
+			};
+			const nonMatchingEvent: InAppMessagingEvent = {
+				name: 'action.taken',
+				metrics: {
+					lotSize: 2000,
+					yearBuilt: 2000,
+					bedrooms: 3,
+					bathrooms: 2,
+					listPrice: 700000,
+				},
+			};
+			const missingMetricsEvent: InAppMessagingEvent = {
+				name: 'action.taken',
+				metrics: {
+					lotSize: 2000,
+					yearBuilt: 2000,
+				},
+			};
+			const noMetricsEvent: InAppMessagingEvent = { name: 'action.taken' };
+
+			// Everything matches if there are no metrics on the message
+			expect(matchesMetrics(message, matchingEvent)).toBe(true);
+			expect(matchesMetrics(message, nonMatchingEvent)).toBe(true);
+			expect(matchesMetrics(message, missingMetricsEvent)).toBe(true);
+			expect(matchesMetrics(message, noMetricsEvent)).toBe(true);
+
+			clearMemo();
+
+			message.Schedule.EventFilter.Dimensions.Metrics = {
+				lotSize: { ComparisonOperator: 'GREATER_THAN', Value: 1000 },
+				yearBuilt: { ComparisonOperator: 'EQUAL', Value: 2000 },
+				bedrooms: { ComparisonOperator: 'LESS_THAN_OR_EQUAL', Value: 3 },
+				bathrooms: { ComparisonOperator: 'GREATER_THAN_OR_EQUAL', Value: 1 },
+				listPrice: { ComparisonOperator: 'LESS_THAN', Value: 700000 },
+			};
+
+			expect(matchesMetrics(message, matchingEvent)).toBe(true);
+			expect(matchesMetrics(message, nonMatchingEvent)).toBe(false);
+			expect(matchesMetrics(message, missingMetricsEvent)).toBe(false);
+			expect(matchesMetrics(message, noMetricsEvent)).toBe(false);
+
+			clearMemo();
+
+			message.Schedule.EventFilter.Dimensions.Metrics = {
+				lotSize: { ComparisonOperator: 'GREATER_OR_LESS_THAN', Value: 1000 },
+			};
+
+			expect(matchesMetrics(message, matchingEvent)).toBe(false);
+		});
+
+		test('memoizes matches', () => {
+			const event: InAppMessagingEvent = {
+				name: 'action.taken',
+				metrics: { lotSize: 2000 },
+			};
+			message.Schedule.EventFilter.Dimensions.Metrics = {
+				lotSize: { ComparisonOperator: 'GREATER_THAN', Value: 1000 },
+			};
+
+			expect(matchesMetrics(message, event)).toBe(true);
+
+			// This is a contrived way of validating the memo logic and should never happen in practice
+			message.Schedule.EventFilter.Dimensions.Metrics = {
+				lotSize: { ComparisonOperator: 'LESS_THAN', Value: 1000 },
+			};
+
+			expect(matchesMetrics(message, event)).toBe(true);
+
+			clearMemo();
+
+			expect(matchesMetrics(message, event)).toBe(false);
+		});
+	});
+
+	test('isBeforeEndDate checks if a message is still not yet at its end', () => {
+		const message = cloneDeep(pinpointInAppMessage);
+
+		expect(isBeforeEndDate(message)).toBe(false);
+
+		// Set the end date to 24 hours from now
+		message.Schedule.EndDate = new Date(
+			new Date().getTime() + HOUR_IN_MS * 24
+		).toISOString();
+
+		expect(isBeforeEndDate(message)).toBe(true);
+
+		message.Schedule.EndDate = null;
+
+		expect(isBeforeEndDate(message)).toBe(true);
+	});
+
+	test('extractContent extracts Pinpoint content into a normalized shape', () => {
+		const message = cloneDeep(pinpointInAppMessage);
+
+		expect(extractContent(message)).toStrictEqual(extractedContent);
+	});
+
+	test('extractMetadata extracts Pinpoint metadata into a flat object', () => {
+		const message = cloneDeep(pinpointInAppMessage);
+
+		expect(extractMetadata(message)).toStrictEqual(extractedMetadata);
+	});
+});
