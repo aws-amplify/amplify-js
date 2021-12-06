551baa166acfe202de7acf333843b2c57e3b8364
Chris F <5827964+cshfang@users.noreply.github.com>
2021-12-06 11:32:10 -0800

    feat(in-app-messaging): Add web support for session cap (#9308)
    * feat(in-app-messaging): Add web support for session cap
    
    * Add undefined check for document
    
    Co-authored-by: Chris Fang <chrfang@amazon.com>

--- packages/notifications/src/InAppMessaging/SessionTracker/SessionTracker.ts
+++ packages/notifications/src/InAppMessaging/SessionTracker/SessionTracker.ts
@@ -10,12 +10,71 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
-
+import { ConsoleLogger as Logger } from '@aws-amplify/core';
 import noop from 'lodash/noop';
-import { SessionStateChangeHandler, SessionTrackerInterface } from './types';
+import {
+	SessionState,
+	SessionStateChangeHandler,
+	SessionTrackerInterface,
+} from './types';
+
+// Per https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
+let hidden: string;
+let visibilityChange: string;
+
+if (document) {
+	if (typeof document.hidden !== 'undefined') {
+		hidden = 'hidden';
+		visibilityChange = 'visibilitychange';
+	} else if (typeof document['msHidden'] !== 'undefined') {
+		hidden = 'msHidden';
+		visibilityChange = 'msvisibilitychange';
+	} else if (typeof document['webkitHidden'] !== 'undefined') {
+		hidden = 'webkitHidden';
+		visibilityChange = 'webkitvisibilitychange';
+	}
+}
+
+const logger = new Logger('InAppMessagingSessionTracker');
 
 export default class SessionTracker implements SessionTrackerInterface {
-	constructor(sessionStateChangeHandler: SessionStateChangeHandler = noop) {}
-	start = noop as SessionTrackerInterface['start'];
-	end = noop as SessionTrackerInterface['end'];
+	private sessionStateChangeHandler: SessionStateChangeHandler;
+
+	constructor(sessionStateChangeHandler: SessionStateChangeHandler = noop) {
+		this.sessionStateChangeHandler = sessionStateChangeHandler;
+	}
+
+	start = (): SessionState => {
+		document?.addEventListener(visibilityChange, this.visibilityChangeHandler);
+		return this.getSessionState();
+	};
+
+	end = (): SessionState => {
+		document?.removeEventListener(
+			visibilityChange,
+			this.visibilityChangeHandler
+		);
+		return this.getSessionState();
+	};
+
+	private getSessionState = (): SessionState => {
+		if (document && !document[hidden]) {
+			return 'started';
+		}
+		// If, for any reason, document is undefined the session will never start
+		return 'ended';
+	};
+
+	private visibilityChangeHandler = () => {
+		if (!document) {
+			return;
+		}
+		if (document[hidden]) {
+			logger.debug('App is now hidden');
+			this.sessionStateChangeHandler('ended');
+		} else {
+			logger.debug('App is now visible');
+			this.sessionStateChangeHandler('started');
+		}
+	};
 }
