a808fd5b2b8fe7df0cc5511c764f8a2af73c0976
James Au <40404256+jamesaucode@users.noreply.github.com>
2022-02-10 12:39:09 -0800

    tsdoc update on @return

--- packages/api-graphql/src/GraphQLAPI.ts
+++ packages/api-graphql/src/GraphQLAPI.ts
@@ -221,7 +221,7 @@ export class GraphQLAPIClass {
 	 *
 	 * @param options - GraphQL Options
 	 * @param [additionalHeaders] - headers to merge in after any `graphql_headers` set in the config
-	 * @returns An Observable if subscription is true, else a promise of the graphql result from the query.
+	 * @returns An Observable if the query is a subscription query, else a promise of the graphql result.
 	 */
 	graphql<T = any>(
 		{ query: paramQuery, variables = {}, authMode, authToken }: GraphQLOptions,
