5b1946786707fed57518114c72eb3f1d03b659dc
chintannp <88387035+chintannp@users.noreply.github.com>
2022-03-28 11:42:26 -0400

    Removed version, size and description args from openDatabase and moved entrypoint files in folders

--- packages/datastore-storage-adapter/SQLiteAdapter.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-	module.exports = require('./dist/aws-amplify-datastore-storage-adapter.min.js');
-} else {
-	module.exports = require('./dist/aws-amplify-datastore-storage-adapter.js');
-}
