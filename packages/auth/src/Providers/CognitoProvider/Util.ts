2620ef8f336fd7b98a6fe540816eafa4410a5c56
James Au <auchu@amazon.com>
2022-04-06 13:09:55 -0700

    add decodeJWT util function

--- packages/auth/src/Providers/CognitoProvider/Util.ts
+++ packages/auth/src/Providers/CognitoProvider/Util.ts
@@ -12,3 +12,21 @@ export const dispatchAuthEvent = (
 ) => {
 	Hub.dispatch('auth', { event, data, message }, 'Auth', AMPLIFY_SYMBOL);
 };
+
+export const decodeJWT = (
+	token: string
+): { [key: string]: string | number | string[] } => {
+	try {
+		const base64URL = token.split('.')[1];
+		const base64 = base64URL.replace(/-/g, '+').replace(/_/g, '/');
+		const json = decodeURIComponent(
+			atob(base64)
+				.split('')
+				.map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+				.join('')
+		);
+		return JSON.parse(json);
+	} catch (err) {
+		return {};
+	}
+};
