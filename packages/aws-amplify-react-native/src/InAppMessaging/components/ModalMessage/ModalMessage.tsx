b698502eb653904816a23b623e8bf8538b05762e
Manoj NB <manojnb@amazon.com>
2022-05-05 12:59:08 -0700

    fix: self closing view

--- packages/aws-amplify-react-native/src/InAppMessaging/components/ModalMessage/ModalMessage.tsx
+++ packages/aws-amplify-react-native/src/InAppMessaging/components/ModalMessage/ModalMessage.tsx
@@ -38,7 +38,7 @@ export default function ModalMessage(props: ModalMessageProps) {
 		<MessageWrapper style={styles.componentWrapper}>
 			<View style={styles.container}>
 				<View style={styles.contentContainer}>
-					<View style={styles.iconButton.container}></View>
+					<View style={styles.iconButton.container} />
 					{hasRenderableImage && (
 						<View style={styles.imageContainer}>
 							<Image source={{ uri: image?.src }} style={styles.image} testID={IN_APP_MESSAGING.IMAGE} />
