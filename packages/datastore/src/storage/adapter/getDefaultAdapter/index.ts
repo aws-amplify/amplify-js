cbed6d49a41ccbe197300b5c3bd223ed26372383
Jon Wire <iambipedal@gmail.com>
2022-06-28 10:53:16 -0500

    getDefaultAdapter docstring

--- packages/datastore/src/storage/adapter/getDefaultAdapter/index.ts
+++ packages/datastore/src/storage/adapter/getDefaultAdapter/index.ts
@@ -3,6 +3,18 @@ import { Adapter } from '..';
 import IndexedDBAdapter from '../IndexedDBAdapter';
 import AsyncStorageAdapter from '../AsyncStorageAdapter';
 
+/**
+ * Explores the environment to determine which adapter should be used if the
+ * app does not provide one.
+ *
+ * Apps can provide a preferred adapter with:
+ *
+ * ```
+ * DataStore.configure({
+ * 	storageAdapter: Adapter
+ * });
+ * ```
+ */
 const getDefaultAdapter: () => Adapter = () => {
 	const { isBrowser } = browserOrNode();
 
