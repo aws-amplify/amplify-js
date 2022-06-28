60232bfbda1941bbac14984d54d60941c5b403fe
Jon Wire <iambipedal@gmail.com>
2022-06-28 15:12:29 -0500

    replicate docstrings to async storage adapter

--- packages/datastore/src/storage/adapter/AsyncStorageAdapter.ts
+++ packages/datastore/src/storage/adapter/AsyncStorageAdapter.ts
@@ -33,10 +33,38 @@ import {
 
 const logger = new Logger('DataStore');
 
+/**
+ * Adapter layer intended to sit between `StorageFacade` and AsyncStorageDatabase.
+ *
+ * @see AsyncStorageDatabase
+ */
 export class AsyncStorageAdapter implements Adapter {
 	private schema: InternalSchema;
+
+	/**
+	 * A function that accepts a Model constructor and determines the
+	 * namespace the model lives in.
+	 *
+	 * @see NamespaceResolver
+	 *
+	 * param modelConstructor — The Model constructor to inspects
+	 * @returns — The namespace as a string.
+	 */
 	private namespaceResolver: NamespaceResolver;
+
+	/**
+	 * Constructs a model and records it with its metadata in a weakset.
+	 * Allows for the separate storage of core model fields and
+	 * Amplify/DataStore metadata fields that the customer app does not want
+	 * exposed.
+	 *
+	 * @param modelConstructor — The model constructor.
+	 * @param init — Init data that would normally be passed to the
+	 * constructor.
+	 * @returns — The initialized model.
+	 */
 	private modelInstanceCreator: ModelInstanceCreator;
+
 	private getModelConstructorByModelName: (
 		namsespaceName: string,
 		modelName: string
@@ -46,6 +74,15 @@ export class AsyncStorageAdapter implements Adapter {
 	private resolve: (value?: any) => void;
 	private reject: (value?: any) => void;
 
+	/**
+	 * Derives the fully qualified name from a model constructor.
+	 *
+	 * This name should exactly equal the table name holding the model
+	 * instances in IndexedDB.
+	 *
+	 * @param modelConstructor A model instance constructor.
+	 * @returns The full table name for the given model.
+	 */
 	private getStorenameForModel(
 		modelConstructor: PersistentModelConstructor<any>
 	) {
@@ -55,12 +92,44 @@ export class AsyncStorageAdapter implements Adapter {
 		return this.getStorename(namespace, modelName);
 	}
 
+	/**
+	 * Given a namespace and model name, generates a fully qualified name.
+	 *
+	 * This name is intended to be used as the table name &mdash; and anywhere
+	 * else an unambiguous name is required for the given namespace-model.
+	 *
+	 * @param namespace The namespace to contextualize the name in. (E.g., USER, STORAGE, SYNC, etc.)
+	 * @param modelName The bare model name to derive from.
+	 * @returns A fully qualified table name.
+	 */
 	private getStorename(namespace: string, modelName: string) {
 		const storeName = `${namespace}_${modelName}`;
 
 		return storeName;
 	}
 
+	/**
+	 * Initializes a local database, no schema init.
+	 *
+	 * Also accepts methods that the adapter should use when determining
+	 * namespaces for fully qualified names, the reverse of this operation,
+	 * and a preferred function to use for model instantiation.
+	 *
+	 * Accepting a preferred function for model instantiation comes with
+	 * possible side effects. E.g., it is completely expected that the instance
+	 * creator function may keep a record metadata in a WeakMap for every
+	 * known Model instance.
+	 *
+	 * SIDE EFFECT:
+	 * 1. Potentially clobbers local database.
+	 * 1. Places the `resolve()` and `reject()` methods on `this` to use as
+	 * "cross-thread" communcation between `setUp()` attempts.
+	 *
+	 * @param theSchema
+	 * @param namespaceResolver
+	 * @param modelInstanceCreator
+	 * @param getModelConstructorByModelName
+	 */
 	async setUp(
 		theSchema: InternalSchema,
 		namespaceResolver: NamespaceResolver,
@@ -94,6 +163,16 @@ export class AsyncStorageAdapter implements Adapter {
 		}
 	}
 
+	/**
+	 * @see Adapter
+	 *
+	 * @param model The model instance to save.
+	 * @param condition The conditions under which the save should succeed.
+	 * Omission attempts to save unconditionally.
+	 * @returns The a tuple of the saved model and operation type
+	 * (INSERT/UPDATE) needed to affect the save. The returned model
+	 * includes the new `id` field if one had to be generated.
+	 */
 	async save<T extends PersistentModel>(
 		model: T,
 		condition?: ModelPredicate<T>
@@ -152,6 +231,14 @@ export class AsyncStorageAdapter implements Adapter {
 		return result;
 	}
 
+	/**
+	 * Attaches related HAS_ONE and BELONGS_TO records (non-recursively / only
+	 * ONE level deep) and instantiates a Model instance from the DTO.
+	 *
+	 * @param namespaceName The model namespace to extract schema info from.
+	 * @param srcModelName The model type name to look for schema info from.
+	 * @param records The instantiated Model records.
+	 */
 	private async load<T>(
 		namespaceName: string,
 		srcModelName: string,
@@ -228,6 +315,13 @@ export class AsyncStorageAdapter implements Adapter {
 		);
 	}
 
+	/**
+	 * @see Adapter
+	 * @param modelConstructor The model type to fetch.
+	 * @param predicate The conditions to filter by.
+	 * @param pagination The page start/size.
+	 * @returns Array of model instances.
+	 */
 	async query<T extends PersistentModel>(
 		modelConstructor: PersistentModelConstructor<T>,
 		predicate?: ModelPredicate<T>,
@@ -264,6 +358,15 @@ export class AsyncStorageAdapter implements Adapter {
 		return await this.load(namespaceName, modelConstructor.name, records);
 	}
 
+	/**
+	 * Looks for a particular model instance by PK from a particular table.
+	 *
+	 * @see getStorename
+	 *
+	 * @param storeName Fully qualified name.
+	 * @param id PK for the desired item.
+	 * @returns A model instance.
+	 */
 	private async getById<T extends PersistentModel>(
 		storeName: string,
 		id: string
@@ -272,12 +375,46 @@ export class AsyncStorageAdapter implements Adapter {
 		return record;
 	}
 
+	/**
+	 * Gets all model instances from a table.
+	 *
+	 * Does not fully instantiate the records. This must still be done with
+	 * `this.load()` on the objects prior to returning to the application.
+	 *
+	 * @param storeName Fully qualified name.
+	 * @returns Array of model instances.
+	 */
 	private async getAll<T extends PersistentModel>(
 		storeName: string
 	): Promise<T[]> {
 		return await this.db.getAll(storeName);
 	}
 
+	/**
+	 * Extracts the ID values form a predicate if it is AND ON ONLY IS an ID
+	 * equality predicate.
+	 *
+	 * For this:
+	 *
+	 * ```
+	 * p => p.id('eq', 'abc')
+	 * ```
+	 *
+	 * Returns `"abc"`.
+	 *
+	 * For any of these:
+	 *
+	 * ```
+	 * p => p.id('gt', 'abc');
+	 * p => p.id('eq', 'abc').otherField('eq', 'value');
+	 * p => p.otherField('eq', 'value')
+	 * ```
+	 *
+	 * Returns `false`.
+	 *
+	 * @param predicates The predicate to inspect.
+	 * @returns The ID value or `false`
+	 */
 	private idFromPredicate<T extends PersistentModel>(
 		predicates: PredicatesGroup<T>
 	) {
@@ -291,6 +428,19 @@ export class AsyncStorageAdapter implements Adapter {
 		return idPredicate && idPredicate.operand;
 	}
 
+	/**
+	 * Returns the array of model instances from a table matching predicate.
+	 *
+	 * This implementation currently requires a full table scan for all
+	 * predicates.
+	 *
+	 * NOTE: Automatic index leveraging is staged in the datastore-laziness
+	 * branch and will be added when laziness is merged.
+	 *
+	 * @param storeName Fully qualified name.
+	 * @param predicates A group of predicates to filter with.
+	 * @returns Array of matching model instances.
+	 */
 	private async filterOnPredicate<T extends PersistentModel>(
 		storeName: string,
 		predicates: PredicatesGroup<T>
@@ -306,6 +456,13 @@ export class AsyncStorageAdapter implements Adapter {
 		return filtered;
 	}
 
+	/**
+	 * Paginates a set of records that is already loaded fully in memory.
+	 *
+	 * @param records Array of records to paginate.
+	 * @param pagination Description of the page to fetch. Omit to fetch all.
+	 * @returns The specified subset (page) from the records.
+	 */
 	private inMemoryPagination<T extends PersistentModel>(
 		records: T[],
 		pagination?: PaginationInput<T>
@@ -331,6 +488,12 @@ export class AsyncStorageAdapter implements Adapter {
 		return records;
 	}
 
+	/**
+	 * @see Adapter
+	 * @param modelConstructor The model type to look for.
+	 * @param firstOrLast Whether to grab the first or last item in PK order.
+	 * @returns A single model instance or `undefined`
+	 */
 	async queryOne<T extends PersistentModel>(
 		modelConstructor: PersistentModelConstructor<T>,
 		firstOrLast: QueryOne = QueryOne.FIRST
@@ -340,6 +503,15 @@ export class AsyncStorageAdapter implements Adapter {
 		return result && this.modelInstanceCreator(modelConstructor, result);
 	}
 
+	/**
+	 * @see Adapter
+	 *
+	 * @param modelOrConstructor The model or model instance to delete.
+	 * @param condition The conditions under which the delete should succeed.
+	 * Omit to attempt to delete unconditionally. An unconditional delete
+	 * @returns A tuple of the model(s) attempted for deletion and the model(s)
+	 * actually deleted.
+	 */
 	async delete<T extends PersistentModel>(
 		modelOrModelConstructor: T | PersistentModelConstructor<T>,
 		condition?: ModelPredicate<T>
@@ -456,6 +628,17 @@ export class AsyncStorageAdapter implements Adapter {
 		}
 	}
 
+	/**
+	 * Deletes all items in the given queue **in-order**.
+	 *
+	 * It is expected that the given queue be given in toplogical order.
+	 *
+	 * SIDE EFFECT:
+	 * 1. Creates a transaction against the database.
+	 * 1. Changes database state/data.
+	 *
+	 * @param deleteQueue
+	 */
 	private async deleteItem<T extends PersistentModel>(
 		deleteQueue?: { storeName: string; items: T[] | IDBValidKey[] }[]
 	) {
@@ -472,13 +655,36 @@ export class AsyncStorageAdapter implements Adapter {
 			}
 		}
 	}
+
 	/**
-	 * Populates the delete Queue with all the items to delete
-	 * @param relations
-	 * @param models
-	 * @param srcModel
-	 * @param nameSpace
-	 * @param deleteQueue
+	 * Recursively collects a list of model instances that would need to be
+	 * deleted in addition to the *given* model records in order to maintain
+	 * referential integrity. Traversal is performed in a depth-first manner to
+	 * ensure items the `deleteQueue` is left in topological sort order.
+	 *
+	 * @todo Factor `relations` out of the signature *or* remove redundant
+	 * "relations" discovery internally.
+	 *
+	 * `relations` is passed in from a line that *consistently* looks like
+	 * this:
+	 *
+	 * ```
+	 * const relations =
+	 * 	this.schema.namespaces[nameSpace].relationships[modelConstructor.name]
+	 * 		.relationTypes;
+	 * ```
+	 *
+	 * This *appears* redundant, as this lookup is repeated internally for each
+	 * given relationship. If this is not actually redundant, replace this TODO
+	 * with an explanation as to why this is not actually redudant.
+	 *
+	 * @param relations The list of relations to treverse when looking for
+	 * models that would break referential integrity.
+	 * @param models The model instances in queue to be deleted.
+	 * @param srcModel The name of the source model.
+	 * @param nameSpace The namespace of the source model.
+	 * @param deleteQueue A queue to accumulate items that will need to be
+	 * deleted in order to perform a full, referentially consistent delete.
 	 */
 	private async deleteTraverse<T extends PersistentModel>(
 		relations: RelationType[],
@@ -566,6 +772,9 @@ export class AsyncStorageAdapter implements Adapter {
 		});
 	}
 
+	/**
+	 * @see Adapter
+	 */
 	async clear(): Promise<void> {
 		await this.db.clear();
 
@@ -573,6 +782,13 @@ export class AsyncStorageAdapter implements Adapter {
 		this.initPromise = undefined;
 	}
 
+	/**
+	 * @see Adapter
+	 *
+	 * @param modelConstructor Model type to save.
+	 * @param items Array of items to save.
+	 * @returns Array of tuples like [SavedItem, SaveOperation][]
+	 */
 	async batchSave<T extends PersistentModel>(
 		modelConstructor: PersistentModelConstructor<any>,
 		items: ModelInstanceMetadata[]
