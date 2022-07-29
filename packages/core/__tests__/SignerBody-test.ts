fc95ce3296a86a316041d631f3aeb11dfc86a6ad
Amelia Hill <amehi4yus@gmail.com>
2022-07-28 17:50:52 -0700

    Add throw error for body attribute on request object

--- packages/core/__tests__/SignerBody-test.ts
+++ packages/core/__tests__/SignerBody-test.ts
@@ -9,43 +9,101 @@ jest.mock('@aws-sdk/util-hex-encoding', () => ({
 }));
 
 describe('Signer test', () => {
-	describe('sign test', () => {
-		test('happy case', () => {
-			const url = 'https://host/some/path';
+	test('Should throw an Error when body attribute is passed to sign method', () => {
+		const url = 'https://host/some/path';
 
-			const request = {
-				url,
-				headers: {},
-			};
-			const access_info = {
-				session_token: 'session_token',
-			};
+		const request_body = {
+			url,
+			headers: {},
+			body: {},
+		};
 
-			const spyon = jest
-				.spyOn(Date.prototype, 'toISOString')
-				.mockReturnValueOnce('0');
+		const request_data = {
+			url,
+			headers: {},
+			data: {},
+		};
 
-			const getDateSpy = jest.spyOn(DateUtils, 'getDateWithClockOffset');
+		const access_info = {
+			session_token: 'session_token',
+		};
 
-			const res = {
-				headers: {
-					Authorization:
-						'AWS4-HMAC-SHA256 Credential=undefined/0/aregion/aservice/aws4_request, SignedHeaders=host;x-amz-date;x-amz-security-token, Signature=encrypt',
-					'X-Amz-Security-Token': 'session_token',
-					host: 'host',
-					'x-amz-date': '0',
-				},
-				url: url,
-			};
-			expect(
-				Signer.sign(request, access_info, {
-					service: 'aservice',
-					region: 'aregion',
-				})
-			).toEqual(res);
-			expect(getDateSpy).toHaveBeenCalledTimes(1);
+		const spyon = jest
+			.spyOn(Date.prototype, 'toISOString')
+			.mockReturnValueOnce('0');
 
-			spyon.mockClear();
-		});
+		const getDateSpy = jest.spyOn(DateUtils, 'getDateWithClockOffset');
+
+		const res = {
+			headers: {
+				Authorization:
+					'AWS4-HMAC-SHA256 Credential=undefined/0/aregion/aservice/aws4_request, 			SignedHeaders=host;x-amz-date;x-amz-security-token, Signature=encrypt',
+				'X-Amz-Security-Token': 'session_token',
+				host: 'host',
+				'x-amz-date': '0',
+			},
+			url: url,
+			data: request_data.data,
+		};
+
+		expect(() => {
+			Signer.sign(request_body, access_info, {
+				service: 'aservice',
+				region: 'aregion',
+			});
+		}).toThrowError(
+			'The attribute "body" was found on the request object. Please use the attribute "data" instead.'
+		);
+
+		expect(getDateSpy).toHaveBeenCalledTimes(0);
+		spyon.mockClear();
+	});
+
+	test('Should NOT throw an Error when data attribute is passed to sign method', () => {
+		const url = 'https://host/some/path';
+
+		const request_body = {
+			url,
+			headers: {},
+			data: {},
+		};
+
+		const request_data = {
+			url,
+			headers: {},
+			data: {},
+		};
+
+		const access_info = {
+			session_token: 'session_token',
+		};
+
+		const spyon = jest
+			.spyOn(Date.prototype, 'toISOString')
+			.mockReturnValueOnce('0');
+
+		const getDateSpy = jest.spyOn(DateUtils, 'getDateWithClockOffset');
+
+		const res = {
+			headers: {
+				Authorization:
+					'AWS4-HMAC-SHA256 Credential=undefined/0/aregion/aservice/aws4_request, 					SignedHeaders=host;x-amz-date;x-amz-security-token, Signature=encrypt',
+				'X-Amz-Security-Token': 'session_token',
+				host: 'host',
+				'x-amz-date': '0',
+			},
+			url: url,
+			data: request_data.data,
+		};
+
+		expect(() => {
+			Signer.sign(request_data, access_info, {
+				service: 'aservice',
+				region: 'aregion',
+			});
+		}).not.toThrowError();
+
+		expect(getDateSpy).toHaveBeenCalledTimes(1);
+		spyon.mockClear();
 	});
 });
