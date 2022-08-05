2ebc19dde4526d5072ee6311c1f0bba54df4e200
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2022-08-04 20:50:46 -0700

    revert changes to authentication machine type

--- packages/auth/src/Providers/CognitoProvider/types/machines/authenticationMachine.ts
+++ packages/auth/src/Providers/CognitoProvider/types/machines/authenticationMachine.ts
@@ -3,12 +3,14 @@ import { CognitoService } from '../../serviceClass';
 import { AmplifyUser } from '../../../../types';
 import { ActorRefFrom } from 'xstate';
 import { signInMachine } from '../../machines/signInMachine';
+import { signUpMachine } from '../../machines/signUpMachine';
 
 export type SignInActorRef = ActorRefFrom<typeof signInMachine>;
+export type SignUpActorRef = ActorRefFrom<typeof signUpMachine>;
 
 export interface AuthMachineContext {
 	// TODO: union other valid actor refs here when we add more actors
-	actorRef?: SignInActorRef;
+	actorRef?: SignInActorRef | SignUpActorRef;
 	config: null | CognitoProviderConfig;
 	service: null | CognitoService;
 	session?: AmplifyUser;
@@ -50,6 +52,13 @@ export type AuthTypestate =
 			context: AuthMachineContext;
 	  }
 	| { value: 'error'; context: AuthMachineContext }
+	| {
+			value: 'signedUp';
+			context: AuthMachineContext & {
+				config: CognitoProviderConfig;
+				service: CognitoService;
+			};
+	  }
 	| {
 			value: 'signingIn';
 			context: AuthMachineContext & {
