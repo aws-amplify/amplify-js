8f078178511eeacc2b8f2b699cb58105fdb58d37
Sridhar <ashwsrir@bcd07413f71a.ant.amazon.com>
2022-08-22 17:52:13 -0700

    fix(interactions): cleanup unused imports

--- packages/interactions/src/types/Providers/AWSLexProvider.ts
+++ packages/interactions/src/types/Providers/AWSLexProvider.ts
@@ -10,11 +10,6 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
-import {
-	PostTextCommandOutput,
-	PostContentCommandOutput,
-} from '@aws-sdk/client-lex-runtime-service';
-
 export interface AWSLexProviderOption {
 	name: string;
 	alias: string;
