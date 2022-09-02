f5eefe22c6c23cd0df3a9eec4ca82d5a7facbea2
Israel <israel.arcos2595@gmail.com>
2022-09-02 15:57:06 -0400

    refactor: add tsconfig file as a third parameter to build.js

--- packages/auth/build.js
+++ packages/auth/build.js
@@ -2,4 +2,8 @@
 
 const build = require('../../scripts/build');
 const tsconfig = require('./tsconfig.json');
+<<<<<<< HEAD
+=======
+
+>>>>>>> df5ad308b (refactor: add tsconfig file as a third parameter to build.js)
 build(process.argv[2], process.argv[3], tsconfig);
