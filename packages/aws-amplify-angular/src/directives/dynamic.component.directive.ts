3dfe2291ead8d77384f01ba6f52761cf0815e8b5
Dustin Noyes <dustin.noyes.dev@gmail.com>
2022-09-12 15:18:03 -0700

    chore/removes ui packages + fixes build issues (#10313)

--- packages/aws-amplify-angular/src/directives/dynamic.component.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-	selector: '[component-host]',
-})
-export class DynamicComponentDirective {
-	constructor(public viewContainerRef: ViewContainerRef) {}
-}
