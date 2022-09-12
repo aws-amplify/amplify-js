7f691ac6c0a626fbe0578a2abf58ff55651ab992
Dustin Noyes <dnnoyes@amazon.com>
2022-09-12 14:38:11 -0700

    chore/removes ui packages + fixes build issues

--- packages/aws-amplify-angular/index.ts
+++
@@ -1,3 +0,0 @@
-export { AmplifyAngularModule } from './src/aws-amplify-angular.module';
-export { AmplifyIonicModule } from './src/aws-amplify-ionic-module';
-export { AmplifyService, AmplifyModules } from './src/providers';
