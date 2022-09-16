7fa351d07227c871ea4186f773c2332fd8947486
Israel Arcos <israel.arcos2595@gmail.com>
2022-09-16 09:43:57 -0400

    fix: remove unsued code

--- packages/auth/build.js
+++ packages/auth/build.js
@@ -2,8 +2,4 @@
 
 const build = require('../../scripts/build');
 const tsconfig = require('./tsconfig.json');
-<<<<<<< HEAD
-=======
-
->>>>>>> df5ad308b (refactor: add tsconfig file as a third parameter to build.js)
-build(process.argv[2], process.argv[3], tsconfig);
+build(process.argv[2], process.argv[3]);
