c6a7bd94710ec93ebd58ef7bf34f726d270eb406
Chris Fang <chrfang@amazon.com>
2022-09-29 16:29:11 -0700

    fix(in-app-messaging): add third party lib to transform for jest

--- packages/aws-amplify-react-native/jest.config.js
+++ packages/aws-amplify-react-native/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	preset: 'react-native',
 	modulePathIgnorePatterns: ['<rootDir>/dist/'],
+	transformIgnorePatterns: ['node_modules/(?!(@react-native|react-native|react-native-safe-area-context)/)'],
 	collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/src/**/*{c,C}onstants.ts'],
 	coverageThreshold: {
 		global: {
