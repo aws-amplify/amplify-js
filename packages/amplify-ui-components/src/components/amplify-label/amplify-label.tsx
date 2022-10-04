bcf4498cc9ecfdeade2e772dfa28c0096fdf438c
Caleb Pollman <cpollman@amazon.com>
2022-10-04 13:00:02 -0700

    chore(legacy-ui): add deprecation warnings (#10384)
    <!--
    Please make sure to read the Pull Request Guidelines:
    
    https://github.com/aws-amplify/amplify-js/blob/main/CONTRIBUTING.md#pull-requests
    -->
    
    #### Description of changes
    Add deprecation warning messages for legacy UI packages and remove
    `private: true` where applicable.
    
    **Legacy UI Packages Exposing Messages**
    
    For the following packages, messages were added directly in their
    primary export file. The messages will be triggered on any import from
    the packages.
    
    - _aws-amplify-angular_
    - _aws-amplify-react_
    - _aws-amplify-vue_
    
    
    In the below packages, messages were added to the `stencil` UI
    components in _@aws-amplify/ui-components_ (in the `componentWillLoad`
    method) used to code-gen their respective contents.
    
    - _@aws-amplify/ui-angular_
    - _@aws-amplify/ui-react_
    - _@aws-amplify/ui-vue_
    
    Placement of the messages in `componentWillLoad` will trigger messages
    during usage of the components. The deviation in the message handling
    from the non-code-gen packages due to:
    - the legacy _@aws-amplify/ui-react_ (`1.x`) package sharing the
    "@aws-amplify/ui-react" namespace with the current
    _@aws-amplify/ui-react_ (`2.x`+) package
    - the current _@aws-amplify/ui-react_ packages including the legacy
    _@aws-amplify/ui-react_ package in [its
    dependecies](https://github.com/aws-amplify/amplify-ui/blob/main/packages/react/package.json#L59),
    and [re-exporting the
    contents](https://github.com/aws-amplify/amplify-ui/blob/main/packages/react/src/legacy.tsx)
    as _@aws-amplify/ui-react/legacy_
    
    **Legacy UI Packages Not Exposing Messages**
    - _@aws-amplify/ui_ - exports CSS files used in the Legacy UI packages
    - _@aws-amplify/ui-storybook_ - private package
    <!--
    Thank you for your Pull Request! Please provide a description above and
    review
    the requirements below.
    -->
    
    
    #### Issue #, if available
    <!-- Also, please reference any associated PRs for documentation
    updates. -->
    NA
    
    
    #### Description of how you validated changes
    
    Manually tested using `@legacy-ui` tagged release
    - ✅ _aws-amplify-angular_
    - ✅ _aws-amplify-react_
    - ✅ _aws-amplify-vue_
    
    - ✅ _@aws-amplify/ui-components_ (via below packages)
      - ✅ _@aws-amplify/ui-react_ 
      - ✅ _@aws-amplify/ui-angular_ 
      - ✅ _@aws-amplify/ui-vue_ 
    
    
    #### Checklist
    <!-- Remove items that do not apply. For completed items, change [ ] to
    [x]. -->
    
    - [ ] PR description included
    - [ ] `yarn test` passes
    - [ ] Tests are [changed or
    added](https://github.com/aws-amplify/amplify-js/blob/main/CONTRIBUTING.md#steps-towards-contributions)
    - [ ] Relevant documentation is changed or added (and PR referenced)
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

--- packages/amplify-ui-components/src/components/amplify-label/amplify-label.tsx
+++ packages/amplify-ui-components/src/components/amplify-label/amplify-label.tsx
@@ -11,6 +11,12 @@ export class AmplifyLabel {
 	/** Reflects the value of the for content property of html element */
 	@Prop() htmlFor: string;
 
+	componentWillLoad() {
+		console.warn(
+			'Version `1.x` of Amplify UI has been deprecated and will be removed in a future major version of `aws-amplify`. Please visit https://ui.docs.amplify.aws/ for the current version of Amplify UI.'
+		);
+	}
+
 	render() {
 		return (
 			<label class="label" htmlFor={this.htmlFor}>
