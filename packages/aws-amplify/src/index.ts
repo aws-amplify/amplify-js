c2c58361d64d6f6aaff4bce2a336c6a60a868153
elorzafe <elorzafe@amazon.com>
2022-11-03 17:52:24 -0700

    Removing sideEffect from aws-amplify

--- packages/aws-amplify/src/index.ts
+++ packages/aws-amplify/src/index.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export { Amplify } from './amplifySingleton';
+export { Amplify } from '@aws-amplify/core';
 export {
 	Analytics,
 	AnalyticsProvider,
