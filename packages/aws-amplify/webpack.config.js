9b81d553824910cdfa662d721f9dbb967fac7a7b
Allan Zheng <zheallan@amazon.com>
2022-11-17 16:23:07 -0800

    chore(aws-amplify): only ship bundled code in aws-amplify

--- packages/aws-amplify/webpack.config.js
+++ packages/aws-amplify/webpack.config.js
@@ -2,24 +2,6 @@ module.exports = {
 	entry: {
 		'aws-amplify.min': './lib-esm/index.js',
 	},
-	externals: [
-		{
-			'@aws-amplify/pubsub': 'aws_amplify_pubsub',
-			'@aws-amplify/cache': 'aws_amplify_cache',
-			'@aws-amplify/auth': 'aws_amplify_auth',
-			'@aws-amplify/core': 'aws_amplify_core',
-			'@aws-amplify/api': 'aws_amplify_api',
-			'@aws-amplify/api-graphql': 'aws_amplify_api-graphql',
-			'@aws-amplify/datastore': 'aws_amplify_datastore',
-			'@aws-amplify/interactions': 'aws_amplify_interactions',
-			'@aws-amplify/geo': 'aws_amplify_geo',
-			'@aws-amplify/storage': 'aws_amplify_storage',
-			'@aws-amplify/analytics': 'aws_amplify_analytics',
-			'@aws-amplify/predictions': 'aws_amplify_predictions',
-			'@aws-amplify/notifications': 'aws_amplify_notifications',
-			'@aws-amplify/xr': 'aws_amplify_xr',
-		},
-	],
 	output: {
 		filename: '[name].js',
 		path: __dirname + '/dist',
