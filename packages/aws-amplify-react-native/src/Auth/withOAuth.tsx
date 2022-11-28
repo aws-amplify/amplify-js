59e3350152a6ac47e7d828f23853f82ba5973f04
Jon Wire <iambipedal@gmail.com>
2022-11-28 16:56:12 -0600

    chore: undo auto-format of non-DS files

--- packages/aws-amplify-react-native/src/Auth/withOAuth.tsx
+++ packages/aws-amplify-react-native/src/Auth/withOAuth.tsx
@@ -14,7 +14,10 @@ import * as React from 'react';
 import { Linking } from 'react-native';
 
 import { Logger, Hub } from '@aws-amplify/core';
-import { Auth, CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
+import {
+	Auth,
+	CognitoHostedUIIdentityProvider,
+} from '@aws-amplify/auth';
 
 const logger = new Logger('withOAuth');
 
@@ -41,10 +44,15 @@ interface IWithOAuthState {
 	user?: any;
 }
 
-export default function withOAuth<Props extends object>(Comp: React.ComponentType<IWithOAuthProps & IOAuthProps>) {
+export default function withOAuth<Props extends object>(
+	Comp: React.ComponentType<IWithOAuthProps & IOAuthProps>
+) {
 	let listeners = [];
 
-	return class WithOAuth extends React.Component<Props & IWithOAuthProps, IWithOAuthState> {
+	return class WithOAuth extends React.Component<
+		Props & IWithOAuthProps,
+		IWithOAuthState
+	> {
 		_isMounted: boolean;
 		urlOpener: any;
 
@@ -67,7 +75,7 @@ export default function withOAuth<Props extends object>(Comp: React.ComponentTyp
 				loading: false,
 			};
 
-			listeners.forEach((listener) => Hub.remove('auth', listener));
+			listeners.forEach(listener => Hub.remove('auth', listener));
 			listeners = [this];
 			this.onHubCapsule = this.onHubCapsule.bind(this);
 			Hub.listen('auth', this.onHubCapsule);
@@ -77,10 +85,10 @@ export default function withOAuth<Props extends object>(Comp: React.ComponentTyp
 			this._isMounted = true;
 			this.setState({ loading: true }, () => {
 				Auth.currentAuthenticatedUser()
-					.then((user) => {
+					.then(user => {
 						this.setState({ user, loading: false });
 					})
-					.catch((error) => {
+					.catch(error => {
 						logger.debug(error);
 						this.setState({ user: null, loading: false });
 					});
@@ -99,7 +107,7 @@ export default function withOAuth<Props extends object>(Comp: React.ComponentTyp
 				switch (payload.event) {
 					case 'signIn':
 					case 'cognitoHostedUI': {
-						Auth.currentAuthenticatedUser().then((user) => {
+						Auth.currentAuthenticatedUser().then(user => {
 							logger.debug('signed in');
 							this.setState({
 								user,
@@ -136,7 +144,9 @@ export default function withOAuth<Props extends object>(Comp: React.ComponentTyp
 
 		_getOAuthConfig() {
 			if (!Auth || typeof Auth.configure !== 'function') {
-				throw new Error('No Auth module found, please ensure @aws-amplify/auth is imported');
+				throw new Error(
+					'No Auth module found, please ensure @aws-amplify/auth is imported'
+				);
 			}
 
 			// @ts-ignore
@@ -152,11 +162,13 @@ export default function withOAuth<Props extends object>(Comp: React.ComponentTyp
 		}
 
 		hostedUISignIn(provider) {
-			this.setState({ loading: true }, () => Auth.federatedSignIn({ provider }));
+			this.setState({ loading: true }, () =>
+				Auth.federatedSignIn({ provider })
+			);
 		}
 
 		signOut() {
-			return Auth.signOut().catch((error) => logger.warn(error));
+			return Auth.signOut().catch(error => logger.warn(error));
 		}
 
 		render() {
@@ -167,11 +179,23 @@ export default function withOAuth<Props extends object>(Comp: React.ComponentTyp
 				loading,
 				oAuthUser,
 				oAuthError,
-				hostedUISignIn: this.hostedUISignIn.bind(this, CognitoHostedUIIdentityProvider.Cognito),
-				facebookSignIn: this.hostedUISignIn.bind(this, CognitoHostedUIIdentityProvider.Facebook),
-				amazonSignIn: this.hostedUISignIn.bind(this, CognitoHostedUIIdentityProvider.Amazon),
-				googleSignIn: this.hostedUISignIn.bind(this, CognitoHostedUIIdentityProvider.Google),
-				customProviderSignIn: (provider) => this.hostedUISignIn(provider),
+				hostedUISignIn: this.hostedUISignIn.bind(
+					this,
+					CognitoHostedUIIdentityProvider.Cognito
+				),
+				facebookSignIn: this.hostedUISignIn.bind(
+					this,
+					CognitoHostedUIIdentityProvider.Facebook
+				),
+				amazonSignIn: this.hostedUISignIn.bind(
+					this,
+					CognitoHostedUIIdentityProvider.Amazon
+				),
+				googleSignIn: this.hostedUISignIn.bind(
+					this,
+					CognitoHostedUIIdentityProvider.Google
+				),
+				customProviderSignIn: provider => this.hostedUISignIn(provider),
 				signOut: this.signOut,
 			};
 
