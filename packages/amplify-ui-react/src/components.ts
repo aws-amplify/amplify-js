d249efe4ac5d82360660a1d7445c6c19f6c3b82e
Manoj NB <manojnb@amazon.com>
2022-11-28 16:34:23 -0800

    chore: leveling with upstream

---
+++ packages/amplify-ui-react/src/components.ts
@@ -0,0 +1,65 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from '@aws-amplify/ui-components';
+
+import { defineCustomElements, applyPolyfills } from '@aws-amplify/ui-components/loader';
+
+applyPolyfills().then(() => defineCustomElements());
+export const AmplifyAmazonButton = /*@__PURE__*/createReactComponent<JSX.AmplifyAmazonButton, HTMLAmplifyAmazonButtonElement>('amplify-amazon-button');
+export const AmplifyAuthContainer = /*@__PURE__*/createReactComponent<JSX.AmplifyAuthContainer, HTMLAmplifyAuthContainerElement>('amplify-auth-container');
+export const AmplifyAuthFields = /*@__PURE__*/createReactComponent<JSX.AmplifyAuthFields, HTMLAmplifyAuthFieldsElement>('amplify-auth-fields');
+export const AmplifyAuth0Button = /*@__PURE__*/createReactComponent<JSX.AmplifyAuth0Button, HTMLAmplifyAuth0ButtonElement>('amplify-auth0-button');
+export const AmplifyAuthenticator = /*@__PURE__*/createReactComponent<JSX.AmplifyAuthenticator, HTMLAmplifyAuthenticatorElement>('amplify-authenticator');
+export const AmplifyButton = /*@__PURE__*/createReactComponent<JSX.AmplifyButton, HTMLAmplifyButtonElement>('amplify-button');
+export const AmplifyChatbot = /*@__PURE__*/createReactComponent<JSX.AmplifyChatbot, HTMLAmplifyChatbotElement>('amplify-chatbot');
+export const AmplifyCheckbox = /*@__PURE__*/createReactComponent<JSX.AmplifyCheckbox, HTMLAmplifyCheckboxElement>('amplify-checkbox');
+export const AmplifyCodeField = /*@__PURE__*/createReactComponent<JSX.AmplifyCodeField, HTMLAmplifyCodeFieldElement>('amplify-code-field');
+export const AmplifyConfirmSignIn = /*@__PURE__*/createReactComponent<JSX.AmplifyConfirmSignIn, HTMLAmplifyConfirmSignInElement>('amplify-confirm-sign-in');
+export const AmplifyConfirmSignUp = /*@__PURE__*/createReactComponent<JSX.AmplifyConfirmSignUp, HTMLAmplifyConfirmSignUpElement>('amplify-confirm-sign-up');
+export const AmplifyContainer = /*@__PURE__*/createReactComponent<JSX.AmplifyContainer, HTMLAmplifyContainerElement>('amplify-container');
+export const AmplifyCountryDialCode = /*@__PURE__*/createReactComponent<JSX.AmplifyCountryDialCode, HTMLAmplifyCountryDialCodeElement>('amplify-country-dial-code');
+export const AmplifyEmailField = /*@__PURE__*/createReactComponent<JSX.AmplifyEmailField, HTMLAmplifyEmailFieldElement>('amplify-email-field');
+export const AmplifyFacebookButton = /*@__PURE__*/createReactComponent<JSX.AmplifyFacebookButton, HTMLAmplifyFacebookButtonElement>('amplify-facebook-button');
+export const AmplifyFederatedButtons = /*@__PURE__*/createReactComponent<JSX.AmplifyFederatedButtons, HTMLAmplifyFederatedButtonsElement>('amplify-federated-buttons');
+export const AmplifyFederatedSignIn = /*@__PURE__*/createReactComponent<JSX.AmplifyFederatedSignIn, HTMLAmplifyFederatedSignInElement>('amplify-federated-sign-in');
+export const AmplifyForgotPassword = /*@__PURE__*/createReactComponent<JSX.AmplifyForgotPassword, HTMLAmplifyForgotPasswordElement>('amplify-forgot-password');
+export const AmplifyFormField = /*@__PURE__*/createReactComponent<JSX.AmplifyFormField, HTMLAmplifyFormFieldElement>('amplify-form-field');
+export const AmplifyFormSection = /*@__PURE__*/createReactComponent<JSX.AmplifyFormSection, HTMLAmplifyFormSectionElement>('amplify-form-section');
+export const AmplifyGoogleButton = /*@__PURE__*/createReactComponent<JSX.AmplifyGoogleButton, HTMLAmplifyGoogleButtonElement>('amplify-google-button');
+export const AmplifyGreetings = /*@__PURE__*/createReactComponent<JSX.AmplifyGreetings, HTMLAmplifyGreetingsElement>('amplify-greetings');
+export const AmplifyHint = /*@__PURE__*/createReactComponent<JSX.AmplifyHint, HTMLAmplifyHintElement>('amplify-hint');
+export const AmplifyIcon = /*@__PURE__*/createReactComponent<JSX.AmplifyIcon, HTMLAmplifyIconElement>('amplify-icon');
+export const AmplifyIconButton = /*@__PURE__*/createReactComponent<JSX.AmplifyIconButton, HTMLAmplifyIconButtonElement>('amplify-icon-button');
+export const AmplifyInput = /*@__PURE__*/createReactComponent<JSX.AmplifyInput, HTMLAmplifyInputElement>('amplify-input');
+export const AmplifyLabel = /*@__PURE__*/createReactComponent<JSX.AmplifyLabel, HTMLAmplifyLabelElement>('amplify-label');
+export const AmplifyLink = /*@__PURE__*/createReactComponent<JSX.AmplifyLink, HTMLAmplifyLinkElement>('amplify-link');
+export const AmplifyLoadingSpinner = /*@__PURE__*/createReactComponent<JSX.AmplifyLoadingSpinner, HTMLAmplifyLoadingSpinnerElement>('amplify-loading-spinner');
+export const AmplifyNav = /*@__PURE__*/createReactComponent<JSX.AmplifyNav, HTMLAmplifyNavElement>('amplify-nav');
+export const AmplifyOauthButton = /*@__PURE__*/createReactComponent<JSX.AmplifyOauthButton, HTMLAmplifyOauthButtonElement>('amplify-oauth-button');
+export const AmplifyPasswordField = /*@__PURE__*/createReactComponent<JSX.AmplifyPasswordField, HTMLAmplifyPasswordFieldElement>('amplify-password-field');
+export const AmplifyPhoneField = /*@__PURE__*/createReactComponent<JSX.AmplifyPhoneField, HTMLAmplifyPhoneFieldElement>('amplify-phone-field');
+export const AmplifyPhotoPicker = /*@__PURE__*/createReactComponent<JSX.AmplifyPhotoPicker, HTMLAmplifyPhotoPickerElement>('amplify-photo-picker');
+export const AmplifyPicker = /*@__PURE__*/createReactComponent<JSX.AmplifyPicker, HTMLAmplifyPickerElement>('amplify-picker');
+export const AmplifyRadioButton = /*@__PURE__*/createReactComponent<JSX.AmplifyRadioButton, HTMLAmplifyRadioButtonElement>('amplify-radio-button');
+export const AmplifyRequireNewPassword = /*@__PURE__*/createReactComponent<JSX.AmplifyRequireNewPassword, HTMLAmplifyRequireNewPasswordElement>('amplify-require-new-password');
+export const AmplifyS3Album = /*@__PURE__*/createReactComponent<JSX.AmplifyS3Album, HTMLAmplifyS3AlbumElement>('amplify-s3-album');
+export const AmplifyS3Image = /*@__PURE__*/createReactComponent<JSX.AmplifyS3Image, HTMLAmplifyS3ImageElement>('amplify-s3-image');
+export const AmplifyS3ImagePicker = /*@__PURE__*/createReactComponent<JSX.AmplifyS3ImagePicker, HTMLAmplifyS3ImagePickerElement>('amplify-s3-image-picker');
+export const AmplifyS3Text = /*@__PURE__*/createReactComponent<JSX.AmplifyS3Text, HTMLAmplifyS3TextElement>('amplify-s3-text');
+export const AmplifyS3TextPicker = /*@__PURE__*/createReactComponent<JSX.AmplifyS3TextPicker, HTMLAmplifyS3TextPickerElement>('amplify-s3-text-picker');
+export const AmplifySection = /*@__PURE__*/createReactComponent<JSX.AmplifySection, HTMLAmplifySectionElement>('amplify-section');
+export const AmplifySelect = /*@__PURE__*/createReactComponent<JSX.AmplifySelect, HTMLAmplifySelectElement>('amplify-select');
+export const AmplifySelectMfaType = /*@__PURE__*/createReactComponent<JSX.AmplifySelectMfaType, HTMLAmplifySelectMfaTypeElement>('amplify-select-mfa-type');
+export const AmplifySignIn = /*@__PURE__*/createReactComponent<JSX.AmplifySignIn, HTMLAmplifySignInElement>('amplify-sign-in');
+export const AmplifySignInButton = /*@__PURE__*/createReactComponent<JSX.AmplifySignInButton, HTMLAmplifySignInButtonElement>('amplify-sign-in-button');
+export const AmplifySignOut = /*@__PURE__*/createReactComponent<JSX.AmplifySignOut, HTMLAmplifySignOutElement>('amplify-sign-out');
+export const AmplifySignUp = /*@__PURE__*/createReactComponent<JSX.AmplifySignUp, HTMLAmplifySignUpElement>('amplify-sign-up');
+export const AmplifyStrike = /*@__PURE__*/createReactComponent<JSX.AmplifyStrike, HTMLAmplifyStrikeElement>('amplify-strike');
+export const AmplifyToast = /*@__PURE__*/createReactComponent<JSX.AmplifyToast, HTMLAmplifyToastElement>('amplify-toast');
+export const AmplifyTooltip = /*@__PURE__*/createReactComponent<JSX.AmplifyTooltip, HTMLAmplifyTooltipElement>('amplify-tooltip');
+export const AmplifyTotpSetup = /*@__PURE__*/createReactComponent<JSX.AmplifyTotpSetup, HTMLAmplifyTotpSetupElement>('amplify-totp-setup');
+export const AmplifyUsernameField = /*@__PURE__*/createReactComponent<JSX.AmplifyUsernameField, HTMLAmplifyUsernameFieldElement>('amplify-username-field');
+export const AmplifyVerifyContact = /*@__PURE__*/createReactComponent<JSX.AmplifyVerifyContact, HTMLAmplifyVerifyContactElement>('amplify-verify-contact');
