07ca263838396f3cd1411fb80daf1100af506732
helgabalashova <helga.stolyarova@gmail.com>
2022-12-14 17:27:11 -0700

    fixes import

--- packages/auth/src/types/options/index.ts
+++ packages/auth/src/types/options/index.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { AuthSignUpOptions } from './AuthSignUpOptions';
+import { AuthSignUpOptions } from './authSignUpOptions';
 
 export {
 	AuthSignUpOptions
