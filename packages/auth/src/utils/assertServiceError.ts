1eeed7ae5d9976ef610b00bbd2cdd80d5e22fb69
Israel Arcos <israel.arcos2595@gmail.com>
2022-12-22 15:41:57 -0500

    chore: update assert service error logic

--- packages/auth/src/utils/assertServiceError.ts
+++ packages/auth/src/utils/assertServiceError.ts
@@ -9,8 +9,8 @@ export function assertServiceError(
 	error: unknown
 ): asserts error is ServiceError {
 	if (
-		!error &&
-		!(error as ServiceError).name &&
+		!error ||
+		!(error as ServiceError).name ||
 		!(error as ServiceError).message
 	) {
 		throw new AuthError({
