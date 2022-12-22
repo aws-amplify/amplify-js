01145528771dd22b28088c8a8c88165965da6911
helgabalashova <helga.stolyarova@gmail.com>
2022-12-21 17:16:44 -0700

    addressed comments

--- packages/auth/__tests__/auth-cognito-provider-unit-test.ts
+++ packages/auth/__tests__/auth-cognito-provider-unit-test.ts
@@ -7,6 +7,7 @@ import {
 	AuthPluginOptions, 
 	AuthSignUpResult, 
 	AuthSignUpStep, 
+	AuthUserAttribute, 
 	CognitoSignUpOptions, 
 	CognitoUserAttributeKey, 
 	SignUpRequest 
@@ -35,16 +36,22 @@ const noUserPoolConfig = {
 	region: 'region'
 };
 
-const signUpRequest: SignUpRequest<CognitoUserAttributeKey, AuthPluginOptions> = {
-	username: 'username',
-	password: 'password',
-	options: {
-		userAttributes: [
-			{userAttributeKey: 'email', value: 'email@email.com'},
-			{userAttributeKey: 'phone_number', value: '+12345678910'}
-		]
-	}
+const createSignUpRequest = (
+	username, 
+	password, 
+	options
+): SignUpRequest<CognitoUserAttributeKey, AuthPluginOptions> => {
+	return {
+		username,
+		password,
+		options
+	};
 };
+
+const userAttributes: AuthUserAttribute<CognitoUserAttributeKey>[] = [
+	{userAttributeKey: 'email', value: 'email@email.com'},
+	{userAttributeKey: 'phone_number', value: '+12345678910'}
+];
 
 const signUpResult: AuthSignUpResult<CognitoUserAttributeKey> = {
 	isSignUpComplete: true,
@@ -53,18 +60,32 @@ const signUpResult: AuthSignUpResult<CognitoUserAttributeKey> = {
 	}
 };
 
+const signUpResultUnconfirmedUser = {
+	isSignUpComplete: false,
+	nextStep: {
+		codeDeliveryDetails: {
+			deliveryMedium: 'deliveryMedium',
+			destination: 'destination',
+			attributeName: 'attrName'
+		},
+		signUpStep: 'CONFIRM_SIGN_UP'
+	}
+};
+
 const provider = new AmazonCognitoProvider(config);
 
 describe('Amazon Cognito Auth unit test', () => {
+	const spyon = jest.spyOn(CognitoIdentityProviderClient.prototype, 'send');
+
 	afterEach(() => {
 		jest.clearAllMocks();
 	});
 
 	describe('Sign up', () => {
 		test('happy case with user attributes', async () => {
-			const spyon = jest.spyOn(CognitoIdentityProviderClient.prototype, 'send');
 			Auth.addPluggable(provider);
-			const result = await Auth.signUp(signUpRequest);
+			const request = createSignUpRequest('username', 'password', { userAttributes });
+			const result = await Auth.signUp(request);
 			const commandObject = {
 				input: {
 					ClientId: 'userPoolId',
@@ -79,8 +100,8 @@ describe('Amazon Cognito Auth unit test', () => {
 			expect(CognitoIdentityProviderClient.prototype.send).toBeCalled();
 			expect(CognitoIdentityProviderClient.prototype.send).toBeCalledWith(expect.objectContaining(commandObject));
 			expect(result).toEqual(signUpResult);
-			spyon.mockClear();
 		});
+
 		test('happy case with validation data', async () => {
 			Auth.addPluggable(provider);
 			const pluginOptions: CognitoSignUpOptions = {
@@ -88,18 +109,11 @@ describe('Amazon Cognito Auth unit test', () => {
 					foo: 'bar'
 				}
 			};
-			const request: SignUpRequest<CognitoUserAttributeKey, AuthPluginOptions> = {
-				username: 'username',
-				password: 'password',
-				options: {
-					userAttributes: [
-						{userAttributeKey: 'email', value: 'email@email.com'},
-						{userAttributeKey: 'phone_number', value: '+12345678910'}
-					],
-					pluginOptions
-				}
+			const options = {
+				userAttributes,
+				pluginOptions
 			};
-			const spyon = jest.spyOn(CognitoIdentityProviderClient.prototype, 'send');
+			const request = createSignUpRequest('username', 'password', options);
 			await Auth.signUp(request);
 			const commandObject = {
 				input: {
@@ -113,30 +127,20 @@ describe('Amazon Cognito Auth unit test', () => {
 					ValidationData: [{'Name': 'foo', 'Value': 'bar'}]
 				}
 			};
-			expect(CognitoIdentityProviderClient.prototype.send).toBeCalled();
-			expect(await CognitoIdentityProviderClient.prototype.send).toBeCalledWith(expect.objectContaining(commandObject));
-			spyon.mockClear();
+			expect(CognitoIdentityProviderClient.prototype.send).toBeCalledWith(expect.objectContaining(commandObject));
 		});
+
 		test('happy case with client metadata', async () => {
-			const pluginOptions: CognitoSignUpOptions = {
-				clientMetadata: {
-					foo: 'bar'
+			const options = {
+				userAttributes,
+				pluginOptions: {
+					clientMetadata: {
+						foo: 'bar'
+					}
 				}
 			};
-			const request: SignUpRequest<CognitoUserAttributeKey, AuthPluginOptions> = {
-				username: 'username',
-				password: 'password',
-				options: {
-					userAttributes: [
-						{userAttributeKey: 'email', value: 'email@email.com'},
-						{userAttributeKey: 'phone_number', value: '+12345678910'}
-					],
-					pluginOptions
-				}
-			};
-			const spyon = jest.spyOn(CognitoIdentityProviderClient.prototype, 'send');
 			Auth.addPluggable(provider);
-			await Auth.signUp(request);
+			await Auth.signUp(createSignUpRequest('username', 'password', options));
 			const commandObject = {
 				input: {
 					ClientId: 'userPoolId',
@@ -151,8 +155,8 @@ describe('Amazon Cognito Auth unit test', () => {
 			};
 			expect(CognitoIdentityProviderClient.prototype.send).toBeCalled();
 			expect(await CognitoIdentityProviderClient.prototype.send).toBeCalledWith(expect.objectContaining(commandObject));
-			spyon.mockClear();
 		});
+
 		test('happy case with code delivery details in result', async () => {
 			const spyon = jest.spyOn(CognitoIdentityProviderClient.prototype, 'send')
 				.mockImplementationOnce((command) => {
@@ -165,73 +169,47 @@ describe('Amazon Cognito Auth unit test', () => {
 						}
 					};
 				});
-			const signUpResult = {
-				isSignUpComplete: false,
-				nextStep: {
-					codeDeliveryDetails: {
-						deliveryMedium: 'deliveryMedium',
-						destination: 'destination',
-						attributeName: 'attrName'
-					},
-					signUpStep: 'CONFIRM_SIGN_UP'
-				}
-			};
 			Auth.addPluggable(provider);
-			const result = await Auth.signUp(signUpRequest);
+			const result = await Auth.signUp(createSignUpRequest('username', 'password', { userAttributes }));
 			expect(CognitoIdentityProviderClient.prototype.send).toBeCalled();
-			expect(result).toEqual(signUpResult);
-			spyon.mockClear();
+			expect(result).toEqual(signUpResultUnconfirmedUser);
 		});
-	});
-	test('client send command fails', async () => {
-		const spyon = jest.spyOn(CognitoIdentityProviderClient.prototype, 'send').mockImplementationOnce(command => {
-			throw new Error('err');
+
+		test('client send command fails', async () => {
+			expect.assertions(1);
+			const spyon = jest.spyOn(CognitoIdentityProviderClient.prototype, 'send').mockImplementationOnce(command => {
+				throw new Error('err');
+			});
+			Auth.addPluggable(provider);
+			const request = createSignUpRequest('username', 'password', { userAttributes });
+			await expect(Auth.signUp(request)).rejects.toThrow(new Error('err'));
 		});
-		Auth.addPluggable(provider);
-		await expect(Auth.signUp(signUpRequest)).rejects.toEqual(new Error('err'));
-		spyon.mockClear();
+
+		test('no userpool in config', async () => {
+			const provider = new AmazonCognitoProvider(noUserPoolConfig);
+			Auth.addPluggable(provider);
+			const request = createSignUpRequest('username', 'password', { userAttributes });
+			await expect(Auth.signUp(request)).rejects.toThrow(new AuthError(AuthErrorTypes.NoConfig));
+			expect(CognitoIdentityProviderClient.prototype.send).toBeCalledTimes(0);
+		});
+
+		test('empty username', async () => {
+			expect.assertions(2);
+			const request = createSignUpRequest('', 'password', { userAttributes }); 
+			Auth.addPluggable(provider);
+			await expect(Auth.signUp(request)).rejects.toThrow(new AuthError(AuthErrorTypes.EmptyUsername));
+			expect(CognitoIdentityProviderClient.prototype.send).toBeCalledTimes(0);
+		});
+
+		test('empty password', async () => {
+			expect.assertions(2);
+			const request = createSignUpRequest('username', '', { userAttributes });
+			Auth.addPluggable(provider);
+			await expect(Auth.signUp(request)).rejects.toThrow(new AuthError(AuthErrorTypes.EmptyPassword));
+			expect(CognitoIdentityProviderClient.prototype.send).toBeCalledTimes(0);
+		});
+		// TODO: test hub events when they are implemented in Sign up function
 	});
-	test ('no userpool in config', async () => {
-		const spyon = jest.spyOn(CognitoIdentityProviderClient.prototype, 'send');
-		const provider = new AmazonCognitoProvider(noUserPoolConfig);
-		Auth.addPluggable(provider);
-		await expect(Auth.signUp(signUpRequest)).rejects.toEqual(new AuthError(AuthErrorTypes.NoConfig));
-		expect(CognitoIdentityProviderClient.prototype.send).toBeCalledTimes(0);
-		spyon.mockClear();
-	});
-	test('empty username', async () => {
-		const request: SignUpRequest<CognitoUserAttributeKey, AuthPluginOptions> = {
-			username: '',
-			password: 'password',
-			options: {
-				userAttributes: [
-					{userAttributeKey: 'email', value: 'email@email.com'},
-					{userAttributeKey: 'phone_number', value: '+12345678910'}
-				]
-			}
-		};
-		const spyon = jest.spyOn(CognitoIdentityProviderClient.prototype, 'send');
-		Auth.addPluggable(provider);
-		await expect(Auth.signUp(request)).rejects.toEqual(new AuthError(AuthErrorTypes.EmptyUsername));
-		expect(CognitoIdentityProviderClient.prototype.send).toBeCalledTimes(0);
-		spyon.mockClear();
-	});
-	test('empty password', async () => {
-		const request: SignUpRequest<CognitoUserAttributeKey, AuthPluginOptions> = {
-			username: 'username',
-			password: '',
-			options: {
-				userAttributes: [
-					{userAttributeKey: 'email', value: 'email@email.com'},
-					{userAttributeKey: 'phone_number', value: '+12345678910'}
-				]
-			}
-		};
-		const spyon = jest.spyOn(CognitoIdentityProviderClient.prototype, 'send');
-		Auth.addPluggable(provider);
-		await expect(Auth.signUp(request)).rejects.toEqual(new AuthError(AuthErrorTypes.EmptyPassword));
-		expect(CognitoIdentityProviderClient.prototype.send).toBeCalledTimes(0);
-		spyon.mockClear();
-	});
-	// TODO: test hub events when they are implemented in Sign up function
+
+	
 });
