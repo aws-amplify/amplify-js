666a0aa889446a1fd178beb1bf239cd47bffa2d0
Israel Arcos <israel.arcos2595@gmail.com>
2022-12-22 12:47:59 -0500

    chore: add documentation to util functions

--- packages/auth/src/utils/throwConfigError.ts
+++ packages/auth/src/utils/throwConfigError.ts
@@ -1,6 +1,15 @@
 import { AuthError } from '../error/AuthError';
 import { AuthConfigErrorMap } from '../error/types/maps/authConfigErrorMap';
 
+/**
+ *  Throws a config error
+ *
+ * @param  errorData config error object
+ *
+ * @throws
+ *  error: {@link AuthError}
+ *
+ */
 export function throwConfigError<
 	ErrorType extends keyof AuthConfigErrorMap
 >(errorData: {
--- packages/auth/src/utils/throwServiceError.ts
+++ packages/auth/src/utils/throwServiceError.ts
@@ -1,6 +1,14 @@
 import { AuthError } from '../error/AuthError';
 import { assertServiceError } from './assertServiceError';
 
+/**
+ *  Throws  service error
+ *
+ * @param  error service error
+ *
+ * @throws
+ *  error: {@link AuthError}
+ */
 export function throwServiceError(error: unknown): never {
 	assertServiceError(error);
 
