746b5c01d72a6bf26b46de596cb8523cd09adc4c
elorzafe <elorzafe@amazon.com>
2023-01-04 16:02:13 -0800

    Removes unused import from PubSub test

--- packages/pubsub/__tests__/PubSub-unit-test.ts
+++ packages/pubsub/__tests__/PubSub-unit-test.ts
@@ -16,15 +16,9 @@ import {
 	mqttTopicMatch,
 } from '../src/Providers';
 
-import {
-	Credentials,
-	Hub,
-	INTERNAL_AWS_APPSYNC_REALTIME_PUBSUB_PROVIDER,
-	Logger,
-	Reachability,
-} from '@aws-amplify/core';
+import { Credentials, Reachability } from '@aws-amplify/core';
 import * as Paho from 'paho-mqtt';
-import { ConnectionState, CONNECTION_STATE_CHANGE } from '../src';
+import { ConnectionState } from '../src';
 import { HubConnectionListener } from './helpers';
 import Observable from 'zen-observable-ts';
 import * as constants from '../src/Providers/constants';
