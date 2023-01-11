e02e0ce6934ce6694e15a3fd288ea8e67b4c0f89
Jim Blanchard <jablanch@amazon.com>
2023-01-11 09:27:33 -0600

    Update GraphQL package exports.

--- packages/api-graphql/src/index.ts
+++ packages/api-graphql/src/index.ts
@@ -6,5 +6,3 @@ export { GraphQLResult, GraphQLAuthError, GRAPHQL_AUTH_MODE } from './types';
 export { GraphQLAPI, GraphQLAPIClass, graphqlOperation } from './GraphQLAPI';
 export * from './types';
 export default GraphQLAPI;
-
-export { query } from './commands';
