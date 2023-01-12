a65b0594cbd3a946e6fa606513da12ec573c490f
Jim Blanchard <jablanch@amazon.com>
2023-01-12 10:30:03 -0600

    Removed `Credentials` from REST client.

--- packages/api-graphql/src/utils.ts
+++ packages/api-graphql/src/utils.ts
@@ -1,4 +1,4 @@
-import { Amplify, Credentials } from '@aws-amplify/core';
+import { Amplify } from '@aws-amplify/core';
 import { RestClient } from '@aws-amplify/api-rest';
 
 // Constants
@@ -6,5 +6,5 @@ export const USER_AGENT_HEADER = 'x-amz-user-agent';
 
 // Instantiate a REST client for GraphQL commands to use internally
 const restClient = new RestClient(Amplify.getConfig());
-restClient.Credentials = Credentials;
+//restClient.Credentials = Credentials;
 export { restClient };
