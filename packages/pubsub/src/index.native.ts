8209beb6e94f3705cae51cd6abf4cf44c984837c
Allan Zheng <zheallan@amazon.com>
2023-01-24 18:21:15 -0800

    chore(pubsub): upgrade UUID to 9.0.0

---
+++ packages/pubsub/src/index.native.ts
@@ -0,0 +1,19 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import 'react-native-get-random-values';
+
+export { PubSub } from './PubSub';
+export { CONNECTION_STATE_CHANGE } from './Providers/constants';
+export { ConnectionState, CONTROL_MSG } from './types';
+export {
+	AWSAppSyncProvider,
+	AWSAppSyncRealTimeProvider,
+	AWSAppSyncRealTimeProviderOptions,
+	AWSIoTProvider,
+	AWSIoTProviderOptions,
+	AbstractPubSubProvider,
+	MqttOverWSProvider,
+	MqttProviderOptions,
+	ObserverQuery,
+	mqttTopicMatch,
+} from './Providers';
