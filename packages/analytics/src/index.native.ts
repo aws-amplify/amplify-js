2753b04c225807a52f00e03c22ecbf77fdcadcaa
Allan Zheng <zheallan@amazon.com>
2023-01-24 17:46:08 -0800

    chore(analytics): upgrade UUID to 9.0.0

---
+++ packages/analytics/src/index.native.ts
@@ -0,0 +1,11 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import 'react-native-get-random-values';
+export { Analytics } from './Analytics';
+export { AnalyticsProvider } from './types';
+export {
+	AWSPinpointProvider,
+	AWSKinesisProvider,
+	AWSKinesisFirehoseProvider,
+	AmazonPersonalizeProvider,
+} from './Providers';
