17dfa2164392736f162be9d2e1c8cd4700e6e87a
Allan Zheng <zheallan@amazon.com>
2023-01-24 18:06:21 -0800

    chore(datastore): upgrade UUID to 9.0.0

---
+++ packages/datastore/src/index.native.ts
@@ -0,0 +1,37 @@
+import 'react-native-get-random-values';
+
+export {
+	DataStore,
+	DataStoreClass,
+	initSchema,
+	ModelInstanceCreator,
+	AsyncCollection,
+	AsyncItem,
+} from './datastore/datastore';
+
+export {
+	Predicates,
+	ModelPredicateCreator,
+	ModelSortPredicateCreator,
+} from './predicates';
+export { Adapter as StorageAdapter } from './storage/adapter';
+
+import {
+	traverseModel,
+	validatePredicate,
+	USER,
+	isNonModelConstructor,
+	isModelConstructor,
+} from './util';
+
+export { NAMESPACES } from './util';
+
+export const utils = {
+	USER,
+	traverseModel,
+	validatePredicate,
+	isNonModelConstructor,
+	isModelConstructor,
+};
+
+export * from './types';
