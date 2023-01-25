c571ee0d662d6fd98b05748a57251bcc33ab8ab3
Allan Zheng <zheallan@amazon.com>
2023-01-24 18:14:20 -0800

    chore(notifications): upgrade UUID to 9.0.0

---
+++ packages/notifications/src/index.native.ts
@@ -0,0 +1,22 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import 'react-native-get-random-values';
+
+export { default as Notifications } from './Notifications';
+export {
+	AWSPinpointUserInfo,
+	InAppMessage,
+	InAppMessageAction,
+	InAppMessageButton,
+	InAppMessageContent,
+	InAppMessageImage,
+	InAppMessageInteractionEvent,
+	InAppMessageLayout,
+	InAppMessageStyle,
+	InAppMessageTextAlign,
+	InAppMessagingConfig,
+	InAppMessagingEvent,
+	UserInfo,
+} from './InAppMessaging';
+export { NotificationsConfig } from './types';
