8b1e3f1032641d3b247a660077d52e4b0929af48
Chris Fang <cshfang@gmail.com>
2023-02-07 16:47:38 -0800

    chore(notifications): Extract common functionalities

---
+++ packages/notifications/src/common/index.ts
@@ -0,0 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export { default as AWSPinpointProviderCommon } from './AWSPinpointProviderCommon';
+export * from './eventListeners';
+export * from './eventListeners/types';
