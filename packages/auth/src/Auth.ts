bf80a8d4f0dc5da9e28b97d7df43d567c8886a84
Dustin Noyes <dustin.noyes@gmail.com>
2023-02-08 13:19:53 -0800

    fix(auth): sends current user with attributes to hub

--- packages/auth/src/Auth.ts
+++ packages/auth/src/Auth.ts
@@ -203,7 +203,7 @@ export class AuthClass {
 			identityPoolId,
 			refreshHandlers,
 			storage: this._storage,
-			identityPoolRegion
+			identityPoolRegion,
 		});
 
 		// initialize cognitoauth client if hosted ui options provided
@@ -1139,7 +1139,11 @@ export class AuthClass {
 					return;
 				},
 				onSuccess: data => {
-					dispatchAuthEvent('signIn', user, `A user ${user.getUsername()} has been signed in`);
+					dispatchAuthEvent(
+						'signIn',
+						user,
+						`A user ${user.getUsername()} has been signed in`
+					);
 					dispatchAuthEvent(
 						'verify',
 						user,
@@ -1182,14 +1186,13 @@ export class AuthClass {
 						} catch (e) {
 							logger.debug('cannot get cognito credentials', e);
 						} finally {
-							that.user = user;
-
+							const currentUser = await this.currentUserPoolUser();
 							dispatchAuthEvent(
 								'signIn',
-								user,
-								`A user ${user.getUsername()} has been signed in`
+								currentUser,
+								`A user ${currentUser.getUsername()} has been signed in`
 							);
-							resolve(user);
+							resolve(currentUser);
 						}
 					},
 					onFailure: err => {
@@ -1428,15 +1431,23 @@ export class AuthClass {
 				user.updateAttributes(
 					attributeList,
 					(err, result, details) => {
-						
 						if (err) {
-							dispatchAuthEvent('updateUserAttributes_failure', err, 'Failed to update attributes');
+							dispatchAuthEvent(
+								'updateUserAttributes_failure',
+								err,
+								'Failed to update attributes'
+							);
 							return reject(err);
 						} else {
 							const attrs = this.createUpdateAttributesResultList(
-								attributes as Record<string, string>, details?.CodeDeliveryDetailsList
+								attributes as Record<string, string>,
+								details?.CodeDeliveryDetailsList
 							);
-							dispatchAuthEvent('updateUserAttributes', attrs, 'Attributes successfully updated');
+							dispatchAuthEvent(
+								'updateUserAttributes',
+								attrs,
+								'Attributes successfully updated'
+							);
 							return resolve(result);
 						}
 					},
@@ -1447,15 +1458,17 @@ export class AuthClass {
 	}
 
 	private createUpdateAttributesResultList(
-		attributes: Record<string, string>, 
-		codeDeliveryDetailsList?: CodeDeliveryDetails []
+		attributes: Record<string, string>,
+		codeDeliveryDetailsList?: CodeDeliveryDetails[]
 	): Record<string, string> {
 		const attrs = {};
 		Object.keys(attributes).forEach(key => {
 			attrs[key] = {
-				isUpdated: true
+				isUpdated: true,
 			};
-			const codeDeliveryDetails = codeDeliveryDetailsList?.find(value => value.AttributeName === key);
+			const codeDeliveryDetails = codeDeliveryDetailsList?.find(
+				value => value.AttributeName === key
+			);
 			if (codeDeliveryDetails) {
 				attrs[key].isUpdated = false;
 				attrs[key].codeDeliveryDetails = codeDeliveryDetails;
