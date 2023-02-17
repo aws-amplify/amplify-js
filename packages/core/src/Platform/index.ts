2ac78d38861d74a45fe238f31d741a337b81d8d1
Erin Beal <erinleig@amazon.com>
2023-02-17 11:16:47 -0700

    feature: draft framework detection

--- packages/core/src/Platform/index.ts
+++ packages/core/src/Platform/index.ts
@@ -47,6 +47,46 @@ const buildUserAgentSuffix = (userAgentSuffix?: UserAgentSuffix) => {
 	return JSON.stringify(codedSuffix);
 };
 
-const detectFramework = () => {
-	return 'JS';
+const detectFrameworks = () => {
+	let frameworks = '';
+
+	if (
+		Array.from(document.querySelectorAll('*')).some(
+			e => e._reactRootContainer !== undefined
+		)
+	)
+		console.log('React.js');
+
+	if (!!document.querySelector('script[id=__NEXT_DATA__]'))
+		frameworks += 'Next.js|';
+
+	if (!!document.querySelector('[id=___gatsby]')) {
+		frameworks += 'Gatsby.js|';
+	}
+
+	if (
+		!!window.angular ||
+		!!document.querySelector(
+			'.ng-binding, [ng-app], [data-ng-app], [ng-controller], [data-ng-controller], [ng-repeat], [data-ng-repeat]'
+		) ||
+		!!document.querySelector(
+			'script[src*="angular.js"], script[src*="angular.min.js"]'
+		)
+	)
+		frameworks += 'Angular.js|';
+
+	if (!!window.getAllAngularRootElements || !!window.ng?.coreTokens?.NgZone)
+		console.log('Angular 2+|');
+
+	if (!!window.Backbone) console.log('Backbone.js');
+	if (!!window.Ember) console.log('Ember.js');
+	if (!!window.Vue) console.log('Vue.js');
+	if (!!window.Meteor) console.log('Meteor.js');
+	if (!!window.Zepto) console.log('Zepto.js');
+	if (!!window.jQuery) console.log('jQuery.js');
+
+	if (!frameworks) {
+		return 'JS';
+	}
+	return frameworks;
 };
