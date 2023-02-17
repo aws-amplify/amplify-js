553e15d27d80bdac2aa18d02125aa1e31bba4cce
Chris Fang <cshfang@gmail.com>
2023-02-17 10:08:32 -0800

    chore(notifications): Update message normalizer

--- packages/notifications/src/PushNotification/utils.ts
+++ packages/notifications/src/PushNotification/utils.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { ConsoleLogger as Logger } from '@aws-amplify/core';
+import { isEmpty } from 'lodash';
 
 import {
 	PushNotificationMessage,
@@ -9,6 +10,11 @@ import {
 } from './types';
 
 const logger = new Logger('Notifications.PushNotification');
+
+// Currently, the pinpoint specifics have not yet been abstracted out of the native payloads, but we are working
+// towards doing so.
+const DEEP_LINK_ACTION = 'pinpoint.deeplink';
+const URL_ACTION = 'pinpoint.url';
 
 export const normalizeNativePermissionStatus = (
 	nativeStatus?
@@ -32,26 +38,14 @@ export const normalizeNativeMessage = (
 		return null;
 	}
 	try {
-		const {
-			body,
-			imageUrl,
-			notificationId,
-			title,
-			rawData: data,
-		} = JSON.parse(nativeMessage.payload);
-		const deeplinkUrl = data?.['pinpoint.deeplink'];
-		const goToUrl = data?.['pinpoint.url'];
+		const payload = JSON.parse(nativeMessage.payload);
+		const { messageId, senderId, sendTime, rawData: data } = payload;
+		const content = getContent(payload);
 		return {
-			messageId: notificationId,
-			senderId: '😭',
-			sendTime: new Date(),
-			content: {
-				body,
-				deeplinkUrl,
-				imageUrl,
-				title,
-				goToUrl,
-			},
+			messageId,
+			sendTime: new Date(sendTime),
+			...(senderId && { senderId }),
+			...(!isEmpty(content) && { content }),
 			data,
 		};
 	} catch (e) {
@@ -59,3 +53,40 @@ export const normalizeNativeMessage = (
 	}
 	return null;
 };
+
+const getContent = (payload): PushNotificationMessage['content'] => {
+	const { action, body, imageUrl, title } = payload;
+	const apnsOptions = getApnsOptions(payload);
+	const fcmOptions = getFcmOptions(payload);
+	return {
+		...(body && { body }),
+		...(imageUrl && { imageUrl }),
+		...(title && { title }),
+		...getAction(action),
+		...(!isEmpty(apnsOptions) && { apnsOptions }),
+		...(!isEmpty(fcmOptions) && { fcmOptions }),
+	};
+};
+
+const getAction = (
+	action
+): Pick<PushNotificationMessage['content'], 'goToUrl' | 'deeplinkUrl'> => {
+	if (action[URL_ACTION]) {
+		return { goToUrl: action[URL_ACTION] };
+	}
+	if (action[DEEP_LINK_ACTION]) {
+		return { deeplinkUrl: action[DEEP_LINK_ACTION] };
+	}
+};
+
+const getApnsOptions = ({
+	subtitle,
+}): PushNotificationMessage['content']['apnsOptions'] => ({
+	...(subtitle && { subtitle }),
+});
+
+const getFcmOptions = ({
+	channelId,
+}): PushNotificationMessage['content']['fcmOptions'] => ({
+	...(channelId && { channelId }),
+});
