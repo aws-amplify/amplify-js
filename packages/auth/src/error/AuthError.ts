b9e331fbd3510b436ab78a37fec198c38cd5a213
Israel Arcos <israel.arcos2595@gmail.com>
2023-03-29 09:20:00 -0500

    refactor: auth error

---
+++ packages/auth/src/error/AuthError.ts
@@ -0,0 +1,25 @@
+type AuthErrorConstructor = {
+	message: string;
+	underlyingException?: Error | unknown;
+	recoverySuggestion?: string;
+	name: string;
+};
+
+export class AuthError extends Error {
+	underlyingException?: Error | unknown;
+	recoverySuggestion?: string;
+	/**
+	 * Creates an Auth error
+	 *
+	 * @param message text that describes the main problem.
+	 * @param underlyingException the underlying cause of the error.
+	 * @param recoverySuggestion suggestion to recover from the error.
+	 */
+	constructor(error: AuthErrorConstructor) {
+		super(error.message);
+
+		this.name = error.name;
+		this.underlyingException = error.underlyingException;
+		this.recoverySuggestion = error.recoverySuggestion;
+	}
+}
