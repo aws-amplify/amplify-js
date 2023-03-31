49f3d26c40b9bbc85cb817654e63b7c50678bf85
Erin Beal <erinleig@amazon.com>
2023-03-31 13:29:29 -0700

    chore: remove part3 code for this iteration

--- packages/core/src/Platform/detectFramework.ts
+++ packages/core/src/Platform/detectFramework.ts
@@ -1,7 +1,3 @@
 import { Framework } from './types';
 
-interface ExtendedWindow extends Window {
-	__REACT_DEVTOOLS_GLOBAL_HOOK__?: any;
-}
-
 export const detectFramework = (): Framework => Framework.None;
