530cf0453f76734550b79e6c8eeaa4cd6c66acd8
Jim Blanchard <jim.l.blanchard@gmail.com>
2023-04-11 18:18:56 -0500

    Revert "Merge branch 'next' into main" (#11220)
    This reverts commit 88b5c26daf30304e9f4b2bf5064bfce545ad06b7, reversing
    changes made to 1116feff67ae991d21b77e5dd73dac66d58e8004.

--- packages/auth/src/providers/cognito/utils/clients/UserPoolClient.ts
+++
@@ -1,9 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-import { Amplify } from '@aws-amplify/core';
-
-export const UserPoolClient = {
-	// TODO: update when config is typed properly
-	region: Amplify.config['aws_cognito_region'],
-	clientId: Amplify.config['aws_user_pools_web_client_id'],
-};
--- packages/auth/src/types/index.ts
+++ packages/auth/src/types/index.ts
@@ -1,29 +1,4 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// TODO: Remove "./Auth" export
 export * from './Auth';
-
-// Enums
-export { AuthSignUpStep } from './enums/AuthSignUpStep';
-export { DeliveryMedium } from './enums/DeliveryMedium';
-
-// Models
-export type { AdditionalInfo } from './models/AdditionalInfo';
-export type { AnyAttribute } from './models/AnyAttribute';
-export type { AuthCodeDeliveryDetails } from './models/AuthCodeDeliveryDetails';
-export type { AuthNextSignUpStep } from './models/AuthNextSignUpStep';
-export type { AuthStandardAttributeKey } from './models/AuthStandardAttributeKey';
-export type { AuthUserAttributeKey } from './models/AuthUserAttributeKey';
-export type { AuthUserAttribute } from './models/AuthUserAttribute';
-export type { GetAttributeKey } from './models/GetAttributeKey';
-
-// Options
-export type { AuthServiceOptions } from './options/AuthServiceOptions';
-export type { AuthSignUpOptions } from './options/AuthSignUpOptions';
-
-// Requests
-export type { SignUpRequest } from './requests/SignUpRequest';
-
-// Results
-export type { AuthSignUpResult } from './results/AuthSignUpResult';
