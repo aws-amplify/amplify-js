56cdd7615b133b02a4b2ae441d5aee19746d225f
helgabalashova <helga.stolyarova@gmail.com>
2023-04-12 12:55:01 -0600

    removed type from index

--- packages/auth/src/types/index.ts
+++ packages/auth/src/types/index.ts
@@ -10,24 +10,24 @@ export { DeliveryMedium } from './enums/DeliveryMedium';
 export { AuthResetPasswordStep } from './enums/AuthResetPasswordStep';
 
 // Models
-export type { AdditionalInfo } from './models/AdditionalInfo';
-export type { AnyAttribute } from './models/AnyAttribute';
-export type { AuthCodeDeliveryDetails } from './models/AuthCodeDeliveryDetails';
-export type { AuthNextSignUpStep } from './models/AuthNextSignUpStep';
-export type { AuthStandardAttributeKey } from './models/AuthStandardAttributeKey';
-export type { AuthUserAttributeKey } from './models/AuthUserAttributeKey';
-export type { AuthUserAttribute } from './models/AuthUserAttribute';
-export type { GetAttributeKey } from './models/GetAttributeKey';
-export type { AuthNextResetPasswordStep } from './models/AuthNextResetPasswordStep';
+export { AdditionalInfo } from './models/AdditionalInfo';
+export { AnyAttribute } from './models/AnyAttribute';
+export { AuthCodeDeliveryDetails } from './models/AuthCodeDeliveryDetails';
+export { AuthNextSignUpStep } from './models/AuthNextSignUpStep';
+export { AuthStandardAttributeKey } from './models/AuthStandardAttributeKey';
+export { AuthUserAttributeKey } from './models/AuthUserAttributeKey';
+export { AuthUserAttribute } from './models/AuthUserAttribute';
+export { GetAttributeKey } from './models/GetAttributeKey';
+export { AuthNextResetPasswordStep } from './models/AuthNextResetPasswordStep';
 
 // Options
-export type { AuthServiceOptions } from './options/AuthServiceOptions';
-export type { AuthSignUpOptions } from './options/AuthSignUpOptions';
+export { AuthServiceOptions } from './options/AuthServiceOptions';
+export { AuthSignUpOptions } from './options/AuthSignUpOptions';
 
 // Requests
-export type { ResetPasswordRequest } from './requests/ResetPasswordRequest';
-export type { SignUpRequest } from './requests/SignUpRequest';
+export { ResetPasswordRequest } from './requests/ResetPasswordRequest';
+export { SignUpRequest } from './requests/SignUpRequest';
 
 // Results
-export type { AuthSignUpResult } from './results/AuthSignUpResult';
-export type { ResetPasswordResult } from './results/ResetPasswordResult';
+export { AuthSignUpResult } from './results/AuthSignUpResult';
+export { ResetPasswordResult } from './results/ResetPasswordResult';
