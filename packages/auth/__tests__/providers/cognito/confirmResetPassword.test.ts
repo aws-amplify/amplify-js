0c28eda7ddacf81576e827dc738f45144cdd1021
helgabalashova <helga.stolyarova@gmail.com>
2023-04-12 12:53:18 -0600

    started tests

---
+++ packages/auth/__tests__/providers/cognito/confirmResetPassword.test.ts
@@ -0,0 +1,9 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+describe('ConfirmResetPassword API happy path cases', () => {
+	test('', () => {
+		
+	})
+
+});
