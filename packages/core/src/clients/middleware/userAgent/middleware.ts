114e295052361c31ffcb6e6be23b60e892811bab
Allan Zheng <zheallan@amazon.com>
2023-04-18 09:43:07 -0700

    feat(clients): refactor user agent middleware

--- packages/core/src/clients/middleware/userAgent/middleware.ts
+++ packages/core/src/clients/middleware/userAgent/middleware.ts
@@ -1,9 +1,12 @@
+import { UserAgent, UserAgentPair } from '@aws-sdk/types';
 import { HttpRequest, HttpResponse } from '../../types/http';
 import { Middleware } from '../../types/core';
 
+export const UA_ESCAPE_REGEX = /[^\!\#\$\%\&\'\*\+\-\.\^\_\`\|\~\d\w]/g;
+
 export interface UserAgentOptions {
 	userAgentHeader?: string;
-	userAgentValue?: string;
+	userAgentValue?: string | UserAgent;
 }
 
 // TODO: incorporate new user agent design
@@ -18,16 +21,36 @@ export const userAgentMiddleware: Middleware<
 	}: UserAgentOptions) =>
 	next => {
 		return async function userAgentMiddleware(request) {
-			if (userAgentValue.trim().length === 0) {
-				const result = await next(request);
-				return result;
-			} else {
-				const headerName = userAgentHeader.toLowerCase();
+			const headerName = userAgentHeader.toLowerCase();
+			if (userAgentValue instanceof Array) {
+				const userAgentStringValue = userAgentValue
+					.map(escapeUserAgent)
+					.join(' ');
+				request.headers[headerName] = request.headers[headerName]
+					? `${request.headers[headerName]} ${userAgentStringValue}`
+					: userAgentStringValue;
+			} else if (
+				typeof userAgentValue === 'string' &&
+				userAgentValue.trim().length > 0
+			) {
 				request.headers[headerName] = request.headers[headerName]
 					? `${request.headers[headerName]} ${userAgentValue}`
 					: userAgentValue;
-				const response = await next(request);
-				return response;
 			}
+			const result = await next(request);
+			return result;
 		};
 	};
+
+const escapeUserAgent = ([name, version]: UserAgentPair): string => {
+	const prefixSeparatorIndex = name.indexOf('/');
+	const prefix = name.substring(0, prefixSeparatorIndex); // If no prefix, prefix is just ""
+	let uaName = name.substring(prefixSeparatorIndex + 1);
+	if (prefix === 'api') {
+		uaName = uaName.toLowerCase();
+	}
+	return [prefix, uaName, version]
+		.filter(item => item && item.length > 0)
+		.map(item => item?.replace(UA_ESCAPE_REGEX, '_'))
+		.join('/');
+};
