64a7d364f204b7fa2d6a271259351818edd86853
erinleigh90 <106691284+erinleigh90@users.noreply.github.com>
2023-04-18 08:44:58 -0700

    chore: add copyright header to detectFramework file
    Co-authored-by: Jim Blanchard <jim.l.blanchard@gmail.com>

--- packages/core/src/Platform/detectFramework.ts
+++ packages/core/src/Platform/detectFramework.ts
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 import { Framework } from './types';
 
 export const detectFramework = () => {
