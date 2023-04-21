58c8f0ab864760ea1b51e099d879e987d5c52f63
Chris Fang <cshfang@gmail.com>
2023-04-21 13:27:18 -0700

    Add test for data hashing with SourceData keys

--- packages/core/__tests__/clients/middleware/signing/signer/signatureV4/utils/dataHashHelpers-test.ts
+++ packages/core/__tests__/clients/middleware/signing/signer/signatureV4/utils/dataHashHelpers-test.ts
@@ -9,6 +9,8 @@ import {
 describe('dataHashHelpers', () => {
 	const key = 'hash-key';
 	const data = 'hash-data';
+	const arrayBuffer = new ArrayBuffer(8);
+	const arrayBufferView = new Uint8Array(new ArrayBuffer(8));
 
 	describe('getHashedData', () => {
 		test('returns hashed data from a key and data', async () => {
@@ -17,6 +19,24 @@ describe('dataHashHelpers', () => {
 					232, 228, 217, 152, 156, 118, 166, 42, 124, 217, 129, 99, 56, 163,
 					190, 131, 62, 148, 54, 170, 101, 55, 158, 118, 42, 239, 245, 136, 153,
 					115, 41, 156,
+				])
+			);
+
+			expect(await getHashedData(arrayBuffer, arrayBuffer)).toStrictEqual(
+				new Uint8Array([
+					243, 117, 24, 10, 186, 146, 136, 132, 1, 241, 145, 155, 228, 168, 113,
+					90, 98, 118, 59, 101, 193, 193, 14, 29, 8, 88, 232, 29, 77, 111, 159,
+					210,
+				])
+			);
+
+			expect(
+				await getHashedData(arrayBufferView, arrayBufferView)
+			).toStrictEqual(
+				new Uint8Array([
+					243, 117, 24, 10, 186, 146, 136, 132, 1, 241, 145, 155, 228, 168, 113,
+					90, 98, 118, 59, 101, 193, 193, 14, 29, 8, 88, 232, 29, 77, 111, 159,
+					210,
 				])
 			);
 		});
@@ -35,6 +55,16 @@ describe('dataHashHelpers', () => {
 		test('returns hashed data from a key and data', async () => {
 			expect(await getHashedDataAsHex(key, data)).toStrictEqual(
 				'e8e4d9989c76a62a7cd9816338a3be833e9436aa65379e762aeff5889973299c'
+			);
+
+			expect(await getHashedDataAsHex(arrayBuffer, arrayBuffer)).toStrictEqual(
+				'f375180aba92888401f1919be4a8715a62763b65c1c10e1d0858e81d4d6f9fd2'
+			);
+
+			expect(
+				await getHashedDataAsHex(arrayBufferView, arrayBufferView)
+			).toStrictEqual(
+				'f375180aba92888401f1919be4a8715a62763b65c1c10e1d0858e81d4d6f9fd2'
 			);
 		});
 
