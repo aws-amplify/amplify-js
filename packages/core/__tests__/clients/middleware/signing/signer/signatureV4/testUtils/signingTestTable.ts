58f9b055b141e4b53eaa190a61812e73a06a4459
Chris Fang <cshfang@gmail.com>
2023-04-21 09:30:34 -0700

    Add missing licensing headers

--- packages/core/__tests__/clients/middleware/signing/signer/signatureV4/testUtils/signingTestTable.ts
+++ packages/core/__tests__/clients/middleware/signing/signer/signatureV4/testUtils/signingTestTable.ts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { SignRequestOptions } from '../../../../../../../src/clients/middleware/signing/signer/signatureV4/types';
 import { HttpRequest } from '../../../../../../../src/clients/types';
 import { credentialsWithToken, signingOptions, url } from './data';
