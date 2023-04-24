ba847a17d27dfba5fa66594d5c6f72e826b7afac
Chris Fang <cshfang@gmail.com>
2023-04-24 12:25:09 -0700

    Remove buffer dependency

--- packages/core/src/clients/middleware/signing/signer/signatureV4/utils/dataHashHelpers.ts
+++ packages/core/src/clients/middleware/signing/signer/signatureV4/utils/dataHashHelpers.ts
@@ -4,7 +4,6 @@
 // TODO: V6 update to different crypto dependency?
 import { Sha256 } from '@aws-crypto/sha256-js';
 import { SourceData } from '@aws-sdk/types';
-import { fromString } from '@aws-sdk/util-buffer-from';
 import { toHex } from '@aws-sdk/util-hex-encoding';
 
 /**
@@ -19,7 +18,7 @@ export const getHashedData = async (
 	data: SourceData
 ): Promise<Uint8Array> => {
 	const sha256 = new Sha256(key);
-	sha256.update(toUint8Array(data));
+	sha256.update(data);
 	const hashedData = await sha256.digest();
 	return hashedData;
 };
@@ -37,18 +36,4 @@ export const getHashedDataAsHex = async (
 ): Promise<string> => {
 	const hashedData = await getHashedData(key, data);
 	return toHex(hashedData);
-};
-
-const isString = (body: SourceData): body is string => typeof body === 'string';
-
-const toUint8Array = (body: SourceData): Uint8Array => {
-	if (isString(body) || ArrayBuffer.isView(body)) {
-		const buf = isString(body) ? fromString(body, 'utf8') : body;
-		return new Uint8Array(
-			buf.buffer,
-			buf.byteOffset,
-			buf.byteLength / Uint8Array.BYTES_PER_ELEMENT
-		);
-	}
-	return new Uint8Array(body);
 };
