41bb35aabac952006ad7ef5f8e60ab8782503317
Chris F <5827964+cshfang@users.noreply.github.com>
2023-04-24 14:09:26 -0700

    feat(clients): Add custom signature v4 signer (#11273)
    * feat(clients): Add custom signature v4 signer
    
    * Updated docstrings
    
    * Extracted common code
    
    * Use == null instead of isNil
    
    * Add unit tests
    
    * Add missing licensing headers
    
    * Use test case options in presign tests
    
    * Fixed comment
    
    * Add test for data hashing with SourceData keys
    
    * Remove buffer dependency
    
    * Remove internal sdk dependency

---
+++ packages/core/__tests__/clients/middleware/signing/signer/signatureV4/utils/getHashedPayload-test.ts
@@ -0,0 +1,41 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { getHashedPayload } from '../../../../../../../src/clients/middleware/signing/signer/signatureV4/utils/getHashedPayload';
+
+describe('getHashedPayload', () => {
+	test('returns empty hash if nullish', async () => {
+		expect(await getHashedPayload(undefined)).toStrictEqual(
+			'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+		);
+		expect(await getHashedPayload(null as any)).toStrictEqual(
+			'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+		);
+	});
+
+	test('returns hashed payload', async () => {
+		expect(await getHashedPayload('string-body')).toStrictEqual(
+			'f4241e27b103e1a1d7f88a1556541718250245fe31c8e695f3e068d3fe837572'
+		);
+	});
+
+	test('works with ArrayBuffer', async () => {
+		expect(await getHashedPayload(new ArrayBuffer(8))).toStrictEqual(
+			'af5570f5a1810b7af78caf4bc70a660f0df51e42baf91d4de5b2328de0e83dfc'
+		);
+
+		expect(
+			await getHashedPayload(new Uint8Array(new ArrayBuffer(8)))
+		).toStrictEqual(
+			'af5570f5a1810b7af78caf4bc70a660f0df51e42baf91d4de5b2328de0e83dfc'
+		);
+	});
+
+	test('returns unsigned payload if not hashable', async () => {
+		for (const scalar of [123.234, true, new Blob()]) {
+			expect(await getHashedPayload(scalar as any)).toStrictEqual(
+				'UNSIGNED-PAYLOAD'
+			);
+		}
+	});
+});
