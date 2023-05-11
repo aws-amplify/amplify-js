720ac606dede7d243f3d5ce08395fc6387a6f35d
AllanZhengYP <zheallan@amazon.com>
2023-05-11 12:33:56 -0700

    fix(core): add URL polyfill for signer in react native (#11362)
    * fix(core): add RN url polyfill for signer code path
    
    * test(notification): unblock unit test failure of rn url polyfill

---
+++ packages/core/src/clients/middleware/signing/signer/signatureV4/index.native.ts
@@ -0,0 +1,9 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import 'react-native-url-polyfill/auto';
+
+// TODO: V6 replace Signer
+export { signRequest } from './signRequest';
+export { presignUrl } from './presignUrl';
+export { TOKEN_QUERY_PARAM } from './constants';
