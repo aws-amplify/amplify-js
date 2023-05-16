7a6fa399bea9d0e928d0e348f104cecae21096d0
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-05-16 13:41:26 -0700

    chore: add plugin level getProperties method

--- packages/storage/src/Storage.ts
+++ packages/storage/src/Storage.ts
@@ -23,6 +23,8 @@ import {
 	StorageListOutput,
 	StorageCopyOutput,
 	UploadTask,
+	StorageGetPropertiesConfig,
+	StorageGetPropertiesOutput,
 } from './types';
 import axios, { CancelTokenSource } from 'axios';
 import { PutObjectCommandInput } from '@aws-sdk/client-s3';
@@ -307,6 +309,27 @@ export class Storage {
 		return axios.isCancel(error);
 	}
 
+	public getProperties<T extends StorageProvider | { [key: string]: any }>(
+		key: string,
+		config?: StorageGetPropertiesConfig<T>
+	): StorageGetPropertiesOutput<T> {
+		const provider = config?.provider || DEFAULT_PROVIDER;
+		const prov = this._pluggables.find(
+			pluggable => pluggable.getProviderName() === provider
+		);
+		if (prov === undefined) {
+			logger.debug('No plugin found with providerName', provider);
+			return Promise.reject(
+				'No plugin found in Storage for the provider'
+			) as StorageGetPropertiesOutput<T>;
+		}
+		const cancelTokenSource = this.getCancellableTokenSource();
+		const responsePromise = prov.getProperties(key, {
+			...config,
+		});
+		this.updateRequestToBeCancellable(responsePromise, cancelTokenSource);
+		return responsePromise as StorageGetPropertiesOutput<T>;
+	}
 	/**
 	 * Put a file in storage bucket specified to configure method
 	 * @param key - key of the object
