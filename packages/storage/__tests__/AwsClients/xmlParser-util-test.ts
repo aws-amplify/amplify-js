4bb90568fad5ea4253ab76f67d3dee1e78d7c44a
AllanZhengYP <zheallan@amazon.com>
2023-06-08 15:11:45 -0700

    feat(s3): dom based xml parser (#11300)
    * feat(clients): add browser xml parser
    
    * feat: implement xml parser
    
    * test(xml): protocol test of xml parser
    
    Co-authored-by: Chris F <5827964+cshfang@users.noreply.github.com>

---
+++ packages/storage/__tests__/AwsClients/xmlParser-util-test.ts
@@ -0,0 +1,22 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import cases from './xmlParser-fixture';
+import { parser as browserParser } from '../../src/AwsClients/S3/utils/xmlParser.browser';
+import { parser as nodeParser } from '../../src/AwsClients/S3/utils/xmlParser';
+
+describe('xmlParser for browsers', () => {
+	cases.forEach(({ spec, xml, expected }) => {
+		it(`should parse ${spec} correctly`, () => {
+			expect(browserParser.parse(xml)).toEqual(expected);
+		});
+	});
+});
+
+describe('xmlParser for non-browsers', () => {
+	cases.forEach(({ spec, xml, expected }) => {
+		it(`should parse ${spec} correctly`, () => {
+			expect(nodeParser.parse(xml)).toEqual(expected);
+		});
+	});
+});
