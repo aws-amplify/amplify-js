7f1be7f983b7d6eae010a436ed4f1df917abec8d
Allan Zheng <zheallan@amazon.com>
2023-06-09 16:14:27 -0700

    chore(core): expose middleware interface

--- packages/core/src/clients/index.ts
+++ packages/core/src/clients/index.ts
@@ -6,10 +6,18 @@ export { getDnsSuffix } from './endpoints';
 export { fetchTransferHandler } from './handlers/fetch';
 export { unauthenticatedHandler } from './handlers/unauthenticated';
 export { authenticatedHandler } from './handlers/authenticated';
-export { composeServiceApi } from './internal/composeServiceApi';
 export {
 	signRequest,
 	presignUrl,
 } from './middleware/signing/signer/signatureV4';
+export { signingMiddleware, SigningOptions } from './middleware/signing';
+export { getHashedPayload } from './middleware/signing/signer/signatureV4';
+export {
+	getRetryDecider,
+	jitteredBackoff,
+	retryMiddleware,
+	RetryOptions,
+} from './middleware/retry';
+export { userAgentMiddleware, UserAgentOptions } from './middleware/userAgent';
 export { parseJsonBody, parseJsonError, parseMetadata } from './serde';
 export * from './types';
