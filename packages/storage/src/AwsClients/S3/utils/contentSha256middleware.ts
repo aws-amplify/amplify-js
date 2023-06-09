983a2593cd15791acb18867bf1d7b6675fb97f83
Allan Zheng <zheallan@amazon.com>
2023-06-09 11:56:23 -0700

    feat(s3): add s3 transfer handler

---
+++ packages/storage/src/AwsClients/S3/utils/contentSha256middleware.ts
@@ -0,0 +1,28 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+	HttpRequest,
+	getHashedPayload,
+} from '@aws-amplify/core/internals/aws-client-utils';
+
+const CONTENT_SHA256_HEADER = 'x-amz-content-sha256';
+
+/**
+ * A middleware that adds the x-amz-content-sha256 header to the request if it is not already present.
+ * It's required for S3 requests in browsers where the request body is sent in 1 chunk.
+ * @see https://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-header-based-auth.html
+ *
+ * @internal
+ */
+export const contentSha256Middleware = (options: {}) => (next: any) =>
+	async function contentSha256Middleware(request: HttpRequest) {
+		if (request.headers[CONTENT_SHA256_HEADER]) {
+			return next(request);
+		} else {
+			const hash = await getHashedPayload(request.body);
+			request.headers[CONTENT_SHA256_HEADER] = hash;
+			const response = await next(request);
+			return response;
+		}
+	};
