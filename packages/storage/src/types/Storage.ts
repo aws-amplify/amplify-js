1135e0a5d5c7c46eb99f203338160a979d40d2df
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-06-12 11:49:34 -0700

    chore: revert back to test bundle size

--- packages/storage/src/types/Storage.ts
+++ packages/storage/src/types/Storage.ts
@@ -86,10 +86,6 @@ type StorageOperationConfig<
 	? LastParameter<T['getProperties']> & {
 			provider: ReturnType<T['getProviderName']>;
 	  }
-	: T extends StorageProviderWithGetProperties & StorageProviderWithCopy
-	? LastParameter<T['getProperties' & 'copy']> & {
-			provider: ReturnType<T['getProviderName']>;
-	  }
 	: LastParameter<T[Exclude<U, 'copy' | 'getProperties'>]> & {
 			provider: ReturnType<T['getProviderName']>;
 	  };
@@ -159,8 +155,6 @@ type PickProviderOutput<
 		? ReturnType<T['copy']>
 		: T extends StorageProviderWithGetProperties
 		? ReturnType<T['getProperties']>
-		: T extends StorageProviderWithCopy & StorageProviderWithGetProperties
-		? ReturnType<T['getProperties' & 'copy']>
 		: ReturnType<T[Exclude<api, 'copy' | 'getProperties'>]>
 	: T extends { provider: string }
 	? T extends { provider: 'AWSS3' }
