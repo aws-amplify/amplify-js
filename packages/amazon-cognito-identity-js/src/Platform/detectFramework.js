08cb5f49bb1baf602e16de575d9c900c4384e1ab
Aaron S <stocaaro@stocad.com>
2023-06-19 14:22:24 -0500

    fix fix

--- packages/amazon-cognito-identity-js/src/Platform/detectFramework.js
+++ packages/amazon-cognito-identity-js/src/Platform/detectFramework.js
@@ -1,10 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { Framework } from './constants';
+import { FRAMEWORK } from './constants';
 
 export const detectFramework = () => {
 	if (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') {
-		return Framework.ReactNative;
+		return FRAMEWORK.ReactNative;
 	}
-	return Framework.None;
+	return FRAMEWORK.None;
 };
