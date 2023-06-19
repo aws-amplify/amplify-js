835b74fa15a143f7490ffcfb7f5811594d5c5a8e
AllanZhengYP <zheallan@amazon.com>
2023-06-19 13:07:08 -0700

    feat(s3): implement multiparts upload APIs (#11514)

--- packages/storage/src/AwsClients/S3/index.ts
+++ packages/storage/src/AwsClients/S3/index.ts
@@ -7,3 +7,20 @@ export {
 	ListObjectsV2Output,
 } from './listObjectsV2';
 export { putObject, PutObjectInput, PutObjectOutput } from './putObject';
+export {
+	abortMultipartUpload,
+	AbortMultipartUploadInput,
+	AbortMultipartUploadOutput,
+} from './abortMultipartUpload';
+export {
+	completeMultipartUpload,
+	CompleteMultipartUploadInput,
+	CompleteMultipartUploadOutput,
+} from './completeMultipartUpload';
+export {
+	createMultipartUpload,
+	CreateMultipartUploadInput,
+	CreateMultipartUploadOutput,
+} from './createMultipartUpload';
+export { listParts, ListPartsInput, ListPartsOutput } from './listParts';
+export { uploadPart, UploadPartInput, UploadPartOutput } from './uploadPart';
