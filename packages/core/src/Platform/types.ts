322d94e45507d7fd1e834fe89ae70aeb7edc68bc
Erin Beal <erinleig@amazon.com>
2023-06-30 13:52:47 -0700

    feat: add parameter to customUserAgentDetails to capture other library information

--- packages/core/src/Platform/types.ts
+++ packages/core/src/Platform/types.ts
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import type { UserAgent as AWSUserAgent } from '@aws-sdk/types';
 
 export enum Framework {
 	// < 100 - Web frameworks
@@ -143,6 +144,7 @@ type UserAgentDetailsWithCategory<T extends Category> =
 
 type CustomUserAgentDetailsBase = {
 	framework?: Framework;
+	additionalInfo?: AWSUserAgent;
 };
 
 export type CustomUserAgentDetails =
