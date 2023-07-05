0d55bee3039c071492b332cb8f572828e653b648
Israel Arcos <israel.arcos2595@gmail.com>
2023-07-05 11:52:54 -0400

    chore: mock localstorage

--- packages/core/__tests__/storage/localStorage-test.ts
+++ packages/core/__tests__/storage/localStorage-test.ts
@@ -3,6 +3,34 @@ import { LocalStorage } from "../../src/StorageHelper";
 const key = 'k'
 const value = 'value'
 
+const localStorageMock = (function () {
+	let store = {};
+  
+	return {
+	  getItem(key) {
+		return store[key];
+	  },
+  
+	  setItem(key, value) {
+		store[key] = value;
+	  },
+  
+	  clear() {
+		store = {};
+	  },
+  
+	  removeItem(key) {
+		delete store[key];
+	  },
+  
+	  getAll() {
+		return store;
+	  },
+	};
+  })();
+  
+Object.defineProperty(window, "localStorage", { value: localStorageMock });
+
 describe('LocalStorage', () => {
 	test('test read/write operations', async () => {
 		await LocalStorage.setItem(key, value);
