6a0182db2a02dd45a2fd2d0c5cf69ffeaf452cc1
David McAfee <mcafd@amazon.com>
2023-07-24 10:57:01 -0700

    fix(data): add license to 'v6.ts'

--- packages/api-graphql/src/internals/v6.ts
+++ packages/api-graphql/src/internals/v6.ts
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 import { GraphQLAPI } from '../GraphQLAPI';
 import { GraphQLOptionsV6, GraphQLResponseV6 } from '../types';
 
