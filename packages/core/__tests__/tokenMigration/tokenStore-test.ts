e7220beff7965fe7c1f15bdd482a6d10c2723556
Israel Arcos <israel.arcos2595@gmail.com>
2023-07-27 12:53:08 -0400

    chore: add initial testing

---
+++ packages/core/__tests__/tokenMigration/tokenStore-test.ts
@@ -0,0 +1,110 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { UserPoolTokenManager } from '../../../src/storage/UserPoolTokenManager';
+import { DefaultTokenStore } from '../../src/singleton/Auth/TokenStore';
+import { LocalStorage } from '@aws-amplify/core';
+import { LegacyUserPoolTokenManager } from '../../../src/storage/LegacyUserPoolManager';
+import { AuthError } from '../../../src/errors/AuthError';
+import { AuthErrorCodes } from '../../../src/common/AuthErrorStrings';
+import { LegacyDeviceKeyTokenManager } from '../../../src/storage/LegacyDeviceKeyTokenManager';
+import { KEY_PREFIX } from '../../../src/storage/constants';
+import {
+	clientId,
+	clockDrift,
+	cognitoUserPoolTokens,
+	config,
+	devicekeyTokens,
+	idToken,
+	userData,
+	username,
+} from './testUtils/mocks';
+import {
+	setLegacyDeviceKeysTokens,
+	setLegacyUserPoolTokens,
+} from './testUtils/helpers';
+
+describe('test default token store', () => {
+	const userPoolManager = new DefaultTokenStore()
+    userPoolManager.
+
+	beforeEach(async () => {
+		await userPoolManager.storeTokens(cognitoUserPoolTokens);
+	});
+	afterEach(async () => {
+		await userPoolManager.clearTokens();
+	});
+
+	test('user pool manager should load tokens ', async () => {
+		expect(await userPoolManager.loadTokens()).toEqual(cognitoUserPoolTokens);
+	});
+
+	test('user pool manager should return null if there are no tokens in storage ', async () => {
+		await userPoolManager.clearTokens();
+
+		expect(await userPoolManager.loadTokens()).toEqual(null);
+	});
+
+	test('user pool token manager should store tokens with a scoped key', async () => {
+		const key = `${KEY_PREFIX}.${clientId}.idToken`;
+
+		expect(await LocalStorage.getItem(key)).toEqual(idToken);
+	});
+});
+
+describe('test legacy userpool token manager', () => {
+	const legacyUserPoolManager = new LegacyUserPoolTokenManager(
+		config,
+		LocalStorage
+	);
+	test('legacy userpool manager should not allow to set tokens', async () => {
+		try {
+			await legacyUserPoolManager.storeTokens(cognitoUserPoolTokens);
+		} catch (error) {
+			expect(error).toBeInstanceOf(AuthError);
+			expect(error.name).toBe(AuthErrorCodes.AuthStorageException);
+		}
+	});
+
+	test('legacy user pool manager should load tokens ', async () => {
+		await setLegacyUserPoolTokens();
+		expect(await legacyUserPoolManager.loadTokens()).toEqual({
+			...cognitoUserPoolTokens,
+			clockDrift,
+			userData,
+			lastAuthUser: username,
+		});
+	});
+
+	test('legacyuser pool manager should return null if there are no tokens in storage ', async () => {
+		await legacyUserPoolManager.clearTokens();
+
+		expect(await legacyUserPoolManager.loadTokens()).toEqual(null);
+	});
+});
+
+describe('test legacy device key token manager', () => {
+	const legacyDeviceKeyManager = new LegacyDeviceKeyTokenManager(
+		config,
+		LocalStorage
+	);
+	test('legacy device key manager should not allow to set tokens', async () => {
+		try {
+			await legacyDeviceKeyManager.storeTokens(devicekeyTokens);
+		} catch (error) {
+			expect(error).toBeInstanceOf(AuthError);
+			expect(error.name).toBe(AuthErrorCodes.AuthStorageException);
+		}
+	});
+
+	test('legacy device key manager should load tokens', async () => {
+		await setLegacyDeviceKeysTokens();
+		expect(await legacyDeviceKeyManager.loadTokens()).toEqual(devicekeyTokens);
+	});
+
+	test('legacy device key manager should return null if there are no tokens in storage ', async () => {
+		await legacyDeviceKeyManager.clearTokens();
+
+		expect(await legacyDeviceKeyManager.loadTokens()).toEqual(null);
+	});
+});
