63170d431bcd4db412494601c55d84a3769e69ee
Allan Zheng <zheallan@amazon.com>
2023-07-29 16:10:51 -0700

    feat(storage): add StorageError and credentials validation

---
+++ packages/storage/src/errors/StorageError.ts
@@ -0,0 +1,30 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+	AmplifyError,
+	ConsoleLogger as Logger,
+	ErrorParams,
+} from '@aws-amplify/core';
+
+export class StorageError extends AmplifyError {
+	constructor(params: ErrorParams) {
+		super(params);
+
+		// TODO: Delete the following 2 lines after we change the build target to >= es2015
+		this.constructor = StorageError;
+		Object.setPrototypeOf(this, StorageError.prototype);
+	}
+}
+
+export enum StorageErrorTypes {
+	NO_CREDENTIALS = 'NO_CREDENTIALS',
+}
+
+const StorageErrorStrings: { [key in StorageErrorTypes]: ErrorParams } = {
+	[StorageErrorTypes.NO_CREDENTIALS]: {
+		name: 'NoCredentials',
+		message: 'No credentials',
+		recoverySuggestion: ``,
+	},
+};
---
+++ packages/storage/src/errors/types/validation.ts
@@ -0,0 +1,14 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { AmplifyErrorMap } from '@aws-amplify/core';
+
+export enum StorageValidationErrorCode {
+	NoCredentials = 'NoCredentials',
+}
+
+export const validationErrorMap: AmplifyErrorMap<StorageValidationErrorCode> = {
+	[StorageValidationErrorCode.NoCredentials]: {
+		message: 'Credentials should not be empty',
+	},
+};
