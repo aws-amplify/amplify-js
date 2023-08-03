bcf5946768820d221833f1cd8cc096bd38b918dd
Erin Beal <erinleig@amazon.com>
2023-08-03 10:59:38 -0700

    build: export InternalCognitoUserPool from internals scope

--- packages/amazon-cognito-identity-js/internals/index.d.ts
+++ packages/amazon-cognito-identity-js/internals/index.d.ts
@@ -1,2 +1,34 @@
+import {
+	NodeCallback,
+	ClientMetadata,
+	CognitoUser,
+	CognitoUserAttribute,
+	ICognitoUserPoolData,
+	ISignUpResult,
+} from 'amazon-cognito-identity-js';
+
 export const addAuthCategoryToCognitoUserAgent: () => void;
 export const addFrameworkToCognitoUserAgent: (content: string) => void;
+
+export class InternalCognitoUserPool {
+	constructor(
+		data: ICognitoUserPoolData,
+		wrapRefreshSessionCallback?: (target: NodeCallback.Any) => NodeCallback.Any
+	);
+
+	public getUserPoolId(): string;
+	public getUserPoolName(): string;
+	public getClientId(): string;
+
+	public signUp(
+		username: string,
+		password: string,
+		userAttributes: CognitoUserAttribute[],
+		validationData: CognitoUserAttribute[],
+		callback: NodeCallback<Error, ISignUpResult>,
+		clientMetadata?: ClientMetadata,
+		userAgentValue?: string
+	): void;
+
+	public getCurrentUser(): CognitoUser | null;
+}
