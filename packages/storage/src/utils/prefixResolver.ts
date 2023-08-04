b3493910250eb607a14e232e5d735587b8033ab2
Allan Zheng <zheallan@amazon.com>
2023-08-04 09:19:11 -0700

    feat(storage): implement default prefix resolver

---
+++ packages/storage/src/utils/prefixResolver.ts
@@ -0,0 +1,32 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { StorageAccessLevel } from '@aws-amplify/core';
+import { assertValidationError } from '../errors/assertValidationErrors';
+import { StorageValidationErrorCode } from '../errors/types/validation';
+
+type PrefixResolverOptions = {
+	accessLevel: StorageAccessLevel;
+	targetIdentityId?: string;
+};
+
+export const prefixResolver = async ({
+	accessLevel,
+	targetIdentityId,
+}: PrefixResolverOptions) => {
+	if (accessLevel === 'private') {
+		assertValidationError(
+			!!targetIdentityId,
+			StorageValidationErrorCode.NoIdentityId
+		);
+		return `private/${targetIdentityId}/`;
+	} else if (accessLevel === 'protected') {
+		assertValidationError(
+			!!targetIdentityId,
+			StorageValidationErrorCode.NoIdentityId
+		);
+		return `protected/${targetIdentityId}/`;
+	} else {
+		return 'public/';
+	}
+};
