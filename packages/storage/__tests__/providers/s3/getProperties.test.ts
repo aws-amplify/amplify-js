7c3f728d2c6f1ed927d5a2457465934792292913
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-06 16:45:03 -0700

    chore: add unit test for getProperties

---
+++ packages/storage/__tests__/providers/s3/getProperties.test.ts
@@ -0,0 +1,42 @@
+import { getProperties } from '../../../src/providers/s3';
+
+jest.mock('../../../src/AwsClients/S3');
+const headObject = jest.fn();
+
+test('getProperties API happy path case', async () => {
+	// TODO test credentials
+	headObject.mockImplementation(() => {
+		return {
+			Key: 'key',
+			ContentLength: '100',
+			ContentType: 'text/plain',
+			ETag: 'etag',
+			LastModified: 'last-modified',
+			Metadata: { key: 'value' },
+		};
+	});
+	const metadata = { key: 'value' };
+	expect(await getProperties({ key: 'key' })).toEqual({
+		key: 'key',
+		contentLength: '100',
+		contentType: 'text/plain',
+		eTag: 'etag',
+		lastModified: 'last-modified',
+		metadata,
+	});
+});
+
+test('getProperties API should return a not found error', async () => {
+	// TODO test credentials
+	headObject.mockImplementation(() =>
+		Object.assign(new Error(), {
+			$metadata: { httpStatusCode: 404 },
+			name: 'NotFound',
+		})
+	);
+	try {
+		await getProperties({ key: 'invalid_key' });
+	} catch (error) {
+		expect(error.$metadata.httpStatusCode).toBe(404);
+	}
+});
