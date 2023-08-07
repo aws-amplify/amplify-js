8c7ccc98d6996ffdb827713b6fbd587a0d1f36ed
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-07 15:37:40 -0700

    chore: add testing

--- packages/storage/__tests__/providers/s3/getUrl.test.ts
+++ packages/storage/__tests__/providers/s3/getUrl.test.ts
@@ -1,2 +1,14 @@
-import { getUrl } from '../../../src/providers/s3';
+import { getUrl } from '../../providers/s3';
 jest.mock('../../../src/AwsClients/S3');
+
+const getPresignedGetObjectUrl = jest.fn();
+describe('getUrl happy path case', () => {
+	test.skip('get presigned url happy case', async () => {
+		// TODO test credentials
+		getPresignedGetObjectUrl.mockReturnValueOnce(new URL('url'));
+		expect(await getUrl({ key: 'key' })).toEqual({
+			url: new URL('url'),
+			expiresAt: new Date('December 17, 2023'),
+		});
+	});
+});
