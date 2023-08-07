99906633e7b0e50048e2e90ff832c79976d59efc
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-07 10:05:40 -0700

    chore: add license in test file

--- packages/storage/__tests__/providers/s3/getProperties.test.ts
+++ packages/storage/__tests__/providers/s3/getProperties.test.ts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { getProperties } from '../../../src/providers/s3';
 
 jest.mock('../../../src/AwsClients/S3');
