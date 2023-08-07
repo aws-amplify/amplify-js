ee8270e07825efee7cb875bd682564985cf0cb25
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-07 15:46:37 -0700

    chore: add unit tests

--- packages/storage/__tests__/providers/s3/getUrl.test.ts
+++ packages/storage/__tests__/providers/s3/getUrl.test.ts
@@ -1,14 +1,42 @@
-import { getUrl } from '../../providers/s3';
+import { getUrl } from '../../../src/providers/s3/apis';
 jest.mock('../../../src/AwsClients/S3');
+const headObject = jest.fn();
 
 const getPresignedGetObjectUrl = jest.fn();
 describe('getUrl happy path case', () => {
 	test.skip('get presigned url happy case', async () => {
 		// TODO test credentials
-		getPresignedGetObjectUrl.mockReturnValueOnce(new URL('url'));
-		expect(await getUrl({ key: 'key' })).toEqual({
-			url: new URL('url'),
-			expiresAt: new Date('December 17, 2023'),
+		headObject.mockImplementation(() => {
+			return {
+				Key: 'key',
+				ContentLength: '100',
+				ContentType: 'text/plain',
+				ETag: 'etag',
+				LastModified: 'last-modified',
+				Metadata: { key: 'value' },
+			};
 		});
+		getPresignedGetObjectUrl.mockReturnValueOnce(new URL('url'));
+		const result = await getUrl({ key: 'key' });
+		expect(result.url).toEqual({
+			url: new URL('url'),
+		});
+	});
+});
+
+describe('getUrl error path case', () => {
+	test.skip('Should return not found error when the object is not found', async () => {
+		headObject.mockImplementation(() =>
+			Object.assign(new Error(), {
+				$metadata: { httpStatusCode: 404 },
+				name: 'NotFound',
+			})
+		);
+		try {
+			await getUrl({ key: 'invalid_key' });
+		} catch (error) {
+			console.log('Error testing', error);
+			expect(error.$metadata?.httpStatusCode).toBe(404);
+		}
 	});
 });
