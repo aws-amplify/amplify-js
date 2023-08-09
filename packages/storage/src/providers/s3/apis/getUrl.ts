ef6206c0d5ca1498f484a4b0c68c48d6b4192e61
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-09 15:25:58 -0700

    chore: removing common functionality

--- packages/storage/src/providers/s3/apis/getUrl.ts
+++ packages/storage/src/providers/s3/apis/getUrl.ts
@@ -1,19 +1,17 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-
-import { AmplifyV6 } from '@aws-amplify/core';
 import { S3GetUrlOptions, S3GetUrlResult } from '../types';
 import { assertValidationError } from '../../../errors/assertValidationErrors';
 import { StorageValidationErrorCode } from '../../../errors/types/validation';
-import { prefixResolver as defaultPrefixResolver } from '../../../utils/prefixResolver ';
 import {
-	SERVICE_NAME as S3_SERVICE_NAME,
 	GetObjectInput,
 	getPresignedGetObjectUrl,
 } from '../../../AwsClients/S3';
 import { getProperties } from './getProperties';
 import { StorageDownloadDataRequest } from '../../../types/params';
 import { GetPropertiesException } from '../types/errors';
+import { getStorageConfig, getFinalKey } from '../utils/apiHelper';
+
 const DEFAULT_PRESIGN_EXPIRATION = 900;
 
 /**
@@ -32,34 +30,24 @@ export const getUrl = async function (
 	req: StorageDownloadDataRequest<S3GetUrlOptions>
 ): Promise<S3GetUrlResult> {
 	const options = req?.options;
-	// TODO extract common functionality
-	const { awsCreds, awsCredsIdentityId } =
-		await AmplifyV6.Auth.fetchAuthSession();
-	assertValidationError(!!awsCreds, StorageValidationErrorCode.NoCredentials);
-	const { bucket, region, defaultAccessLevel } = AmplifyV6.getConfig().Storage;
-	assertValidationError(!!bucket, StorageValidationErrorCode.NoBucket);
-	assertValidationError(!!region, StorageValidationErrorCode.NoRegion);
+	const { awsCredsIdentityId, awsCreds, defaultAccessLevel, bucket, region } =
+		await getStorageConfig();
 	const { key, options: { accessLevel = defaultAccessLevel } = {} } = req;
-	const { prefixResolver = defaultPrefixResolver } =
-		AmplifyV6.libraryOptions?.Storage ?? {};
 	assertValidationError(!!key, StorageValidationErrorCode.NoKey);
 	if (options?.validateObjectExistence) {
 		await getProperties(key);
 	}
-	const finalKey =
-		prefixResolver({
-			accessLevel,
-			targetIdentityId: awsCredsIdentityId,
-		}) + key;
+	const finalKey = getFinalKey(accessLevel, awsCredsIdentityId, key);
 	const getUrlParams: GetObjectInput = {
 		Bucket: bucket,
 		Key: finalKey,
 	};
 	const getUrlOptions = {
+		accessLevel,
+		targetIdentityId: awsCredsIdentityId,
 		expiration: options?.expiration ?? DEFAULT_PRESIGN_EXPIRATION,
 		credentials: awsCreds,
 		signingRegion: region,
-		signingService: S3_SERVICE_NAME,
 		region,
 	};
 	let result: S3GetUrlResult;
