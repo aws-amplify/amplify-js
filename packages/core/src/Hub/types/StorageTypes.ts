368f6ea016b9173ece8a3a84c857f7c9272c4cb2
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-16 16:49:04 -0700

    chore: add storage types

---
+++ packages/core/src/Hub/types/StorageTypes.ts
@@ -0,0 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// TODO Need to update types of data and add more events
+export type StorageHubEventData = { event: 'upload'; data: any };
