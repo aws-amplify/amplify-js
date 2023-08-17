adf775a9ae0586e7ff9d5c9bed34c4681d3dba52
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-17 08:46:14 -0700

    chore: add missing license header

--- packages/core/src/Hub/types/index.ts
+++ packages/core/src/Hub/types/index.ts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 export * from './HubTypes';
 export * from './AnalyticsTypes';
 export * from './AuthTypes';
