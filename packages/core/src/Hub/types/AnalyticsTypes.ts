2e5f757e1b9e485c6fd91c00d9f7b463f4c052b5
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-17 17:14:39 -0700

    chore: remove Analytics types

---
+++ packages/core/src/Hub/types/AnalyticsTypes.ts
@@ -0,0 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// TODO Need to update types of data
+export type NotificationsHubEventData = { event: 'record'; data: any };
