4646400a6c896b4ad0bf4b3211666db56b8f3d19
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-20 22:12:01 -0700

    chore: change import of fetchAuth session

--- packages/storage/src/providers/s3/utils/resolveCredentials.ts
+++ packages/storage/src/providers/s3/utils/resolveCredentials.ts
@@ -1,13 +1,14 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { AmplifyV6 } from '@aws-amplify/core';
+import { fetchAuthSession } from 'aws-amplify/auth';
 import { assertValidationError } from '../../../errors/utils/assertValidationError';
 import { StorageValidationErrorCode } from '../../../errors/types/validation';
 
 export async function resolveCredentials() {
-	// TODO[kvramya7] import fetchAuthSession directly from `aws-amplify`
-	const { identityId, credentials } = await AmplifyV6.Auth.fetchAuthSession();
+	const { identityId, credentials } = await fetchAuthSession({
+		forceRefresh: false,
+	});
 	assertValidationError(
 		!!credentials,
 		StorageValidationErrorCode.NoCredentials
