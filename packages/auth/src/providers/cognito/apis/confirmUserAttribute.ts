9916aa27ec5c94ce3be65cf711cfc66de48c42e0
Israel Arcos <israel.arcos2595@gmail.com>
2023-08-23 10:02:07 -0400

    chore: address pr comments

--- packages/auth/src/providers/cognito/apis/confirmUserAttribute.ts
+++ packages/auth/src/providers/cognito/apis/confirmUserAttribute.ts
@@ -1,29 +1,27 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import { AmplifyV6 as Amplify } from '@aws-amplify/core';
+import { assertTokenProviderConfig } from '@aws-amplify/core/internals/utils';
 import { AuthValidationErrorCode } from '../../../errors/types/validation';
 import { assertValidationError } from '../../../errors/utils/assertValidationError';
 import { ConfirmUserAttributeRequest } from '../../../types/requests';
 import { verifyUserAttribute } from '../utils/clients/CognitoIdentityProvider';
 import { VerifyUserAttributeException } from '../types/errors';
-import {
-	AmplifyV6 as Amplify,
-	assertTokenProviderConfig,
-} from '@aws-amplify/core';
 import { fetchAuthSession } from '../../../';
 import { getRegion } from '../utils/clients/CognitoIdentityProvider/utils';
 import { assertAuthTokens } from '../utils/types';
 import { CognitoUserAttributeKey } from '../types';
 
 /**
- * Confirm the user attribute with the confirmation code.
+ * Confirms a user attribute with the confirmation code. 
  *
- * @param confirmUserAttributeRequest - The ConfirmUserAttributeRequest
+ * @param confirmUserAttributeRequest - The ConfirmUserAttributeRequest 
  *
  * @throws  -{@link AuthValidationErrorCode } -
  * Thrown when `confirmationCode` is not defined.
  *
- * @throws  -{@link VerifyUserAttributeException }
+ * @throws  -{@link VerifyUserAttributeException } - Thrown due to an invalid confirmation code or attribute.
  *
  * @throws AuthTokenConfigException - Thrown when the token provider config is invalid.
  */
