94c3e0fd3c33625ccaf9f09f3dea7e5fee12fb2f
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-23 11:58:08 -0700

    chore: change event and types in AuthTypes

--- packages/core/src/Hub/types/AuthTypes.ts
+++ packages/core/src/Hub/types/AuthTypes.ts
@@ -3,33 +3,8 @@
 
 // TODO Need to update types of data
 export type AuthHubEventData =
-	| { event: 'signIn'; data: any }
-	| { event: 'signUp'; data: any }
-	| { event: 'signUpFailure'; data: any }
-	| { event: 'signIn_failure'; data: any }
-	| { event: 'confirmSignUp'; data: any }
-	| { event: 'signOut'; data: any }
-	| { event: 'cognitoHostedUI'; data: any }
-	| { event: 'tokenRefresh_failure'; data: Error | undefined }
-	| { event: 'completeNewPassword_failure'; data: Error }
-	| { event: 'userDeleted'; data: string }
-	| { event: 'updateUserAttributes_failure'; data: Error }
-	| { event: 'updateUserAttributes'; data: Record<string, string> }
-	| { event: 'forgotPassword_failure'; data: Error }
-	| { event: 'verify'; data: any }
-	| { event: 'tokenRefresh'; data: undefined }
-	| { event: 'configured'; data: null }
-	| { event: 'autoSignIn'; data: any }
-	| { event: 'forgotPassword'; data: any }
-	| {
-			event: 'parsingCallbackUrl';
-			data: {
-				url: string | undefined;
-			};
-	  }
-	| { event: 'customOAuthState'; data: string }
-	| { event: 'cognitoHostedUI_failure'; data: Error }
-	| { event: 'customState_failure'; data: Error }
-	| { event: 'forgotPasswordSubmit'; data: any }
-	| { event: 'forgotPasswordSubmit_failure'; data: Error }
-	| { event: 'autoSignIn_failure'; data: null };
+	| { event: 'signInWithRedirect' } // Used when an oauth flow is done
+	| { event: 'tokenRefresh' } // used when a token is refreshed
+	| { event: 'tokenRefresh_failure' } // used when the refresh of tokens failed
+	| { event: 'customOAuthState' }
+	| { event: 'customState_failure' };
