0a11c0083e687ab13c687b0b01fc0f192b379032
Sridhar <ashwsrir@amazon.com>
2023-08-22 17:22:18 -0700

    fix: build errors

--- packages/storage/src/Storage.ts
+++ packages/storage/src/Storage.ts
@@ -3,7 +3,7 @@
 
 import { Amplify } from '@aws-amplify/core';
 import { ConsoleLogger as Logger } from '@aws-amplify/core/internals/utils';
-import { AWSS3Provider } from './providers';
+import { AWSS3Provider } from './providers/AWSS3Provider';
 import {
 	StorageCopySource,
 	StorageCopyDestination,
