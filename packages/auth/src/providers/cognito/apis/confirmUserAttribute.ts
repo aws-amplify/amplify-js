7cddc7cea7e42102cd8f0d4c32d893cd8ca8e4ce
israx <70438514+israx@users.noreply.github.com>
2023-08-23 16:57:16 -0400

    feat(auth): add confirmUserAttribute API (#11845)
    * feat: add types
    
    * feat: add confirmUserAttribute API
    
    * chore: add tests
    
    * chore: address previous feedback
    
    * chore: fix lint tests
    
    * chore: address pr comments
    
    * fix unit test

---
+++ packages/auth/src/providers/cognito/apis/confirmUserAttribute.ts
@@ -0,0 +1,50 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { AmplifyV6 as Amplify } from '@aws-amplify/core';
+import { assertTokenProviderConfig } from '@aws-amplify/core/internals/utils';
+import { AuthValidationErrorCode } from '../../../errors/types/validation';
+import { assertValidationError } from '../../../errors/utils/assertValidationError';
+import { ConfirmUserAttributeRequest } from '../../../types/requests';
+import { verifyUserAttribute } from '../utils/clients/CognitoIdentityProvider';
+import { VerifyUserAttributeException } from '../types/errors';
+import { fetchAuthSession } from '../../../';
+import { getRegion } from '../utils/clients/CognitoIdentityProvider/utils';
+import { assertAuthTokens } from '../utils/types';
+import { CognitoUserAttributeKey } from '../types';
+
+/**
+ * Confirms a user attribute with the confirmation code. 
+ *
+ * @param confirmUserAttributeRequest - The ConfirmUserAttributeRequest 
+ *
+ * @throws  -{@link AuthValidationErrorCode } -
+ * Thrown when `confirmationCode` is not defined.
+ *
+ * @throws  -{@link VerifyUserAttributeException } - Thrown due to an invalid confirmation code or attribute.
+ *
+ * @throws AuthTokenConfigException - Thrown when the token provider config is invalid.
+ */
+export async function confirmUserAttribute(
+	confirmUserAttributeRequest: ConfirmUserAttributeRequest<CognitoUserAttributeKey>
+): Promise<void> {
+	const authConfig = Amplify.getConfig().Auth;
+	assertTokenProviderConfig(authConfig);
+	const { confirmationCode, userAttributeKey } = confirmUserAttributeRequest;
+	assertValidationError(
+		!!confirmationCode,
+		AuthValidationErrorCode.EmptyConfirmUserAttributeCode
+	);
+	const { tokens } = await fetchAuthSession({ forceRefresh: false });
+	assertAuthTokens(tokens);
+	await verifyUserAttribute(
+		{
+			region: getRegion(authConfig.userPoolId),
+		},
+		{
+			AccessToken: tokens.accessToken.toString(),
+			AttributeName: userAttributeKey,
+			Code: confirmationCode,
+		}
+	);
+}
