155370b49bbedee7f751e14e89ce8df725f4c0dd
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-23 08:13:03 -0700

    chore: remove storage types

--- packages/core/src/Hub/types/StorageTypes.ts
+++
@@ -1,5 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// TODO Need to update types of data and add more events
-export type StorageHubEventData = { event: 'upload'; data: any };
