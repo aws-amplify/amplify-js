7f8b9bb5cdecfc06ffe01670d3025f2b3c1d5c88
Jim Blanchard <jablanch@amazon.com>
2023-08-24 07:14:11 -0500

    Initial commit.

--- packages/core/src/providers/pinpoint/utils/EventBuffer.ts
+++ packages/core/src/providers/pinpoint/utils/EventBuffer.ts
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { AnalyticsAction, ConsoleLogger as Logger } from '@aws-amplify/core/internals/utils';
+import { ConsoleLogger as Logger } from '../../../Logger';
 import {
 	putEvents,
 	PutEventsInput,
