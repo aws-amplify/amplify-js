b92813a12a1b2f01870412d71d77b6e4f2cf1c68
Israel Arcos <israel.arcos2595@gmail.com>
2023-08-24 09:09:34 -0400

    feat: add types

--- packages/auth/src/types/requests.ts
+++ packages/auth/src/types/requests.ts
@@ -135,3 +135,9 @@ export type UpdateUserAttributesRequest<
 export type ConfirmUserAttributeRequest<
 	UserAttributeKey extends AuthUserAttributeKey = AuthUserAttributeKey
 > = { userAttributeKey: UserAttributeKey; confirmationCode: string };
+
+/**
+ * Constructs a `FetchUserAttributes` request.
+ * @param recache - whether to recache the user
+ */
+export type FetchUserAttributesRequest = { recache: boolean };
