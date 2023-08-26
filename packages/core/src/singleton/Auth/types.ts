4787376910ee988c6c4a5969fb9898845bd477d1
ManojNB <manojnb95@gmail.com>
2023-08-25 17:45:58 -0700

    fix: clear out identityId as well when clearing credentials for sign out

--- packages/core/src/singleton/Auth/types.ts
+++ packages/core/src/singleton/Auth/types.ts
@@ -50,7 +50,7 @@ export interface AWSCredentialsAndIdentityIdProvider {
 	getCredentialsAndIdentityId: (
 		getCredentialsOptions: GetCredentialsOptions
 	) => Promise<AWSCredentialsAndIdentityId>;
-	clearCredentials: () => void;
+	clearCredentialsAndIdenityId: () => void;
 }
 
 export type TokenProvider = {
