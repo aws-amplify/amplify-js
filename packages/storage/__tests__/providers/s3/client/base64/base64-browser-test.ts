f3c85a7cbfa1ede57e64eeb6939e957ecf267905
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-08-28 15:43:11 -0700

    chore: add new files

---
+++ packages/storage/__tests__/providers/s3/client/base64/base64-browser-test.ts
@@ -0,0 +1,18 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { TextEncoder, TextDecoder } from 'util';
+import { toBase64TestCases } from './cases';
+import { toBase64 } from '../../../../../src/providers/s3/utils/client/runtime/base64/index.native';
+
+Object.assign(global, { TextDecoder, TextEncoder });
+
+describe('base64 until for browser', () => {
+	describe('toBase64()', () => {
+		for (let { input, expected } of toBase64TestCases) {
+			it(`it should base64 encode ${input}`, () => {
+				expect(toBase64(input)).toStrictEqual(expected);
+			});
+		}
+	});
+});
