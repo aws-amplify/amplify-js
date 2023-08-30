4e6b4440b90c18800c52d1d675f04da48c86e33e
AllanZhengYP <zheallan@amazon.com>
2023-08-30 16:04:00 -0700

    chore(storage): remove custom client support of SSE-C & re-enable skipped tests (#11932)
    ---------
    
    Co-authored-by: Jim Blanchard <jim.l.blanchard@gmail.com>
    Co-authored-by: Aaron S <94858815+stocaaro@users.noreply.github.com>

--- packages/storage/src/providers/s3/utils/client/uploadPart.ts
+++ packages/storage/src/providers/s3/utils/client/uploadPart.ts
@@ -17,7 +17,6 @@ import {
 	map,
 	parseXmlError,
 	s3TransferHandler,
-	serializeObjectSsecOptionsToHeaders,
 	serializePathnameObjectKey,
 } from './utils';
 
@@ -25,16 +24,7 @@ import {
 // and will be set by browser or fetch polyfill.
 export type UploadPartInput = Pick<
 	UploadPartCommandInput,
-	| 'PartNumber'
-	| 'Body'
-	| 'UploadId'
-	| 'Bucket'
-	| 'Key'
-	| 'ContentMD5'
-	| 'SSECustomerAlgorithm'
-	| 'SSECustomerKey'
-	// TODO(AllanZhengYP): remove in V6.
-	| 'SSECustomerKeyMD5'
+	'PartNumber' | 'Body' | 'UploadId' | 'Bucket' | 'Key' | 'ContentMD5'
 >;
 
 export type UploadPartOutput = Pick<
@@ -47,7 +37,6 @@ const uploadPartSerializer = async (
 	endpoint: Endpoint
 ): Promise<HttpRequest> => {
 	const headers = {
-		...(await serializeObjectSsecOptionsToHeaders(input)),
 		...assignStringVariables({ 'content-md5': input.ContentMD5 }),
 	};
 	headers['content-type'] = 'application/octet-stream';
