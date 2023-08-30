c2e511a3a28d7e4df37620a8895c11611cc3d37f
Chris Fang <cshfang@gmail.com>
2023-08-30 14:02:26 -0700

    chore: Update parseAWSExports to v6 config

--- packages/core/__tests__/parseAWSExports-test.ts
+++ packages/core/__tests__/parseAWSExports-test.ts
@@ -2,102 +2,72 @@ import { parseAWSExports } from '../src/parseAWSExports';
 
 describe('Parser', () => {
 	test('aws_mobile_analytics_app_id', () => {
+		const appId = 'app-id';
+		const bucket = 'bucket';
+		const identityPoolId = 'identity-pool-id';
+		const userPoolId = 'user-pool-id';
+		const userPoolClientId = 'user-pool-client-id';
+		const signUpVerificationMethod = 'link';
+		const region = 'region';
+		const amazonLocationService = {
+			maps: {
+				items: {
+					geoJsExampleMap1: {
+						style: 'VectorEsriStreets',
+					},
+					geoJsExampleMap2: {
+						style: 'VectorEsriTopographic',
+					},
+				},
+				default: 'geoJsExampleMap1',
+			},
+			search_indices: {
+				items: ['geoJSSearchExample'],
+				default: 'geoJSSearchExample',
+			},
+			region,
+		};
 		expect(
 			parseAWSExports({
-				aws_cognito_identity_pool_id: 'a',
-				aws_user_pools_id: 'b',
-				aws_mobile_analytics_app_id: 'c',
-				aws_mobile_analytics_app_region: '',
+				aws_cognito_identity_pool_id: identityPoolId,
+				aws_cognito_sign_up_verification_method: signUpVerificationMethod,
 				aws_mandatory_sign_in: 'enable',
-				aws_user_pools_web_client_id: '',
-				aws_cognito_region: '',
+				aws_mobile_analytics_app_id: appId,
+				aws_mobile_analytics_app_region: region,
+				aws_user_files_s3_bucket: bucket,
+				aws_user_files_s3_bucket_region: region,
+				aws_user_pools_id: userPoolId,
+				aws_user_pools_web_client_id: userPoolClientId,
 				geo: {
-					amazon_location_service: {
-						maps: {
-							items: {
-								geoJsExampleMap1: {
-									style: 'VectorEsriStreets',
-								},
-								geoJsExampleMap2: {
-									style: 'VectorEsriTopographic',
-								},
-							},
-							default: 'geoJsExampleMap1',
-						},
-						search_indices: {
-							items: ['geoJSSearchExample'],
-							default: 'geoJSSearchExample',
-						},
-						region: 'us-west-2',
-					},
+					amazon_location_service: amazonLocationService,
 				},
 			})
 		).toStrictEqual({
 			Analytics: {
 				Pinpoint: {
-					appId: 'c',
-					region: '',
+					appId,
+					region,
 				},
 			},
 			Auth: {
-				identityPoolId: 'a',
-				identityPoolRegion: '',
-				mandatorySignIn: true,
-				region: '',
-				userPoolId: 'b',
-				userPoolWebClientId: '',
-				signUpVerificationMethod: 'code',
+				Cognito: {
+					identityPoolId,
+					allowGuestAccess: false,
+					signUpVerificationMethod,
+					userPoolId,
+					userPoolClientId,
+				},
 			},
 			Geo: {
-				AmazonLocationService: {
-					maps: {
-						items: {
-							geoJsExampleMap1: {
-								style: 'VectorEsriStreets',
-							},
-							geoJsExampleMap2: {
-								style: 'VectorEsriTopographic',
-							},
-						},
-						default: 'geoJsExampleMap1',
-					},
-					search_indices: {
-						items: ['geoJSSearchExample'],
-						default: 'geoJSSearchExample',
-					},
-					region: 'us-west-2',
-				},
+				AmazonLocationService: amazonLocationService,
 			},
 			Storage: {
-				aws_cognito_identity_pool_id: 'a',
-				aws_cognito_region: '',
-				aws_mandatory_sign_in: 'enable',
-				aws_mobile_analytics_app_id: 'c',
-				aws_mobile_analytics_app_region: '',
-				aws_user_pools_id: 'b',
-				aws_user_pools_web_client_id: '',
-				geo: {
-					amazon_location_service: {
-						maps: {
-							items: {
-								geoJsExampleMap1: {
-									style: 'VectorEsriStreets',
-								},
-								geoJsExampleMap2: {
-									style: 'VectorEsriTopographic',
-								},
-							},
-							default: 'geoJsExampleMap1',
-						},
-						search_indices: {
-							items: ['geoJSSearchExample'],
-							default: 'geoJSSearchExample',
-						},
-						region: 'us-west-2',
-					},
+				S3: {
+					bucket,
+					region,
+					dangerouslyConnectToHttpEndpointForTesting: undefined,
 				},
 			},
-			Logging: {},
 		});
 	});
 });
