7a3196922d413d66a499a2813934709eeaa1e3ad
Jim Blanchard <jablanch@amazon.com>
2023-08-30 18:16:32 -0500

    chore: Remove more types.

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,22 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import {
-	CognitoHostedUIIdentityProvider,
-	SignUpParams,
-	GRAPHQL_AUTH_MODE,
-} from './types/Auth';
-import { AuthErrorStrings } from './common/AuthErrorStrings';
-
-export {
-	CognitoHostedUIIdentityProvider,
-	SignUpParams,
-	AuthErrorStrings,
-	GRAPHQL_AUTH_MODE,
-};
-
-// Default provider APIs & types
+// Default provider APIs & enums
 export * from './providers/cognito';
 export * from './types/enums';
+
+export { AuthError } from './errors/AuthError';
 
 export { fetchAuthSession } from '@aws-amplify/core';
