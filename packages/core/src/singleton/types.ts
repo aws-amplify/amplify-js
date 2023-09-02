9c21be05a052c465eb7b6200a5d6b680e551462f
David McAfee <mcafd@amazon.com>
2023-09-01 19:08:54 -0700

    remove pubsub imports

--- packages/core/src/singleton/types.ts
+++ packages/core/src/singleton/types.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { APIGraphQLConfig, LibraryAPIGraphQLOptions } from
+import { APIGraphQLConfig, LibraryAPIGraphQLOptions } from './API/types';
 import { AnalyticsConfig } from './Analytics/types';
 import {
 	AuthConfig,
