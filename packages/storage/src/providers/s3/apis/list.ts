48792cf910ce871b8b2e29251ff6c29b5b8d2ddc
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-09-06 09:15:09 -0700

    fix: fix linting in description

--- packages/storage/src/providers/s3/apis/list.ts
+++ packages/storage/src/providers/s3/apis/list.ts
@@ -12,7 +12,8 @@ import { list as listInternal } from './internal/list';
 
 type S3ListApi = {
 	/**
-	 * List files from your S3 bucket. You can paginate through files by passing page size and nextToken from previous api call.
+	 * List files from your S3 bucket.
+	 * You can paginate through files by passing page size and nextToken from previous api call.
 	 * @param {StorageListRequest<StorageListPaginateOptions>} req - The request object
 	 * @return {Promise<S3ListPaginateResult>} - Promise resolves to list of keys and metadata with
 	 * pageSize defaulting to 1000. Additionally the result will include a nextToken if there are more items to retrieve
