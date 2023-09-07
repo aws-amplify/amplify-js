f2d11179a57d2fb74caefbb460a69b13c723503c
Venkata Ramyasri Kota <34170013+kvramyasri7@users.noreply.github.com>
2023-09-07 09:45:19 -0700

    Update packages/storage/src/providers/s3/apis/getUrl.ts
    Co-authored-by: AllanZhengYP <zheallan@amazon.com>

--- packages/storage/src/providers/s3/apis/getUrl.ts
+++ packages/storage/src/providers/s3/apis/getUrl.ts
@@ -9,7 +9,7 @@ import { getUrl as getUrlInternal } from './internal/getUrl';
 /**
  * Get a temporary presigned URL to download the specified S3 object.
  * The presigned URL expires when the associated role used to sign the request expires or
- * the option  `expires in` is reached.The `expiresAt` property indicates when the URL MAY expire.
+ * the option  `expiresIn` is reached. The `expiresAt` property in the output object indicates when the URL MAY expire.
  * By default, it will not validate the object that exists in S3. If you set the `options.validateObjectExistence`
  * to true, this method will verify the given object already exists in S3 before returning a presigned
  * URL, and will throw {@link StorageError} if the object does not exist.
