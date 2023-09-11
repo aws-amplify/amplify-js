73dc34743a2cb08ea6a5e35c0bebfa097b85b600
David McAfee <mcafd@amazon.com>
2023-09-11 12:17:39 -0700

    add API category export mapping to 'aws-amplify'

---
+++ packages/aws-amplify/src/api/index.ts
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+This file maps exports from `aws-amplify/api`. It provides access to the default API provider and category utils.
+*/
+export * from '@aws-amplify/api';
