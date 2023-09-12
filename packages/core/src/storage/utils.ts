46bf307ccba11eef4ed6b2bcc31428ba344ed81a
Chris Fang <cshfang@gmail.com>
2023-09-12 15:26:10 -0700

    Simplify local and sessionStorage checks

--- packages/core/src/storage/utils.ts
+++ packages/core/src/storage/utils.ts
@@ -3,32 +3,16 @@
 
 import { InMemoryStorage } from './InMemoryStorage';
 
-const TEST_KEY = 'aws.amplify.test-ls';
-
 /**
  * @internal
  * @returns Either a reference to window.localStorage or an in-memory storage as fallback
  */
-export const getDefaultStorageWithFallback = (): Storage => {
-	try {
-		window.localStorage.setItem(TEST_KEY, '');
-		window.localStorage.removeItem(TEST_KEY);
-		return window.localStorage;
-	} catch (_) {
-		return new InMemoryStorage();
-	}
-};
+export const getDefaultStorageWithFallback = (): Storage =>
+	window?.localStorage ? window.localStorage : new InMemoryStorage();
 
 /**
  * @internal
  * @returns Either a reference to window.sessionStorage or an in-memory storage as fallback
  */
-export const getSessionStorageWithFallback = (): Storage => {
-	try {
-		window.sessionStorage.setItem(TEST_KEY, '');
-		window.sessionStorage.removeItem(TEST_KEY);
-		return window.sessionStorage;
-	} catch (_) {
-		return new InMemoryStorage();
-	}
-};
+export const getSessionStorageWithFallback = (): Storage =>
+	window?.sessionStorage ? window.sessionStorage : new InMemoryStorage();
