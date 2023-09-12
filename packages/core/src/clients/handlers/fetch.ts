2119707b7b7bfbb0a6548982cad70d22979bd67d
Hui Zhao <zhohz@amazon.com>
2023-09-12 15:43:32 -0700

    chore(core): remove the isomorphic-unfetch dependency

--- packages/core/src/clients/handlers/fetch.ts
+++ packages/core/src/clients/handlers/fetch.ts
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import 'isomorphic-unfetch'; // TODO: remove this dependency in v6
 import { HttpRequest, HttpResponse, HttpTransferOptions } from '../types/http';
 import { TransferHandler } from '../types/core';
 import { withMemoization } from '../utils/memoization';
