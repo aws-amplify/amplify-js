e2f80b25a632646766a83ee36f2f3df7f7e58eb6
Allan Zheng <zheallan@amazon.com>
2023-09-13 10:23:35 -0700

    feat(storage): export additional types

--- packages/storage/src/types/index.ts
+++ packages/storage/src/types/index.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export { DownloadTask, TransferProgressEvent, UploadTask } from './common';
+export { TransferProgressEvent, TransferTaskState } from './common';
 export {
 	StorageOperationInput,
 	StorageListInput,
