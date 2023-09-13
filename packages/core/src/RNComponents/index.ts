bf5b5c969512055b569209bc1bfada1d6f346a6f
Hui Zhao <zhohz@amazon.com>
2023-09-12 17:29:38 -0700

    chore(core): remove no longer used isNode check

--- packages/core/src/RNComponents/index.ts
+++ packages/core/src/RNComponents/index.ts
@@ -1,16 +1,16 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { browserOrNode } from '../Util/JS';
+import { isBrowser } from '../Util/JS';
 import { StorageHelper } from '../StorageHelper';
 
 export const Linking = {};
 export const AppState = {
 	addEventListener: (action: any, handler: any) => undefined,
-	currentState: 'active'
+	currentState: 'active',
 };
 
 // if not in react native, just use local storage
-export const AsyncStorage = browserOrNode().isBrowser
+export const AsyncStorage = isBrowser()
 	? new StorageHelper().getStorage()
 	: undefined;
