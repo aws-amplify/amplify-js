359e1bc69794cd41fb352034dbef525fb2d41cd0
Di Wu <diwud@amazon.com>
2023-09-13 11:40:27 -0700

    chore(analytics): setup module for analytics service provider kinesis

---
+++ packages/analytics/src/providers/kinesis/apis/identifyUser.ts
@@ -0,0 +1,10 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { IdentifyUserParameters } from '../types';
+
+export const identifyUser = async (
+	parameters: IdentifyUserParameters
+): Promise<void> => {
+	throw new Error('Not Yet Implemented!');
+};
---
+++ packages/analytics/src/providers/kinesis/apis/record.ts
@@ -0,0 +1,8 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { RecordParameters } from '../types';
+
+export const record = (parameters: RecordParameters): void => {
+	throw new Error('Not Yet Implemented!');
+};
