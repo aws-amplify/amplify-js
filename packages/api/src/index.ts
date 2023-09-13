d3162560bb39a027f264e38d8aeca90ca660387d
David McAfee <mcafd@amazon.com>
2023-09-13 15:20:56 -0700

    add todo to api exports

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// TODO(v6): revisit exports
+
 export { GraphQLQuery, GraphQLSubscription } from './types';
 import { API } from './API';
 
