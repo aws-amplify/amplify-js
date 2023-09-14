5e2414af8f796024d6fba2433e48b8a8a72df335
elorzafe <elorzafe@amazon.com>
2023-09-14 12:31:12 -0700

    update workspace

--- packages/datastore/src/datastore/datastore.ts
+++ packages/datastore/src/datastore/datastore.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { InternalAPI } from '@aws-amplify/api/internals';
-import { Amplify, Hub, Cache } from '@aws-amplify/core';
+import { Hub, Cache } from '@aws-amplify/core';
 
 import {
 	Draft,
