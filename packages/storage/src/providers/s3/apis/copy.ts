e753f3bb255c88b470b8819a89aff4983bf2cd41
AllanZhengYP <zheallan@amazon.com>
2023-09-14 16:49:02 -0700

    feat(storage): change enums to string unions (#12056)
    ---------
    
    Co-authored-by: Ashwin Kumar <ashwinkumar2468@gmail.com>

--- packages/storage/src/providers/s3/apis/copy.ts
+++ packages/storage/src/providers/s3/apis/copy.ts
@@ -2,16 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Amplify } from '@aws-amplify/core';
-import { CopyInput, CopyOutput } from '../types';
+import { CopyInput, CopyOutput, S3Exception } from '../types';
 import { copy as copyInternal } from './internal/copy';
+import { StorageValidationErrorCode } from '../../../errors/types/validation';
 
 /**
  * Copy an object from a source object to a new object within the same bucket. Can optionally copy files across
  * different level or identityId (if source object's level is 'protected').
  *
- * @async
- * @param {CopyInput} input - The request object.
- * @return {Promise<CopyOutput>} Promise resolves upon successful copy of the object.
+ * @param input - The CopyInput object.
+ * @returns Output containing the destination key.
  * @throws service: {@link S3Exception} - Thrown when checking for existence of the object
  * @throws validation: {@link StorageValidationErrorCode } - Thrown when
  * source or destination key are not defined.
--- packages/storage/src/providers/s3/apis/remove.ts
+++ packages/storage/src/providers/s3/apis/remove.ts
@@ -7,8 +7,8 @@ import { remove as removeInternal } from './internal/remove';
 
 /**
  * Remove a file from your S3 bucket.
- * @param {RemoveInput} The input object
- * @return {Promise<RemoveOutput>} - Promise resolves upon successful removal of the object
+ * @param input - The RemoveInput object.
+ * @return Output containing the removed object key
  * @throws service: {@link S3Exception} - S3 service errors thrown while getting properties
  * @throws validation: {@link StorageValidationErrorCode } - Validation errors thrown
  */
