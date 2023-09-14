d5af6b32598c5034b042ac668e8298e2e6c402fb
Sridhar <ashwsrir@amazon.com>
2023-09-14 04:11:32 -0700

    chore(auth): add doc string

--- packages/auth/src/providers/cognito/types/inputs.ts
+++ packages/auth/src/providers/cognito/types/inputs.ts
@@ -31,53 +31,110 @@ import {
 	AuthVerifyTOTPSetupInput,
 } from '../../../types';
 
+/**
+ * Input type for Cognito getCurrentUser API.
+ */
 export type GetCurrentUserInput = AuthGetCurrentUserInput;
 
+/**
+ * Input type for Cognito confirmResetPassword API.
+ */
 export type ConfirmResetPasswordInput =
 	AuthConfirmResetPasswordInput<ConfirmResetPasswordOptions>;
 
+/**
+ * Input type for Cognito confirmSignIn API.
+ */
 export type ConfirmSignInInput = AuthConfirmSignInInput<ConfirmSignInOptions>;
 
+/**
+ * Input type for Cognito confirmSignUp API.
+ */
 export type ConfirmSignUpInput = AuthConfirmSignUpInput<ConfirmSignUpOptions>;
 
+/**
+ * Input type for Cognito confirmUserAttribute API.
+ */
 export type ConfirmUserAttributeInput =
 	AuthConfirmUserAttributeInput<CognitoUserAttributeKey>;
 
+/**
+ * Input type for Cognito resendSignUpCode API.
+ */
 export type ResendSignUpCodeInput =
 	AuthResendSignUpCodeInput<ResendSignUpCodeOptions>;
 
+/**
+ * Input type for Cognito resetPassword API.
+ */
 export type ResetPasswordInput = AuthResetPasswordInput<ResetPasswordOptions>;
 
+/**
+ * Input type for Cognito signIn API.
+ */
 export type SignInInput = AuthSignInInput<SignInOptions>;
 
+/**
+ * Input type for Cognito signInWithCustomAuth API.
+ */
 export type SignInWithCustomAuthInput = AuthSignInInput<SignInOptions>;
 
+/**
+ * Input type for Cognito signInWithCustomSRPAuth API.
+ */
 export type SignInWithCustomSRPAuthInput = AuthSignInInput<SignInOptions>;
 
+/**
+ * Input type for Cognito signInWithSRP API.
+ */
 export type SignInWithSRPInput = AuthSignInInput<SignInOptions>;
 
+/**
+ * Input type for Cognito signInWithUserPasswordInput API.
+ */
 export type SignInWithUserPasswordInput = AuthSignInInput<SignInOptions>;
 
+/**
+ * Input type for Cognito signInWithRedirect API.
+ */
 export type SignInWithRedirectInput = AuthSignInWithRedirectInput;
 
+/**
+ * Input type for Cognito signOut API.
+ */
 export type SignOutInput = AuthSignOutInput;
 
+/**
+ * Input type for Cognito signUp API.
+ */
 export type SignUpInput = AuthSignUpInput<
 	CognitoUserAttributeKey,
 	SignUpOptions
 >;
 
+/**
+ * Input type for Cognito updateMFAPreference API.
+ */
 export type UpdateMFAPreferenceInput = {
 	sms?: MFAPreference;
 	totp?: MFAPreference;
 };
 
+/**
+ * Input type for Cognito updatePassword API.
+ */
 export type UpdatePasswordInput = AuthUpdatePasswordInput;
 
+/**
+ * Input type for Cognito updateUserAttributes API.
+ */
 export type UpdateUserAttributesInput = AuthUpdateUserAttributesInput<
 	CognitoUserAttributeKey,
 	UpdateUserAttributesOptions
 >;
 
+/**
+ * Input type for Cognito verifyTOTPSetup API.
+ */
 export type VerifyTOTPSetupInput =
 	AuthVerifyTOTPSetupInput<VerifyTOTPSetupOptions>;
--- packages/auth/src/providers/cognito/types/outputs.ts
+++ packages/auth/src/providers/cognito/types/outputs.ts
@@ -21,41 +21,86 @@ export type FetchMFAPreferenceOutput = {
 	preferred?: MFAType;
 };
 
+/**
+ * Output type for Cognito fetchUserAttributes API.
+ */
 export type FetchUserAttributesOutput =
 	AuthUserAttribute<CognitoUserAttributeKey>;
 
+/**
+ * Output type for Cognito getCurrentUser API.
+ */
 export type GetCurrentUserOutput = AuthUser;
 
+/**
+ * Output type for Cognito confirmSignIn API.
+ */
 export type ConfirmSignInOutput = AuthSignInOutput;
 
+/**
+ * Output type for Cognito confirmSignUp API.
+ */
 export type ConfirmSignUpOutput = AuthSignUpOutput<
 	AuthStandardAttributeKey | CustomAttribute
 >;
 
+/**
+ * Output type for Cognito resendSignUpCode API.
+ */
 export type ResendSignUpCodeOutput =
 	AuthCodeDeliveryDetails<CognitoUserAttributeKey>;
 
+/**
+ * Output type for Cognito resetPassword API.
+ */
 export type ResetPasswordOutput = AuthResetPasswordOutput<
 	AuthStandardAttributeKey | CustomAttribute
 >;
 
+/**
+ * Output type for Cognito setUpTOTP API.
+ */
 export type SetUpTOTPOutput = TOTPSetupDetails;
 
+/**
+ * Output type for Cognito signIn API.
+ */
 export type SignInOutput = AuthSignInOutput;
 
+/**
+ * Output type for Cognito signInWithCustomAuth API.
+ */
 export type SignInWithCustomAuthOutput = AuthSignInOutput;
 
+/**
+ * Output type for Cognito signInWithSRP API.
+ */
 export type SignInWithSRPOutput = AuthSignInOutput;
 
+/**
+ * Output type for Cognito signInWithUserPassword API.
+ */
 export type SignInWithUserPasswordOutput = AuthSignInOutput;
 
+/**
+ * Output type for Cognito signInWithCustomSRPAuth API.
+ */
 export type SignInWithCustomSRPAuthOutput = AuthSignInOutput;
 
+/**
+ * Output type for Cognito signOut API.
+ */
 export type SignOutOutput = AuthSignOutOutput;
 
+/**
+ * Output type for Cognito signUp API.
+ */
 export type SignUpOutput = AuthSignUpOutput<
 	AuthStandardAttributeKey | CustomAttribute
 >;
 
+/**
+ * Output type for Cognito updateUserAttributes API.
+ */
 export type UpdateUserAttributesOutput =
 	AuthUpdateUserAttributesOutput<CognitoUserAttributeKey>;
