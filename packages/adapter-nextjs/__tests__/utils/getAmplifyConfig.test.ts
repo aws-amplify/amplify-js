c2a98db38ed81e5cd539bf333265203c9dfe1151
Hui Zhao <zhohz@amazon.com>
2023-09-15 14:35:06 -0700

    feat(adapter-nextjs): remove the need of calling parseAmplifyConfig in withAmplify

--- packages/adapter-nextjs/__tests__/utils/getAmplifyConfig.test.ts
+++ packages/adapter-nextjs/__tests__/utils/getAmplifyConfig.test.ts
@@ -1,25 +1,51 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { NextConfig } from 'next';
 import getConfig from 'next/config';
+import { parseAWSExports } from '@aws-amplify/core/internals/utils';
 import { getAmplifyConfig } from '../../src/utils/getAmplifyConfig';
 import { AmplifyError } from '@aws-amplify/core/internals/utils';
 
 jest.mock('next/config');
+jest.mock('@aws-amplify/core/internals/utils');
 
 const mockGetConfig = getConfig as jest.Mock;
+const mockParseAWSExports = parseAWSExports as jest.Mock;
 
 describe('getAmplifyConfig', () => {
+	const mockLegacyConfig = {
+		aws_project_region: 'us-west-2',
+		aws_cognito_identity_pool_id: '123',
+		aws_cognito_region: 'aws_cognito_region',
+		aws_user_pools_id: 'abc',
+		aws_user_pools_web_client_id: 'def',
+		oauth: {},
+		aws_cognito_username_attributes: [],
+		aws_cognito_social_providers: [],
+		aws_cognito_signup_attributes: [],
+		aws_cognito_mfa_configuration: 'OFF',
+		aws_cognito_mfa_types: ['SMS'],
+		aws_cognito_password_protection_settings: {
+			passwordPolicyMinLength: 8,
+			passwordPolicyCharacters: [],
+		},
+		aws_cognito_verification_mechanisms: ['PHONE_NUMBER'],
+		aws_user_files_s3_bucket: 'bucket',
+		aws_user_files_s3_bucket_region: 'us-east-1',
+	};
 	const mockAmplifyConfig = {
 		Auth: {
-			identityPoolId: '123',
-			userPoolId: 'abc',
-			userPoolWebClientId: 'def',
+			Cognito: {
+				identityPoolId: '123',
+				userPoolId: 'abc',
+				userPoolClientId: 'def',
+			},
 		},
 		Storage: {
-			bucket: 'bucket',
-			region: 'us-east-1',
+			S3: {
+				bucket: 'bucket',
+				region: 'us-east-1',
+			},
 		},
 	};
 
@@ -33,6 +59,13 @@ describe('getAmplifyConfig', () => {
 
 		const result = getAmplifyConfig();
 		expect(result).toEqual(mockAmplifyConfig);
+	});
+
+	it('should invoke parseAWSConfig when using the legacy shaped config', () => {
+		process.env.amplifyConfig = JSON.stringify(mockLegacyConfig);
+
+		getAmplifyConfig();
+		expect(mockParseAWSExports).toHaveBeenCalledWith(mockLegacyConfig);
 	});
 
 	it('should attempt to get amplifyConfig via getConfig provided by Next.js as a fallback', () => {
