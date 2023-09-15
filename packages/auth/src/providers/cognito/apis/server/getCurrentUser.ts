9a94fce7dfe8bf97cbcc561e122ebfa8d1bfc2b1
Israel Arcos <israel.arcos2595@gmail.com>
2023-09-15 09:37:28 -0400

    chore: remove input types

--- packages/auth/src/providers/cognito/apis/server/getCurrentUser.ts
+++ packages/auth/src/providers/cognito/apis/server/getCurrentUser.ts
@@ -5,13 +5,12 @@ import {
 	AmplifyServer,
 	getAmplifyServerContext,
 } from '@aws-amplify/core/internals/adapter-core';
-import { GetCurrentUserOutput, GetCurrentUserInput } from '../../types';
+import { GetCurrentUserOutput } from '../../types';
 import { getCurrentUser as getCurrentUserInternal } from '../internal/getCurrentUser';
 
 /**
  * Gets the current user from the idToken.
  *
- * @param input - The GetCurrentUserInput object.
  * @returns GetCurrentUserOutput
  * @throws - {@link InitiateAuthException} - Thrown when the service fails to refresh the tokens.
  * @throws AuthTokenConfigException - Thrown when the token provider config is invalid.
