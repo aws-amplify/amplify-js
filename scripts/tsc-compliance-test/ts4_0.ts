4a46e15434598454c12c91c4073519730228f58a
Allan Zheng <zheallan@amazon.com>
2023-09-18 15:26:51 -0700

    chore: add documentation

--- scripts/tsc-compliance-test/ts4_0.ts
+++ scripts/tsc-compliance-test/ts4_0.ts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import * as Amplify from 'aws-amplify';
 import * as analytics from 'aws-amplify/analytics';
 
