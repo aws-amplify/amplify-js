d8c84247d00451fcf827a5e5e019338ea95960d9
ManojNB <manojnb95@gmail.com>
2023-09-18 17:43:28 -0700

    feat: setup notifications package

--- packages/aws-amplify/src/notifications/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-/*
-This file maps exports from `aws-amplify/notifications`. It provides access to the top level notifications category.
-*/
-export * from '@aws-amplify/notifications';
