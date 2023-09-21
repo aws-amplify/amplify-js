49bb74b8169e995ab89bde3402330a97a3cac210
Hui Zhao <hui@Huis-MBP.lan>
2023-09-21 09:51:47 -0700

    feat(core): add convert util (base64)
    - move the generateRandomString out from Util to utils

---
+++ packages/core/src/utils/convert/types.ts
@@ -0,0 +1,17 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export interface Base64EncoderConvertOptions {
+	urlSafe: boolean;
+}
+
+export interface Base64Encoder {
+	convert(
+		input: Uint8Array | string,
+		options?: Base64EncoderConvertOptions
+	): string;
+}
+
+export interface Base64Decoder {
+	convert(input: string): string;
+}
