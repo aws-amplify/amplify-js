6b2f785cc7120151b8ca751d2a6b4b14fe67a02a
ManojNB <manojnb95@gmail.com>
2023-09-21 10:24:25 -0700

    wip: review comments addressed

---
+++ packages/aws-amplify/src/notifications/in-app-messaging/pinpoint/index.ts
@@ -0,0 +1,9 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+This file maps exports from `aws-amplify/notifications/in-app-messaging/pinpoint`. It provides access to the Pinpoint provider of the InAppMessaging sub-category.
+*/
+
+// TODO(V6): this import is not available, needs investigation
+// export * from '@aws-amplify/notifications/in-app-messaging/pinpoint';
