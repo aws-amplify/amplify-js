4b8241fc967a1b3a90cdbc537f079b2c58cea230
Di Wu <diwud@amazon.com>
2023-09-22 15:37:14 -0700

    add unit test cases for resolveCredentials

---
+++ packages/analytics/__tests__/providers/kinesis/utils/resolveCredentials.test.ts
@@ -0,0 +1,36 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { fetchAuthSession } from '@aws-amplify/core';
+import { resolveCredentials } from '../../../../src/providers/pinpoint/utils';
+import { AnalyticsError } from '../../../../src';
+
+jest.mock('@aws-amplify/core');
+describe('Analytics Kinesis Provider Util: resolveCredentials', () => {
+	const credentials = {
+		credentials: {
+			accessKeyId: 'access-key-id',
+			secretAccessKey: 'secret-access-key',
+			sessionToken: 'session-token',
+		},
+		identityId: 'identity-id',
+	};
+	const mockFetchAuthSession = fetchAuthSession as jest.Mock;
+
+	beforeEach(() => {
+		mockFetchAuthSession.mockReset();
+	});
+
+	it('resolves required credentials', async () => {
+		mockFetchAuthSession.mockResolvedValue(credentials);
+		expect(await resolveCredentials()).toStrictEqual(credentials);
+	});
+
+	it('throws if credentials are missing', async () => {
+		mockFetchAuthSession.mockReturnValue({
+			...credentials,
+			credentials: undefined,
+		});
+		await expect(resolveCredentials()).rejects.toBeInstanceOf(AnalyticsError);
+	});
+});
