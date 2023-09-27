798707bba042301d4318291127d25e40cd6bcaac
ManojNB <manojnb@amazon.com>
2023-09-26 17:35:59 -0700

    feat(notifications): setup package structure and update V6 import paths (#12079)
    * fix: ranming of folder and imports
    
    * fix: new line eof and some folder names
    
    * fix: make notificaitons private false and fix lint errros
    
    * chore: remove certain tests that can be added incrementally later
    
    * fix: add test folder structure
    
    * fix: added todo for coverage
    
    ---------
    
    Co-authored-by: Jim Blanchard <jim.l.blanchard@gmail.com>

---
+++ packages/aws-amplify/src/in-app-messaging/pinpoint/index.ts
@@ -0,0 +1,8 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+This file maps exports from `aws-amplify/in-app-messaging/pinpoint`. 
+It provides access to the Pinpoint provider of the InAppMessaging sub-category.
+*/
+export * from '@aws-amplify/notifications/in-app-messaging/pinpoint';
