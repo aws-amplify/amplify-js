8d27d05e8eb9a8df0dcf849dd6d7ec86f90c3729
David McAfee <mcafd@amazon.com>
2023-09-28 22:04:15 -0700

    feat(data): refine GraphQL API exports

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,14 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// TODO(v6): revisit exports
-
-export { GraphQLQuery, GraphQLSubscription } from './types';
 import { API } from './API';
-
-export { GraphQLAuthError } from '@aws-amplify/api-graphql';
-
-export type { GraphQLResult } from '@aws-amplify/api-graphql';
 
 const generateClient = API.generateClient;
 
