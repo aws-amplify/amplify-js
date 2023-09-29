fa7e604b6084142d4ef5b98f999dfc9a124c460b
David McAfee <mcafd@amazon.com>
2023-09-29 09:09:57 -0700

    include exports used by DataStore

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,7 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// TODO(v6): revisit exports
+
 import { API } from './API';
+
+// Used by Datastore
+export { GraphQLAuthError } from '@aws-amplify/api-graphql';
+
+// Used by DataStore
+export type { GraphQLResult } from '@aws-amplify/api-graphql';
 
 const generateClient = API.generateClient;
 
