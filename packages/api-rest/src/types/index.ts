c9ef37871aa1490a9133e9602c90cef265796d72
Allan Zheng <zheallan@amazon.com>
2023-09-29 22:47:54 -0700

    chore: unblock test

--- packages/api-rest/src/types/index.ts
+++ packages/api-rest/src/types/index.ts
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import { DocumentType } from '@aws-amplify/core/internals/utils';
 
 export type GetOptions = RestApiOptionsBase;
 export type PostOptions = RestApiOptionsBase;
