3834368bba386ad0725fdeefb64b644167183403
Hui Zhao <zhohz@amazon.com>
2023-10-02 11:33:12 -0700

    chore(react-native): add callout comments to the module loaders

--- packages/react-native/src/moduleLoaders/loadAsyncStorage.ts
+++ packages/react-native/src/moduleLoaders/loadAsyncStorage.ts
@@ -5,6 +5,8 @@ import type { AsyncStorageStatic } from '@react-native-async-storage/async-stora
 
 export const loadAsyncStorage = (): AsyncStorageStatic => {
 	try {
+		// metro bundler requires static string for loading module.
+		// See: https://facebook.github.io/metro/docs/configuration/#dynamicdepsinpackages
 		const module = require('@react-native-async-storage/async-storage')
 			?.default as AsyncStorageStatic;
 		if (module) {
@@ -15,6 +17,9 @@ export const loadAsyncStorage = (): AsyncStorageStatic => {
 			'Ensure `@react-native-async-storage/async-storage` is installed and linked.'
 		);
 	} catch (e) {
+		// The error parsing logic cannot be extract as with metro the `require`
+		// would be confused when there is a `import` in the same file importing
+		// another module and that causes error
 		const message = (e as Error).message.replace(
 			/undefined/g,
 			'@react-native-async-storage/async-storage'
