0afd65d10e5ed84bec6083430b04eee34d430d31
Hui Zhao <zhohz@amazon.com>
2023-10-02 11:33:12 -0700

    chore(react-native): add callout comments to the module loaders

--- packages/react-native/src/moduleLoaders/loadNetInfo.ts
+++ packages/react-native/src/moduleLoaders/loadNetInfo.ts
@@ -7,6 +7,8 @@ type NetInfoModule = typeof NetInfo;
 
 export const loadNetInfo = (): NetInfoModule => {
 	try {
+		// metro bundler requires static string for loading module.
+		// See: https://facebook.github.io/metro/docs/configuration/#dynamicdepsinpackages
 		const module = require('@react-native-community/netinfo')
 			?.default as NetInfoModule;
 		if (module) {
@@ -17,6 +19,9 @@ export const loadNetInfo = (): NetInfoModule => {
 			'Ensure `@react-native-community/netinfo` is installed and linked.'
 		);
 	} catch (e) {
+		// The error parsing logic cannot be extract as with metro the `require`
+		// would be confused when there is a `import` in the same file importing
+		// another module and that causes error
 		const message = (e as Error).message.replace(
 			/undefined/g,
 			'@react-native-community/netinfo'
