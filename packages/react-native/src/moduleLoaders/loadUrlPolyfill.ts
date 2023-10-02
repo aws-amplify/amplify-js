1c380af9c96becb67e9176480e12dd4221093748
Hui Zhao <zhohz@amazon.com>
2023-10-02 11:33:12 -0700

    chore(react-native): add callout comments to the module loaders

--- packages/react-native/src/moduleLoaders/loadUrlPolyfill.ts
+++ packages/react-native/src/moduleLoaders/loadUrlPolyfill.ts
@@ -3,8 +3,13 @@
 
 export const loadUrlPolyfill = () => {
 	try {
+		// metro bundler requires static string for loading module.
+		// See: https://facebook.github.io/metro/docs/configuration/#dynamicdepsinpackages
 		require('react-native-url-polyfill/auto');
 	} catch (e) {
+		// The error parsing logic cannot be extract as with metro the `require`
+		// would be confused when there is a `import` in the same file importing
+		// another module and that causes error
 		const message = (e as Error).message.replace(
 			/undefined/g,
 			'@react-native-community/netinfo'
