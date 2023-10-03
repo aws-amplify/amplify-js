478f3a1808c80c85b9f80b3519b44dfa36bf9910
Di Wu <im@wudi.me>
2023-10-03 15:35:03 -0700

    feat(analytics): add flushEvents API for service provider Personalize

--- packages/aws-amplify/__tests__/exports.test.ts
+++ packages/aws-amplify/__tests__/exports.test.ts
@@ -83,6 +83,7 @@ describe('aws-amplify Exports', () => {
 			expect(Object.keys(analyticsPersonalizeExports)).toMatchInlineSnapshot(`
 			Array [
 			  "record",
+			  "flushEvents",
 			]
 			`);
 		});
