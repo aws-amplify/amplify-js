a05c4c0dab536db38988ab9258fdf9bcb284f33a
Ashwin Kumar <ashwsrir@amazon.com>
2023-10-03 11:56:22 -0700

    fix: update unit test

--- packages/auth/__tests__/providers/cognito/fetchDevices.test.ts
+++ packages/auth/__tests__/providers/cognito/fetchDevices.test.ts
@@ -1,86 +1,140 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import { AuthError } from '../../../src/errors/AuthError';
 import { fetchDevices } from '../../../src/providers/cognito';
-import { authAPITestParams } from './testUtils/authApiTestParams';
+import * as clients from '../../../src/providers/cognito/utils/clients/CognitoIdentityProvider';
 import { Amplify } from '@aws-amplify/core';
-import * as cogIdentityProviderClients from '../../../src/providers/cognito/utils/clients/CognitoIdentityProvider';
-import {
-	ListDevicesCommandInput,
-	ListDevicesCommandOutput,
-} from '../../../src/providers/cognito/utils/clients/CognitoIdentityProvider/types';
-import { deviceManagementAPIConstants } from './testUtils/deviceManagementAPIConstants';
+import { decodeJWT } from '@aws-amplify/core/internals/utils';
+import * as authUtils from '../../../src';
+import { fetchTransferHandler } from '@aws-amplify/core/internals/aws-client-utils';
+import { buildMockErrorResponse, mockJsonResponse } from './testUtils/data';
+import { ListDevicesException } from '../../../src/providers/cognito/types/errors';
+jest.mock('@aws-amplify/core/lib/clients/handlers/fetch');
 
-const fetchAuthSessionSpy = jest.spyOn(Amplify.Auth, 'fetchAuthSession');
-const configSpy = jest.spyOn(Amplify, 'getConfig');
-const listDevicesSpy = jest.spyOn(cogIdentityProviderClients, 'listDevices');
+Amplify.configure({
+	Auth: {
+		Cognito: {
+			userPoolClientId: '111111-aaaaa-42d8-891d-ee81a1549398',
+			userPoolId: 'us-west-2_zzzzz',
+			identityPoolId: 'us-west-2:xxxxxx',
+		},
+	},
+});
+const mockedAccessToken =
+	'test_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+const dateEpoch = 1.696296885807e9;
+const date = new Date(dateEpoch * 1000);
+const clientResponseDevice = {
+	DeviceAttributes: [{ Name: 'attributeName', Value: 'attributeValue' }],
+	DeviceCreateDate: dateEpoch,
+	DeviceKey: 'DeviceKey',
+	DeviceLastAuthenticatedDate: dateEpoch,
+	DeviceLastModifiedDate: dateEpoch,
+};
+const apiOutputDevice = {
+	id: 'DeviceKey',
+	name: undefined,
+	attributes: {
+		attributeName: 'attributeValue',
+	},
+	createDate: date,
+	lastModifiedDate: date,
+	lastAuthenticatedDate: date,
+};
 
-describe('fetchDevices API Happy Path Cases:', () => {
-	beforeAll(() => {
-		listDevicesSpy.mockImplementation(
-			async ({}, params: ListDevicesCommandInput) => {
-				return deviceManagementAPIConstants.ListDevicesClientResultHappy as ListDevicesCommandOutput;
-			}
-		);
-		fetchAuthSessionSpy.mockImplementation(async () => {
-			return {
-				tokens: authAPITestParams.ValidAuthTokens,
-			};
-		});
-		configSpy.mockImplementation(() => authAPITestParams.validAuthConfig);
+describe('fetchDevices API happy path cases', () => {
+	let fetchAuthSessionsSpy;
+	beforeEach(() => {
+		fetchAuthSessionsSpy = jest
+			.spyOn(authUtils, 'fetchAuthSession')
+			.mockImplementationOnce(
+				async (): Promise<{ tokens: { accessToken: any } }> => {
+					return {
+						tokens: {
+							accessToken: decodeJWT(mockedAccessToken),
+						},
+					};
+				}
+			);
 	});
-	afterAll(() => {
-		listDevicesSpy.mockClear();
-		fetchAuthSessionSpy.mockClear();
-		configSpy.mockClear();
+	afterEach(() => {
+		fetchAuthSessionsSpy.mockClear();
 	});
-	test('fetchDevices API should return a valid deviceId and deviceName', async () => {
-		const result = await fetchDevices();
-		expect(result).toEqual(
-			deviceManagementAPIConstants.ListDevicesAPIResultHappy
+
+	it('should fetch devices and parse client response correctly with and without device name', async () => {
+		// expect.assertions(3);
+		const deviceName = {
+			Name: 'device_name',
+			Value: 'test-device-name',
+		};
+
+		const fetchDevicesClientSpy = jest
+			.spyOn(clients, 'listDevices')
+			.mockImplementationOnce(async () => {
+				return {
+					Devices: [
+						{
+							...clientResponseDevice,
+							DeviceKey: 'DeviceKey1',
+							DeviceAttributes: [
+								...clientResponseDevice.DeviceAttributes,
+								deviceName,
+							],
+						},
+						{ ...clientResponseDevice, DeviceKey: 'DeviceKey2' },
+					],
+					$metadata: {},
+				};
+			});
+
+		expect(await fetchDevices()).toEqual([
+			{
+				...apiOutputDevice,
+				id: 'DeviceKey1',
+				name: deviceName.Value,
+				attributes: {
+					...apiOutputDevice.attributes,
+					[deviceName.Name]: deviceName.Value,
+				},
+			},
+			{ ...apiOutputDevice, id: 'DeviceKey2' },
+		]);
+		expect(fetchDevicesClientSpy).toHaveBeenCalledWith(
+			expect.objectContaining({ region: 'us-west-2' }),
+			expect.objectContaining({
+				AccessToken: mockedAccessToken,
+				Limit: 60,
+			})
 		);
+		expect(fetchDevicesClientSpy).toBeCalledTimes(1);
 	});
 });
 
-describe('fetchDevices API Error Path Cases:', () => {
-	beforeEach(() => {
-		listDevicesSpy.mockImplementationOnce(
-			async ({}, params: ListDevicesCommandInput) => {
-				return deviceManagementAPIConstants.ListDevicesClientResultHappy as ListDevicesCommandOutput;
-			}
+describe('fetchDevices API error path cases', () => {
+	it('should raise service error', async () => {
+		expect.assertions(2);
+		jest
+			.spyOn(authUtils, 'fetchAuthSession')
+			.mockImplementationOnce(
+				async (): Promise<{ tokens: { accessToken: any } }> => {
+					return {
+						tokens: {
+							accessToken: decodeJWT(mockedAccessToken),
+						},
+					};
+				}
+			);
+		(fetchTransferHandler as jest.Mock).mockResolvedValue(
+			mockJsonResponse(
+				buildMockErrorResponse(ListDevicesException.InvalidParameterException)
+			)
 		);
-	});
-	afterAll(() => {
-		listDevicesSpy.mockClear();
-		fetchAuthSessionSpy.mockClear();
-		configSpy.mockClear();
-	});
-	test('fetchDevices API should throw an AuthError when there are no tokens available', async () => {
-		fetchAuthSessionSpy.mockImplementationOnce(async () => {
-			return {
-				tokens: undefined,
-			};
-		});
-		const res = fetchDevices();
-		expect(res).rejects.toHaveProperty('name', 'Invalid Auth Tokens');
-		expect(res).rejects.toHaveProperty('message', 'No Auth Tokens were found');
-	});
-	test('fetchDevices API should throw an AuthError when there are no tokens available', async () => {
-		fetchAuthSessionSpy.mockImplementationOnce(async () => {
-			return {
-				tokens: authAPITestParams.ValidAuthTokens,
-			};
-		});
-		configSpy.mockImplementationOnce(() => {
-			delete authAPITestParams.validAuthConfig.Auth?.userPoolId;
-			return authAPITestParams.validAuthConfig;
-		});
-		const res = fetchDevices();
-
-		expect(res).rejects.toHaveProperty('name', 'InvalidUserPoolId');
-		expect(res).rejects.toHaveProperty(
-			'message',
-			'Invalid user pool id provided.'
-		);
+		try {
+			await fetchDevices();
+		} catch (error) {
+			expect(error).toBeInstanceOf(AuthError);
+			expect(error.name).toBe(ListDevicesException.InvalidParameterException);
+		}
 	});
 });
