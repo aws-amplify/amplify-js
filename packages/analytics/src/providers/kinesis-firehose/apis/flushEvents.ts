73911217a2a1eae5b94a7947fe02c1298a33099b
Di Wu <im@wudi.me>
2023-10-03 15:44:18 -0700

    feat(analytics): add flushEvents API for service provider KDF (#12174)
    * feat(analytics): add flushEvents API for service provider KDF
    
    * update comment

---
+++ packages/analytics/src/providers/kinesis-firehose/apis/flushEvents.ts
@@ -0,0 +1,34 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { getEventBuffer, resolveConfig } from '../utils';
+import { resolveCredentials } from '../../../utils';
+import { ConsoleLogger } from '@aws-amplify/core/internals/utils';
+
+const logger = new ConsoleLogger('KinesisFirehose');
+
+/**
+ * Flushes all buffered Kinesis events to the service.
+ *
+ * @note
+ * This API will make a best-effort attempt to flush events from the buffer. Events recorded immediately after invoking
+ * this API may not be included in the flush.
+ */
+export const flushEvents = () => {
+	const { region, flushSize, flushInterval, bufferSize, resendLimit } =
+		resolveConfig();
+	resolveCredentials()
+		.then(({ credentials, identityId }) =>
+			getEventBuffer({
+				region,
+				flushSize,
+				flushInterval,
+				bufferSize,
+				credentials,
+				identityId,
+				resendLimit,
+			})
+		)
+		.then(eventBuffer => eventBuffer.flushAll())
+		.catch(e => logger.warn('Failed to flush events.', e));
+};
