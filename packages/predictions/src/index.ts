68df2ed625537bd839b5316bee6cd2af6db43d5b
Erin Beal <erinleig@amazon.com>
2023-10-09 16:54:12 -0700

    chore: remove deleted classes from exports

--- packages/predictions/src/index.ts
+++ packages/predictions/src/index.ts
@@ -1,20 +1,11 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { Predictions } from './Predictions';
+export { Predictions } from './Predictions';
 
-import {
+export {
 	AmazonAIConvertPredictionsProvider,
 	AmazonAIIdentifyPredictionsProvider,
-	AmazonAIPredictionsProvider,
 	AmazonAIInterpretPredictionsProvider,
 } from './Providers';
 
 export * from './types';
-
-export {
-	Predictions,
-	AmazonAIIdentifyPredictionsProvider,
-	AmazonAIConvertPredictionsProvider,
-	AmazonAIPredictionsProvider,
-	AmazonAIInterpretPredictionsProvider,
-};
