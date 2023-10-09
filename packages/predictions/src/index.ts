b095794fcb7ce9815bd7b4c88cc688beb370b80f
Erin Beal <erinleig@amazon.com>
2023-10-09 16:58:43 -0700

    chore: don't export providers

--- packages/predictions/src/index.ts
+++ packages/predictions/src/index.ts
@@ -2,10 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0
 export { Predictions } from './Predictions';
 
-export {
-	AmazonAIConvertPredictionsProvider,
-	AmazonAIIdentifyPredictionsProvider,
-	AmazonAIInterpretPredictionsProvider,
-} from './Providers';
-
 export * from './types';
