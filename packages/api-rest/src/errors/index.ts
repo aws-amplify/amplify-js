f8a7145f67d9db12fc52608b4202e08c02613feb
AllanZhengYP <zheallan@amazon.com>
2023-10-10 15:20:33 -0700

    fix(storage): align cancel behavior with api-rest (#12239)
    * fix(api-rest): only set custom cancel message when supplied
    
    * fix(storage): cancel accepts custom message instead of error instances
    
    ---------
    
    Co-authored-by: Aaron S <94858815+stocaaro@users.noreply.github.com>

--- packages/api-rest/src/errors/index.ts
+++ packages/api-rest/src/errors/index.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export { CancelledError, isCancelError } from './CancelledError';
+export { CanceledError, isCancelError } from './CanceledError';
 export { RestApiError } from './RestApiError';
 export { assertValidationError } from './assertValidatonError';
 export { RestApiValidationErrorCode, validationErrorMap } from './validation';
