d9cf430f9bfbc166fd2b90b5dacb18c2eaa56643
elorzafe <elorzafe@amazon.com>
2023-10-11 12:30:00 -0700

    adds license to new files

--- packages/auth/src/providers/cognito/utils/userContextData.native.ts
+++ packages/auth/src/providers/cognito/utils/userContextData.native.ts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 // TODO: add support after https://amazon-cognito-assets.us-east-1.amazoncognito.com/amazon-cognito-advanced-security-data.min.js can be imported
 
 export function getUserContextData({
