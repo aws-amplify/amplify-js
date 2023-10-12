260a82fa56680cafee49517aa9b259e5b65c22b1
Erin Beal <erinleig@amazon.com>
2023-10-12 09:55:14 -0700

    chore: only export input and output types

--- packages/predictions/src/index.ts
+++ packages/predictions/src/index.ts
@@ -2,4 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 export { Predictions } from './Predictions';
 
-export * from './types';
+export {
+	TranslateTextInput,
+	TranslateTextOutput,
+	TextToSpeechInput,
+	TextToSpeechOutput,
+	SpeechToTextInput,
+	SpeechToTextOutput,
+	IdentifyTextInput,
+	IdentifyTextOutput,
+	IdentifyLabelsOutput,
+	IdentifyLabelsInput,
+	IdentifyEntitiesInput,
+	IdentifyEntitiesOutput,
+	InterpretTextOutput,
+	InterpretTextInput,
+} from './types';
