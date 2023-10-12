78a3ceccb51e0bf09de2df8e34bff8cc34a12e07
Jon Wire <iambipedal@gmail.com>
2023-10-12 11:03:26 -0500

    chore: add license headers

--- packages/api-graphql/src/internals/generateModelsProperty.ts
+++ packages/api-graphql/src/internals/generateModelsProperty.ts
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 import { ModelTypes } from '@aws-amplify/amplify-api-next-types-alpha';
 import {
 	initializeModel,
