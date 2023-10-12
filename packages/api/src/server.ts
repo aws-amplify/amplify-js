fa47e109f1b2e9071a33f900f09a43e451a65fb9
Jon Wire <iambipedal@gmail.com>
2023-10-12 09:30:17 -0500

    refactor: move generateClient core to api-graphql, add surface tests (#12268)
    * refactor: move generateClient core to api-graphql, add surface tests
    
    * chore: add license header
    
    * chore: bump bundle size limits

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -2,17 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export { GraphQLQuery, GraphQLSubscription } from './types';
-import {
-	graphql,
-	cancel,
-	isCancelError,
-} from '@aws-amplify/api-graphql/internals';
+import { generateClient as internalGenerateClient } from '@aws-amplify/api-graphql/internals';
 import {
 	AmplifyServer,
 	getAmplifyServerContext,
 } from '@aws-amplify/core/internals/adapter-core';
 
-import { __amplify, V6Client } from '@aws-amplify/api-graphql';
+import { V6Client } from '@aws-amplify/api-graphql';
 
 export type {
 	GraphQLResult,
@@ -25,12 +21,9 @@ export type {
 export function generateClient<T extends Record<any, any> = never>(
 	contextSpec: AmplifyServer.ContextSpec
 ): V6Client<T> {
-	return {
-		[__amplify]: getAmplifyServerContext(contextSpec).amplify,
-		graphql,
-		cancel,
-		isCancelError,
-	};
+	return internalGenerateClient({
+		amplify: getAmplifyServerContext(contextSpec).amplify,
+	});
 }
 
 export {
