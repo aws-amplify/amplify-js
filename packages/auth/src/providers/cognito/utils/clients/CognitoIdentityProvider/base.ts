5574b2ec629f994836070d78d3f21209c87f225a
Jim Blanchard <jablanch@amazon.com>
2023-10-12 12:01:34 -0500

    feat: Rename `endpoint` to `userPoolEndpoint`

--- packages/auth/src/providers/cognito/utils/clients/CognitoIdentityProvider/base.ts
+++ packages/auth/src/providers/cognito/utils/clients/CognitoIdentityProvider/base.ts
@@ -28,7 +28,7 @@ const SERVICE_NAME = 'cognito-idp';
  */
 const endpointResolver = ({ region }: EndpointResolverOptions) => {
 	const authConfig = Amplify.getConfig().Auth?.Cognito;
-	const customURL = authConfig?.endpoint;
+	const customURL = authConfig?.userPoolEndpoint;
 	const defaultURL = new URL(
 		`https://${SERVICE_NAME}.${region}.${getDnsSuffix(region)}`
 	);
