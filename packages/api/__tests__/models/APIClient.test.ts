171516fec8bbd952b5b1a6a36cb30d97f06347a9
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2023-10-16 18:43:25 -0400

    bump types package and add recursive flatten (#12314)

--- packages/api/__tests__/models/APIClient.test.ts
+++ packages/api/__tests__/models/APIClient.test.ts
@@ -1,4 +1,4 @@
-import { normalizeMutationInput, initializeModel } from '../../src/APIClient';
+import { normalizeMutationInput, flattenItems } from '../../src/APIClient';
 import modelIntroSchema from '../assets/model-introspection';
 
 describe('APIClient', () => {
@@ -38,4 +38,120 @@ describe('APIClient', () => {
 	});
 
 	test('initializeModel', () => {});
+});
+
+describe('flattenItems', () => {
+	test('no-op on get without relationships', () => {
+		const getResponse = { getPost: { id: 'myPost' } };
+
+		const expected = { getPost: { id: 'myPost' } };
+
+		const flattened = flattenItems(getResponse);
+
+		expect(flattened).toEqual(expected);
+	});
+
+	test('flatten list without relationships', () => {
+		const listResponse = {
+			listPost: { items: [{ id: 'myPost' }, { id: 'myPost2' }] },
+		};
+
+		const expected = {
+			listPost: [{ id: 'myPost' }, { id: 'myPost2' }],
+		};
+
+		const flattened = flattenItems(listResponse);
+
+		expect(flattened).toEqual(expected);
+	});
+
+	test('flatten list with relationships', () => {
+		const listResponse = {
+			listPosts: {
+				items: [
+					{
+						id: 'post1',
+						comments: {
+							items: [
+								{
+									id: 'comment1',
+									content: 'my comment 1',
+									meta: {
+										items: [{ id: 'meta1' }],
+									},
+									post: {
+										id: 'post1',
+										comments: {
+											items: [
+												{
+													id: 'comment1',
+													content: 'my comment 1',
+													meta: {
+														items: [{ id: 'meta1' }],
+													},
+												},
+											],
+										},
+									},
+								},
+								{
+									id: 'comment1',
+									content: 'my comment 1',
+									meta: {
+										items: [{ id: 'meta1' }],
+									},
+								},
+							],
+						},
+					},
+				],
+			},
+		};
+
+		const expected = {
+			listPosts: [
+				{
+					id: 'post1',
+					comments: [
+						{
+							id: 'comment1',
+							content: 'my comment 1',
+							meta: [
+								{
+									id: 'meta1',
+								},
+							],
+							post: {
+								id: 'post1',
+								comments: [
+									{
+										id: 'comment1',
+										content: 'my comment 1',
+										meta: [
+											{
+												id: 'meta1',
+											},
+										],
+									},
+								],
+							},
+						},
+						{
+							id: 'comment1',
+							content: 'my comment 1',
+							meta: [
+								{
+									id: 'meta1',
+								},
+							],
+						},
+					],
+				},
+			],
+		};
+
+		const flattened = flattenItems(listResponse);
+
+		expect(flattened).toEqual(expected);
+	});
 });
