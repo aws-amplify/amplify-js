39a3ad3278b451b9e5a74929211628d2e33bb7b0
Chris F <5827964+cshfang@users.noreply.github.com>
2023-10-17 09:38:27 -0700

    Update packages/notifications/src/pushNotifications/providers/pinpoint/apis/getPermissionStatus.ts
    Co-authored-by: Hui Zhao <10602282+HuiSF@users.noreply.github.com>

--- packages/notifications/src/pushNotifications/providers/pinpoint/apis/getPermissionStatus.ts
+++ packages/notifications/src/pushNotifications/providers/pinpoint/apis/getPermissionStatus.ts
@@ -8,16 +8,16 @@ import { GetPermissionStatus, GetPermissionStatusOutput } from '../types';
  * Returns a string representing the current status of user permissions to display push notifications. The possible
  * statuses are as follows:
  *
- *   'shouldRequest' - No permissions have been requested yet. It is idiomatic at this time to simply request for
- *   permissions from the user.
+ *   * `'shouldRequest'` - No permissions have been requested yet. It is idiomatic at this time to simply request for
+ *   * permissions from the user.
  *
- *   'shouldExplainThenRequest' - It is recommended at this time to provide some context or rationale to the user
+ *   * `'shouldExplainThenRequest'` - It is recommended at this time to provide some context or rationale to the user
  *   explaining why you want to send them push notifications before requesting for permissions.
  *
- *   'granted' - Permissions have been granted by the user. No further actions are needed and their app is ready to
+ *   * `'granted'` - Permissions have been granted by the user. No further actions are needed and their app is ready to
  *   display notifications.
  *
- *   'denied' - Permissions have been denied by the user. Further attempts to request permissions will no longer
+ *   * `'denied'` - Permissions have been denied by the user. Further attempts to request permissions will no longer
  *   trigger a permission dialog. Your app should now either degrade gracefully or prompt your user to grant the
  *   permissions needed in their device settings.
  *
