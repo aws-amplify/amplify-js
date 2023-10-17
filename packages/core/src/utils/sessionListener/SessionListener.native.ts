6ec22d3584ebf1da625b834689060873c2a26453
Jim Blanchard <jablanch@amazon.com>
2023-10-16 20:26:14 -0500

    Fix

--- packages/core/src/utils/sessionListener/SessionListener.native.ts
+++ packages/core/src/utils/sessionListener/SessionListener.native.ts
@@ -11,6 +11,10 @@ import {
 
 let stateChangeListeners = new Set<SessionStateChangeListener>();
 
+const isActive = (appState?: string) => appState === 'active';
+const isInactive = (appState?: string) =>
+	appState === 'inactive' || appState === 'background';
+
 export class SessionListenerClass implements SessionListenerInterface {
 	private currentAppState?: string;
 
@@ -27,7 +31,7 @@ export class SessionListenerClass implements SessionListenerInterface {
 	) {
 		stateChangeListeners.add(listener);
 
-		// Notify new handlers of the current status on add
+		// Notify new handlers of the current state on add
 		if (notifyOnAdd) {
 			listener(this.getSessionState());
 		}
@@ -42,40 +46,31 @@ export class SessionListenerClass implements SessionListenerInterface {
 
 		if (
 			(this.currentAppState === undefined ||
-				this.currentAppState.match(/inactive|background/)) &&
-			nextAppState === 'active'
+				isInactive(this.currentAppState)) &&
+			isActive(nextAppState)
 		) {
 			console.log('+ Session listener active');
-			this.currentAppState = nextAppState;
-			this.notifyHandlers();
-		} else if (
-			this.currentAppState?.match(/active/) &&
-			nextAppState.match(/inactive|background/)
-		) {
+			this.notifyHandlers('started');
+		} else if (isActive(this.currentAppState) && isInactive(nextAppState)) {
 			console.log('+ Session listener background');
-			this.currentAppState = nextAppState;
-			this.notifyHandlers();
+			this.notifyHandlers('ended');
 		}
+
+		this.currentAppState = nextAppState;
 	}
 
-	private notifyHandlers() {
-		const sessionState = this.getSessionState();
-
-		console.log('+ Notify handlers state', sessionState);
+	private notifyHandlers(state: SessionState) {
+		console.log('+ Notify handlers state', state);
 
 		stateChangeListeners.forEach(listener => {
-			listener(sessionState);
+			listener(state);
 		});
 	}
 
 	private getSessionState = (): SessionState => {
 		console.log('+ getSessionState current state', this.currentAppState);
 
-		// Current app state will be undefined when the app is first started
-		if (
-			this.currentAppState === undefined ||
-			this.currentAppState === 'active'
-		) {
+		if (isActive(this.currentAppState)) {
 			return 'started';
 		}
 
