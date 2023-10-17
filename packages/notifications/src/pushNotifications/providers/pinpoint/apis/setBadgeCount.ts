7464ad4a572a2238a27b8458bd0f486145bcae6b
Chris Fang <cshfang@gmail.com>
2023-10-17 12:00:19 -0700

    Fix comment line length

--- packages/notifications/src/pushNotifications/providers/pinpoint/apis/setBadgeCount.ts
+++ packages/notifications/src/pushNotifications/providers/pinpoint/apis/setBadgeCount.ts
@@ -5,9 +5,9 @@ import { PlatformNotSupportedError } from '@aws-amplify/core/internals/utils';
 import { SetBadgeCount } from '../types';
 
 /**
- * Sets the current badge count (the number on the top right corner of your app's icon). Setting the badge count to 0 (zero) will remove
- * the badge from your app's icon. This function is safe to call (but will be ignored) even when your React Native app
- * is running on platforms where badges are not supported.
+ * Sets the current badge count (the number on the top right corner of your app's icon). Setting the badge count
+ * to 0 (zero) will remove the badge from your app's icon. This function is safe to call (but will be ignored) even
+ * when your React Native app is running on platforms where badges are not supported.
  *
  * @throws platform: {@link PlatformNotSupportedError} - Thrown if called against an unsupported platform. Currently,
  * only React Native is supported by this API.
