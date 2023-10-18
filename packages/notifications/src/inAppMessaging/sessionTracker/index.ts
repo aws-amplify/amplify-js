a09b1bdf0a7e307b9694cbce735a4fa318a1bcae
Jim Blanchard <jablanch@amazon.com>
2023-10-18 09:37:43 -0500

    feat: Integrate In-App messaging with SessionListener.

--- packages/notifications/src/inAppMessaging/sessionTracker/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-import { default as SessionTracker } from './SessionTracker';
-export * from './types';
-export default SessionTracker;
