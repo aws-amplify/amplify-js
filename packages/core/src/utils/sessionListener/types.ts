c5ca240faf6e94bdb4c709ff434253096879d573
Jim Blanchard <jim.l.blanchard@gmail.com>
2023-10-18 11:39:50 -0500

    feat: Pinpoint configureAutoTrack & tracker migration (#12322)

---
+++ packages/core/src/utils/sessionListener/types.ts
@@ -0,0 +1,11 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export type SessionState = 'started' | 'ended';
+
+export type SessionStateChangeListener = (state: SessionState) => void;
+
+export interface SessionListenerInterface {
+	addStateChangeListener: (listener: SessionStateChangeListener) => void;
+	removeStateChangeListener: (listener: SessionStateChangeListener) => void;
+}
