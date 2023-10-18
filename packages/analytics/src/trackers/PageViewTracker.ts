2fbeecb5f0235bd368c12ecbca1ef500e3ab8b7e
Jim Blanchard <jablanch@amazon.com>
2023-10-18 08:44:06 -0500

    Fix safety check.

--- packages/analytics/src/trackers/PageViewTracker.ts
+++ packages/analytics/src/trackers/PageViewTracker.ts
@@ -14,9 +14,7 @@ const logger = new ConsoleLogger('PageViewTracker');
 const DEFAULT_EVENT_NAME = 'pageView';
 const DEFAULT_APP_TYPE = 'singlePage';
 const DEFAULT_URL_PROVIDER = () => {
-	return isBrowser()
-		? window.location.origin + window.location.pathname
-		: undefined!;
+	return window.location.origin + window.location.pathname;
 };
 const PREV_URL_STORAGE_KEY = 'aws-amplify-analytics-prevUrl';
 
