1e317a10a7a7e980919b0d78e397bcff1461fec5
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2023-10-21 14:27:45 -0400

    add license to new file

--- packages/adapter-nextjs/src/generateServerClient.ts
+++ packages/adapter-nextjs/src/generateServerClient.ts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { generateClient as internalGenerateClient } from '@aws-amplify/api-graphql/internals';
 import { getAmplifyServerContext } from '@aws-amplify/core/internals/adapter-core';
 
