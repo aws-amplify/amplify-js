193f48c43986a020954b89424dac04338f01eab9
Allan Zheng <zheallan@amazon.com>
2023-10-23 16:07:25 -0700

    feat: remove error class workaround for ES5

--- packages/notifications/src/pushNotifications/errors/PushNotificationError.ts
+++ packages/notifications/src/pushNotifications/errors/PushNotificationError.ts
@@ -9,10 +9,5 @@ import {
 export class PushNotificationError extends AmplifyError {
 	constructor(params: AmplifyErrorParams) {
 		super(params);
-
-		// Hack for making the custom error class work when transpiled to es5
-		// TODO: Delete the following 2 lines after we change the build target to >= es2015
-		this.constructor = PushNotificationError;
-		Object.setPrototypeOf(this, PushNotificationError.prototype);
 	}
 }
