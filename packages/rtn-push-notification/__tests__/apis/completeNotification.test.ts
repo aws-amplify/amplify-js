6b24cdb76c0999fbb27208a2012394b3db694c86
Chris F <5827964+cshfang@users.noreply.github.com>
2023-10-23 15:08:56 -0700

    chore: Add unit tests to push notifications native module (#12398)
    Co-authored-by: ManojNB <manojnb@amazon.com>

---
+++ packages/rtn-push-notification/__tests__/apis/completeNotification.test.ts
@@ -0,0 +1,32 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { completeNotification } from '../../src/apis/completeNotification';
+import { nativeModule } from '../../src/nativeModule';
+import { completionHandlerId } from '../testUtils/data';
+
+jest.mock('../../src/nativeModule', () => ({
+	nativeModule: {
+		completeNotification: jest.fn(),
+	},
+}));
+
+describe('completeNotification', () => {
+	// assert mocks
+	const mockCompleteNotificationNative =
+		nativeModule.completeNotification as jest.Mock;
+
+	beforeAll(() => {
+		mockCompleteNotificationNative.mockResolvedValue(42);
+	});
+
+	afterEach(() => {
+		mockCompleteNotificationNative.mockClear();
+	});
+
+	it('calls the native completeNotification', () => {
+		completeNotification(completionHandlerId);
+
+		expect(mockCompleteNotificationNative).toBeCalledWith(completionHandlerId);
+	});
+});
