b4234175c1e5a464165b5a8920ce84ef149ce385
Ashwin Kumar <ashwsrir@amazon.com>
2023-10-23 17:37:12 -0700

    fix(interactions): align type names and use named params

--- packages/interactions/src/types/Interactions.ts
+++ packages/interactions/src/types/Interactions.ts
@@ -1,8 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { CompletionCallback, InteractionsResponse } from './Response';
-
 export type InteractionsTextMessage = {
 	content: string;
 	options: {
@@ -21,10 +19,11 @@ export type InteractionsMessage =
 	| InteractionsTextMessage
 	| InteractionsVoiceMessage;
 
-export interface IInteractions {
-	send(
-		botName: string,
-		message: string | InteractionsMessage
-	): Promise<InteractionsResponse>;
-	onComplete(botName: string, callback: CompletionCallback): void;
-}
+export type InteractionsOnCompleteCallback = (
+	error?: Error,
+	completion?: InteractionsResponse
+) => void;
+
+export type InteractionsResponse = {
+	[key: string]: any;
+};
