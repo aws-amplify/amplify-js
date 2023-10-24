791f97630dd8a091ea5084af23bf89a342371447
Jon Wire <iambipedal@gmail.com>
2023-10-24 11:26:10 -0500

    fix missing license headers

--- packages/api-graphql/src/utils/findIndexByFields.ts
+++ packages/api-graphql/src/utils/findIndexByFields.ts
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 /**
  * Iterates through a collection to find a matching item and returns the index.
  *
