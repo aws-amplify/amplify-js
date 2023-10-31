dadf13471a7330c7bf8d94e7c1a8afa2f538786a
Hui Zhao <zhohz@amazon.com>
2023-10-31 11:21:57 -0700

    fix(core): remove used symbol and remove usage of Symbol.for

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -4,17 +4,6 @@
 // Logging constants
 export const AWS_CLOUDWATCH_CATEGORY = 'Logging';
 
-/**
- * This Symbol is used to reference an internal-only PubSub provider that
- * is used for AppSync/GraphQL subscriptions in the API category.
- */
-const hasSymbol =
-	typeof Symbol !== 'undefined' && typeof Symbol.for === 'function';
-
-export const INTERNAL_AWS_APPSYNC_REALTIME_PUBSUB_PROVIDER = hasSymbol
-	? Symbol.for('INTERNAL_AWS_APPSYNC_REALTIME_PUBSUB_PROVIDER')
-	: '@@INTERNAL_AWS_APPSYNC_REALTIME_PUBSUB_PROVIDER';
-
 export const USER_AGENT_HEADER = 'x-amz-user-agent';
 
 // Error exception code constants
