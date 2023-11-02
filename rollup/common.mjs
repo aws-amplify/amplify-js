74fc5743bb6f01cc376a8585c51f5c5b38fe81a5
Hui Zhao <zhohz@amazon.com>
2023-11-02 11:21:59 -0700

    fix(repo): let rollup generate sourcemap

--- rollup/common.mjs
+++ rollup/common.mjs
@@ -3,30 +3,34 @@
 
 const defaultTSConfigPath = './tsconfig.json';
 
+/** @type {import("rollup").OutputOptions}*/
 export const cjsOutput = {
 	dir: 'dist/cjs',
 	format: 'cjs',
 	entryFileNames: '[name].js',
 	preserveModulesRoot: 'src',
+	sourcemap: true,
 };
 
 export const cjsTSOptions = {
 	outDir: 'dist/cjs',
 	declaration: false, // declarations are handled by the ESM build
-	sourceMap: true,
+	sourceMap: false,
 	tsconfig: defaultTSConfigPath,
 };
 
+/** @type {import("rollup").OutputOptions}*/
 export const emsOutput = {
 	dir: 'dist/esm',
 	format: 'es',
 	entryFileNames: '[name].mjs',
 	preserveModulesRoot: 'src',
+	sourcemap: true,
 };
 
 export const emsTSOptions = {
 	outDir: 'dist/esm',
 	declaration: true,
-	sourceMap: true,
+	sourceMap: false,
 	tsconfig: defaultTSConfigPath,
 };
