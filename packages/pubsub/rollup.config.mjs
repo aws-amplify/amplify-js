951b6ac09c6c080ad6f4869fad3396328de0ddfe
Jim Blanchard <jablanch@amazon.com>
2023-11-03 15:24:50 -0500

    fix: Properly bundle Paho Vendor file.

--- packages/pubsub/rollup.config.mjs
+++ packages/pubsub/rollup.config.mjs
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
 import { getInputForGlob } from '../../rollup/utils.mjs';
@@ -17,12 +18,18 @@ const config = defineConfig([
 	// CJS config
 	{
 		input: input,
+		external: [
+			fileURLToPath(new URL('src/vendor/paho-mqtt.js', import.meta.url)),
+		],
 		output: cjsOutput,
 		plugins: [typescript(cjsTSOptions)],
 	},
 	// ESM config
 	{
 		input: input,
+		external: [
+			fileURLToPath(new URL('src/vendor/paho-mqtt.js', import.meta.url)),
+		],
 		output: emsOutput,
 		plugins: [typescript(emsTSOptions)],
 	},
