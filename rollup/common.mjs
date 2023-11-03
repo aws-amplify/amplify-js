570bf15b7b7c373a9c99af1c6d46b29d54b7c9d9
Hui Zhao <zhohz@amazon.com>
2023-11-02 20:41:29 -0700

    fix(repo): let cjs build using CommonJS module

--- rollup/common.mjs
+++ rollup/common.mjs
@@ -16,6 +16,7 @@ export const cjsOutput = {
 export const cjsTSOptions = {
 	outDir: 'dist/cjs',
 	declaration: false, // declarations are handled by the ESM build
+	module: 'CommonJS',
 	sourceMap: false,
 	tsconfig: defaultTSConfigPath,
 	tsBuildInfoFile: 'dist/meta/cjs.tsbuildinfo',
