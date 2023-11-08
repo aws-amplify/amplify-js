5f52c64a47d3828f0f0f20f6588566647ccf871f
Chris F <5827964+cshfang@users.noreply.github.com>
2023-11-07 16:11:43 -0800

    fix(auth): Do not sign out client if Oauth signout fails (#12520)
    * fix(auth): Do not sign out client if Oauth signout fails
    
    * Do not block local data/tokens clearing for private sessions
    
    * Update to perform slightly different OAuth handling on web vs native
    
    ---------
    
    Co-authored-by: Jim Blanchard <jim.l.blanchard@gmail.com>

---
+++ packages/auth/src/providers/cognito/utils/oauth/handleOAuthSignOut.ts
@@ -0,0 +1,24 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { CognitoUserPoolConfig } from '@aws-amplify/core';
+import { OpenAuthSessionResult } from '../../../../utils/types';
+import { DefaultOAuthStore } from '../../utils/signInWithRedirectStore';
+import { completeOAuthSignOut } from './completeOAuthSignOut';
+import { oAuthSignOutRedirect } from './oAuthSignOutRedirect';
+
+export const handleOAuthSignOut = async (
+	cognitoConfig: CognitoUserPoolConfig,
+	store: DefaultOAuthStore
+): Promise<void | OpenAuthSessionResult> => {
+	const { isOAuthSignIn } = await store.loadOAuthSignIn();
+
+	// Clear everything before attempting to visted logout endpoint since the current application
+	// state could be wiped away on redirect
+	await completeOAuthSignOut(store);
+
+	if (isOAuthSignIn) {
+		// On web, this will always end up being a void action
+		return oAuthSignOutRedirect(cognitoConfig);
+	}
+};
