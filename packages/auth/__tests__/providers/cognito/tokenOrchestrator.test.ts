2bad2a3d12020963eca3fe1a42b5f33ce3e9353d
ManojNB <manojnb95@gmail.com>
2023-11-07 16:24:19 -0800

    chore: additional unit tests for identityIdStore and tokenOrchestrator

---
+++ packages/auth/__tests__/providers/cognito/tokenOrchestrator.test.ts
@@ -0,0 +1,78 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { TokenOrchestrator } from '../../../src/providers/cognito';
+import { Hub, ResourcesConfig } from '@aws-amplify/core';
+import { authAPITestParams } from './testUtils/authApiTestParams';
+import { AMPLIFY_SYMBOL } from '@aws-amplify/core/internals/utils';
+jest.mock('@aws-amplify/core', () => ({
+	...jest.requireActual('@aws-amplify/core'),
+	Hub: {
+		dispatch: jest.fn(),
+		listen: jest.fn(),
+	},
+}));
+
+const mockAuthTokenStore = {
+	getLastAuthUser: jest.fn(),
+	loadTokens: jest.fn(),
+	storeTokens: jest.fn(),
+	clearTokens: jest.fn(),
+	setKeyValueStorage: jest.fn(),
+	getDeviceMetadata: jest.fn(),
+	clearDeviceMetadata: jest.fn(),
+};
+const mockTokenRefresher = jest.fn();
+const validAuthConfig: ResourcesConfig = {
+	Auth: {
+		Cognito: {
+			userPoolId: 'us-east-1_test-id',
+			identityPoolId: 'us-east-1:test-id',
+			userPoolClientId: 'test-id',
+			allowGuestAccess: true,
+		},
+	},
+};
+
+describe('TokenOrchestrator', () => {
+	const tokenOrchestrator = new TokenOrchestrator();
+	describe('Happy Path Cases:', () => {
+		beforeEach(() => {
+			tokenOrchestrator.setAuthConfig(validAuthConfig.Auth!);
+			tokenOrchestrator.setAuthTokenStore(mockAuthTokenStore);
+			tokenOrchestrator.setTokenRefresher(mockTokenRefresher);
+		});
+		it('Should get tokens', async () => {
+			mockAuthTokenStore.loadTokens.mockResolvedValue(
+				authAPITestParams.ValidAuthTokens
+			);
+			mockAuthTokenStore.getLastAuthUser.mockResolvedValue('test-username');
+
+			const tokensRes = await tokenOrchestrator.getTokens();
+			expect(tokensRes).toEqual({
+				accessToken: authAPITestParams.ValidAuthTokens.accessToken,
+				idToken: authAPITestParams.ValidAuthTokens.idToken,
+				signInDetails: undefined,
+			});
+		});
+		it('Should call tokenRefresher and return valid tokens', async () => {
+			mockAuthTokenStore.loadTokens.mockResolvedValue(
+				authAPITestParams.ExpiredAuthTokens
+			);
+			mockAuthTokenStore.getLastAuthUser.mockResolvedValue('test-username');
+			mockTokenRefresher.mockResolvedValue(authAPITestParams.ValidAuthTokens);
+			const tokensRes = await tokenOrchestrator.getTokens();
+			expect(tokensRes).toEqual({
+				accessToken: authAPITestParams.ValidAuthTokens.accessToken,
+				idToken: authAPITestParams.ValidAuthTokens.idToken,
+				signInDetails: undefined,
+			});
+			expect(Hub.dispatch).toHaveBeenCalledWith(
+				'auth',
+				{ event: 'tokenRefresh' },
+				'Auth',
+				AMPLIFY_SYMBOL
+			);
+		});
+	});
+});
