964572ef0f4a8e01145064a4569d08ab6e335ec3
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2023-11-16 09:59:13 -0500

    Export generateClient return types

--- packages/adapter-nextjs/src/api/index.ts
+++ packages/adapter-nextjs/src/api/index.ts
@@ -5,3 +5,8 @@ export {
 	generateServerClientUsingReqRes,
 	generateServerClientUsingCookies,
 } from './generateServerClient';
+
+export {
+	V6ClientSSRCookies as ClientUsingSSRCookies,
+	V6ClientSSRRequest as ClientUsingSSRReq,
+} from '@aws-amplify/api-graphql';
