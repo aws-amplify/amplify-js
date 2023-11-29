93d565f3f771472a482b2d2b7e400aee543db553
Aaron S <stocaaro@stocad.com>
2023-11-29 11:02:57 -0600

    Add license

--- packages/api-graphql/src/utils/errors/repackageAuthError.ts
+++ packages/api-graphql/src/utils/errors/repackageAuthError.ts
@@ -1,5 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { GraphQLError } from 'graphql';
-import { GraphQLResult } from '../../types';
 
 type ErrorObject = {
 	errors: GraphQLError[];
