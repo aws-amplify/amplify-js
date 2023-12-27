05f1fa75bc00815a4513ace7a9084e3f9f6da1e6
Israel Arcos <israel.arcos2595@gmail.com>
2023-12-27 16:41:59 -0500

    chore: add debouncing logic to fetchAuthSesion

--- packages/core/src/singleton/apis/fetchAuthSession.ts
+++ packages/core/src/singleton/apis/fetchAuthSession.ts
@@ -5,8 +5,32 @@ import { fetchAuthSession as fetchAuthSessionInternal } from './internal/fetchAu
 import { Amplify } from '../Amplify';
 import { AuthSession, FetchAuthSessionOptions } from '../Auth/types';
 
+let inflightAuthSessionPromise: Promise<AuthSession>;
+let inflightAuthSessionPromiseCounter = 0;
+
+async function debouncedFetchAuhtSession(
+	options?: FetchAuthSessionOptions
+): Promise<AuthSession> {
+	if (inflightAuthSessionPromiseCounter === 0) {
+		inflightAuthSessionPromise = new Promise(async (resolve, reject) => {
+			try {
+				const session = await fetchAuthSessionInternal(Amplify, options);
+				resolve(session);
+			} catch (error) {
+				reject(error);
+			}
+		});
+	}
+	inflightAuthSessionPromiseCounter++;
+	try {
+		return await inflightAuthSessionPromise;
+	} finally {
+		inflightAuthSessionPromiseCounter--;
+	}
+}
+
 export const fetchAuthSession = (
 	options?: FetchAuthSessionOptions
 ): Promise<AuthSession> => {
-	return fetchAuthSessionInternal(Amplify, options);
+	return debouncedFetchAuhtSession(options);
 };
