81753ce5778a9c4cd21aad1b3fbeff320429e425
Ashwin Kumar <ashwsrir@amazon.com>
2024-01-05 13:58:58 -0800

    fix: add index file inside provideName/apis

--- packages/logger/src/providers/console/index.ts
+++ packages/logger/src/providers/console/index.ts
@@ -1,6 +1,4 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export { enable } from './apis/enable';
-export { disable } from './apis/disable';
-export { getProvider } from './apis/getProvider';
+export { enable, disable, getProvider } from './apis';
