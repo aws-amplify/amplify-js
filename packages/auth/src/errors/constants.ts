033e97e8429d97e0124a6ad3d7901775c88e746c
Israel Arcos <israel.arcos2595@gmail.com>
2024-01-05 16:39:49 -0500

    chore: address feedback

--- packages/auth/src/errors/constants.ts
+++ packages/auth/src/errors/constants.ts
@@ -25,4 +25,4 @@ export const invalidOriginException = new AuthError({
 	recoverySuggestion: 'Please call signInWithRedirect from the same origin.',
 });
 export const OAUTH_SIGNOUT_EXCEPTION = 'OAuthSignOutException';
-export const TOKEN_REFRESH_EXCEPTION = 'TOKEN_REFRESH_EXCEPTION';
+export const TOKEN_REFRESH_EXCEPTION = 'TokenRefreshException';
