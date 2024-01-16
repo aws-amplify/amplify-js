b46967dc5324431e046c0ca65803ef17fd645b4e
Israel Arcos <israel.arcos2595@gmail.com>
2024-01-16 10:23:15 -0800

    chore: fix unit test

--- packages/core/src/utils/deDupeAsyncRequests.ts
+++ packages/core/src/utils/deDupeAsyncRequests.ts
@@ -1,6 +1,14 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// this will make the tsc-complience-test to pass
+type Awaited<T> = T extends null | undefined
+	? T // special case for `null | undefined` when not in `--strictNullChecks` mode
+	: T extends object & { then(onfulfilled: infer F, ...args: infer _): any } // `await` only unwraps object types with a callable `then`. Non-object types are not unwrapped
+	  ? F extends (value: infer V, ...args: infer _) => any // if the argument to `then` is callable, extracts the first argument
+			? Awaited<V> // recursively unwrap the value
+			: never // the argument to `then` was not callable
+	  : T; //
 /**
  * returns inflight promise if there hasn't been resolved yet
  *
