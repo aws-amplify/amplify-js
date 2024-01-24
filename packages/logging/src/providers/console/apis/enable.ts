1f2c0267c2e7e7558162976b386cd3d7a8ce62cb
Ashwin Kumar <ashwsrir@amazon.com>
2024-01-23 23:40:37 -0800

    fix(logging): remove console provider

--- packages/logging/src/providers/console/apis/enable.ts
+++
@@ -1,5 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// TODO: pending implementation
-export const enable = (): void => {};
