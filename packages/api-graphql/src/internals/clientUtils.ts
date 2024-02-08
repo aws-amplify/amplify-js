af52ac4ee5c69670e7b5e28fea90bbd897e16a5b
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2024-02-08 09:53:39 -0500

    add license and bundle size

--- packages/api-graphql/src/internals/clientUtils.ts
+++ packages/api-graphql/src/internals/clientUtils.ts
@@ -1,4 +1,7 @@
-import {
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import type {
 	SchemaModel,
 	ModelAttribute,
 	SecondaryIndexAttribute,
