29a6a480aaf9cedcd7f1a17bb473d768ce8dec0b
Jim Blanchard <jablanch@amazon.com>
2024-02-14 12:26:03 -0600

    PR cleanup.

--- packages/aws-amplify/src/analytics/kinesis/index.ts
+++ packages/aws-amplify/src/analytics/kinesis/index.ts
@@ -1,7 +1,4 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-/**
- * @category kinesis 
- */
 export * from '@aws-amplify/analytics/kinesis';
