88be9fb58568f663548dac19805a479c11061bec
Ashwin Kumar <ashwsrir@amazon.com>
2024-03-09 00:28:08 +0530

    feat: add BIND_ALL_LOG_LEVELS support

--- packages/core/__tests__/ConsoleLogger.test.ts
+++ packages/core/__tests__/ConsoleLogger.test.ts
@@ -1,7 +1,18 @@
 import { ConsoleLogger } from '../src';
-import { LoggingProvider } from '../src/Logger/types';
+import { LoggingProvider, LogType } from '../src/Logger/types';
 
 describe('ConsoleLogger', () => {
+	beforeAll(() => {
+		jest.spyOn(console, 'log').mockImplementation(() => {});
+		jest.spyOn(console, 'error').mockImplementation(() => {});
+		jest.spyOn(console, 'warn').mockImplementation(() => {});
+		jest.spyOn(console, 'info').mockImplementation(() => {});
+		jest.spyOn(console, 'debug').mockImplementation(() => {});
+	});
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
 	describe('pluggables', () => {
 		/*it('should store pluggables correctly when addPluggable is called', () => {
 			const provider = new AWSCloudWatchProvider();
@@ -44,17 +55,89 @@ describe('ConsoleLogger', () => {
 			expect(pluggables).toHaveLength(0);
 		});
 	});
+
+	describe('log level filter', () => {
+		afterEach(() => {
+			jest.clearAllMocks();
+		});
+		const logEventToConsoleApiDefaultMapping: Record<string, string> = {
+			error: 'error',
+			warn: 'warn',
+			info: 'log',
+			debug: 'log',
+			verbose: 'log',
+		};
+		const levelVerbose: [string, LogType, number][] = [
+			['verbose', LogType.VERBOSE, 1],
+			['debug', LogType.VERBOSE, 1],
+			['info', LogType.VERBOSE, 1],
+			['warn', LogType.VERBOSE, 1],
+			['error', LogType.VERBOSE, 1],
+		];
+		const levelDebug: [string, LogType, number][] = [
+			['verbose', LogType.DEBUG, 0],
+			['debug', LogType.DEBUG, 1],
+			['info', LogType.DEBUG, 1],
+			['warn', LogType.DEBUG, 1],
+			['error', LogType.DEBUG, 1],
+		];
+		const levelInfo: [string, LogType, number][] = [
+			['verbose', LogType.INFO, 0],
+			['debug', LogType.INFO, 0],
+			['info', LogType.INFO, 1],
+			['warn', LogType.INFO, 1],
+			['error', LogType.INFO, 1],
+		];
+		const levelWarn: [string, LogType, number][] = [
+			['verbose', LogType.WARN, 0],
+			['debug', LogType.WARN, 0],
+			['info', LogType.WARN, 0],
+			['warn', LogType.WARN, 1],
+			['error', LogType.WARN, 1],
+		];
+		const levelError: [string, LogType, number][] = [
+			['verbose', LogType.ERROR, 0],
+			['debug', LogType.ERROR, 0],
+			['info', LogType.ERROR, 0],
+			['warn', LogType.ERROR, 0],
+			['error', LogType.ERROR, 1],
+		];
+		const levelNone: [string, LogType, number][] = [
+			['verbose', LogType.NONE, 0],
+			['debug', LogType.NONE, 0],
+			['info', LogType.NONE, 0],
+			['warn', LogType.NONE, 0],
+			['error', LogType.NONE, 0],
+		];
+
+		it.each([
+			...levelVerbose,
+			...levelDebug,
+			...levelInfo,
+			...levelWarn,
+			...levelError,
+			...levelNone,
+		])(
+			'can user log a %p log event if current level is %p',
+			(logEvent, currentLogLevel, result) => {
+				const loggerName = 'test-logger';
+				const message = `${logEvent} log message`;
+				const consoleEvent = logEventToConsoleApiDefaultMapping[logEvent];
+				ConsoleLogger.LOG_LEVEL = currentLogLevel;
+
+				const logger = new ConsoleLogger(loggerName);
+				(logger[logEvent as keyof ConsoleLogger] as Function)(message);
+
+				expect(
+					console[consoleEvent as keyof typeof console],
+				).toHaveBeenCalledTimes(result);
+			},
+		);
+	});
+
 	describe('log to console', () => {
 		const mockDate = new Date('2024-01-01T17:30:00Z');
 		jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
-
-		beforeEach(() => {
-			jest.spyOn(console, 'log').mockImplementation(() => {});
-			jest.spyOn(console, 'error').mockImplementation(() => {});
-			jest.spyOn(console, 'warn').mockImplementation(() => {});
-			jest.spyOn(console, 'info').mockImplementation(() => {});
-			jest.spyOn(console, 'debug').mockImplementation(() => {});
-		});
 
 		afterEach(() => {
 			jest.clearAllMocks();
@@ -63,14 +146,42 @@ describe('ConsoleLogger', () => {
 		it.each<[string, string, string]>([
 			['error', 'error', 'ERROR'],
 			['warn', 'warn', 'WARN'],
+			['info', 'log', 'INFO'],
+			['debug', 'log', 'DEBUG'],
+			['verbose', 'log', 'VERBOSE'],
+		])(
+			'logger.%p log event should call console.%p API',
+			(logEvent, consoleEvent, logLevel) => {
+				const loggerName = 'test-logger';
+				const message = `${logEvent} log message`;
+				ConsoleLogger.LOG_LEVEL = logLevel;
+
+				const logger = new ConsoleLogger(loggerName);
+				(logger[logEvent as keyof ConsoleLogger] as Function)(message);
+
+				expect(
+					console[consoleEvent as keyof typeof console],
+				).toHaveBeenCalledTimes(1);
+				expect(
+					console[consoleEvent as keyof typeof console],
+				).toHaveBeenCalledWith(
+					`[${logLevel}] 30:00.0 ${loggerName} - ${message}`,
+				);
+			},
+		);
+
+		it.each<[string, string, string]>([
+			['error', 'error', 'ERROR'],
+			['warn', 'warn', 'WARN'],
 			['info', 'info', 'INFO'],
 			['debug', 'debug', 'DEBUG'],
 			['verbose', 'log', 'VERBOSE'],
 		])(
-			'should call console.%p for %p log level',
+			'logger.%p log event should call console.%p API when BIND_ALL_LOG_LEVELS is enabled',
 			(logEvent, consoleEvent, logLevel) => {
 				const loggerName = 'test-logger';
 				const message = `${logEvent} log message`;
+				ConsoleLogger.BIND_ALL_LOG_LEVELS = true;
 				ConsoleLogger.LOG_LEVEL = logLevel;
 
 				const logger = new ConsoleLogger(loggerName);
