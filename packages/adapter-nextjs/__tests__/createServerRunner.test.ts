dab0904032ae5a4c6e2dfe95776be8ba6d78103c
Hui Zhao <zhohz@amazon.com>
2024-03-11 13:03:33 -0700

    chore(adapter-nextjs): run yarn lint:fix

--- packages/adapter-nextjs/__tests__/createServerRunner.test.ts
+++ packages/adapter-nextjs/__tests__/createServerRunner.test.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { ResourcesConfig, sharedInMemoryStorage } from '@aws-amplify/core';
+
 import { NextServer } from '../src/types';
 
 const mockAmplifyConfig: ResourcesConfig = {
