22d042d7fd3709fbbf9963ab9ae5f4908a1298e5
Chris F <5827964+cshfang@users.noreply.github.com>
2024-03-19 11:57:53 -0700

    chore: Refactor setConflictHandler to avoid directly exporting let (#13143)
    Co-authored-by: erinleigh90 <106691284+erinleigh90@users.noreply.github.com>

--- packages/notifications/src/inAppMessaging/providers/pinpoint/utils/index.ts
+++ packages/notifications/src/inAppMessaging/providers/pinpoint/utils/index.ts
@@ -1,6 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+export {
+	getConflictHandler,
+	setConflictHandler,
+} from './conflictHandlerManager';
 export { resolveConfig } from './resolveConfig';
 export { resolveCredentials } from './resolveCredentials';
 export { getInAppMessagingUserAgentString } from './userAgent';
