8d7a0dbe3bcfe8af0b3f9964a88c0a3b530b9960
Hui Zhao <zhohz@amazon.com>
2024-03-20 11:46:21 -0700

    Revert "do not mock console functions globaly but noopify them"
    This reverts commit 913b11e2959e20014b093910f8661da1dc7a2b2e.

--- jest.setup.js
+++ jest.setup.js
@@ -1,14 +1,12 @@
 // Suppress console messages printing during unit tests.
 // Comment out log level as necessary (e.g. while debugging tests)
-const noop = () => undefined;
-
 global.console = {
 	...console,
-	log: noop,
-	debug: noop,
-	info: noop,
-	warn: noop,
-	error: noop,
+	log: jest.fn(),
+	debug: jest.fn(),
+	info: jest.fn(),
+	warn: jest.fn(),
+	error: jest.fn(),
 };
 
 // React Native global
