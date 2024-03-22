644aee18c5d5e385d2314f610df75f48b5e9faf7
Erin Beal <erinleig@amazon.com>
2024-03-22 15:35:28 -0700

    chore: add tsdocs to server getUrl

--- packages/storage/src/providers/s3/apis/server/getUrl.ts
+++ packages/storage/src/providers/s3/apis/server/getUrl.ts
@@ -9,6 +9,23 @@ import {
 import { GetUrlInput, GetUrlOutput } from '../../types';
 import { getUrl as getUrlInternal } from '../internal/getUrl';
 
+/**
+ * Get a temporary presigned URL to download the specified S3 object.
+ * The presigned URL expires when the associated role used to sign the request expires or
+ * the option  `expiresIn` is reached. The `expiresAt` property in the output object indicates when the URL MAY expire.
+ *
+ * By default, it will not validate the object that exists in S3. If you set the `options.validateObjectExistence`
+ * to true, this method will verify the given object already exists in S3 before returning a presigned
+ * URL, and will throw `StorageError` if the object does not exist.
+ *
+ * @param contextSpec - The isolated server context.
+ * @param input - The `GetUrlInput` object.
+ * @returns Presigned URL and timestamp when the URL MAY expire.
+ * @throws service: `S3Exception` - thrown when checking for existence of the object
+ * @throws validation: `StorageValidationErrorCode` - Validation errors
+ * thrown either username or key are not defined.
+ *
+ */
 export const getUrl = async (
 	contextSpec: AmplifyServer.ContextSpec,
 	input: GetUrlInput,
