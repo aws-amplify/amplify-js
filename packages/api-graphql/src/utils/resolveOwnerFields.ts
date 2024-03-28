19ecf146d8b60fb1721f8691c7800b0a340c37db
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2024-03-28 14:43:47 -0400

    fix(api-graphql): default selection set regression

--- packages/api-graphql/src/utils/resolveOwnerFields.ts
+++ packages/api-graphql/src/utils/resolveOwnerFields.ts
@@ -42,7 +42,10 @@ export function resolveOwnerFields(model: Model): string[] {
 			for (const rule of attr.properties.rules) {
 				if (rule.allow === 'owner') {
 					ownerFields.add(rule.ownerField || 'owner');
-				} else if (rule.allow === 'groups') {
+				} else if (rule.allow === 'groups' && rule.groupsField !== undefined) {
+					// only valid for dynamic group(s)
+					// static group auth will have an array of predefined groups in the attribute, groups: string[]
+					// but `groupsField` will be undefined
 					ownerFields.add(rule.groupsField);
 				}
 			}
