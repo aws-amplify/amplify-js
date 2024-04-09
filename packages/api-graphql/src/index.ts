1480fa792307cb0cc494d25aa5ffd3dee8ff841d
Jon Wire <iambipedal@gmail.com>
2024-04-09 14:57:56 -0500

    checkpoint

--- packages/api-graphql/src/index.ts
+++ packages/api-graphql/src/index.ts
@@ -2,6 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export { GraphQLAPI, GraphQLAPIClass, graphqlOperation } from './GraphQLAPI';
-export type * from '@aws-amplify/data-schema';
+export * from './types';
 
 export { CONNECTION_STATE_CHANGE } from './Providers/constants';
