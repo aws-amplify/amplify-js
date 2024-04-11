0e2d74e3cdd4ce28182c32e66c8b04e2d57f9563
ashika112 <akasivis@amazon.com>
2024-04-10 18:39:47 -0700

    update copy return types

--- packages/storage/src/providers/s3/types/outputs.ts
+++ packages/storage/src/providers/s3/types/outputs.ts
@@ -56,6 +56,51 @@ export type GetPropertiesOutput = ItemWithKeyAndPath;
 /**
  * Output type for S3 Copy API.
  */
+<<<<<<< HEAD
+=======
+export type ListAllOutput = StrictUnion<
+	ListAllOutputWithPath | ListAllOutputWithPrefix
+>;
+
+/**
+ * Output type for S3 list API. Lists bucket objects with pagination.
+ */
+export type ListPaginateOutput = StrictUnion<
+	ListPaginateOutputWithPath | ListPaginateOutputWithPrefix
+>;
+
+/**
+ * @deprecated Use {@link ListAllOutputWithPath} instead.
+ * Output type for S3 list API. Lists all bucket objects.
+ */
+export type ListAllOutputWithPrefix = StorageListOutput<ListOutputItemWithKey>;
+
+/**
+ * Output type for S3 list API. Lists all bucket objects.
+ */
+export type ListAllOutputWithPath = StorageListOutput<ListOutputItemWithPath>;
+
+/**
+ * @deprecated Use {@link ListPaginateOutputWithPath} instead.
+ * Output type for S3 list API. Lists bucket objects with pagination.
+ */
+export type ListPaginateOutputWithPrefix =
+	StorageListOutput<ListOutputItemWithKey> & {
+		nextToken?: string;
+	};
+
+/**
+ * Output type for S3 list API. Lists bucket objects with pagination.
+ */
+export type ListPaginateOutputWithPath =
+	StorageListOutput<ListOutputItemWithPath> & {
+		nextToken?: string;
+	};
+
+/**
+ * Output type for S3 Copy API.
+ */
+>>>>>>> 28ca2f3ec (update copy return types)
 export type CopyOutput = Pick<ItemWithKeyAndPath, 'key' | 'path'>;
 
 /**
