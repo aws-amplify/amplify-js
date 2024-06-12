814ccf34f53ee1d863017eb64333d23aecda5892
JoonWon Choi <joonwonc@amazon.com>
2024-06-11 18:35:53 -0700

    Fix error dependency path

--- packages/auth/src/providers/cognito/utils/storage/syncSessionStorage.ts
+++ packages/auth/src/providers/cognito/utils/storage/syncSessionStorage.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { PlatformNotSupportedError } from '@aws-amplify/core/src/errors';
+import { PlatformNotSupportedError } from '@aws-amplify/core/internals/utils';
 
 import { SyncKeyValueStorageInterface } from '../../types';
 
