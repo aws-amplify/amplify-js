8918e335c54cd26df8ed312c5c5b8a167f0b0322
Allan Zheng <zheallan@amazon.com>
2024-07-12 10:40:24 -0700

    Revert "doc(storage): add disclaimer"
    This reverts commit eadb58f871b99e513e36a97838f49d2034d142eb.

--- packages/storage/src/storageBrowser/index.ts
+++ packages/storage/src/storageBrowser/index.ts
@@ -1,12 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-/**
- * NOTE: The APIs exported from this file are ONLY intended for usage by
- * Amplify UI. To use location-related features, please use
- * @aws-amplify/ui-react-storage
- */
-
 export { createLocationCredentialsStore } from './locationCredentialsStore';
 export { createManagedAuthConfigAdapter } from './managedAuthConfigAdapter/createManagedAuthConfigAdapter';
 export { GetLocationCredentials, ListLocations } from './types';
