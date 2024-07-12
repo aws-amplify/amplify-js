cf53b2231c61a46af0ea3433b44c08f17a944aa2
Allan Zheng <zheallan@amazon.com>
2024-07-12 10:39:44 -0700

    doc(storage): add disclaimer

--- packages/storage/src/storageBrowser/index.ts
+++ packages/storage/src/storageBrowser/index.ts
@@ -1,6 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+/**
+ * NOTE: The APIs exported from this file are ONLY intended for usage by
+ * Amplify UI. To use location-related features, please use
+ * @aws-amplify/ui-react-storage
+ */
+
 export {
 	listCallerAccessGrants,
 	ListCallerAccessGrantsInput,
