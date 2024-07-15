d46660d8eb62b2079684ae425cae44ab53d7aa51
Allan Zheng <zheallan@amazon.com>
2024-07-15 10:58:59 -0700

    feat: list-locations api

--- packages/storage/src/storageBrowser/managedAuthConfigAdapter/createListLocationsHandler.ts
+++ packages/storage/src/storageBrowser/managedAuthConfigAdapter/createListLocationsHandler.ts
@@ -2,8 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { CredentialsProvider, ListLocations } from '../types';
+import { logger } from '../../utils';
 
-export interface CreateListLocationsHandlerInput {
+declare const listCallerAccessGrants: (config: any, input: any) => Promise<any>;
+
+const MAX_PAGE_SIZE = 1000;
+
+interface CreateListLocationsHandlerInput {
 	accountId: string;
 	credentialsProvider: CredentialsProvider;
 	region: string;
@@ -13,6 +18,26 @@ export const createListLocationsHandler = (
 	// eslint-disable-next-line unused-imports/no-unused-vars
 	input: CreateListLocationsHandlerInput,
 ): ListLocations => {
-	// TODO(@AllanZhengYP)
-	throw new Error('Not Implemented');
+	const config = {
+		credentials: input.credentialsProvider,
+		region: input.region,
+	};
+
+	return async (handlerInput = {}) => {
+		const { nextToken, pageSize } = handlerInput;
+		logger.debug(`list available locations from account ${input.accountId}`);
+		const params = {
+			AccountId: input.accountId,
+			NextToken: nextToken,
+			MaxKeys: pageSize,
+		};
+		if (!!pageSize && pageSize > MAX_PAGE_SIZE) {
+			logger.debug(`defaulting pageSize to ${MAX_PAGE_SIZE}.`);
+			params.MaxKeys = MAX_PAGE_SIZE;
+		}
+		const { result } = await listCallerAccessGrants(config, params);
+
+		// TODO
+		return result;
+	};
 };
