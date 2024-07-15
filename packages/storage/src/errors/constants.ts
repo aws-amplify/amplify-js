53daae65947397535ec5590888964d0fcdf88249
Israel Arcos <israel.arcos2595@gmail.com>
2024-07-15 09:33:38 -0400

    chore: fix unit tests

--- packages/storage/src/errors/constants.ts
+++ packages/storage/src/errors/constants.ts
@@ -1,2 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 export const NO_STORAGE_CONFIG = 'NoStorageConfig';
 export const INVALID_STORAGE_PATH = 'InvalidStoragePath';
