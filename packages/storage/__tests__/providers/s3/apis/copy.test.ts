wuuxigh/amplify-js-draft

feat: add notModifiedSince and eTag to copy

packages/storage/__tests__/providers/s3/apis/copy.test.ts
@@ -228,6 +228,42 @@ describe('copy API', () => {
 					},
 				);
 			});
+
+			it('should pass notModifiedSince to copyObject', async () => {
+				const mockDate = 'mock-date' as any;
+				await copyWrapper({
+					source: {
+						key: 'sourceKey',
+						notModifiedSince: mockDate,
+					},
+					destination: { key: 'destinationKey' },
+				});
+				expect(copyObject).toHaveBeenCalledTimes(1);
+				expect(copyObject).toHaveBeenCalledWith(
+					expect.anything(),
+					expect.objectContaining({
+						CopySourceIfModifiedSince: mockDate,
+					}),
+				);
+			});
+
+			it('should pass eTag to copyObject', async () => {
+				const mockEtag = 'mock-etag';
+				await copyWrapper({
+					source: {
+						key: 'sourceKey',
+						eTag: mockEtag,
+					},
+					destination: { key: 'destinationKey' },
+				});
+				expect(copyObject).toHaveBeenCalledTimes(1);
+				expect(copyObject).toHaveBeenCalledWith(
+					expect.anything(),
+					expect.objectContaining({
+						CopySourceIfMatch: mockEtag,
+					}),
+				);
+			});
 		});
 
 		describe('With path', () => {
@@ -308,6 +344,42 @@ describe('copy API', () => {
 						CopySource: `${bucket}/sourcePath`,
 						Key: 'destinationPath',
 					},
+				);
+			});
+
+			it('should pass notModifiedSince to copyObject', async () => {
+				const mockDate = 'mock-date' as any;
+				await copyWrapper({
+					source: {
+						path: 'sourceKey',
+						notModifiedSince: mockDate,
+					},
+					destination: { path: 'destinationKey' },
+				});
+				expect(copyObject).toHaveBeenCalledTimes(1);
+				expect(copyObject).toHaveBeenCalledWith(
+					expect.anything(),
+					expect.objectContaining({
+						CopySourceIfModifiedSince: mockDate,
+					}),
+				);
+			});
+
+			it('should pass eTag to copyObject', async () => {
+				const mockEtag = 'mock-etag';
+				await copyWrapper({
+					source: {
+						path: 'sourceKey',
+						eTag: mockEtag,
+					},
+					destination: { path: 'destinationKey' },
+				});
+				expect(copyObject).toHaveBeenCalledTimes(1);
+				expect(copyObject).toHaveBeenCalledWith(
+					expect.anything(),
+					expect.objectContaining({
+						CopySourceIfMatch: mockEtag,
+					}),
 				);
 			});
 		});

packages/storage/__tests__/providers/s3/utils/client/S3/cases/copyObject.ts
@@ -23,6 +23,8 @@ const copyObjectHappyCase: ApiFunctionalTestCase<typeof copyObject> = [
 		CacheControl: 'cacheControl',
 		ContentType: 'contentType',
 		ACL: 'acl',
+		CopySourceIfMatch: 'eTag',
+		CopySourceIfModifiedSince: new Date(0),
 	},
 	expect.objectContaining({
 		url: expect.objectContaining({
@@ -34,6 +36,8 @@ const copyObjectHappyCase: ApiFunctionalTestCase<typeof copyObject> = [
 			'cache-control': 'cacheControl',
 			'content-type': 'contentType',
 			'x-amz-acl': 'acl',
+			'x-amz-copy-source-if-match': 'eTag',
+			'x-amz-copy-source-if-modified-since': '1970-01-01T00:00:00.000Z',
 		}),
 	}),
 	{

packages/storage/src/providers/s3/apis/internal/copy.ts
@@ -105,6 +105,8 @@ const copyWithPath = async (
 		destination: finalCopyDestination,
 		bucket: destBucket,
 		s3Config,
+		notModifiedSince: input.source.notModifiedSince,
+		eTag: input.source.eTag,
 	});
 
 	return { path: finalCopyDestination };
@@ -162,6 +164,8 @@ export const copyWithKey = async (
 		destination: finalCopyDestination,
 		bucket: destBucket,
 		s3Config,
+		notModifiedSince: input.source.notModifiedSince,
+		eTag: input.source.eTag,
 	});
 
 	return {
@@ -174,11 +178,15 @@ const serviceCopy = async ({
 	destination,
 	bucket,
 	s3Config,
+	notModifiedSince,
+	eTag,
 }: {
 	source: string;
 	destination: string;
 	bucket: string;
 	s3Config: ResolvedS3Config;
+	notModifiedSince?: Date;
+	eTag?: string;
 }) => {
 	await copyObject(
 		{
@@ -190,6 +198,8 @@ const serviceCopy = async ({
 			CopySource: source,
 			Key: destination,
 			MetadataDirective: 'COPY', // Copies over metadata like contentType as well
+			CopySourceIfMatch: eTag,
+			CopySourceIfModifiedSince: notModifiedSince,
 		},
 	);
 };

aws-amplify/amplify-js

packages/storage/__tests__/providers/s3/apis/copy.test.ts
@@ -228,6 +228,42 @@ describe('copy API', () => {
 					},
 				);
 			});
+
+			it('should pass notModifiedSince to copyObject', async () => {
+				const mockDate = 'mock-date' as any;
+				await copyWrapper({
+					source: {
+						key: 'sourceKey',
+						notModifiedSince: mockDate,
+					},
+					destination: { key: 'destinationKey' },
+				});
+				expect(copyObject).toHaveBeenCalledTimes(1);
+				expect(copyObject).toHaveBeenCalledWith(
+					expect.anything(),
+					expect.objectContaining({
+						CopySourceIfUnmodifiedSince: mockDate,
+					}),
+				);
+			});
+
+			it('should pass eTag to copyObject', async () => {
+				const mockEtag = 'mock-etag';
+				await copyWrapper({
+					source: {
+						key: 'sourceKey',
+						eTag: mockEtag,
+					},
+					destination: { key: 'destinationKey' },
+				});
+				expect(copyObject).toHaveBeenCalledTimes(1);
+				expect(copyObject).toHaveBeenCalledWith(
+					expect.anything(),
+					expect.objectContaining({
+						CopySourceIfMatch: mockEtag,
+					}),
+				);
+			});
 		});
 
 		describe('With path', () => {
@@ -308,6 +344,42 @@ describe('copy API', () => {
 						CopySource: `${bucket}/sourcePath`,
 						Key: 'destinationPath',
 					},
+				);
+			});
+
+			it('should pass notModifiedSince to copyObject', async () => {
+				const mockDate = 'mock-date' as any;
+				await copyWrapper({
+					source: {
+						path: 'sourcePath',
+						notModifiedSince: mockDate,
+					},
+					destination: { path: 'destinationPath' },
+				});
+				expect(copyObject).toHaveBeenCalledTimes(1);
+				expect(copyObject).toHaveBeenCalledWith(
+					expect.anything(),
+					expect.objectContaining({
+						CopySourceIfUnmodifiedSince: mockDate,
+					}),
+				);
+			});
+
+			it('should pass eTag to copyObject', async () => {
+				const mockEtag = 'mock-etag';
+				await copyWrapper({
+					source: {
+						path: 'sourcePath',
+						eTag: mockEtag,
+					},
+					destination: { path: 'destinationPath' },
+				});
+				expect(copyObject).toHaveBeenCalledTimes(1);
+				expect(copyObject).toHaveBeenCalledWith(
+					expect.anything(),
+					expect.objectContaining({
+						CopySourceIfMatch: mockEtag,
+					}),
 				);
 			});
 		});

packages/storage/__tests__/providers/s3/utils/client/S3/cases/copyObject.ts
@@ -23,6 +23,8 @@ const copyObjectHappyCase: ApiFunctionalTestCase<typeof copyObject> = [
 		CacheControl: 'cacheControl',
 		ContentType: 'contentType',
 		ACL: 'acl',
+		CopySourceIfMatch: 'eTag',
+		CopySourceIfUnmodifiedSince: new Date(0),
 	},
 	expect.objectContaining({
 		url: expect.objectContaining({
@@ -34,6 +36,8 @@ const copyObjectHappyCase: ApiFunctionalTestCase<typeof copyObject> = [
 			'cache-control': 'cacheControl',
 			'content-type': 'contentType',
 			'x-amz-acl': 'acl',
+			'x-amz-copy-source-if-match': 'eTag',
+			'x-amz-copy-source-if-unmodified-since': '1970-01-01T00:00:00.000Z',
 		}),
 	}),
 	{

packages/storage/src/providers/s3/apis/internal/copy.ts
@@ -105,6 +105,8 @@ const copyWithPath = async (
 		destination: finalCopyDestination,
 		bucket: destBucket,
 		s3Config,
+		notModifiedSince: input.source.notModifiedSince,
+		eTag: input.source.eTag,
 	});
 
 	return { path: finalCopyDestination };
@@ -162,6 +164,8 @@ export const copyWithKey = async (
 		destination: finalCopyDestination,
 		bucket: destBucket,
 		s3Config,
+		notModifiedSince: input.source.notModifiedSince,
+		eTag: input.source.eTag,
 	});
 
 	return {
@@ -174,11 +178,15 @@ const serviceCopy = async ({
 	destination,
 	bucket,
 	s3Config,
+	notModifiedSince,
+	eTag,
 }: {
 	source: string;
 	destination: string;
 	bucket: string;
 	s3Config: ResolvedS3Config;
+	notModifiedSince?: Date;
+	eTag?: string;
 }) => {
 	await copyObject(
 		{
@@ -190,6 +198,8 @@ const serviceCopy = async ({
 			CopySource: source,
 			Key: destination,
 			MetadataDirective: 'COPY', // Copies over metadata like contentType as well
+			CopySourceIfMatch: eTag,
+			CopySourceIfUnmodifiedSince: notModifiedSince,
 		},
 	);
 };