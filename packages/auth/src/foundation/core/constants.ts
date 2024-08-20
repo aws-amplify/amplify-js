5de1cde940c81c78ce30c5e4c4015de1a724aec5
Hui Zhao <zhohz@amazon.com>
2024-08-20 14:28:06 -0700

    chore(auth): add missing license header

--- packages/auth/src/foundation/core/constants.ts
+++ packages/auth/src/foundation/core/constants.ts
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 /**
  * The service name used to sign requests if the API requires authentication.
  */
--- packages/auth/src/foundation/factories/serviceClients/cognitoIdentityProvider/createChangePasswordClient.ts
+++ packages/auth/src/foundation/factories/serviceClients/cognitoIdentityProvider/createChangePasswordClient.ts
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 import { composeServiceApi } from '@aws-amplify/core/internals/aws-client-utils/composers';
 
 import { cognitoUserPoolTransferHandler } from './shared/handler';
