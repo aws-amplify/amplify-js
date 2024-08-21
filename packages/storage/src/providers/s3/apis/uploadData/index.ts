f0bff43ddba6d23d0e997fe741913ae0a4493a9c
Erin Beal <erinleig@amazon.com>
2024-08-21 08:55:16 -0700

    fix: revert uploadData example code

--- packages/storage/src/providers/s3/apis/uploadData/index.ts
+++ packages/storage/src/providers/s3/apis/uploadData/index.ts
@@ -162,9 +162,3 @@ export function uploadData(input: UploadDataInput | UploadDataWithPathInput) {
 		});
 	}
 }
-
-uploadData({
-	path: '',
-	data: '',
-	options: { bucket: { bucketName: '', region: '', expectedBucketOwner: '' } },
-});
