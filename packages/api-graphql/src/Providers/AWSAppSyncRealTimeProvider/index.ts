45545b2d7c2bd26e21a4362074e7761322a419a9
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2024-08-27 13:38:29 -0400

    remove unnecessary import

--- packages/api-graphql/src/Providers/AWSAppSyncRealTimeProvider/index.ts
+++ packages/api-graphql/src/Providers/AWSAppSyncRealTimeProvider/index.ts
@@ -1,7 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { URLSearchParams } from 'url';
-
 import { Observable, SubscriptionLike } from 'rxjs';
 import { GraphQLError } from 'graphql';
 import {
