01ab4a612ed8fc815b7f9f48b48bc42291829174
Ashwin Kumar <ashwsrir@amazon.com>
2024-09-24 16:20:08 -0700

    code cleanup

--- packages/storage/src/foundation/factories/serviceClients/s3data/shared/serde/index.ts
+++ packages/storage/src/foundation/factories/serviceClients/s3data/shared/serde/index.ts
@@ -1,5 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export { createDeleteObjectSerializer } from './CreateDeleteObjectSerializer';
-export { createDeleteObjectDeserializer } from './CreateDeleteObjectDeserializer';
+export { createDeleteObjectSerializer } from './createDeleteObjectSerializer';
+export { createDeleteObjectDeserializer } from './createDeleteObjectDeserializer';
