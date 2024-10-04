9cbc71d8a78ce8944db9642d7222efaec55f99e1
ashika112 <akasivis@amazon.com>
2024-10-04 12:41:26 -0700

    update the listLocation handler

--- packages/storage/src/internals/amplifyAuthConfigAdapter/createAmplifyAuthConfigAdapter.ts
+++ packages/storage/src/internals/amplifyAuthConfigAdapter/createAmplifyAuthConfigAdapter.ts
@@ -1,10 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { Amplify, fetchAuthSession } from '@aws-amplify/core';
 
 import { ListPaths } from '../types/credentials';
 
-import { generateLocationsFromPaths } from './generateLocationsFromPaths';
+import { createAmplifyListLocationsHandler } from './createAmplifyListLocationsHandler';
 
 export interface AuthConfigAdapter {
 	listLocations: ListPaths;
@@ -14,26 +13,4 @@ export const createAmplifyAuthConfigAdapter = (): AuthConfigAdapter => {
 	const listLocations = createAmplifyListLocationsHandler();
 
 	return { listLocations };
-};
-
-const createAmplifyListLocationsHandler = (): ListPaths => {
-	const { buckets } = Amplify.getConfig().Storage!.S3!;
-
-	return async function listLocations() {
-		if (!buckets) {
-			return { locations: [] };
-		}
-
-		const { tokens, identityId } = await fetchAuthSession();
-		const userGroups = tokens?.accessToken.payload['cognito:groups'];
-
-		const locations = generateLocationsFromPaths({
-			buckets,
-			tokens: !!tokens,
-			identityId,
-			userGroup: userGroups && (userGroups as any)[0], // TODO: fix this edge case
-		});
-
-		return { locations };
-	};
 };
