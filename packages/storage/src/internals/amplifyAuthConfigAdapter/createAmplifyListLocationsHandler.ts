c8af4548de19860107d18fc02e8d859ad0d9ee45
ashika112 <akasivis@amazon.com>
2024-10-07 14:29:22 -0700

    add pagination logic

--- packages/storage/src/internals/amplifyAuthConfigAdapter/createAmplifyListLocationsHandler.ts
+++ packages/storage/src/internals/amplifyAuthConfigAdapter/createAmplifyListLocationsHandler.ts
@@ -3,7 +3,7 @@
 
 import { Amplify, fetchAuthSession } from '@aws-amplify/core';
 
-import { ListPaths, ListPathsOutput } from '../types/credentials';
+import { ListPaths, PathAccess } from '../types/credentials';
 
 import { generateLocationsFromPaths } from './generateLocationsFromPaths';
 
@@ -11,18 +11,24 @@ export const createAmplifyListLocationsHandler = (): ListPaths => {
 	const { buckets } = Amplify.getConfig().Storage!.S3!;
 	// eslint-disable-next-line no-debugger
 	debugger;
-	let cachedResult: Record<string, ListPathsOutput> | null = null;
+	let cachedResult: Record<string, { locations: PathAccess[] }> | null = null;
 
-	return async function listLocations() {
+	return async function listLocations(input = {}) {
 		if (!buckets) {
 			return { locations: [] };
 		}
+		const { pageSize, nextToken } = input;
 
 		const { tokens, identityId } = await fetchAuthSession();
 		const userGroups = tokens?.accessToken.payload['cognito:groups'];
 		const cacheKey = JSON.stringify({ identityId, userGroups }) + `${!!tokens}`;
 
-		if (cachedResult && cachedResult[cacheKey]) return cachedResult[cacheKey];
+		if (cachedResult && cachedResult[cacheKey])
+			return getPaginatedResult({
+				locations: cachedResult[cacheKey].locations,
+				pageSize,
+				nextToken,
+			});
 
 		cachedResult = {};
 
@@ -35,6 +41,44 @@ export const createAmplifyListLocationsHandler = (): ListPaths => {
 
 		cachedResult[cacheKey] = { locations };
 
-		return cachedResult[cacheKey];
+		return getPaginatedResult({
+			locations: cachedResult[cacheKey].locations,
+			pageSize,
+			nextToken,
+		});
+	};
+};
+
+const getPaginatedResult = ({
+	locations,
+	pageSize,
+	nextToken,
+}: {
+	locations: PathAccess[];
+	pageSize?: number;
+	nextToken?: string;
+}) => {
+	if (pageSize) {
+		if (nextToken) {
+			const start = -nextToken;
+			const end = start > pageSize ? start + pageSize : undefined;
+
+			return {
+				locations: locations.slice(start, end),
+				nextToken: end ? `${end}` : undefined,
+			};
+		}
+
+		return {
+			locations: locations.slice(0, pageSize),
+			nextToken:
+				locations.length > pageSize
+					? `${locations.length - pageSize}`
+					: undefined,
+		};
+	}
+
+	return {
+		locations,
 	};
 };
