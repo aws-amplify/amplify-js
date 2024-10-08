193a98877bc5771d9601d2e5a141742863ce0c96
Pierre Levavasseur <pierre@scorenco.com>
2024-10-08 11:08:23 +0200

    fix: add storage availability check for localStorage and sessionStorage with fallback

--- packages/core/src/storage/utils.ts
+++ packages/core/src/storage/utils.ts
@@ -4,11 +4,40 @@
 import { InMemoryStorage } from './InMemoryStorage';
 
 /**
+ * Checks if Web Storage (localStorage or sessionStorage) is available.
+ *
+ * Based on MDN documentation:
+ * https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#testing_for_availability
+ */
+const storageAvailable = (type: 'localStorage' | 'sessionStorage') => {
+	let storage;
+
+	try {
+		storage = window[type];
+		const x = '__storage_test__';
+		storage.setItem(x, x);
+		storage.removeItem(x);
+
+		return true;
+	} catch (e) {
+		console.log(e);
+
+		return (
+			e instanceof DOMException &&
+			e.name === 'QuotaExceededError' &&
+			// acknowledge QuotaExceededError only if there's something already stored
+			storage &&
+			storage.length !== 0
+		);
+	}
+};
+
+/**
  * @internal
  * @returns Either a reference to window.localStorage or an in-memory storage as fallback
  */
 export const getLocalStorageWithFallback = (): Storage =>
-	typeof window !== 'undefined' && window.localStorage
+	storageAvailable('localStorage')
 		? window.localStorage
 		: new InMemoryStorage();
 
@@ -17,6 +46,6 @@ export const getLocalStorageWithFallback = (): Storage =>
  * @returns Either a reference to window.sessionStorage or an in-memory storage as fallback
  */
 export const getSessionStorageWithFallback = (): Storage =>
-	typeof window !== 'undefined' && window.sessionStorage
+	storageAvailable('sessionStorage')
 		? window.sessionStorage
 		: new InMemoryStorage();
