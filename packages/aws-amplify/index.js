460ff0c98b6648d80326e91c37a5fd82555da0ba
yuhengshs <yuhengsh@gmail.com>
2024-10-21 12:47:36 -0700

    chore: update indentation error pubsub/package.json

--- packages/aws-amplify/index.js
+++ packages/aws-amplify/index.js
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 'use strict';
-// nice
 if (process.env.NODE_ENV === 'production') {
 	module.exports = require('./dist/aws-amplify.min.js');
 } else {
