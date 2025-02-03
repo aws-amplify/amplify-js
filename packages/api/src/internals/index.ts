d5b9a84f375a85dd2194f78975d21d2ced0ab47f
Hui Zhao <zhohz@amazon.com>
2025-02-03 15:03:25 -0800

    fix(aws-amplify|api): internals export paths

--- packages/api/src/internals/index.ts
+++ packages/api/src/internals/index.ts
@@ -2,3 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 export { InternalAPI, InternalAPIClass } from './InternalAPI';
 export { generateClientWithAmplifyInstance } from '@aws-amplify/api-graphql/internals/server';
+export {
+	V6ClientSSRCookies,
+	V6ClientSSRRequest,
+} from '@aws-amplify/api-graphql';
+export {
+	CommonPublicClientOptions,
+	DefaultCommonClientOptions,
+} from '@aws-amplify/api-graphql/internals';
--- packages/aws-amplify/src/api/internals.ts
+++ packages/aws-amplify/src/api/internals.ts
@@ -1,12 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export { generateClientWithAmplifyInstance } from '@aws-amplify/api/internals';
 export {
+	generateClientWithAmplifyInstance,
 	V6ClientSSRCookies,
 	V6ClientSSRRequest,
-} from '@aws-amplify/api-graphql';
-export {
 	CommonPublicClientOptions,
 	DefaultCommonClientOptions,
-} from '@aws-amplify/api-graphql/internals';
+} from '@aws-amplify/api/internals';
