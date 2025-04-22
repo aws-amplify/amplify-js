eecbaf6b4b1a6d0f488f7a36ff3aa77e9dbb039c
James Jarvis <jjarvisp@amazon.com>
2025-04-21 17:40:27 -0700

    feat: add ios webauthn support

--- packages/auth/src/client/utils/passkey/getPasskey.native.ts
+++ packages/auth/src/client/utils/passkey/getPasskey.native.ts
@@ -1,8 +1,31 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { PlatformNotSupportedError } from '@aws-amplify/core/internals/utils';
+import { loadAmplifyRtnPasskeys } from '@aws-amplify/react-native';
 
-export const getPasskey = async () => {
-	throw new PlatformNotSupportedError();
+import { PasskeyGetOptionsJson, PasskeyGetResultJson } from './types';
+import {
+	PasskeyErrorCode,
+	assertPasskeyError,
+	handlePasskeyAuthenticationError,
+} from './errors';
+import { getIsPasskeySupported } from './getIsPasskeySupported';
+
+export const getPasskey = async (
+	input: PasskeyGetOptionsJson,
+): Promise<PasskeyGetResultJson> => {
+	try {
+		const isPasskeySupported = getIsPasskeySupported();
+
+		assertPasskeyError(
+			isPasskeySupported,
+			PasskeyErrorCode.PasskeyNotSupported,
+		);
+
+		const credential = await loadAmplifyRtnPasskeys().getPasskey(input);
+
+		return credential;
+	} catch (err: unknown) {
+		throw handlePasskeyAuthenticationError(err);
+	}
 };
--- packages/auth/src/client/utils/passkey/registerPasskey.native.ts
+++ packages/auth/src/client/utils/passkey/registerPasskey.native.ts
@@ -1,8 +1,31 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { PlatformNotSupportedError } from '@aws-amplify/core/internals/utils';
+import { loadAmplifyRtnPasskeys } from '@aws-amplify/react-native';
 
-export const registerPasskey = async () => {
-	throw new PlatformNotSupportedError();
+import { PasskeyCreateOptionsJson, PasskeyCreateResultJson } from './types';
+import {
+	PasskeyErrorCode,
+	assertPasskeyError,
+	handlePasskeyRegistrationError,
+} from './errors';
+import { getIsPasskeySupported } from './getIsPasskeySupported';
+
+export const registerPasskey = async (
+	input: PasskeyCreateOptionsJson,
+): Promise<PasskeyCreateResultJson> => {
+	try {
+		const isPasskeySupported = getIsPasskeySupported();
+
+		assertPasskeyError(
+			isPasskeySupported,
+			PasskeyErrorCode.PasskeyNotSupported,
+		);
+
+		const credential = await loadAmplifyRtnPasskeys().createPasskey(input);
+
+		return credential;
+	} catch (err: unknown) {
+		throw handlePasskeyRegistrationError(err);
+	}
 };
