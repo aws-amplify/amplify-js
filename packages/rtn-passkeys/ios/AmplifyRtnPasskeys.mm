217aa0fb87c2a028f70e85db9caa7a30b20c7ddb
James Jarvis <jjarvisp@amazon.com>
2025-05-07 21:43:44 -0700

    feat: simplify objc swift adapter pattern

--- packages/rtn-passkeys/ios/AmplifyRtnPasskeys.mm
+++ packages/rtn-passkeys/ios/AmplifyRtnPasskeys.mm
@@ -8,63 +8,57 @@
               resolve:(nonnull RCTPromiseResolveBlock)resolve
                reject:(nonnull RCTPromiseRejectBlock)reject {
 
-	NSMutableDictionary *rawInput = [@{
-		@"challenge" : input.challenge(),
-		@"rp" : @{@"id" : input.rp().id_()},
-		@"user" : @{@"id" : input.user().id_(), @"name" : input.user().name()},
-	} mutableCopy];
-	
-	NSMutableArray *excludeCredentials = [NSMutableArray array];
-	
+	NSMutableArray *excludeCredentials = [@[] mutableCopy];
+
 	if (input.excludeCredentials().has_value()) {
 		auto credentials = input.excludeCredentials().value();
 		for (const auto &credential : credentials) {
-			[excludeCredentials addObject:@{ @"id": credential.id_() }];
+			[excludeCredentials addObject:credential.id_()];
 		}
 	}
-	
-	rawInput[@"excludeCredentials"] = excludeCredentials;
-	
 
-  return [[AmplifyRtnPasskeysHelper alloc] createPasskey:rawInput
-                                                 resolve:resolve
-                                                  reject:reject];
+	return [[AmplifyRtnPasskeysHelper alloc] createPasskey:input.rp().id_()
+	                                                userId:input.user().id_()
+	                                              userName:input.user().name()
+	                                             challenge:input.challenge()
+	                                    excludeCredentials:excludeCredentials
+	                                               resolve:resolve
+	                                                reject:reject];
 }
 
 - (void)getPasskey:(JS::NativeAmplifyRtnPasskeys::PasskeyGetOptionsJson &)input
            resolve:(nonnull RCTPromiseResolveBlock)resolve
             reject:(nonnull RCTPromiseRejectBlock)reject {
 
-	NSMutableDictionary *rawInput = [@{
-		@"rpId" : input.rpId(),
-		@"challenge" : input.challenge(),
-		@"userVerification" : input.userVerification()
-	} mutableCopy];
-	
-	NSMutableArray *allowCredentials = [NSMutableArray array];
-	
+	NSMutableArray *allowCredentials = [@[] mutableCopy];
+
 	if (input.allowCredentials().has_value()) {
 		auto credentials = input.allowCredentials().value();
 		for (const auto &credential : credentials) {
-			[allowCredentials addObject:@{ @"id": credential.id_() }];
+			[allowCredentials addObject:credential.id_()];
 		}
 	}
-	
-	rawInput[@"allowCredentials"] = allowCredentials;
 
-  return [[AmplifyRtnPasskeysHelper alloc] getPasskey:rawInput resolve:resolve reject:reject];
+	return [[AmplifyRtnPasskeysHelper alloc] getPasskey:input.rpId()
+	                                          challenge:input.challenge()
+	                                   userVerification:input.userVerification()
+	                                   allowCredentials:allowCredentials
+	                                            resolve:resolve
+	                                             reject:reject];
 }
 
 - (nonnull NSNumber *)getIsPasskeySupported {
-  return [AmplifyRtnPasskeysHelper getIsPasskeySupported];
+	return [AmplifyRtnPasskeysHelper getIsPasskeySupported];
 }
 
 + (NSString *)moduleName {
-  return @"AmplifyRtnPasskeys";
+	return @"AmplifyRtnPasskeys";
 }
 
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params {
-	return std::make_shared<facebook::react::NativeAmplifyRtnPasskeysSpecJSI>(params);
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params {
+	return std::make_shared<facebook::react::NativeAmplifyRtnPasskeysSpecJSI>(
+	    params);
 }
 
 @end
