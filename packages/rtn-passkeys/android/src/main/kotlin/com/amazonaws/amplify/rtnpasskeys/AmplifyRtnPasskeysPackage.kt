5d287ef7e900aa1ef65dd4c753cd1a71447720e4
James Jarvis <jjarvisp@amazon.com>
2025-05-17 16:16:59 -0700

    feat: add android support

---
+++ packages/rtn-passkeys/android/src/main/kotlin/com/amazonaws/amplify/rtnpasskeys/AmplifyRtnPasskeysPackage.kt
@@ -0,0 +1,32 @@
+package com.amazonaws.amplify.rtnpasskeys
+
+import com.facebook.react.BaseReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.model.ReactModuleInfo
+import com.facebook.react.module.model.ReactModuleInfoProvider
+
+class AmplifyRtnPasskeysPackage : BaseReactPackage() {
+  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
+    return if (name == AmplifyRtnPasskeysModule.NAME) {
+      AmplifyRtnPasskeysModule(reactContext)
+    } else {
+      null
+    }
+  }
+
+  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
+    return ReactModuleInfoProvider {
+      val moduleInfos: MutableMap<String, ReactModuleInfo> = HashMap()
+      moduleInfos[AmplifyRtnPasskeysModule.NAME] = ReactModuleInfo(
+        AmplifyRtnPasskeysModule.NAME,
+        AmplifyRtnPasskeysModule.NAME,
+        false,  // canOverrideExistingModule
+        false,  // needsEagerInit
+        false,  // isCxxModule
+        true // isTurboModule
+      )
+      moduleInfos
+    }
+  }
+}
