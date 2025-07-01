e22d3a886afa8145c90347531302eed84bb75592
Adrian Joshua Strutt <adjosst@amazon.com>
2025-07-01 14:33:24 +0200

    add isBrowser again

--- packages/auth/src/providers/cognito/credentialsProvider/IdentityIdStore.ts
+++ packages/auth/src/providers/cognito/credentialsProvider/IdentityIdStore.ts
@@ -7,7 +7,10 @@ import {
 	Identity,
 	KeyValueStorageInterface,
 } from '@aws-amplify/core';
-import { assertIdentityPoolIdConfig } from '@aws-amplify/core/internals/utils';
+import {
+	assertIdentityPoolIdConfig,
+	isBrowser,
+} from '@aws-amplify/core/internals/utils';
 
 import { getAuthStorageKeys } from '../tokenProvider/TokenStore';
 import { AuthKeys } from '../tokenProvider/types';
@@ -80,11 +83,9 @@ export class DefaultIdentityIdStore implements IdentityIdStore {
 		} else {
 			this._primaryIdentityId = identity.id;
 			// Clear locally stored guest id
-			this.keyValueStorage.getItem(this._authKeys.identityId).then(item => {
-				if (item) {
-					this.keyValueStorage.removeItem(this._authKeys.identityId);
-				}
-			});
+			if (this._hasGuestIdentityId || isBrowser()) {
+				this.keyValueStorage.removeItem(this._authKeys.identityId);
+			}
 			this._hasGuestIdentityId = false;
 		}
 	}
