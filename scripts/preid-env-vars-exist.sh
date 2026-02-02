4aeda2ff0ce1a8737b357ff8e66290133f3bb759
Michael Sober <msober@amazon.com>
2026-02-02 12:45:27 +0100

    fix: preid workflow permissions

--- scripts/preid-env-vars-exist.sh
+++ scripts/preid-env-vars-exist.sh
@@ -1,9 +1,4 @@
-if [ -z "$PREID_PREFIX" ]; then
+if [ -z "$PREID" ]; then
   echo >&2 "Fatal error: PREID_PREFIX not set"
   exit 2
-fi
-
-if [ -z "$PREID_HASH_SUFFIX" ]; then
-  echo >&2 "Fatal error: PREID_HASH_SUFFIX not set"
-  exit 2
-fi
+f
