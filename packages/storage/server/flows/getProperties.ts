f0580232396623fe9ea0b706ce879778d03a37d2
osama-rizk <osos2026@gmail.com>
2026-02-27 17:34:24 +0100

    update get properties

--- packages/storage/server/flows/getProperties.ts
+++ packages/storage/server/flows/getProperties.ts
@@ -1,11 +1,19 @@
+/* eslint-disable import/no-relative-packages */
+/* eslint-disable import/no-extraneous-dependencies */
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { AmplifyServer, getAmplifyServerContext } from '@aws-amplify/core/internals/adapter-core';
+import {
+	AmplifyServer,
+	getAmplifyServerContext,
+} from '@aws-amplify/core/internals/adapter-core';
 import { StorageAction } from '@aws-amplify/core/internals/utils';
 
 import { GetPropertiesInput } from '../../src/providers/s3/types';
-import { getPropertiesFlow, resolveGetPropertiesDependencies } from '../../foundation';
+import {
+	getPropertiesFlow,
+	resolveGetPropertiesDependencies,
+} from '../../foundation';
 
 export const getProperties = async (
 	contextSpec: AmplifyServer.ContextSpec,
@@ -13,6 +21,11 @@ export const getProperties = async (
 	action?: StorageAction,
 ) => {
 	const amplifyServerContext = getAmplifyServerContext(contextSpec);
-	const dependencies = await resolveGetPropertiesDependencies(amplifyServerContext.amplify, input, action);
+	const dependencies = await resolveGetPropertiesDependencies(
+		amplifyServerContext.amplify,
+		input,
+		action,
+	);
+
 	return getPropertiesFlow(input, dependencies, action);
 };
