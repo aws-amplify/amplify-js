74ea9d88c6d9839d8bdc06eb90d3829444d9e3c3
osama-rizk <osos2026@gmail.com>
2026-05-04 15:53:37 +0200

    add conditional exports

--- packages/aws-amplify/src/storage/client.ts
+++
@@ -1,8 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-/*
-This file maps exports from `aws-amplify/storage/client`.
-It provides access to the Storage client-side APIs and category utils.
-*/
-export * from '@aws-amplify/storage/client';
