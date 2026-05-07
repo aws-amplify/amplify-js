21db6e5626c24e2ef31d1f3a5f52edf122289589
Philipp Andreas Paul <phandpau@amazon.de>
2026-05-07 16:39:25 +0200

    fix(api-graphql): add GraphQLAPI Proxy shim for backwards compatibility
    Export GraphQLAPI as a Proxy that lazily delegates to
    createGraphQLAPI(getGlobalContext()) on property access. This preserves
    the v6 import path without requiring context at import time.

--- packages/api-graphql/src/index.ts
+++ packages/api-graphql/src/index.ts
@@ -1,7 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import { getGlobalContext } from '@aws-amplify/core';
+
 import * as events from './internals/events';
+import { GraphQLAPIClass, createGraphQLAPI } from './GraphQLAPI';
 
 export { events };
 
@@ -14,3 +17,15 @@ export * from './types';
 
 export { CONNECTION_STATE_CHANGE } from './Providers/constants';
 export * from './internals/events/types';
+
+// Backwards-compat: lazy singleton that defers to createGraphQLAPI(getGlobalContext())
+export const GraphQLAPI = new Proxy(
+	{} as InstanceType<typeof GraphQLAPIClass>,
+	{
+		get(_target, prop, receiver) {
+			const instance = createGraphQLAPI(getGlobalContext());
+
+			return Reflect.get(instance, prop, receiver);
+		},
+	},
+);
