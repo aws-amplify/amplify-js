e79301dfeaa1938db710cb026a6809234924f2ac
Philipp Andreas Paul <phandpau@amazon.de>
2026-05-08 15:32:51 +0200

    fix(api-graphql): defer GraphQLAPI throw to invocation, not property access
    Use hasGlobalContext() guard so that grabbing a reference at module load
    (const gql = GraphQLAPI.graphql) no longer throws. The error is deferred
    until the function is actually called without configure().

--- packages/api-graphql/src/index.ts
+++ packages/api-graphql/src/index.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { getGlobalContext } from '@aws-amplify/core';
+import { getGlobalContext, hasGlobalContext } from '@aws-amplify/core';
 
 import * as events from './internals/events';
 import { GraphQLAPIClass, createGraphQLAPI } from './GraphQLAPI';
@@ -22,10 +22,18 @@ export * from './internals/events/types';
 export const GraphQLAPI = new Proxy(
 	{} as InstanceType<typeof GraphQLAPIClass>,
 	{
-		get(_target, prop, receiver) {
+		get(_target, prop, _receiver) {
+			if (!hasGlobalContext()) {
+				// Return a callable that throws on invocation, not on access
+				return (..._args: unknown[]) => {
+					throw new Error(
+						'Amplify has not been configured. Call Amplify.configure() before using GraphQLAPI.',
+					);
+				};
+			}
 			const instance = createGraphQLAPI(getGlobalContext());
 
-			return Reflect.get(instance, prop, receiver);
+			return Reflect.get(instance, prop, instance);
 		},
 	},
 );
